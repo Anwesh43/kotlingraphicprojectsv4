2ee1faab1794092b4e43207cf219781268a5d376
Anwesh43 <anweshthecool0@gmail.com>
2023-10-11 11:16:58 +0530

    rightcircletosemiview: adding view in activity

--- app/src/main/java/com/example/android_kotlin_graphic_projectsv4/MainActivity.kt
+++ app/src/main/java/com/example/android_kotlin_graphic_projectsv4/MainActivity.kt
@@ -2,10 +2,11 @@ package com.example.android_kotlin_graphic_projectsv4
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.rightcircletosemiview.RightCircleToSemiView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        //setContentView(R.layout.activity_main)
     }
 }
