efdd0d381934fe39c8a4e3c3f309d6aac6851c54
Anwesh43 <anweshthecool0@gmail.com>
2023-10-11 18:42:33 +0530

    rightcircletosemiview: adding Animator class

--- rightcircletosemiview/src/main/java/com/example/rightcircletosemiview/RightCircleToSemiView.kt
+++ rightcircletosemiview/src/main/java/com/example/rightcircletosemiview/RightCircleToSemiView.kt
@@ -104,6 +104,34 @@ class RightCircleToSemiView(ctx : Context) : View(ctx) {
         }
     }
 
+    data class Animator(var view : View, var animated : Boolean = false) {
+
+        fun animate(cb : () -> Unit) {
+            if (animated) {
+                cb()
+                try {
+                    Thread.sleep(delay)
+                    view.invalidate()
+                } catch(ex : Exception) {
+
+                }
+            }
+        }
+
+        fun start() {
+            if (!animated) {
+                animated = true
+                view.postInvalidate()
+            }
+        }
+
+        fun stop() {
+            if (animated) {
+                animated = false
+            }
+        }
+    }
+
     companion object {
         fun create(activity : Activity) : RightCircleToSemiView {
             val view : RightCircleToSemiView = RightCircleToSemiView(activity)
