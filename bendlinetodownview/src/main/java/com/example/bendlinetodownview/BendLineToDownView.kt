6fc079c126bb0d9a0c11ac8e2fe3912513f18d1f
Anwesh43 <anweshthecool0@gmail.com>
2023-10-12 11:02:35 +0530

    bendlinetodownview: adding BLTDNode class

--- bendlinetodownview/src/main/java/com/example/bendlinetodownview/BendLineToDownView.kt
+++ bendlinetodownview/src/main/java/com/example/bendlinetodownview/BendLineToDownView.kt
@@ -116,4 +116,45 @@ class BendLineToDownView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class BLTDNode(var i : Int = 0, val state : State = State()) {
+
+        private var next : BLTDNode? = null
+        private var prev : BLTDNode? = null
+
+        init {
+
+        }
+
+        fun addNeighbor() {
+            if (i < colors.size - 1) {
+                next = BLTDNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawBLTDNode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : BLTDNode {
+            var curr : BLTDNode? = prev
+            if (dir == 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
