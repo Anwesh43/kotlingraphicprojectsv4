7bfa0ac6d15881d9168a922eb1dbb999e153ea70
Anwesh43 <anweshthecool0@gmail.com>
2023-10-12 11:25:47 +0530

    bendlinetodownview: adding view in activity

--- app/src/main/java/com/example/android_kotlin_graphic_projectsv4/MainActivity.kt
+++ app/src/main/java/com/example/android_kotlin_graphic_projectsv4/MainActivity.kt
@@ -2,13 +2,13 @@ package com.example.android_kotlin_graphic_projectsv4
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.example.rightcircletosemiview.RightCircleToSemiView
+import com.example.bendlinetodownview.BendLineToDownView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         supportActionBar?.hide()
         //setContentView(R.layout.activity_main)
-        RightCircleToSemiView.create(this)
+        BendLineToDownView.create(this)
     }
 }
