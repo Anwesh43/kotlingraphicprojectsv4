c6a3261929783bd20bcfc395372fc77b1a12e5e9
Anwesh43 <anweshthecool0@gmail.com>
2023-10-12 10:19:48 +0530

    bendlinetodownview: adding Int and Float extension methods

--- bendlinetodownview/src/main/java/com/example/bendlinetodownview/BendLineToDownView.kt
+++ bendlinetodownview/src/main/java/com/example/bendlinetodownview/BendLineToDownView.kt
@@ -22,3 +22,7 @@ val sizeFactor : Float = 4.9f
 val delay : Long = 20
 val backColor : Int = Color.parseColor("#BDBDBD")
 val rot : Float = 45f
+
+fun Int.inverse() : Float = 1f / this
+fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
+fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
