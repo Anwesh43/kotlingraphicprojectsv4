b371884771a8b267730962692dd1dba269dd51a0
Anwesh43 <anweshthecool0@gmail.com>
2023-10-13 13:21:49 +0530

    parallellinesemicircleview: adding view in activity

--- app/src/main/java/com/example/android_kotlin_graphic_projectsv4/MainActivity.kt
+++ app/src/main/java/com/example/android_kotlin_graphic_projectsv4/MainActivity.kt
@@ -2,13 +2,13 @@ package com.example.android_kotlin_graphic_projectsv4
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.example.bendlinetodownview.BendLineToDownView
+import com.example.parallellinesemicircleview.ParallelLineSemiCircleView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         supportActionBar?.hide()
         //setContentView(R.layout.activity_main)
-        BendLineToDownView.create(this)
+        ParallelLineSemiCircleView.create(this)
     }
 }
