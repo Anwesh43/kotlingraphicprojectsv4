1468a2cb9e105e257862453262af406692fbce08
Anwesh43 <anweshthecool0@gmail.com>
2023-10-13 11:27:03 +0530

    parallellinesemicircleview: adding PLSCNode class

--- parallellinesemicircleview/src/main/java/com/example/parallellinesemicircleview/ParallelLineSemiCircleView.kt
+++ parallellinesemicircleview/src/main/java/com/example/parallellinesemicircleview/ParallelLineSemiCircleView.kt
@@ -134,4 +134,45 @@ class ParallelLineSemiCircleView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class PLSCNode(var i : Int = 0, val state : State = State()) {
+
+        private var next : PLSCNode? = null
+        private var prev : PLSCNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < colors.size - 1) {
+                next = PLSCNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawPLSCNode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : PLSCNode {
+            var curr : PLSCNode? = prev
+            if (dir == 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
