41471ac164154411da2211abf64ab5c746b86684
Anwesh43 <anweshthecool0@gmail.com>
2023-10-13 11:28:24 +0530

    parallellinesemicircleview: adding ParallelLineSemiCircle class

--- parallellinesemicircleview/src/main/java/com/example/parallellinesemicircleview/ParallelLineSemiCircleView.kt
+++ parallellinesemicircleview/src/main/java/com/example/parallellinesemicircleview/ParallelLineSemiCircleView.kt
@@ -175,4 +175,27 @@ class ParallelLineSemiCircleView(ctx : Context) : View(ctx) {
             return this
         }
     }
+
+    data class ParallelLineSemiCircle(var i : Int) {
+
+        private var curr : PLSCNode = PLSCNode(0)
+        private var dir : Int = 1
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            curr.draw(canvas, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            curr.update {
+                curr = curr.getNext(dir) {
+                    dir *= -1
+                }
+                cb(it)
+            }
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            curr.startUpdating(cb)
+        }
+    }
 }
