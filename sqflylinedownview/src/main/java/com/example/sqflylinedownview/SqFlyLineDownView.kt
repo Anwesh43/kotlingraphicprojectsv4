a4b01fd1fd3870cbdc890e28875985f975b1b08e
Anwesh43 <anweshthecool0@gmail.com>
2023-10-15 12:05:21 +0530

    sqflylinedownview: adding Int and Float extension methods

--- sqflylinedownview/src/main/java/com/example/sqflylinedownview/SqFlyLineDownView.kt
+++ sqflylinedownview/src/main/java/com/example/sqflylinedownview/SqFlyLineDownView.kt
@@ -25,3 +25,6 @@ val deg : Float = 45f
 val sizeFactor : Float = 4.9f
 val strokeFactor : Float = 90f
 
+fun Int.inverse() : Float = 1f / this
+fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
+fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
