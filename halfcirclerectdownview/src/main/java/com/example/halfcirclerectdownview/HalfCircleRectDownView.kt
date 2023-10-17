24cd916782f9a2dde873a7ccea981ee6ab6cff47
Anwesh43 <anweshthecool0@gmail.com>
2023-10-17 10:36:04 +0530

    halfcirclerectdownview: adding HalfCircleRectDown class

--- halfcirclerectdownview/src/main/java/com/example/halfcirclerectdownview/HalfCircleRectDownView.kt
+++ halfcirclerectdownview/src/main/java/com/example/halfcirclerectdownview/HalfCircleRectDownView.kt
@@ -156,4 +156,27 @@ class HalfCircleRectDownView(ctx : Context) : View(ctx) {
             return this
         }
     }
+
+    data class HalfCircleRectDown(var i : Int) {
+
+        private var curr : HCRDNode = HCRDNode(0)
+        private var dir : Int = 1
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            curr.draw(canvas, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            curr.update {
+                curr = curr.getNext(dir) {
+                    dir *= -1
+                }
+                cb(it)
+            }
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            curr.startUpdating(cb)
+        }
+    }
 }
