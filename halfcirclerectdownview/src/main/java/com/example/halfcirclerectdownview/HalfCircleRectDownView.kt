edbb80c4f9908cd77338b33856148f846a587cb8
Anwesh43 <anweshthecool0@gmail.com>
2023-10-17 09:54:19 +0530

    halfcirclerectdownview: adding Cnavas extension methods

--- halfcirclerectdownview/src/main/java/com/example/halfcirclerectdownview/HalfCircleRectDownView.kt
+++ halfcirclerectdownview/src/main/java/com/example/halfcirclerectdownview/HalfCircleRectDownView.kt
@@ -26,3 +26,29 @@ val scGap : Float = 0.04f / parts
 fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
+
+fun Canvas.drawXY(x : Float, y : Float, cb : () -> Unit) {
+    save()
+    translate(x, y)
+    cb()
+    restore()
+}
+
+fun Canvas.drawHalfCircleRectDown(scale : Float, w : Float, h : Float, paint : Paint) {
+    val size : Float = Math.min(w, h) / sizeFactor
+    val dsc : (Int) -> Float = {
+        scale.divideScale(it, parts)
+    }
+    drawXY(w / 2, h / 2 - (h / 2 + size) * dsc(3)) {
+        rotate(rot * dsc(2))
+        drawRect(RectF(0f, -size / 2, size * dsc(0), size / 2), paint)
+        drawArc(RectF(-size, -size / 2, 0f, size / 2), 0f, 180f * dsc(1), true, paint)
+    }
+}
+
+fun Canvas.drawHCRDNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = Color.parseColor(colors[i])
+    drawHalfCircleRectDown(scale, w, h, paint)
+}
