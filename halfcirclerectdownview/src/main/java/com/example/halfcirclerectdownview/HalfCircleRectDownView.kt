372e5181be625ff4385d05cbc8e5c31cc16da787
Anwesh43 <anweshthecool0@gmail.com>
2023-10-17 10:44:35 +0530

    halfcirclerectdownview: adding renderer in view

--- halfcirclerectdownview/src/main/java/com/example/halfcirclerectdownview/HalfCircleRectDownView.kt
+++ halfcirclerectdownview/src/main/java/com/example/halfcirclerectdownview/HalfCircleRectDownView.kt
@@ -55,14 +55,16 @@ fun Canvas.drawHCRDNode(i : Int, scale : Float, paint : Paint) {
 
 class HalfCircleRectDownView(ctx : Context) : View(ctx) {
 
-    override fun onDraw(canvas : Canvas) {
+    private val renderer : Renderer = Renderer(this)
 
+    override fun onDraw(canvas : Canvas) {
+        renderer.render(canvas)
     }
 
     override fun onTouchEvent(event : MotionEvent) : Boolean {
         when (event.action) {
             MotionEvent.ACTION_DOWN -> {
-
+                renderer.handleTap()
             }
         }
         return true
