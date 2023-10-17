03dacc702c8c84190fcb1f7c2590ede455042fac
Anwesh43 <anweshthecool0@gmail.com>
2023-10-17 10:30:34 +0530

    halfcirclerectdownview: adding Animator class

--- halfcirclerectdownview/src/main/java/com/example/halfcirclerectdownview/HalfCircleRectDownView.kt
+++ halfcirclerectdownview/src/main/java/com/example/halfcirclerectdownview/HalfCircleRectDownView.kt
@@ -87,4 +87,32 @@ class HalfCircleRectDownView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class Animator(var view : View, var animated : Boolean = false) {
+
+        fun animate(cb : () -> Unit) {
+            if (animated) {
+                cb()
+                try {
+                    Thread.sleep(delay)
+                    view.invalidate()
+                } catch(ex : Exception) {
+
+                }
+            }
+        }
+
+        fun start() {
+            if (!animated) {
+                animated = true
+                view.postInvalidate()
+            }
+        }
+
+        fun stop() {
+            if (animated) {
+                animated = false
+            }
+        }
+    }
 }
