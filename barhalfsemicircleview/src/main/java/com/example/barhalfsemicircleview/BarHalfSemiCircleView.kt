dfae7e9cc7eb7da8d6974d6194d8cfd6380f82ac
Anwesh43 <anweshthecool0@gmail.com>
2023-10-18 12:03:08 +0530

    barhalfsemicircleview: adding Renderer class

--- barhalfsemicircleview/src/main/java/com/example/barhalfsemicircleview/BarHalfSemiCircleView.kt
+++ barhalfsemicircleview/src/main/java/com/example/barhalfsemicircleview/BarHalfSemiCircleView.kt
@@ -186,4 +186,27 @@ class BarHalfSemiCircleView(ctx : Context) : View(ctx) {
             curr.startUpdating(cb)
         }
     }
+
+    data class Renderer(var view : BarHalfSemiCircleView) {
+
+        private val animator : Animator = Animator(view)
+        private val bhsc : BarHalfSemiCircle = BarHalfSemiCircle(0)
+        private val paint : Paint = Paint(Paint.ANTI_ALIAS_FLAG)
+
+        fun render(canvas : Canvas) {
+            canvas.drawColor(backColor)
+            bhsc.draw(canvas, paint)
+            animator.animate {
+                bhsc.update {
+                    animator.stop()
+                }
+            }
+        }
+
+        fun handleTap() {
+            bhsc.startUpdating {
+                animator.start()
+            }
+        }
+    }
 }
