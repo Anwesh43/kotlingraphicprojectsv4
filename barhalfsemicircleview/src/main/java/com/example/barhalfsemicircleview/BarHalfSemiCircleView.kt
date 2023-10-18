3a1b5432e92a149478f258cfca161a7e7bf41a30
Anwesh43 <anweshthecool0@gmail.com>
2023-10-18 11:49:44 +0530

    barhalfsemicircleview: adding BarHalfSemiCircleView class

--- barhalfsemicircleview/src/main/java/com/example/barhalfsemicircleview/BarHalfSemiCircleView.kt
+++ barhalfsemicircleview/src/main/java/com/example/barhalfsemicircleview/BarHalfSemiCircleView.kt
@@ -59,3 +59,19 @@ fun Canvas.drawBHSCNode(i : Int, scale : Float, paint : Paint) {
     paint.color = Color.parseColor(colors[i])
     drawBarHalfSemiCircle(scale, w, h, paint)
 }
+
+class BarHalfSemiCircleView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
