76faa9b4a67f423355c9983507d7f6a92a68d16e
Anwesh43 <anweshthecool0@gmail.com>
2023-10-18 11:48:43 +0530

    barhalfsemicircleview: adding Canvas extension methods

--- barhalfsemicircleview/src/main/java/com/example/barhalfsemicircleview/BarHalfSemiCircleView.kt
+++ barhalfsemicircleview/src/main/java/com/example/barhalfsemicircleview/BarHalfSemiCircleView.kt
@@ -27,3 +27,35 @@ val rFactor : Float = 4f
 fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
+
+fun Canvas.drawXY(x : Float, y : Float, cb : () -> Unit) {
+    save()
+    translate(x, y)
+    cb()
+    restore()
+}
+
+fun Canvas.drawBarHalfSemiCircle(scale : Float, w : Float, h : Float, paint : Paint) {
+    val size : Float = Math.min(w, h) / sizeFactor
+    val dsc : (Int) -> Float = {
+        scale.divideScale(it, parts)
+    }
+    val r : Float = size / rFactor
+    drawXY(w / 2, h * 0.5f * (1 - dsc(3))) {
+        rotate(rot * dsc(2))
+        drawRect(RectF(-r, 0f, r, size * dsc(0)), paint)
+        for (j in 0..1) {
+            drawXY(0f, 0f) {
+                scale(1f - 2 * j, 1f)
+                drawArc(RectF(0f, 0f, 2 * r, 2 * r), -90f, 180f * dsc(1), true, paint)
+            }
+        }
+    }
+}
+
+fun Canvas.drawBHSCNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = Color.parseColor(colors[i])
+    drawBarHalfSemiCircle(scale, w, h, paint)
+}
