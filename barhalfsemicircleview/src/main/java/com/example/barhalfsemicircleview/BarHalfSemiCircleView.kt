6eebbe676f319720329c25fc86585c4c597f5ab8
Anwesh43 <anweshthecool0@gmail.com>
2023-10-18 11:56:59 +0530

    barhalfsemicircleview: adding BHSCNode class

--- barhalfsemicircleview/src/main/java/com/example/barhalfsemicircleview/BarHalfSemiCircleView.kt
+++ barhalfsemicircleview/src/main/java/com/example/barhalfsemicircleview/BarHalfSemiCircleView.kt
@@ -122,4 +122,45 @@ class BarHalfSemiCircleView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class BHSCNode(var i : Int = 0, val state : State = State()) {
+
+        private var next : BHSCNode? = null
+        private var prev : BHSCNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < colors.size - 1) {
+                next = BHSCNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawBHSCNode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : BHSCNode {
+            var curr : BHSCNode? = prev
+            if (dir == 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
