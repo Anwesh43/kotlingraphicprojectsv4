a7cbcf18a5ca42638e5ece2e7856a038f6a05c1c
Anwesh43 <anweshthecool0@gmail.com>
2023-10-19 12:26:29 +0530

    bendarcrotdownview: adding BARDNode class

--- bendarcrotdownview/src/main/java/com/example/bendarcrotdownview/BendArcRotDownView.kt
+++ bendarcrotdownview/src/main/java/com/example/bendarcrotdownview/BendArcRotDownView.kt
@@ -125,4 +125,45 @@ class BendArcRotDownView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class BARDNode(var i : Int = 0, val state : State = State()) {
+
+        private var next : BARDNode? = null
+        private var prev : BARDNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < colors.size - 1) {
+                next = BARDNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawBARDNode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : BARDNode {
+            var curr : BARDNode? = prev
+            if (dir == 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
