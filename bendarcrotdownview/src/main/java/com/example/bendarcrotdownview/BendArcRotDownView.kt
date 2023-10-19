ca95d01261e2c66ea4115f43c9bc545ee43e8129
Anwesh43 <anweshthecool0@gmail.com>
2023-10-19 11:55:10 +0530

    bendarcrotdownview: adding constantsa nd libraries

---
+++ bendarcrotdownview/src/main/java/com/example/bendarcrotdownview/BendArcRotDownView.kt
@@ -0,0 +1,26 @@
+package com.example.bendarcrotdownview
+
+import android.view.View
+import android.view.MotionEvent
+import android.graphics.Paint
+import android.graphics.Canvas
+import android.graphics.Color
+import android.graphics.RectF
+import android.app.Activity
+import android.content.Context
+
+val colors : Array<String> = arrayOf(
+    "#1A237E",
+    "#EF5350",
+    "#AA00FF",
+    "#C51162",
+    "#00C853"
+)
+val rot : Float = 90f
+val sizeFactor : Float = 4.9f
+val parts : Int = 4
+val scGap : Float = 0.04f / parts
+val strokeFactor : Float = 90f
+val backColor : Int = Color.parseColor("#BDBDBD")
+val sweep : Float = 60f
+val start : Float = 90f
