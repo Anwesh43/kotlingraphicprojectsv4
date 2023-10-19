6bd633dc6eaa81883981aec9e71941afa028f238
Anwesh43 <anweshthecool0@gmail.com>
2023-10-19 12:12:34 +0530

    bendarcrotdownview: adding Canvas extension methods

--- bendarcrotdownview/src/main/java/com/example/bendarcrotdownview/BendArcRotDownView.kt
+++ bendarcrotdownview/src/main/java/com/example/bendarcrotdownview/BendArcRotDownView.kt
@@ -28,3 +28,36 @@ val start : Float = 90f
 fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
+
+fun Canvas.drawXY(x : Float, y : Float, cb : () -> Unit) {
+    save()
+    translate(x, y)
+    cb()
+    restore()
+}
+
+fun Canvas.drawBendArcRotDown(scale : Float, w : Float, h : Float, paint : Paint) {
+    val size : Float = Math.min(w, h) / sizeFactor
+    val dsc : (Int) -> Float = {
+        scale.divideScale(it, parts)
+    }
+    drawXY(w / 2, h / 2 + (h / 2) * dsc(3)) {
+        rotate(rot * dsc(2))
+        for (j in 0..1) {
+            drawXY(0f, 0f) {
+                scale(1f, 1f - 2 * j)
+                drawArc(RectF(-size / 2, -size, size / 2, 0f), start - sweep * dsc(0), sweep * 2f * dsc(0), false, paint)
+            }
+        }
+    }
+}
+
+fun Canvas.drawBARDNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = Color.parseColor(colors[i])
+    paint.strokeCap = Paint.Cap.ROUND
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    paint.style = Paint.Style.STROKE
+    drawBendArcRotDown(scale, w, h, paint)
+}
