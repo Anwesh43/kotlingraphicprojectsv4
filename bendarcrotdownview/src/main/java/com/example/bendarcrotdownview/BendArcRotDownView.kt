c2b3ae1c778353fb6b4c381301e0a60e7d1d0125
Anwesh43 <anweshthecool0@gmail.com>
2023-10-19 12:24:28 +0530

    bendarcrotdownview: adding Animated class

--- bendarcrotdownview/src/main/java/com/example/bendarcrotdownview/BendArcRotDownView.kt
+++ bendarcrotdownview/src/main/java/com/example/bendarcrotdownview/BendArcRotDownView.kt
@@ -24,6 +24,7 @@ val strokeFactor : Float = 90f
 val backColor : Int = Color.parseColor("#BDBDBD")
 val sweep : Float = 60f
 val start : Float = 90f
+val delay : Long = 20
 
 fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
@@ -93,6 +94,34 @@ class BendArcRotDownView(ctx : Context) : View(ctx) {
             if (dir == 0f) {
                 dir = 1f - 2 * prevScale
                 cb()
+            }
+        }
+    }
+
+    data class Animator(var view : View, var animated : Boolean = false) {
+
+        fun animate(cb : () -> Unit) {
+            if (animated) {
+                cb()
+                try {
+                    Thread.sleep(delay)
+                    view.invalidate()
+                } catch(ex : Exception) {
+
+                }
+            }
+        }
+
+        fun start() {
+            if (!animated) {
+                animated = true
+                view.postInvalidate()
+            }
+        }
+
+        fun stop() {
+            if (animated) {
+                animated = false
             }
         }
     }
