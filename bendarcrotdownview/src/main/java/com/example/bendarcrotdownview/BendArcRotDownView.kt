1dc110840ed5fde54b6444ca8ef1a63f2dc25290
Anwesh43 <anweshthecool0@gmail.com>
2023-10-19 12:07:36 +0530

    bendarcrotdownview: adding Int and Float extension methods

--- bendarcrotdownview/src/main/java/com/example/bendarcrotdownview/BendArcRotDownView.kt
+++ bendarcrotdownview/src/main/java/com/example/bendarcrotdownview/BendArcRotDownView.kt
@@ -24,3 +24,7 @@ val strokeFactor : Float = 90f
 val backColor : Int = Color.parseColor("#BDBDBD")
 val sweep : Float = 60f
 val start : Float = 90f
+
+fun Int.inverse() : Float = 1f / this
+fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
+fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
