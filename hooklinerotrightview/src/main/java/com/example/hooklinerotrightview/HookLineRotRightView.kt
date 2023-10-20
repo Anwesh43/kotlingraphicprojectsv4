277ebcad5a5e6f1cfcd6a81bed8c0f47ba225f09
Anwesh43 <anweshthecool0@gmail.com>
2023-10-20 11:43:46 +0530

    hooklinerotrightview: adding HookLineRotRightView class

--- hooklinerotrightview/src/main/java/com/example/hooklinerotrightview/HookLineRotRightView.kt
+++ hooklinerotrightview/src/main/java/com/example/hooklinerotrightview/HookLineRotRightView.kt
@@ -65,3 +65,19 @@ fun Canvas.drawHLRRNode(i : Int, scale : Float, paint : Paint) {
     paint.style = Paint.Style.STROKE
     drawHookLineRotRight(scale, w, h, paint)
 }
+
+class HookLineRotRightView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
