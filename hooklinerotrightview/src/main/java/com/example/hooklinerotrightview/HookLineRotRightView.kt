8b38209c2a43a18dfe9b4e51388e391e7d494be4
Anwesh43 <anweshthecool0@gmail.com>
2023-10-20 11:31:17 +0530

    hooklinerotrightview: adding Canvas extension methods

--- hooklinerotrightview/src/main/java/com/example/hooklinerotrightview/HookLineRotRightView.kt
+++ hooklinerotrightview/src/main/java/com/example/hooklinerotrightview/HookLineRotRightView.kt
@@ -24,7 +24,44 @@ val sizeFactor : Float = 4.9f
 val delay : Long = 20
 val backColor : Int = Color.parseColor("#BDBDBD")
 val scGap : Float = 0.04f / parts
+val rFactor : Float = 6f
 
 fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
+
+fun Canvas.drawXY(x : Float, y : Float, cb : () -> Unit) {
+    save()
+    translate(x, y)
+    cb()
+    restore()
+}
+
+fun Canvas.drawHookLineRotRight(scale : Float, w : Float, h : Float, paint : Paint) {
+    val size : Float = Math.min(w, h) / sizeFactor
+    val dsc : (Int) -> Float = {
+        scale.divideScale(it, parts)
+    }
+    val r : Float = size / rFactor
+    drawXY(w / 2, h / 2) {
+        for (j in 0..1) {
+            drawXY(0f, 0f) {
+                scale(1f - 2 * j, 1f - 2  * j)
+                drawLine(0f, 0f, 0f, size * dsc(0), paint)
+                drawXY(0f, -size) {
+                    drawArc(RectF(0f, -r / 2, r, r / 2), sweep * (1 - dsc(1)), sweep * dsc(1), false, paint)
+                }
+            }
+        }
+    }
+}
+
+fun Canvas.drawHLRRNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = Color.parseColor(colors[i])
+    paint.strokeCap = Paint.Cap.ROUND
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    paint.style = Paint.Style.STROKE
+    drawHookLineRotRight(scale, w, h, paint)
+}
