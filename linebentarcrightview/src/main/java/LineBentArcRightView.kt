dbe34ddeab7def0bd4281cc2c5cd60eaa8d680d1
Anwesh43 <anweshthecool0@gmail.com>
2023-10-22 10:20:51 +0530

    linebentarcrightview: adding LineBentArcRight class

--- linebentarcrightview/src/main/java/LineBentArcRightView.kt
+++ linebentarcrightview/src/main/java/LineBentArcRightView.kt
@@ -161,4 +161,27 @@ class LineBentArcRightView(ctx : Context) : View(ctx) {
             return this
         }
     }
+
+    data class LineBentArcRight(var i : Int) {
+
+        private var curr : LBARNode = LBARNode(0)
+        private var dir : Int = 1
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            curr.draw(canvas, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            curr.update {
+                curr = curr.getNext(dir) {
+                    dir *= -1
+                }
+                cb(it)
+            }
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            curr.startUpdating(cb)
+        }
+    }
 }
