0cb32f381618c310bdc9060979b45b634cdfdf5a
Anwesh43 <anweshthecool0@gmail.com>
2023-10-22 10:02:46 +0530

    linebentarcrightview: adding constants and libraries

---
+++ linebentarcrightview/src/main/java/LineBentArcRightView.kt
@@ -0,0 +1,24 @@
+import android.view.View
+import android.view.MotionEvent
+import android.app.Activity
+import android.content.Context
+import android.graphics.Paint
+import android.graphics.RectF
+import android.graphics.Color
+import android.graphics.Canvas
+
+val colors : Array<String> = arrayOf(
+    "#1A237E",
+    "#EF5350",
+    "#AA00FF",
+    "#C51162",
+    "#00C853"
+)
+val rot : Float = 45f
+val deg : Float = 180f
+val strokeFactor : Float = 90f
+val sizeFactor : Float = 4.9f
+val delay : Long = 20
+val backColor : Int = Color.parseColor("#BDBDBD")
+val parts : Int = 4
+val scGap : Float = 0.04f / parts
