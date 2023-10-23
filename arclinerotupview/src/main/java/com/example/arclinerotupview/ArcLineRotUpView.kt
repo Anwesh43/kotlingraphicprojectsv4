9eef4753e2c94dbf48ad8aebe712bceaa02fb76a
Anwesh43 <anweshthecool0@gmail.com>
2023-10-23 14:08:55 +0530

    arclinerotupview: adding ArcLineRotUp class

--- arclinerotupview/src/main/java/com/example/arclinerotupview/ArcLineRotUpView.kt
+++ arclinerotupview/src/main/java/com/example/arclinerotupview/ArcLineRotUpView.kt
@@ -164,4 +164,27 @@ class ArcLineRotUpView(ctx : Context) : View(ctx) {
             return this
         }
     }
+
+    data class ArcLineRotUp(var i : Int) {
+
+        private var curr : ALRUNode = ALRUNode(0)
+        private var dir : Int = 1
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            curr.draw(canvas, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            curr.update {
+                curr = curr.getNext(dir) {
+                    dir *= -1
+                }
+                cb(it)
+            }
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            curr.startUpdating(cb)
+        }
+    }
 }
