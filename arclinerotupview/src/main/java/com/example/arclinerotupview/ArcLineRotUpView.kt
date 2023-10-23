23d1bf5aa5d61cfba8056b2704a4c765b80c0ce1
Anwesh43 <anweshthecool0@gmail.com>
2023-10-23 13:51:07 +0530

    arclinerotupview: adding ArcLineRotUpView class

--- arclinerotupview/src/main/java/com/example/arclinerotupview/ArcLineRotUpView.kt
+++ arclinerotupview/src/main/java/com/example/arclinerotupview/ArcLineRotUpView.kt
@@ -60,3 +60,19 @@ fun Canvas.drawALRUNode(i : Int, scale : Float, paint : Paint) {
     paint.strokeWidth = Math.min(w, h) / strokeFactor
     drawArcLineRotUp(scale, w, h, paint)
 }
+
+class ArcLineRotUpView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
