56604cdb8fee312badf1533a3e2f1d1bc3f77593
Anwesh43 <anweshthecool0@gmail.com>
2023-10-23 14:15:30 +0530

    arclinerotupview: adding view in activity

--- app/src/main/java/com/example/android_kotlin_graphic_projectsv4/MainActivity.kt
+++ app/src/main/java/com/example/android_kotlin_graphic_projectsv4/MainActivity.kt
@@ -2,12 +2,13 @@ package com.example.android_kotlin_graphic_projectsv4
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.arclinerotupview.ArcLineRotUpView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         supportActionBar?.hide()
         //setContentView(R.layout.activity_main)
-        LineBentArcRightView.create(this)
+        ArcLineRotUpView.create(this)
     }
 }
