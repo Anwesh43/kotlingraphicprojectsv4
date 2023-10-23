91c88ac844b3776a7e9348d2030a43219d6a75f8
Anwesh43 <anweshthecool0@gmail.com>
2023-10-23 13:56:36 +0530

    arclinerotupview: adding ALRUNode class

--- arclinerotupview/src/main/java/com/example/arclinerotupview/ArcLineRotUpView.kt
+++ arclinerotupview/src/main/java/com/example/arclinerotupview/ArcLineRotUpView.kt
@@ -123,4 +123,45 @@ class ArcLineRotUpView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class ALRUNode(var i : Int = 0, val state : State = State()) {
+
+        private var next : ALRUNode? = null
+        private var prev : ALRUNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < colors.size - 1) {
+                next = ALRUNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawALRUNode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : ALRUNode {
+            var curr : ALRUNode? = prev
+            if (dir == 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
