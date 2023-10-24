bbb4dbd3a08d74413dfd9e67fdded622daf72036
Anwesh43 <anweshthecool0@gmail.com>
2023-10-24 11:37:37 +0530

    linerotsemicircleview: adding Canvas extension methods

--- linerotsemicircleview/src/main/java/com/example/linerotsemicircleview/LineRotSemiCircleView.kt
+++ linerotsemicircleview/src/main/java/com/example/linerotsemicircleview/LineRotSemiCircleView.kt
@@ -22,10 +22,44 @@ val strokeFactor : Float = 90f
 val delay : Long = 20
 val backColor : Int = Color.parseColor("#BDBDBD")
 val deg : Float = 180f
+val parts : Int = 4
+val scGap : Float = 0.04f / 4
 
 fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
 
+fun Canvas.drawXY(x : Float, y : Float, cb : () -> Unit) {
+    save()
+    translate(x, y)
+    cb()
+    restore()
+}
 
+fun Canvas.drawLineRotSemiCircle(scale : Float, w : Float, h : Float, paint : Paint) {
+    val size : Float = Math.min(w, h) / sizeFactor
+    val dsc : (Int) -> Float = {
+        scale.divideScale(it, parts)
+    }
+    val r : Float = size / 2
+    drawXY(w / 2 + (w / 2) * dsc(3), h / 2) {
+        rotate(-rot * dsc(1))
+        drawLine(0f, 0f, 0f, size * dsc(0), paint)
+        for (j in 0..1) {
+            val sc : Float = dsc(2)
+            drawXY(r * j, 0f) {
+                val sck : Float = sc.divideScale(1 - j, 2)
+                drawArc(RectF(0f, -r / 2, r, r / 2), 0f, deg * sck, true, paint)
+            }
+        }
+    }
+}
 
+fun Canvas.drawLRSCNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = Color.parseColor(colors[i])
+    paint.strokeCap = Paint.Cap.ROUND
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    drawLineRotSemiCircle(scale, w, h, paint)
+}
