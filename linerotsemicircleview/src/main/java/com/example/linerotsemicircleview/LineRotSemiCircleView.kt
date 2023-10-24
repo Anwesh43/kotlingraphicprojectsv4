6ce8283d0c371827628ff92f121ce3d84e00ea69
Anwesh43 <anweshthecool0@gmail.com>
2023-10-24 12:21:06 +0530

    linerotsemicircleview: adding LRSCNode class

--- linerotsemicircleview/src/main/java/com/example/linerotsemicircleview/LineRotSemiCircleView.kt
+++ linerotsemicircleview/src/main/java/com/example/linerotsemicircleview/LineRotSemiCircleView.kt
@@ -126,4 +126,45 @@ class LineRotSemiCircleView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class LRSCNode(var i : Int = 0, val state : State = State()) {
+
+        private var next : LRSCNode? = null
+        private var prev : LRSCNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < colors.size - 1) {
+                next = LRSCNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawLRSCNode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : LRSCNode {
+            var curr : LRSCNode? = prev
+            if (dir == 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
