9619b66388c50bea01f90b03555690cacba2ab8f
Anwesh43 <anweshthecool0@gmail.com>
2023-10-24 11:38:53 +0530

    linerotsemicircleview: adding LineRotSemiCircleView class

--- linerotsemicircleview/src/main/java/com/example/linerotsemicircleview/LineRotSemiCircleView.kt
+++ linerotsemicircleview/src/main/java/com/example/linerotsemicircleview/LineRotSemiCircleView.kt
@@ -63,3 +63,19 @@ fun Canvas.drawLRSCNode(i : Int, scale : Float, paint : Paint) {
     paint.strokeWidth = Math.min(w, h) / strokeFactor
     drawLineRotSemiCircle(scale, w, h, paint)
 }
+
+class LineRotSemiCircleView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
