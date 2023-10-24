6e5172c6f08d6b2a74b801be4bcf843b7375d471
Anwesh43 <anweshthecool0@gmail.com>
2023-10-24 11:26:28 +0530

    linerotsemicircleview: adding constants and libraries

---
+++ linerotsemicircleview/src/main/java/com/example/linerotsemicircleview/LineRotSemiCircleView.kt
@@ -0,0 +1,25 @@
+package com.example.linerotsemicircleview
+
+import android.view.View
+import android.view.MotionEvent
+import android.graphics.Paint
+import android.graphics.Color
+import android.graphics.Canvas
+import android.graphics.RectF
+import android.content.Context
+import android.app.Activity
+
+val colors : Array<String> = arrayOf(
+    "#1A237E",
+    "#EF5350",
+    "#AA00FF",
+    "#C51162",
+    "#00C853"
+)
+val rot : Float = 90f
+val sizeFactor : Float = 4.9f
+val strokeFactor : Float = 90f
+val delay : Long = 20
+val backColor : Int = Color.parseColor("#BDBDBD")
+val deg : Float = 180f
+
