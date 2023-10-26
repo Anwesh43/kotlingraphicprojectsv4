8805f21dbac5d23a474d7df5376b4436d26d7f7d
Anwesh43 <anweshthecool0@gmail.com>
2023-10-26 09:09:31 +0530

    arcsqexpandrightview: adding Renderer in View

--- arcsqexpandrightview/src/main/java/com/example/arcsqexpandrightview/ArcSqExpandRightView.kt
+++ arcsqexpandrightview/src/main/java/com/example/arcsqexpandrightview/ArcSqExpandRightView.kt
@@ -57,14 +57,16 @@ fun Canvas.drawASERNode(i : Int, scale : Float, paint : Paint) {
 
 class ArcSqExpandRightView(ctx : Context) : View(ctx) {
 
-    override fun onDraw(canvas : Canvas) {
+    private val renderer : Renderer = Renderer(this)
 
+    override fun onDraw(canvas : Canvas) {
+        renderer.render(canvas)
     }
 
     override fun onTouchEvent(event : MotionEvent) : Boolean {
         when (event.action) {
             MotionEvent.ACTION_DOWN -> {
-
+                renderer.handleTap()
             }
         }
         return true
