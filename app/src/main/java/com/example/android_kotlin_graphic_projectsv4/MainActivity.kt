999d1e0f434a0fdd3d8dda127cb381671215d9b7
Anwesh43 <anweshthecool0@gmail.com>
2023-10-26 09:12:59 +0530

    arcsqexpandrightview: adding view in activity

--- app/src/main/java/com/example/android_kotlin_graphic_projectsv4/MainActivity.kt
+++ app/src/main/java/com/example/android_kotlin_graphic_projectsv4/MainActivity.kt
@@ -2,13 +2,14 @@ package com.example.android_kotlin_graphic_projectsv4
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.example.linerotsemicircleview.LineRotSemiCircleView
+import com.example.arcsqexpandrightview.ArcSqExpandRightView
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         supportActionBar?.hide()
         //setContentView(R.layout.activity_main)
-        LineRotSemiCircleView.create(this)
+        ArcSqExpandRightView.create(this)
     }
 }
