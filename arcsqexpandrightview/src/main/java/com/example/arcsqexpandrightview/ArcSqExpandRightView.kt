83ea51653e850f127a9b1ce85f07929e46bba6f6
Anwesh43 <anweshthecool0@gmail.com>
2023-10-26 09:02:39 +0530

    arcsqexpandrightview: adding ASERNode class

--- arcsqexpandrightview/src/main/java/com/example/arcsqexpandrightview/ArcSqExpandRightView.kt
+++ arcsqexpandrightview/src/main/java/com/example/arcsqexpandrightview/ArcSqExpandRightView.kt
@@ -117,4 +117,45 @@ class ArcSqExpandRightView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class ASERNode(var i : Int = 0, val state : State = State()) {
+
+        private var next : ASERNode? = null
+        private var prev : ASERNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < colors.size - 1) {
+                next = ASERNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawASERNode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : ASERNode {
+            var curr : ASERNode? = prev
+            if (dir == 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
