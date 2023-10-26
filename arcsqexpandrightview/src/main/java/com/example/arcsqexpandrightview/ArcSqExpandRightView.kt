71b6674486faa1b0f40ae72bc85dfd3b294b15ed
Anwesh43 <anweshthecool0@gmail.com>
2023-10-26 09:06:28 +0530

    arcsqexpandrightview: adding Renderer class

--- arcsqexpandrightview/src/main/java/com/example/arcsqexpandrightview/ArcSqExpandRightView.kt
+++ arcsqexpandrightview/src/main/java/com/example/arcsqexpandrightview/ArcSqExpandRightView.kt
@@ -181,4 +181,27 @@ class ArcSqExpandRightView(ctx : Context) : View(ctx) {
             curr.startUpdating(cb)
         }
     }
+
+    data class Renderer(var view : ArcSqExpandRightView) {
+
+        private val animator : Animator = Animator(view)
+        private val aser : ArcSqExpandRight = ArcSqExpandRight(0)
+        private val paint : Paint = Paint(Paint.ANTI_ALIAS_FLAG)
+
+        fun render(canvas : Canvas) {
+            canvas.drawColor(backColor)
+            aser.draw(canvas, paint)
+            animator.animate {
+                aser.update {
+                    animator.stop()
+                }
+            }
+        }
+
+        fun handleTap() {
+            aser.startUpdating {
+                animator.start()
+            }
+        }
+    }
 }
