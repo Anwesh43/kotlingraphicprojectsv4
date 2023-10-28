33aa326e8dc4ad44cfebbed90a1bafce96ac2336
Anwesh43 <anweshthecool0@gmail.com>
2023-10-28 11:34:23 +0530

    bilineupjoinleftview: adding Canvas extension methods

--- bilineupjoinleftview/src/main/java/com/example/bilineupjoinleftview/BiLineUpJoinLeftView.kt
+++ bilineupjoinleftview/src/main/java/com/example/bilineupjoinleftview/BiLineUpJoinLeftView.kt
@@ -28,3 +28,39 @@ val rFactor : Float = 3.2f
 fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
+
+fun Canvas.drawXY(x : Float, y : Float, cb : () -> Unit) {
+    save()
+    translate(x, y)
+    cb()
+    restore()
+}
+
+fun Canvas.drawBiLineUpJoinLeft(scale : Float, w : Float, h : Float, paint : Paint) {
+    val size : Float = Math.min(w, h) / sizeFactor
+    val dsc : (Int) -> Float = {
+        scale.divideScale(it, parts)
+    }
+    val r : Float = size / rFactor
+    drawXY(w / 2 - (w / 2 + size) * dsc(3), h / 2) {
+        for (j in 0..1) {
+            drawXY(0f, 0f) {
+                scale(1f, 1f - 2 * j)
+                drawXY(0f, (h / 2) * (1 - dsc(j))) {
+                    rotate(-rot * dsc(2))
+                    drawLine(0f, 0f, 0f, size, paint)
+                }
+                drawArc(RectF(-r, -r, r, r), 0f, rot * (dsc(j) - dsc(2)), true, paint)
+            }
+        }
+    }
+}
+
+fun Canvas.drawBLUJLNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = Color.parseColor(colors[i])
+    paint.strokeCap = Paint.Cap.ROUND
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    drawBiLineUpJoinLeft(scale, w, h, paint)
+}
