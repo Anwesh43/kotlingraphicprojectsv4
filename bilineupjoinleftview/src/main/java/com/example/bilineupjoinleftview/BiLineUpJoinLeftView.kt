3802da699e2cbe9f548cc694a98f0ac6e8b0a2ee
Anwesh43 <anweshthecool0@gmail.com>
2023-10-28 13:01:30 +0530

    bilineupjoinleftview: adding Renderer in View

--- bilineupjoinleftview/src/main/java/com/example/bilineupjoinleftview/BiLineUpJoinLeftView.kt
+++ bilineupjoinleftview/src/main/java/com/example/bilineupjoinleftview/BiLineUpJoinLeftView.kt
@@ -67,14 +67,16 @@ fun Canvas.drawBLUJLNode(i : Int, scale : Float, paint : Paint) {
 
 class BiLineUpJoinLeftView(ctx : Context) : View(ctx) {
 
-    override fun onDraw(canvas : Canvas) {
+    private val renderer : Renderer = Renderer(this)
 
+    override fun onDraw(canvas : Canvas) {
+        renderer.render(canvas)
     }
 
     override fun onTouchEvent(event : MotionEvent) : Boolean {
         when (event.action) {
             MotionEvent.ACTION_DOWN -> {
-
+                renderer.handleTap()
             }
         }
         return true
