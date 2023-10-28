58eda8490e9e4c4cc2b4ff8144b6527f41bf9d4d
Anwesh43 <anweshthecool0@gmail.com>
2023-10-28 11:36:19 +0530

    bilineupjoinleftview: adding BiLineUpJoinLeftView class

--- bilineupjoinleftview/src/main/java/com/example/bilineupjoinleftview/BiLineUpJoinLeftView.kt
+++ bilineupjoinleftview/src/main/java/com/example/bilineupjoinleftview/BiLineUpJoinLeftView.kt
@@ -64,3 +64,19 @@ fun Canvas.drawBLUJLNode(i : Int, scale : Float, paint : Paint) {
     paint.strokeWidth = Math.min(w, h) / strokeFactor
     drawBiLineUpJoinLeft(scale, w, h, paint)
 }
+
+class BiLineUpJoinLeftView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
