b2975a5f56872efd16662c1db94dc52dde95b0ce
Anwesh43 <anweshthecool0@gmail.com>
2023-10-28 12:58:14 +0530

    bilineupjoinleftview: adding BLUJLNode class

--- bilineupjoinleftview/src/main/java/com/example/bilineupjoinleftview/BiLineUpJoinLeftView.kt
+++ bilineupjoinleftview/src/main/java/com/example/bilineupjoinleftview/BiLineUpJoinLeftView.kt
@@ -127,4 +127,45 @@ class BiLineUpJoinLeftView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class BLUJLNode(var i : Int = 0, val state : State = State()) {
+
+        private var next : BLUJLNode? = null
+        private var prev : BLUJLNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < colors.size - 1) {
+                next = BLUJLNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawBLUJLNode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : BLUJLNode {
+            var curr : BLUJLNode? = prev
+            if (dir == 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
