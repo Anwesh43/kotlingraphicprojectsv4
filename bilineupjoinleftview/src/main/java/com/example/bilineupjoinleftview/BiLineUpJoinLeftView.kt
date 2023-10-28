9ec72429490f46a5bd0f726ab981163fd7318822
Anwesh43 <anweshthecool0@gmail.com>
2023-10-28 13:01:06 +0530

    bilineupjoinleftview: adding Renderer class

--- bilineupjoinleftview/src/main/java/com/example/bilineupjoinleftview/BiLineUpJoinLeftView.kt
+++ bilineupjoinleftview/src/main/java/com/example/bilineupjoinleftview/BiLineUpJoinLeftView.kt
@@ -191,4 +191,27 @@ class BiLineUpJoinLeftView(ctx : Context) : View(ctx) {
             curr.startUpdating(cb)
         }
     }
+
+    data class Renderer(var view : BiLineUpJoinLeftView) {
+
+        private val animator : Animator = Animator(view)
+        private val blujl : BiLineUpJoinLeft = BiLineUpJoinLeft(0)
+        private val paint : Paint = Paint(Paint.ANTI_ALIAS_FLAG)
+
+        fun render(canvas : Canvas) {
+            canvas.drawColor(backColor)
+            blujl.draw(canvas, paint)
+            animator.animate {
+                blujl.update {
+                    animator.stop()
+                }
+            }
+        }
+
+        fun handleTap() {
+            blujl.startUpdating {
+                animator.start()
+            }
+        }
+    }
 }
