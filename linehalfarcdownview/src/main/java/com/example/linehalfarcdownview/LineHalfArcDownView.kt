5987089137fc906bf2f46d491ee0ec07b73fd663
Anwesh43 <anweshthecool0@gmail.com>
2023-10-29 12:18:18 +0530

    linehalfarcdownview: adding Canvas extension methods

--- linehalfarcdownview/src/main/java/com/example/linehalfarcdownview/LineHalfArcDownView.kt
+++ linehalfarcdownview/src/main/java/com/example/linehalfarcdownview/LineHalfArcDownView.kt
@@ -16,7 +16,7 @@ val colors : Array<String> = arrayOf(
     "#C51162",
     "#00C853"
 )
-val parts : Int = 4
+val parts : Int = 5
 val scGap : Float = 0.04f / parts
 val strokeFactor : Float = 90f
 val delay : Long = 20
@@ -27,3 +27,36 @@ val sizeFactor : Float = 4.9f
 fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
+
+fun Canvas.drawXY(x : Float, y : Float, cb : () -> Unit) {
+    save()
+    translate(x, y)
+    cb()
+    restore()
+}
+
+fun Canvas.drawLineHalfArcDown(scale : Float, w : Float, h : Float, paint : Paint) {
+    val size : Float = Math.min(w, h) / sizeFactor
+    val dsc : (Int) -> Float = {
+        scale.divideScale(it, parts)
+    }
+    drawXY(w / 2 + (w / 2 + size) * dsc(4), h / 2) {
+        drawXY(0f, 0f) {
+            rotate(rot * dsc(2))
+            drawLine(0f, 0f,  -size * dsc(0), 0f, paint)
+        }
+        drawXY(0f, 0f) {
+            rotate(rot * dsc(3))
+            drawArc(RectF(-size / 2, -size / 4, 0f, size / 4), 0f, 180f * dsc(0), true, paint)
+        }
+    }
+}
+
+fun Canvas.drawLHADNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = Color.parseColor(colors[i])
+    paint.strokeCap = Paint.Cap.ROUND
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    drawLineHalfArcDown(scale, w, h, paint)
+}
