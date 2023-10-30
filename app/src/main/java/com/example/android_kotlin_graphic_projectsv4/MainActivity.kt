31a0977360a268f8dcd9f6c31a18a17505632280
Anwesh43 <anweshthecool0@gmail.com>
2023-10-30 10:30:49 +0530

    bilinehalfarcdownview: adding view in activity

--- app/src/main/java/com/example/android_kotlin_graphic_projectsv4/MainActivity.kt
+++ app/src/main/java/com/example/android_kotlin_graphic_projectsv4/MainActivity.kt
@@ -2,7 +2,6 @@ package com.example.android_kotlin_graphic_projectsv4
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.example.linehalfarcdownview.LineHalfArcDownView
 
 
 class MainActivity : AppCompatActivity() {
@@ -10,6 +9,6 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         supportActionBar?.hide()
         //setContentView(R.layout.activity_main)
-        LineHalfArcDownView.create(this)
+        BiLineHalfArcDownView.create(this)
     }
 }
