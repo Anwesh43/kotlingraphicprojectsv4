fbb5107ac1a3fb0e79f16e7f602c1690e1f1e7db
Anwesh43 <anweshthecool0@gmail.com>
2023-10-30 10:28:48 +0530

    bilinehalfarcdownview: adding Renderer class

--- bilinehalfarcdownview/src/main/java/BiLineHalfArcDownView.kt
+++ bilinehalfarcdownview/src/main/java/BiLineHalfArcDownView.kt
@@ -187,4 +187,27 @@ class BiLineHalfArcDownView(ctx : Context) : View(ctx) {
             curr.startUpdating(cb)
         }
     }
+
+    data class Renderer(var view : BiLineHalfArcDownView) {
+
+        private val animator : Animator = Animator(view)
+        private val blhad : BiLineHalfArcDown = BiLineHalfArcDown(0)
+        private val paint : Paint = Paint(Paint.ANTI_ALIAS_FLAG)
+
+        fun render(canvas : Canvas) {
+            canvas.drawColor(backColor)
+            blhad.draw(canvas, paint)
+            animator.animate {
+                blhad.update {
+                    animator.stop()
+                }
+            }
+        }
+
+        fun handleTap() {
+            blhad.startUpdating {
+                animator.start()
+            }
+        }
+    }
 }
