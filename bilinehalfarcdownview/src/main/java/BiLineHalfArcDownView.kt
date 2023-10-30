95deebd8671c6a016b0e975f2cc91c67fc8bdea2
Anwesh43 <anweshthecool0@gmail.com>
2023-10-30 10:27:31 +0530

    bilinehalfarcdownview: adding BiLineHalfArcDown class

--- bilinehalfarcdownview/src/main/java/BiLineHalfArcDownView.kt
+++ bilinehalfarcdownview/src/main/java/BiLineHalfArcDownView.kt
@@ -164,4 +164,27 @@ class BiLineHalfArcDownView(ctx : Context) : View(ctx) {
             return this
         }
     }
+
+    data class BiLineHalfArcDown(var i : Int) {
+
+        private var curr : BLHADNode = BLHADNode(0)
+        private var dir : Int = 1
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            curr.draw(canvas, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            curr.update {
+                curr = curr.getNext(dir) {
+                    dir *= -1
+                }
+                cb(it)
+            }
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            curr.startUpdating(cb)
+        }
+    }
 }
