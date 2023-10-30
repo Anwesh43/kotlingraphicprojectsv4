2cea97a6d961414f11c033ee4ba904e8953f6ddc
Anwesh43 <anweshthecool0@gmail.com>
2023-10-30 10:26:20 +0530

    bilinehalfarcdownview: adding BLHADNode class

--- bilinehalfarcdownview/src/main/java/BiLineHalfArcDownView.kt
+++ bilinehalfarcdownview/src/main/java/BiLineHalfArcDownView.kt
@@ -123,4 +123,45 @@ class BiLineHalfArcDownView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class BLHADNode(var i : Int = 0, val state : State = State()) {
+
+        private var prev : BLHADNode? = null
+        private var next : BLHADNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < colors.size - 1) {
+                next = BLHADNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawBLHADNode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : BLHADNode {
+            var curr : BLHADNode? = prev
+            if (dir == 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
