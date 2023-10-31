8a7d74e9a2636ededc3231f4b8de824360767356
Anwesh43 <anweshthecool0@gmail.com>
2023-10-31 09:59:23 +0530

    lineslanthalfarcview: adding LineSlantHalfArcView class

--- lineslanthalfarcview/src/main/java/com/example/lineslanthalfarcview/LineSlantHalfArcView.kt
+++ lineslanthalfarcview/src/main/java/com/example/lineslanthalfarcview/LineSlantHalfArcView.kt
@@ -64,3 +64,19 @@ fun Canvas.drawLSHANode(i : Int, scale : Float, paint : Paint) {
     paint.strokeWidth = Math.min(w, h) / strokeFactor
     drawLineSlantHalfArc(scale, w, h, paint)
 }
+
+class LineSlantHalfArcView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
