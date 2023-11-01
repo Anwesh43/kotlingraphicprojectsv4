7c7ef7cffdfbc79c9a70ff032a9d2b23b1e4176d
Anwesh43 <anweshthecool0@gmail.com>
2023-11-01 13:59:04 +0530

    linebisectrotview: adding view in activity

--- app/src/main/java/com/example/android_kotlin_graphic_projectsv4/MainActivity.kt
+++ app/src/main/java/com/example/android_kotlin_graphic_projectsv4/MainActivity.kt
@@ -2,7 +2,7 @@ package com.example.android_kotlin_graphic_projectsv4
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.example.lineslanthalfarcview.LineSlantHalfArcView
+import com.example.linebisectrotview.LineBisectRotView
 
 
 class MainActivity : AppCompatActivity() {
@@ -10,6 +10,6 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         supportActionBar?.hide()
         //setContentView(R.layout.activity_main)
-        LineSlantHalfArcView.create(this)
+        LineBisectRotView.create(this)
     }
 }
