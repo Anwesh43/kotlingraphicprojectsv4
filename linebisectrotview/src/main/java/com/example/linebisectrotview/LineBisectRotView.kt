c33337b668541c6c5e7f2bf0ab1036693af3714d
Anwesh43 <anweshthecool0@gmail.com>
2023-11-01 13:48:44 +0530

    linebisectrotview: adding LBRNode class

--- linebisectrotview/src/main/java/com/example/linebisectrotview/LineBisectRotView.kt
+++ linebisectrotview/src/main/java/com/example/linebisectrotview/LineBisectRotView.kt
@@ -119,4 +119,45 @@ class LineBisectRotView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class LBRNode(var i : Int = 0, val state : State = State()) {
+
+        private var next : LBRNode? = null
+        private var prev : LBRNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < colors.size - 1) {
+                next = LBRNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawLBRNode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : LBRNode {
+            var curr : LBRNode? = prev
+            if (dir == 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
