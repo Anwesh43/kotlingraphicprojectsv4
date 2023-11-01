7d7d288d4467d31fde5695cb8d0c8d07c599bfca
Anwesh43 <anweshthecool0@gmail.com>
2023-11-01 13:39:15 +0530

    linebisectrotview: adding Canvas extension methods

--- linebisectrotview/src/main/java/com/example/linebisectrotview/LineBisectRotView.kt
+++ linebisectrotview/src/main/java/com/example/linebisectrotview/LineBisectRotView.kt
@@ -26,4 +26,33 @@ val rot : Float = 90f
 fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
-fun Float.bisectRot(j : Int) : Float = rot * (1f - 2 * j) * (2 + j) * 0.5f
+fun Float.bisectRot(j : Int) : Float = this * (1f - 2 * j) * (2 + j) * 0.5f
+
+fun Canvas.drawXY(x : Float, y : Float, cb : () -> Unit) {
+    save()
+    translate(x, y)
+    cb()
+    restore()
+}
+
+fun Canvas.drawLineBisectRot(scale : Float, w : Float, h : Float, paint : Paint) {
+    val size : Float = Math.min(w, h) / sizeFactor
+    val dsc : (Int) -> Float = {
+        scale.divideScale(it, parts)
+    }
+    drawXY(w / 2 + (w / 2 + size) * dsc(3), h / 2) {
+        for (j in 0..1) {
+            drawXY(0f, 0f) {
+                rotate(rot.bisectRot(j) * dsc(j + 1))
+                drawLine(0f, 0f, 0f, -size * dsc(0), paint)
+            }
+        }
+    }
+}
+
+fun Canvas.drawLBRNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = Color.parseColor(colors[i])
+    drawLineBisectRot(scale, w, h, paint)
+}
