07122f5e95361a326814c95aa70da78e218c03da
Anwesh43 <anweshthecool0@gmail.com>
2023-11-03 13:22:23 +0530

    linebentleftview: adding LineBentLeftView class

--- linebentleftview/src/main/java/com/example/linebentleftview/LineBentLeftView.kt
+++ linebentleftview/src/main/java/com/example/linebentleftview/LineBentLeftView.kt
@@ -60,3 +60,19 @@ fun Canvas.drawLBLNode(i : Int, scale : Float, paint : Paint) {
     paint.strokeWidth = Math.min(w, h) / strokeFactor
     drawLineBentLeft(scale, w, h, paint)
 }
+
+class LineBentLeftView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
