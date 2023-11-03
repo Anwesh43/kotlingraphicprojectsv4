33c168a1083e65a89e215128ba9407e5d9e08fa7
Anwesh43 <anweshthecool0@gmail.com>
2023-11-03 13:54:47 +0530

    linebentleftview: adding LineBentLeft class

--- linebentleftview/src/main/java/com/example/linebentleftview/LineBentLeftView.kt
+++ linebentleftview/src/main/java/com/example/linebentleftview/LineBentLeftView.kt
@@ -164,4 +164,27 @@ class LineBentLeftView(ctx : Context) : View(ctx) {
             return this
         }
     }
+
+    data class LineBentLeft(var i : Int = 0, val state : State = State()) {
+
+        private var curr : LBLNode = LBLNode(0)
+        private var dir : Int = 1
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            curr.draw(canvas, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            curr.update {
+                curr = curr.getNext(dir) {
+                    dir *= -1
+                }
+                cb(it)
+            }
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            curr.startUpdating(cb)
+        }
+    }
 }
