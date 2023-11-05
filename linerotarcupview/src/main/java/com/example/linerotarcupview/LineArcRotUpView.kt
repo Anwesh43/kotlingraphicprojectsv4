21c65a4f5c03f87acb2933629ede1a1f4ede573b
Anwesh43 <anweshthecool0@gmail.com>
2023-11-06 00:21:29 +0530

    linerotarcupview: adding static create method

--- linerotarcupview/src/main/java/com/example/linerotarcupview/LineArcRotUpView.kt
+++ linerotarcupview/src/main/java/com/example/linerotarcupview/LineArcRotUpView.kt
@@ -215,4 +215,13 @@ class LineArcRotUpView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    companion object {
+
+        fun create(activity : Activity) : LineArcRotUpView {
+            val view : LineArcRotUpView = LineArcRotUpView(activity)
+            activity.setContentView(view)
+            return view
+        }
+    }
 }
