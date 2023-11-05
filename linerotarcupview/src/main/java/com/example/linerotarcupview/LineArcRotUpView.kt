f33802f1efe14888d96cf1c186a22b1213714c40
Anwesh43 <anweshthecool0@gmail.com>
2023-11-06 00:41:30 +0530

    halfarclinedownview: adding stroke as style

--- linerotarcupview/src/main/java/com/example/linerotarcupview/LineArcRotUpView.kt
+++ linerotarcupview/src/main/java/com/example/linerotarcupview/LineArcRotUpView.kt
@@ -61,6 +61,7 @@ fun Canvas.drawLARUNode(i : Int, scale : Float, paint : Paint) {
     paint.color = Color.parseColor(colors[i])
     paint.strokeCap = Paint.Cap.ROUND
     paint.strokeWidth = Math.min(w, h) / strokeFactor
+    paint.style = Paint.Style.STROKE
     drawLineArcRotUp(scale, w, h, paint)
 }
 
