522cb2042f79f95bcb8a9e4a4458eac2666f9fbc
Anwesh43 <anweshthecool0@gmail.com>
2023-11-06 00:23:42 +0530

    linerotarcupview: adding view in activty

--- app/src/main/java/com/example/android_kotlin_graphic_projectsv4/MainActivity.kt
+++ app/src/main/java/com/example/android_kotlin_graphic_projectsv4/MainActivity.kt
@@ -2,7 +2,7 @@ package com.example.android_kotlin_graphic_projectsv4
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.example.lineblockrotupview.LineBlockRotUpView
+import com.example.linerotarcupview.LineArcRotUpView
 
 
 class MainActivity : AppCompatActivity() {
@@ -10,6 +10,6 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         supportActionBar?.hide()
         //setContentView(R.layout.activity_main)
-        LineBlockRotUpView.create(this)
+        LineArcRotUpView.create(this)
     }
 }
