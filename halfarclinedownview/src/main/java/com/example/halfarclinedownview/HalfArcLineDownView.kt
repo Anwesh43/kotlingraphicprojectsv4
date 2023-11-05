c9b82eaf51e0d6b656110b53b92f5fc6a824da01
Anwesh43 <anweshthecool0@gmail.com>
2023-11-06 01:20:23 +0530

    halfarclinedownview: adding HalfArcLineDownView class

--- halfarclinedownview/src/main/java/com/example/halfarclinedownview/HalfArcLineDownView.kt
+++ halfarclinedownview/src/main/java/com/example/halfarclinedownview/HalfArcLineDownView.kt
@@ -58,3 +58,19 @@ fun Canvas.drawHALDNode(i : Int, scale : Float, paint : Paint) {
     paint.strokeWidth = Math.min(w, h) / strokeFactor
     drawHalfArcLineDown(scale, w, h, paint)
 }
+
+class HalfArcLineDownView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
