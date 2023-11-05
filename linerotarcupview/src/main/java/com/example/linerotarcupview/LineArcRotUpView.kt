34d0d691793edaec90813616c13b5caf8be32b28
Anwesh43 <anweshthecool0@gmail.com>
2023-11-05 12:16:18 +0530

    linerotarcupview: adding constants and libraries

---
+++ linerotarcupview/src/main/java/com/example/linerotarcupview/LineArcRotUpView.kt
@@ -0,0 +1,27 @@
+package com.example.linerotarcupview
+
+import android.view.View
+import android.view.MotionEvent
+import android.app.Activity
+import android.content.Context
+import android.graphics.Paint
+import android.graphics.Canvas
+import android.graphics.Color
+import android.graphics.RectF
+
+val colors : Array<String> = arrayOf(
+    "#1A237E",
+    "#EF5350",
+    "#AA00FF",
+    "#C51162",
+    "#00C853"
+)
+val parts : Int = 3
+val scGap : Float = 0.03f/ 3
+val strokeFactor : Float = 90f
+val sizeFactor : Float = 4.9f
+val delay : Long = 20
+val backColor : String = "#BDBDBD"
+val rot : Float = 180f
+val deg : Float = 360f
+
