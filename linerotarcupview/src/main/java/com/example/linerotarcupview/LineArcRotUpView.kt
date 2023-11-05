ac5bf22f7431c5cfa7334112d4b898457a98275d
Anwesh43 <anweshthecool0@gmail.com>
2023-11-05 12:22:29 +0530

    linerotarcupview: adding Canvas extension methods

--- linerotarcupview/src/main/java/com/example/linerotarcupview/LineArcRotUpView.kt
+++ linerotarcupview/src/main/java/com/example/linerotarcupview/LineArcRotUpView.kt
@@ -23,8 +23,43 @@ val sizeFactor : Float = 4.9f
 val delay : Long = 20
 val backColor : String = "#BDBDBD"
 val rot : Float = 180f
-val deg : Float = 360f
-
+val rFactor : Float = 3.2f
 fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
+
+fun Canvas.drawXY(x : Float, y : Float, cb : () -> Unit) {
+    save()
+    translate(x, y)
+    cb()
+    restore()
+}
+
+fun Canvas.drawLineArcRotUp(scale : Float, w : Float, h : Float, paint : Paint) {
+    val size : Float = Math.min(w, h) / sizeFactor
+    val dsc : (Int) -> Float = {
+        scale.divideScale(it, parts)
+    }
+    val r : Float = size / rFactor
+    drawXY(w / 2, h / 2) {
+        for (j in 0..1) {
+            scale(1f - 2 * j, 1f)
+            drawXY(0f, 0f) {
+                drawArc(RectF(-r, -r, r, r), -90f, rot * dsc(1), false, paint)
+            }
+            drawXY(0f, -h * 0.5f * (1 - dsc(0))) {
+                rotate(rot * dsc(1))
+                drawLine(0f, 0f, 0f, -size, paint)
+            }
+        }
+    }
+}
+
+fun Canvas.drawLARUNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = Color.parseColor(colors[i])
+    paint.strokeCap = Paint.Cap.ROUND
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    drawLineArcRotUp(scale, w, h, paint)
+}
