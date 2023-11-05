7586deef063a4c58c1a3dbd566223f1c96296149
Anwesh43 <anweshthecool0@gmail.com>
2023-11-05 13:00:54 +0530

    linerotarcupview: adding Renderer class

--- linerotarcupview/src/main/java/com/example/linerotarcupview/LineArcRotUpView.kt
+++ linerotarcupview/src/main/java/com/example/linerotarcupview/LineArcRotUpView.kt
@@ -190,4 +190,27 @@ class LineArcRotUpView(ctx : Context) : View(ctx) {
             curr.startUpdating(cb)
         }
     }
+
+    data class Renderer(var view : LineArcRotUpView) {
+
+        private val animator : Animator = Animator(view)
+        private val laru : LineArcRotUp = LineArcRotUp(0)
+        private val paint : Paint = Paint(Paint.ANTI_ALIAS_FLAG)
+
+        fun render(canvas : Canvas) {
+            canvas.drawColor(Color.parseColor(backColor))
+            laru.draw(canvas, paint)
+            animator.animate {
+                laru.update {
+                    animator.stop()
+                }
+            }
+        }
+
+        fun handleTap() {
+            laru.startUpdating {
+                animator.start()
+            }
+        }
+    }
 }
