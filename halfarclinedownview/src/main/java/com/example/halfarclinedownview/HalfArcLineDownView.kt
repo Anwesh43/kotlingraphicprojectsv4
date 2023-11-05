353ae5c961194c96f4adc6e33e2af5be23bd3cc7
Anwesh43 <anweshthecool0@gmail.com>
2023-11-06 00:58:16 +0530

    halfarclinedownview: adding Canvas extension methods

--- halfarclinedownview/src/main/java/com/example/halfarclinedownview/HalfArcLineDownView.kt
+++ halfarclinedownview/src/main/java/com/example/halfarclinedownview/HalfArcLineDownView.kt
@@ -27,3 +27,34 @@ val backColor : Int = Color.parseColor("#BDBDBD")
 fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
+
+fun Canvas.drawXY(x : Float, y : Float, cb : () -> Unit) {
+    save()
+    translate(x, y)
+    cb()
+    restore()
+}
+
+fun Canvas.drawHalfArcLineDown(scale : Float, w : Float, h : Float, paint : Paint) {
+    val size : Float = Math.min(w, h) / sizeFactor
+    val dsc : (Int) -> Float = {
+        scale.divideScale(it, parts)
+    }
+    drawXY(w / 2, h / 2) {
+        drawXY((w / 2) * (1 - dsc(1)), 0f) {
+            rotate(-rot * dsc(2))
+            drawLine(0f, 0f, size, 0f, paint)
+        }
+        drawArc(RectF(-size, -size / 2, 0f, size / 2), 0f, 180f * dsc(0), false, paint)
+    }
+}
+
+fun Canvas.drawHALDNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = Color.parseColor(colors[i])
+    paint.strokeCap = Paint.Cap.ROUND
+    paint.style = Paint.Style.STROKE
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    drawHalfArcLineDown(scale, w, h, paint)
+}
