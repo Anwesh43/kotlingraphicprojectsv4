ed9e421c2d80b93cf37ab150fd5766e8535ce038
Anwesh43 <anweshthecool0@gmail.com>
2023-11-06 10:31:48 +0530

    halfarclinedownview: adding static create method

--- halfarclinedownview/src/main/java/com/example/halfarclinedownview/HalfArcLineDownView.kt
+++ halfarclinedownview/src/main/java/com/example/halfarclinedownview/HalfArcLineDownView.kt
@@ -210,4 +210,13 @@ class HalfArcLineDownView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    companion object {
+
+        fun create(activity : Activity) : HalfArcLineDownView {
+            val view : HalfArcLineDownView = HalfArcLineDownView(activity)
+            activity.setContentView(view)
+            return view
+        }
+    }
 }
