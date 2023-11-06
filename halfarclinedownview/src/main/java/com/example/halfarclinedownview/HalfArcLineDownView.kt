2ecdf4b62634f2e770761356e06842795167c3b1
Anwesh43 <anweshthecool0@gmail.com>
2023-11-06 10:30:37 +0530

    halfarclinedownview: adding Renderer class

--- halfarclinedownview/src/main/java/com/example/halfarclinedownview/HalfArcLineDownView.kt
+++ halfarclinedownview/src/main/java/com/example/halfarclinedownview/HalfArcLineDownView.kt
@@ -185,4 +185,27 @@ class HalfArcLineDownView(ctx : Context) : View(ctx) {
             curr.startUpdating(cb)
         }
     }
+
+    data class Renderer(var view : HalfArcLineDownView) {
+
+        private val animator : Animator = Animator(view)
+        private val hald : HalfArcLineDown = HalfArcLineDown(0)
+        private val paint : Paint = Paint(Paint.ANTI_ALIAS_FLAG)
+
+        fun render(canvas : Canvas) {
+            canvas.drawColor(backColor)
+            hald.draw(canvas, paint)
+            animator.animate {
+                hald.update {
+                    animator.stop()
+                }
+            }
+        }
+
+        fun handleTap() {
+            hald.startUpdating {
+                animator.start()
+            }
+        }
+    }
 }
