acbc692ad2114f34899864ebddcc3c542e10b1c3
Anwesh43 <anweshthecool0@gmail.com>
2023-11-06 10:28:29 +0530

    halfarclinedownview: adding HalfArcLineDown class

--- halfarclinedownview/src/main/java/com/example/halfarclinedownview/HalfArcLineDownView.kt
+++ halfarclinedownview/src/main/java/com/example/halfarclinedownview/HalfArcLineDownView.kt
@@ -162,4 +162,27 @@ class HalfArcLineDownView(ctx : Context) : View(ctx) {
             return this
         }
     }
+
+    data class HalfArcLineDown(var i : Int) {
+
+        private var curr : HALDNode = HALDNode(0)
+        private var dir : Int = 1
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            curr.draw(canvas, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            curr.update {
+                curr.getNext(dir) {
+                    dir *= -1
+                }
+                cb(it)
+            }
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            curr.startUpdating(cb)
+        }
+    }
 }
