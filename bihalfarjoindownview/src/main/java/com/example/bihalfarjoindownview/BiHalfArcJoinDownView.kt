b218faac7c9121ff62e05db4cb572fe858ab44be
Anwesh43 <anweshthecool0@gmail.com>
2023-11-08 11:40:58 +0530

    bihalfarcjoindownview: adding BHAJDNode class

--- bihalfarjoindownview/src/main/java/com/example/bihalfarjoindownview/BiHalfArcJoinDownView.kt
+++ bihalfarjoindownview/src/main/java/com/example/bihalfarjoindownview/BiHalfArcJoinDownView.kt
@@ -120,4 +120,45 @@ class BiHalfArcJoinDownView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class BHAJDNode(var i : Int = 0, val state : State = State()) {
+
+        private var next : BHAJDNode? = null
+        private var prev : BHAJDNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < colors.size - 1) {
+                next = BHAJDNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawBHAJDNode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : BHAJDNode {
+            var curr : BHAJDNode? = prev
+            if (dir == 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
