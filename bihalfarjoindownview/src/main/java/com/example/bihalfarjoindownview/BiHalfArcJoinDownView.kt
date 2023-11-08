ba10d909b3fccb66e5696cb11eb9fe4f7535b841
Anwesh43 <anweshthecool0@gmail.com>
2023-11-08 11:26:04 +0530

    bihalfarcjoindownview: adding Canvas extension methods

--- bihalfarjoindownview/src/main/java/com/example/bihalfarjoindownview/BiHalfArcJoinDownView.kt
+++ bihalfarjoindownview/src/main/java/com/example/bihalfarjoindownview/BiHalfArcJoinDownView.kt
@@ -27,3 +27,33 @@ val rot : Float = 90f
 fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
+
+fun Canvas.drawXY(x : Float, y : Float, cb : () -> Unit) {
+    save()
+    translate(x, y)
+    cb()
+    restore()
+}
+
+fun Canvas.drawBiHalfArcJoinDown(scale : Float, w : Float, h : Float, paint : Paint) {
+    val size : Float = Math.min(w, h) / sizeFactor
+    val dsc : (Int) -> Float = {
+        scale.divideScale(it, parts)
+    }
+    drawXY(w / 2, h / 2 + (h / 2) * dsc(3)) {
+        for (j in 0..1) {
+            drawXY(0f, 0f) {
+                scale(1f - 2 * j, 1f)
+                rotate(rot * dsc(2))
+                drawArc(RectF(0f, -size / 2, size, size / 2), 180f, 180f * dsc(j), false, paint)
+            }
+        }
+    }
+}
+
+fun Canvas.drawBHAJDNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = Color.parseColor(colors[i])
+    drawBiHalfArcJoinDown(scale, w, h, paint)
+}
