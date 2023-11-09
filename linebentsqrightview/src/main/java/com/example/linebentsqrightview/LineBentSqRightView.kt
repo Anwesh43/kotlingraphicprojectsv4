c0465594cf348491ef0d02e2157b4cce54426a88
Anwesh43 <anweshthecool0@gmail.com>
2023-11-09 11:47:24 +0530

    linebentsqrightview: adding Canvas extension methods

--- linebentsqrightview/src/main/java/com/example/linebentsqrightview/LineBentSqRightView.kt
+++ linebentsqrightview/src/main/java/com/example/linebentsqrightview/LineBentSqRightView.kt
@@ -22,8 +22,40 @@ val strokeFactor : Float = 90f
 val sizeFactor : Float = 4.9f
 val delay : Long = 20
 val backColor : Int = Color.parseColor("#BDBDBD")
-val rot : Float = 90f
+val rot : Float = 135f
 
 fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
+
+fun Canvas.drawXY(x : Float, y : Float, cb : () -> Unit) {
+    save()
+    translate(x, y)
+    cb()
+    restore()
+}
+
+fun Canvas.drawLineBentSqRight(scale : Float, w : Float, h : Float, paint : Paint) {
+    val size : Float = Math.min(w, h) / sizeFactor
+    val dsc : (Int) -> Float = {
+        scale.divideScale(it, parts)
+    }
+    drawXY(w / 2 + (w / 2) * dsc(3), h / 2) {
+        drawXY(0f, -h * 0.5f * (1 - dsc(1))) {
+            rotate(rot * dsc(2))
+            drawLine(0f, 0f, 0f, -size, paint)
+        }
+        drawXY(w * 0.5f * (1 - dsc(0)), 0f) {
+            drawRect(RectF(0f, 0f, size / 2, size / 2), paint)
+        }
+    }
+}
+
+fun Canvas.drawLBSRNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = Color.parseColor(colors[i])
+    paint.strokeCap = Paint.Cap.ROUND
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    drawLineBentSqRight(scale, w, h, paint)
+}
