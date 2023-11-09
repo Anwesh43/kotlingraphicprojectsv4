20b1df635bdb6dad11d4f1f02c593a9ed555ab3c
Anwesh43 <anweshthecool0@gmail.com>
2023-11-09 11:36:37 +0530

    linebentsqrightview: adding constants and libraries

---
+++ linebentsqrightview/src/main/java/com/example/linebentsqrightview/LineBentSqRightView.kt
@@ -0,0 +1,25 @@
+package com.example.linebentsqrightview
+
+import android.view.View
+import android.view.MotionEvent
+import android.graphics.Paint
+import android.graphics.Canvas
+import android.graphics.RectF
+import android.graphics.Color
+import android.content.Context
+import android.app.Activity
+
+val colors : Array<String> = arrayOf(
+    "#1A237E",
+    "#EF5350",
+    "#AA00FF",
+    "#C51162",
+    "#00C853"
+)
+val parts : Int = 4
+val scGap : Float = 0.04f / parts
+val strokeFactor : Float = 90f
+val sizeFactor : Float = 4.9f
+val delay : Long = 20
+val backColor : Int = Color.parseColor("#BDBDBD")
+val rot : Float = 90f
