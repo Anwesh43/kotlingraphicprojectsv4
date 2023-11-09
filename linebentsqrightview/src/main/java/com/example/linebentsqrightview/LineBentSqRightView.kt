b00515922bf28cb5aac6ae98c7ed2a15664671b7
Anwesh43 <anweshthecool0@gmail.com>
2023-11-09 12:22:52 +0530

    linebentsqrightview: adding LineBentSqRight class

--- linebentsqrightview/src/main/java/com/example/linebentsqrightview/LineBentSqRightView.kt
+++ linebentsqrightview/src/main/java/com/example/linebentsqrightview/LineBentSqRightView.kt
@@ -147,7 +147,7 @@ class LineBentSqRightView(ctx : Context) : View(ctx) {
             state.update(cb)
         }
 
-        fun startUdpating(cb : () -> Unit) {
+        fun startUpdating(cb : () -> Unit) {
             state.startUpdating(cb)
         }
 
@@ -161,6 +161,29 @@ class LineBentSqRightView(ctx : Context) : View(ctx) {
             }
             cb()
             return this
+        }
+    }
+
+    data class LineBentSqRight(var i : Int = 0) {
+
+        private var curr : LBSRNode = LBSRNode(0)
+        private var dir : Int = 1
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            curr.draw(canvas, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            curr.update {
+                curr = curr.getNext(dir) {
+                    dir *= -1
+                }
+                cb(it)
+            }
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            curr.startUpdating(cb)
         }
     }
 }
