93450ce6af865ba32aa6bc1014660fbc492f2359
Anwesh43 <anweshthecool0@gmail.com>
2023-11-10 09:44:54 +0530

    halfarclineleftview: adding Animator class

--- halfarclineleftview/src/main/java/com/example/halfarclineleftview/HalfArcLineLeftView.kt
+++ halfarclineleftview/src/main/java/com/example/halfarclineleftview/HalfArcLineLeftView.kt
@@ -160,4 +160,32 @@ class HalfArcLineLeftView(ctx : Context) : View(ctx) {
             curr.startUpdating(cb)
         }
     }
+
+    data class Animaor(var view : View, var animated : Boolean = false) {
+
+        fun animate(cb : () -> Unit) {
+            if (animated) {
+                cb()
+                try {
+                    Thread.sleep(delay)
+                    view.invalidate()
+                } catch(ex : Exception) {
+
+                }
+            }
+        }
+
+        fun start() {
+            if (!animated) {
+                animated = true
+                view.postInvalidate()
+            }
+        }
+
+        fun stop() {
+            if (animated) {
+                animated = false
+            }
+        }
+    }
 }
