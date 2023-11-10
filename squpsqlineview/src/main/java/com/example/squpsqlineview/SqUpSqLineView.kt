97e494ad5f8eaa4bd0816a975e83e84acdeed2eb
Anwesh43 <anweshthecool0@gmail.com>
2023-11-11 03:40:18 +0530

    squpsqlineview: adding Renderer class

--- squpsqlineview/src/main/java/com/example/squpsqlineview/SqUpSqLineView.kt
+++ squpsqlineview/src/main/java/com/example/squpsqlineview/SqUpSqLineView.kt
@@ -186,4 +186,27 @@ class SqUpSqLineView(ctx : Context) : View(ctx) {
             curr.startUpdating(cb)
         }
     }
+
+    data class Renderer(var view : SqUpSqLineView) {
+
+        private val animator : Animator = Animator(view)
+        private val susl : SqUpSqLine = SqUpSqLine(0)
+        private val paint : Paint = Paint(Paint.ANTI_ALIAS_FLAG)
+
+        fun render(canvas : Canvas) {
+            canvas.drawColor(backColor)
+            susl.draw(canvas, paint)
+            animator.animate {
+                susl.update {
+                    animator.stop()
+                }
+            }
+        }
+
+        fun handleTap() {
+            susl.startUpdating {
+                animator.start()
+            }
+        }
+    }
 }
