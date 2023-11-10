9768f54a9024a7c83e7faae32598f4ebf2ee2d55
Anwesh43 <anweshthecool0@gmail.com>
2023-11-11 03:31:06 +0530

    squpsqlineview: adding SUSLNode class

--- squpsqlineview/src/main/java/com/example/squpsqlineview/SqUpSqLineView.kt
+++ squpsqlineview/src/main/java/com/example/squpsqlineview/SqUpSqLineView.kt
@@ -122,4 +122,45 @@ class SqUpSqLineView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class SUSLNode(var i : Int = 0, val state : State = State()) {
+
+        private var next : SUSLNode? = null
+        private var prev : SUSLNode? = null
+
+        init {
+
+        }
+
+        fun addNeighbor() {
+            if (i < colors.size - 1) {
+                next = SUSLNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawSUSLNode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : SUSLNode {
+            var curr : SUSLNode? = prev
+            if (dir == 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
