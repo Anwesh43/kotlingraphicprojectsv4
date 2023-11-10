df776c13c1409b5c5e1bca0167d2bbefa3cad2f5
Anwesh43 <anweshthecool0@gmail.com>
2023-11-10 09:48:52 +0530

    halfarclineleftview: adding view in activity

--- app/src/main/java/com/example/android_kotlin_graphic_projectsv4/MainActivity.kt
+++ app/src/main/java/com/example/android_kotlin_graphic_projectsv4/MainActivity.kt
@@ -2,7 +2,8 @@ package com.example.android_kotlin_graphic_projectsv4
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.example.linebentsqrightview.LineBentSqRightView
+import com.example.halfarclineleftview.HalfArcLineLeftView
+
 
 
 class MainActivity : AppCompatActivity() {
@@ -10,6 +11,6 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         supportActionBar?.hide()
         //setContentView(R.layout.activity_main)
-        LineBentSqRightView.create(this)
+        HalfArcLineLeftView.create(this)
     }
 }
