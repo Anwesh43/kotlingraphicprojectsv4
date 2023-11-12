8d239ea68fc64204b1e2784a98f9727fcd2c3b12
Anwesh43 <anweshthecool0@gmail.com>
2023-11-13 04:37:57 +0530

    tetrisblockrotrightview: adding view in activity

--- app/src/main/java/com/example/android_kotlin_graphic_projectsv4/MainActivity.kt
+++ app/src/main/java/com/example/android_kotlin_graphic_projectsv4/MainActivity.kt
@@ -2,8 +2,7 @@ package com.example.android_kotlin_graphic_projectsv4
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.example.lineupdownhalfcircleview.LineUpDownHalfCircleView
-
+import com.example.tetrisblockrotrightview.TetrisBlockRotRightView
 
 
 class MainActivity : AppCompatActivity() {
@@ -11,6 +10,6 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         supportActionBar?.hide()
         //setContentView(R.layout.activity_main)
-        LineUpDownHalfCircleView.create(this)
+        TetrisBlockRotRightView.create(this)
     }
 }
