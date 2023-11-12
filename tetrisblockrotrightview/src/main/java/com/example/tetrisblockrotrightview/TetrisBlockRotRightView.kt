2305d880dd6093a4ef0e6d49889b8692d50c79dd
Anwesh43 <anweshthecool0@gmail.com>
2023-11-13 04:26:03 +0530

    tetrisblockrotrightview: adding TetrisBlockRotRightView class

--- tetrisblockrotrightview/src/main/java/com/example/tetrisblockrotrightview/TetrisBlockRotRightView.kt
+++ tetrisblockrotrightview/src/main/java/com/example/tetrisblockrotrightview/TetrisBlockRotRightView.kt
@@ -56,3 +56,19 @@ fun Canvas.drawTBRRNode(i : Int, scale : Float, paint : Paint) {
     paint.color = Color.parseColor(colors[i])
     drawTetrisBlockRotRight(scale, w, h, paint)
 }
+
+class TetrisBlockRotRightView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
