2b418bb619c00bd4c33bbb37515209ceb89e8c33
Anwesh43 <anweshthecool0@gmail.com>
2023-11-13 04:24:58 +0530

    tetrisblockrotrightview: adding Canvas extension methods

--- tetrisblockrotrightview/src/main/java/com/example/tetrisblockrotrightview/TetrisBlockRotRightView.kt
+++ tetrisblockrotrightview/src/main/java/com/example/tetrisblockrotrightview/TetrisBlockRotRightView.kt
@@ -26,3 +26,33 @@ val rot : Float = 90f
 fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
+
+fun Canvas.drawXY(x : Float, y : Float, cb : () -> Unit) {
+    save()
+    translate(x, y)
+    cb()
+    restore()
+}
+
+fun Canvas.drawTetrisBlockRotRight(scale : Float, w : Float, h : Float, paint : Paint) {
+    val size : Float = Math.min(w, h) / sizeFactor
+    val dsc : (Int) -> Float = {
+        scale.divideScale(it, parts)
+    }
+    drawXY(w / 2, h / 2) {
+        rotate(rot * dsc(2))
+        drawXY(w * 0.5f * (1 - dsc(0)), 0f) {
+            drawRect(RectF(-size / 2, 0f, size / 2, size / 2), paint)
+        }
+        drawXY(0f, -h * 0.5f * (1 - dsc(1))) {
+            drawRect(RectF(-size / 4, -size / 2, size / 4, 0f), paint)
+        }
+    }
+}
+
+fun Canvas.drawTBRRNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = Color.parseColor(colors[i])
+    drawTetrisBlockRotRight(scale, w, h, paint)
+}
