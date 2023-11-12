76ca0467c1c0df090b4cfb25f39bd2719ddc5f31
Anwesh43 <anweshthecool0@gmail.com>
2023-11-13 04:19:45 +0530

    tetrisblockrotrightview: adding constants and libraries

---
+++ tetrisblockrotrightview/src/main/java/com/example/tetrisblockrotrightview/TetrisBlockRotRightView.kt
@@ -0,0 +1,24 @@
+package com.example.tetrisblockrotrightview
+
+import android.view.View
+import android.view.MotionEvent
+import android.graphics.Paint
+import android.graphics.Canvas
+import android.graphics.Color
+import android.graphics.RectF
+import android.content.Context
+import android.app.Activity
+
+val colors : Array<String> = arrayOf(
+    "#1A237E",
+    "#EF5350",
+    "#AA00FF",
+    "#C51162",
+    "#00C853"
+)
+val parts : Int = 4
+val scGap : Float = 0.04f / parts
+val sizeFactor : Float = 4.9f
+val delay : Long = 20
+val backColor : Int = Color.parseColor("#BDBDBD")
+val rot : Float = 90f
