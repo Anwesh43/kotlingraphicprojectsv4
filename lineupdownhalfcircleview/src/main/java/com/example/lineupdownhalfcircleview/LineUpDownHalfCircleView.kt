35ab598f3556f5a09e616df439c4956a674cc008
Anwesh43 <anweshthecool0@gmail.com>
2023-11-12 11:51:08 +0530

    lineupdownhalfcircleview: adding Canvas extension methods

--- lineupdownhalfcircleview/src/main/java/com/example/lineupdownhalfcircleview/LineUpDownHalfCircleView.kt
+++ lineupdownhalfcircleview/src/main/java/com/example/lineupdownhalfcircleview/LineUpDownHalfCircleView.kt
@@ -28,3 +28,35 @@ val sweep : Float = 180f
 fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
+
+fun Canvas.drawXY(x : Float, y : Float, cb : () -> Unit) {
+    save()
+    translate(x, y)
+    cb()
+    restore()
+}
+
+fun Canvas.drawLineUpDownHalfCircle(scale : Float, w : Float, h : Float, paint: Paint) {
+    val size : Float = Math.min(w, h) / strokeFactor
+    val dsc : (Int) -> Float = {
+        scale.divideScale(it, parts)
+    }
+    drawXY(w / 2, h / 2 + (h / 2 + size) * dsc(3)) {
+        drawXY(0f, 0f) {
+            rotate(rot * dsc(1))
+            drawArc(RectF(-size / 2, -size / 2, size / 2, size / 2), -sweep / 2, sweep * dsc(0), true, paint)
+        }
+        drawXY(0f, -h * 0.5f * (1 - dsc(2))) {
+            drawLine(0f, 0f, 0f, -size, paint)
+        }
+    }
+}
+
+fun Canvas.drawLUDHCNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = Color.parseColor(colors[i])
+    paint.strokeCap = Paint.Cap.ROUND
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    drawLineUpDownHalfCircle(scale, w, h, paint)
+}
