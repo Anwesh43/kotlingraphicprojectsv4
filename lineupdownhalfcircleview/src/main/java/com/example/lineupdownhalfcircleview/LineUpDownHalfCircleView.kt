116df06cc469e30a6be0d93bd2e7f1d6632b1da7
Anwesh43 <anweshthecool0@gmail.com>
2023-11-12 11:52:44 +0530

    lineupdownhalfcircleview: adding LineUpDownHalfCircleView class

--- lineupdownhalfcircleview/src/main/java/com/example/lineupdownhalfcircleview/LineUpDownHalfCircleView.kt
+++ lineupdownhalfcircleview/src/main/java/com/example/lineupdownhalfcircleview/LineUpDownHalfCircleView.kt
@@ -60,3 +60,19 @@ fun Canvas.drawLUDHCNode(i : Int, scale : Float, paint : Paint) {
     paint.strokeWidth = Math.min(w, h) / strokeFactor
     drawLineUpDownHalfCircle(scale, w, h, paint)
 }
+
+class LineUpDownHalfCircleView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
