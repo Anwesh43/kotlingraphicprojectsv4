9de24bef6096acb98fe464c8db3266b5b0b35048
Anwesh43 <anweshthecool0@gmail.com>
2023-11-15 02:06:24 +0530

    linearcrotdownview: adding Int and Float extension methods

--- linearcrotdownview/src/main/java/com/example/linearcrotdownview/LineArcRotDownView.kt
+++ linearcrotdownview/src/main/java/com/example/linearcrotdownview/LineArcRotDownView.kt
@@ -23,3 +23,7 @@ val delay : Long = 20
 val backColor : Int = Color.parseColor("#BDBDBD")
 val parts : Int = 4
 val scGap : Float = 0.04f / parts
+
+fun Int.inverse() : Float = 1f / this
+fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
+fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
