e5640c070ecd5861942fa34aa8b59e2a31b70b0d
Anwesh43 <anweshthecool0@gmail.com>
2023-11-14 15:28:07 +0530

    linerothalfarcrightview: adding Renderer class

--- linerothalfarcrightview/src/main/java/com/example/linerothalfarcrightview/LineRotHalfArcView.kt
+++ linerothalfarcrightview/src/main/java/com/example/linerothalfarcrightview/LineRotHalfArcView.kt
@@ -185,4 +185,27 @@ class LineRotHalfArcView(ctx : Context) : View(ctx) {
             curr.startUpdating(cb)
         }
     }
+
+    data class Renderer(var view : LineRotHalfArcView) {
+
+        private val animator : Animator = Animator(view)
+        private val lrha : LineRotHalfArc = LineRotHalfArc(0)
+        private val paint : Paint = Paint(Paint.ANTI_ALIAS_FLAG)
+
+        fun render(canvas : Canvas) {
+            canvas.drawColor(backColor)
+            lrha.draw(canvas, paint)
+            animator.animate {
+                lrha.update {
+                    animator.stop()
+                }
+            }
+        }
+
+        fun handleTap() {
+            lrha.startUpdating {
+                animator.start()
+            }
+        }
+    }
 }
