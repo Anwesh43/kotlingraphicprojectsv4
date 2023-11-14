b55c9df66634c8ebfec244ceca0b4875f117c861
Anwesh43 <anweshthecool0@gmail.com>
2023-11-15 02:13:27 +0530

    linearcrotdownview: adding LineArcRotDownView class

--- linearcrotdownview/src/main/java/com/example/linearcrotdownview/LineArcRotDownView.kt
+++ linearcrotdownview/src/main/java/com/example/linearcrotdownview/LineArcRotDownView.kt
@@ -57,3 +57,19 @@ fun Canvas.drawLARDNode(i : Int, scale : Float, paint : Paint) {
     paint.strokeCap = Paint.Cap.ROUND
     drawLineArcRotDown(scale, w, h, paint)
 }
+
+class LineArcRotDownView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
