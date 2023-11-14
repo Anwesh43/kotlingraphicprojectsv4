7d1eeb5d1793b94621e9a2810339640900e51c41
Anwesh43 <anweshthecool0@gmail.com>
2023-11-15 02:12:10 +0530

    linearcrotdownview: adding Canvas  extension methods

--- linearcrotdownview/src/main/java/com/example/linearcrotdownview/LineArcRotDownView.kt
+++ linearcrotdownview/src/main/java/com/example/linearcrotdownview/LineArcRotDownView.kt
@@ -27,3 +27,33 @@ val scGap : Float = 0.04f / parts
 fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
+
+fun Canvas.drawXY(x : Float, y : Float, cb : () -> Unit) {
+    save()
+    translate(x, y)
+    cb()
+    restore()
+}
+
+fun Canvas.drawLineArcRotDown(scale : Float, w : Float, h : Float, paint : Paint) {
+    val size : Float = Math.min(w, h) / sizeFactor
+    val dsc : (Int) -> Float = {
+        scale.divideScale(it, parts)
+    }
+    drawXY(w / 2 + (w / 2 + size) * dsc(3), h / 2) {
+        rotate(rot * dsc(2))
+        drawXY(0f, -size / 2) {
+            drawArc(RectF(-size / 2, -size / 2, size / 2, size / 2), 0f, 180f * dsc(0), true, paint)
+        }
+        drawLine(0f, 0f, 0f, size * dsc(1), paint)
+    }
+}
+
+fun Canvas.drawLARDNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = Color.parseColor(colors[i])
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    paint.strokeCap = Paint.Cap.ROUND
+    drawLineArcRotDown(scale, w, h, paint)
+}
