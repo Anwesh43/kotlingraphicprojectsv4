9736da595c549f8d466003e81052af8cbd7ab3ba
Anwesh43 <anweshthecool0@gmail.com>
2023-11-19 12:47:15 +0530

    linebenthalfcircleview: adding LBHCNode class

--- linebenthalfcircleview/src/main/java/com/example/linebenthalfcircleview/LineBentHalfCircleView.kt
+++ linebenthalfcircleview/src/main/java/com/example/linebenthalfcircleview/LineBentHalfCircleView.kt
@@ -121,4 +121,45 @@ class LineBentHalfCircleView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class LBHCNode(var i : Int = 0, val state : State = State()) {
+
+        private var next : LBHCNode? = null
+        private var prev : LBHCNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < colors.size - 1) {
+                next = LBHCNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawLBHCNode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : (Float) -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : LBHCNode {
+            var curr : LBHCNode? = prev
+            if (dir == 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
