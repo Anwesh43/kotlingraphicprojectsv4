0c0cdbd221e1e1aafe44fb5099643b01cd3e534c
Anwesh43 <anweshthecool0@gmail.com>
2023-11-19 12:20:27 +0530

    linebenthalfcircleview: adding LineBentHalfCircleView class

--- linebenthalfcircleview/src/main/java/com/example/linebenthalfcircleview/LineBentHalfCircleView.kt
+++ linebenthalfcircleview/src/main/java/com/example/linebenthalfcircleview/LineBentHalfCircleView.kt
@@ -58,3 +58,19 @@ fun Canvas.drawLBHCNode(i : Int, scale : Float, paint : Paint) {
     paint.strokeWidth = Math.min(w, h) / strokeFactor
     drawLineBentHalfCircle(scale, w, h, paint)
 }
+
+class LineBentHalfCircleView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
