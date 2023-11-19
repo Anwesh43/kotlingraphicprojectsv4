a5a6c8e63b4ee6720051f8246577bf8508926055
Anwesh43 <anweshthecool0@gmail.com>
2023-11-19 12:18:34 +0530

    linebenthalfcircleview: adding Canvas extension methods

--- linebenthalfcircleview/src/main/java/com/example/linebenthalfcircleview/LineBentHalfCircleView.kt
+++ linebenthalfcircleview/src/main/java/com/example/linebenthalfcircleview/LineBentHalfCircleView.kt
@@ -28,3 +28,33 @@ val sweep : Float = 180f
 fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
+
+fun Canvas.drawXY(x : Float, y : Float, cb : () -> Unit) {
+    save()
+    translate(x, y)
+    cb()
+    restore()
+}
+
+fun Canvas.drawLineBentHalfCircle(scale : Float, w : Float, h : Float, paint : Paint) {
+    val size : Float = Math.min(w, h) / sizeFactor
+    val dsc : (Int) -> Float = {
+        scale.divideScale(it, parts)
+    }
+    drawXY(w / 2, h / 2 + (h / 2 + size) * dsc(3)) {
+        drawXY(0f, -h * 0.5f * (1 - dsc(0))) {
+            rotate(rot * dsc(1))
+            drawLine(0f, 0f, 0f, -size, paint)
+        }
+        drawArc(RectF(-size, -size / 2, 0f, size / 2), 0f, sweep * dsc(2), true, paint)
+    }
+}
+
+fun Canvas.drawLBHCNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = Color.parseColor(colors[i])
+    paint.strokeCap = Paint.Cap.ROUND
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    drawLineBentHalfCircle(scale, w, h, paint)
+}
