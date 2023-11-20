2a28ebfbc9d6bc93620ae9014b8ffcd2a5da7ef9
Anwesh43 <anweshthecool0@gmail.com>
2023-11-20 08:29:42 +0530

    linecentersemicircleview: adding Renderer in View

--- linecentersemicircleview/src/main/java/com/example/linecentersemicircleview/LineCenterSemiCircleView.kt
+++ linecentersemicircleview/src/main/java/com/example/linecentersemicircleview/LineCenterSemiCircleView.kt
@@ -64,14 +64,16 @@ fun Canvas.drawLCSCNode(i : Int, scale : Float, paint : Paint) {
 
 class LineCenterSemiCircleView(ctx : Context) : View(ctx) {
 
-    override fun onDraw(canvas : Canvas) {
+    private val renderer : Renderer = Renderer(this)
 
+    override fun onDraw(canvas : Canvas) {
+        renderer.render(canvas)
     }
 
     override fun onTouchEvent(event : MotionEvent) : Boolean {
         when (event.action) {
             MotionEvent.ACTION_DOWN -> {
-
+                renderer.handleTap()
             }
         }
         return true
