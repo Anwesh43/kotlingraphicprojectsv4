ce8f0113f4e79337953355fab37bf5fdc8085071
Anwesh43 <anweshthecool0@gmail.com>
2023-11-20 08:16:21 +0530

    linecentersemicircleview: adding LineCenterSemiCircleView class

--- linecentersemicircleview/src/main/java/com/example/linecentersemicircleview/LineCenterSemiCircleView.kt
+++ linecentersemicircleview/src/main/java/com/example/linecentersemicircleview/LineCenterSemiCircleView.kt
@@ -61,3 +61,19 @@ fun Canvas.drawLCSCNode(i : Int, scale : Float, paint : Paint) {
     paint.strokeWidth = Math.min(w, h) / strokeFactor
     drawLineCenterSemiCircle(scale, w, h, paint)
 }
+
+class LineCenterSemiCircleView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
