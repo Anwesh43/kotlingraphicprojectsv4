e67994ac86a22f237952e4dcf8d7dfcc9f14d3d1
Anwesh43 <anweshthecool0@gmail.com>
2023-11-20 08:25:57 +0530

    linecentersemicircleview: adding LCSCNode class

--- linecentersemicircleview/src/main/java/com/example/linecentersemicircleview/LineCenterSemiCircleView.kt
+++ linecentersemicircleview/src/main/java/com/example/linecentersemicircleview/LineCenterSemiCircleView.kt
@@ -124,4 +124,45 @@ class LineCenterSemiCircleView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class LCSCNode(var i : Int = 0, val state : State = State()) {
+
+        private var next : LCSCNode? = null
+        private var prev : LCSCNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < colors.size - 1) {
+                next = LCSCNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawLCSCNode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : LCSCNode {
+            var curr : LCSCNode? = prev
+            if (dir == 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
