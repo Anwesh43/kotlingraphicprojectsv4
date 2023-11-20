c8c35e06ba371554063d1858fde64d97ba487b49
Anwesh43 <anweshthecool0@gmail.com>
2023-11-20 08:14:59 +0530

    linecentersemicircleview: adding Canvas extension methods

--- linecentersemicircleview/src/main/java/com/example/linecentersemicircleview/LineCenterSemiCircleView.kt
+++ linecentersemicircleview/src/main/java/com/example/linecentersemicircleview/LineCenterSemiCircleView.kt
@@ -27,3 +27,37 @@ val backColor : Int = Color.parseColor("#BDBDBD")
 fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
+
+fun Canvas.drawXY(x : Float, y : Float, cb : () -> Unit) {
+    save()
+    translate(x, y)
+    cb()
+    restore()
+}
+
+fun Canvas.drawLineCenterSemiCircle(scale : Float, w : Float, h : Float, paint : Paint) {
+    val size : Float = Math.min(w, h) / sizeFactor
+    val dsc : (Int) -> Float = {
+        scale.divideScale(it, parts)
+    }
+    drawXY(w / 2, h / 2) {
+        drawXY(0f, h * 0.5f * (1 - dsc(0))) {
+            rotate(rot * dsc(1))
+            drawLine(0f, 0f, 0f, size, paint)
+        }
+        for (j in 0..1) {
+            drawXY(size * j, 0f) {
+                drawArc(RectF(0f, -size / 4, size / 2, size / 4), 180f, 180f * dsc(2).divideScale(j, 2), true, paint)
+            }
+        }
+    }
+}
+
+fun Canvas.drawLCSCNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = Color.parseColor(colors[i])
+    paint.strokeCap = Paint.Cap.ROUND
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    drawLineCenterSemiCircle(scale, w, h, paint)
+}
