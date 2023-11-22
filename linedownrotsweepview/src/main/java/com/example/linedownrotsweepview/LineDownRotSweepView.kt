3d101bd6e43c6f0030465c9cc163402908387fc6
Anwesh43 <anweshthecool0@gmail.com>
2023-11-23 01:17:50 +0530

    linedownrotsweepview: adding LineDonwRotSweep class

--- linedownrotsweepview/src/main/java/com/example/linedownrotsweepview/LineDownRotSweepView.kt
+++ linedownrotsweepview/src/main/java/com/example/linedownrotsweepview/LineDownRotSweepView.kt
@@ -163,4 +163,27 @@ class LineDownRotSweepView(ctx : Context) : View(ctx) {
             return this
         }
     }
+
+    data class LineDownRotSweep(var i : Int) {
+
+        private var curr : LDRSNode = LDRSNode(0)
+        private var dir : Int = 1
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            curr.draw(canvas, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            curr.update {
+                curr = curr.getNext(dir) {
+                    dir *= -1
+                }
+                cb(it)
+            }
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            curr.startUpdating(cb)
+        }
+    }
 }
