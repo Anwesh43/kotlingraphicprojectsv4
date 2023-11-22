c4c87896274e799a67d8db80c35e9eef35daf13e
Anwesh43 <anweshthecool0@gmail.com>
2023-11-22 21:41:24 +0530

    linedownrotsweepview: adding Canvas extension methods

--- linedownrotsweepview/src/main/java/com/example/linedownrotsweepview/LineDownRotSweepView.kt
+++ linedownrotsweepview/src/main/java/com/example/linedownrotsweepview/LineDownRotSweepView.kt
@@ -29,3 +29,34 @@ fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
 
+fun Canvas.drawXY(x : Float, y : Float, cb : () -> Unit) {
+    save()
+    translate(x, y)
+    cb()
+    restore()
+}
+
+fun Canvas.drawLineDownRotSweep(scale : Float, w : Float, h : Float, paint : Paint) {
+    val size : Float = Math.min(w, h) / sizeFactor
+    val dsc : (Int) -> Float = {
+        scale.divideScale(it, parts)
+    }
+    drawXY(w / 2, h / 2 + (h / 2) * dsc(3)) {
+        drawXY(0f, -h * 0.5f * (1 - dsc(0))) {
+            rotate(rot * dsc(1))
+            drawLine(0f, 0f, 0f, -size, paint)
+        }
+        drawArc(RectF(-size, -size, size, size), 0f, 180f * dsc(2), false, paint)
+    }
+}
+
+fun Canvas.drawLDRSNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = Color.parseColor(colors[i])
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    paint.style = Paint.Style.STROKE
+    paint.strokeCap = Paint.Cap.ROUND
+    drawLineDownRotSweep(scale, w, h, paint)
+}
+
