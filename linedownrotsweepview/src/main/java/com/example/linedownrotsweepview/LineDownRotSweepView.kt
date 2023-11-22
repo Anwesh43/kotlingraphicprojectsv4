4a476f593e5c731b70f252398bfc824ee1d56fca
Anwesh43 <anweshthecool0@gmail.com>
2023-11-23 01:16:35 +0530

    linedownrotsweepview: adding LDRSNode class

--- linedownrotsweepview/src/main/java/com/example/linedownrotsweepview/LineDownRotSweepView.kt
+++ linedownrotsweepview/src/main/java/com/example/linedownrotsweepview/LineDownRotSweepView.kt
@@ -122,4 +122,45 @@ class LineDownRotSweepView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class LDRSNode(var i : Int = 0, val state : State = State()) {
+
+        private var next : LDRSNode? = null
+        private var prev : LDRSNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < colors.size - 1) {
+                next = LDRSNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawLDRSNode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : LDRSNode {
+            var curr : LDRSNode? = prev
+            if (dir == 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
