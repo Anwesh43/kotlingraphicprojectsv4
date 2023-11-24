d4dae07dfb0507c84e193cf9dd6961c1fb7e9b34
Anwesh43 <anweshthecool0@gmail.com>
2023-11-24 15:45:34 +0530

    trisqrotdownview: adding TriSqRotDownView class

--- trisqrotdownview/src/main/java/com/example/trisqrotdownview/TriSqDownRotView.kt
+++ trisqrotdownview/src/main/java/com/example/trisqrotdownview/TriSqDownRotView.kt
@@ -60,3 +60,19 @@ fun Canvas.drawTSDRNode(i : Int, scale : Float, paint : Paint) {
     paint.strokeWidth = Math.min(w, h) / strokeFactor
     drawTriSqDownRot(scale, w, h, paint)
 }
+
+class TriSqDownRotView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
