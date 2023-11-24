e88ee487d4b13486836a941f6668ce3e2b6ff787
Anwesh43 <anweshthecool0@gmail.com>
2023-11-24 17:27:42 +0530

    trisqrotdownview: adding view in activity

--- app/src/main/java/com/example/android_kotlin_graphic_projectsv4/MainActivity.kt
+++ app/src/main/java/com/example/android_kotlin_graphic_projectsv4/MainActivity.kt
@@ -2,7 +2,7 @@ package com.example.android_kotlin_graphic_projectsv4
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.example.linedownrotsweepview.LineDownRotSweepView
+import com.example.trisqrotdownview.TriSqDownRotView
 
 
 class MainActivity : AppCompatActivity() {
@@ -10,6 +10,6 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         supportActionBar?.hide()
         //setContentView(R.layout.activity_main)
-        LineDownRotSweepView.create(this)
+        TriSqDownRotView.create(this)
     }
 }
