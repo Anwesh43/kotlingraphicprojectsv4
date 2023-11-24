0c01a2ab1ed7029bb812fb09f8ee4f0a72fc8af4
Anwesh43 <anweshthecool0@gmail.com>
2023-11-24 15:57:49 +0530

    trisqrotdownview: adding Renderer class

--- trisqrotdownview/src/main/java/com/example/trisqrotdownview/TriSqDownRotView.kt
+++ trisqrotdownview/src/main/java/com/example/trisqrotdownview/TriSqDownRotView.kt
@@ -187,4 +187,27 @@ class TriSqDownRotView(ctx : Context) : View(ctx) {
             curr.startUpdating(cb)
         }
     }
+
+    data class Renderer(var view : TriSqDownRotView) {
+
+        private val animator : Animator = Animator(view)
+        private val tsdr : TriSqDownRot = TriSqDownRot(0)
+        private val paint : Paint = Paint(Paint.ANTI_ALIAS_FLAG)
+
+        fun render(canvas : Canvas) {
+            canvas.drawColor(backColor)
+            tsdr.draw(canvas, paint)
+            animator.animate {
+                tsdr.update {
+                    animator.stop()
+                }
+            }
+        }
+
+        fun handleTap() {
+            tsdr.startUpdating {
+                animator.start()
+            }
+        }
+    }
 }
