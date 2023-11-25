c6393b59b7da8deb7b0d668f1f50d07d25e314b1
Anwesh43 <anweshthecool0@gmail.com>
2023-11-26 02:38:16 +0530

    biarcbentlinerightview: adding Animator class

--- biarcbentlinerightview/src/main/java/com/example/biarcbentlinerightview/BiArcBentLineRotView.kt
+++ biarcbentlinerightview/src/main/java/com/example/biarcbentlinerightview/BiArcBentLineRotView.kt
@@ -96,4 +96,32 @@ class BiArcBentLineRotView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class Animator(var view : View, var animated : Boolean = false) {
+
+        fun animate(cb : () -> Unit) {
+            if (animated) {
+                cb()
+                try {
+                    Thread.sleep(delay)
+                    view.invalidate()
+                } catch(ex : Exception) {
+
+                }
+            }
+        }
+
+        fun start() {
+            if (!animated) {
+                animated = true
+                view.postInvalidate()
+            }
+        }
+
+        fun stop() {
+            if (animated) {
+                animated = false
+            }
+        }
+    }
 }
