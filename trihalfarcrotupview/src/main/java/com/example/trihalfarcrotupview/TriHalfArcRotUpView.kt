352ee151c205dabbad5dd61864775c80f78aaf42
Anwesh43 <anweshthecool0@gmail.com>
2023-11-27 00:56:44 +0530

    trihalfarcrotupview: adding Canvas extension methods

--- trihalfarcrotupview/src/main/java/com/example/trihalfarcrotupview/TriHalfArcRotUpView.kt
+++ trihalfarcrotupview/src/main/java/com/example/trihalfarcrotupview/TriHalfArcRotUpView.kt
@@ -27,3 +27,38 @@ val rot : Float = 180f
 fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
+
+fun Canvas.drawXY(x : Float, y : Float, cb : () -> Unit) {
+    save()
+    translate(x, y)
+    cb()
+    restore()
+}
+
+fun Canvas.drawTriHalfArcRotUp(scale : Float, w : Float, h : Float, paint : Paint) {
+    val size : Float = Math.min(w, h) / sizeFactor
+    val dsc : (Int) -> Float = {
+        scale.divideScale(it, parts)
+    }
+    drawXY(w / 2, h / 2) {
+        drawXY(0f, 0f) {
+            rotate(rot * dsc(2))
+            drawArc(RectF(-size, -size / 2, 0f, size / 2), 180f, 180f * dsc(0), true, paint)
+        }
+        for (j in 0..1) {
+            drawXY(size * 0.5f * j, -size * 0.5f * j) {
+                drawLine(0f, 0f, size * 0.5f * dsc(1).divideScale(j, 2),  -size * 0.5f * (1f - 2 * j) * dsc(1).divideScale(j, 2), paint)
+            }
+        }
+
+    }
+}
+
+fun Canvas.drawTHARUNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = Color.parseColor(colors[i])
+    paint.strokeCap = Paint.Cap.ROUND
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    drawTriHalfArcRotUp(scale, w, h, paint)
+}
