aa1e70b2de7ea53924e19c0a790ad65c78ee0376
Anwesh43 <anweshthecool0@gmail.com>
2023-11-27 01:11:41 +0530

    trihalfarcrotupview: adding Renderer class

--- trihalfarcrotupview/src/main/java/com/example/trihalfarcrotupview/TriHalfArcRotUpView.kt
+++ trihalfarcrotupview/src/main/java/com/example/trihalfarcrotupview/TriHalfArcRotUpView.kt
@@ -189,4 +189,27 @@ class TriHalfArcRotUpView(ctx : Context) : View(ctx) {
             curr.startUpdating(cb)
         }
     }
+
+    data class Renderer(var view : TriHalfArcRotUpView) {
+
+        private val animator : Animator = Animator(view)
+        private val tharu : TriHalfArcRotUp = TriHalfArcRotUp(0)
+        private val paint : Paint = Paint(Paint.ANTI_ALIAS_FLAG)
+
+        fun render(canvas : Canvas) {
+            canvas.drawColor(backColor)
+            tharu.draw(canvas, paint)
+            animator.animate {
+                tharu.update {
+                    animator.stop()
+                }
+            }
+        }
+
+        fun handleTap() {
+            tharu.startUpdating {
+                animator.start()
+            }
+        }
+    }
 }
