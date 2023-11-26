f433466b14a49715fcc25df534a1cd6de15335bc
Anwesh43 <anweshthecool0@gmail.com>
2023-11-27 00:58:30 +0530

    trihalfarcrotupview: adding TriHalfArcRotUpView class

--- trihalfarcrotupview/src/main/java/com/example/trihalfarcrotupview/TriHalfArcRotUpView.kt
+++ trihalfarcrotupview/src/main/java/com/example/trihalfarcrotupview/TriHalfArcRotUpView.kt
@@ -62,3 +62,19 @@ fun Canvas.drawTHARUNode(i : Int, scale : Float, paint : Paint) {
     paint.strokeWidth = Math.min(w, h) / strokeFactor
     drawTriHalfArcRotUp(scale, w, h, paint)
 }
+
+class TriHalfArcRotUpView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
