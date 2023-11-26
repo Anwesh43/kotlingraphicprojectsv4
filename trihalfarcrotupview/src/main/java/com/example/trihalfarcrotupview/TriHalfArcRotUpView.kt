0734f1753cbda4cd8db056fb36eba5155211ca5d
Anwesh43 <anweshthecool0@gmail.com>
2023-11-27 01:04:42 +0530

    trihalfarcrotupview: adding THARUNode class

--- trihalfarcrotupview/src/main/java/com/example/trihalfarcrotupview/TriHalfArcRotUpView.kt
+++ trihalfarcrotupview/src/main/java/com/example/trihalfarcrotupview/TriHalfArcRotUpView.kt
@@ -125,4 +125,45 @@ class TriHalfArcRotUpView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class THARUNode(var i : Int = 0, val state : State = State()) {
+
+        private var prev : THARUNode? = null
+        private var next : THARUNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < colors.size - 1) {
+                next = THARUNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawTHARUNode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : THARUNode {
+            var curr : THARUNode? = prev
+            if (dir == 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
