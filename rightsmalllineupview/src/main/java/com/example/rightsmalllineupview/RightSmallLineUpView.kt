4128447df6f573cd7991f81c3b964ed8bc4d350e
Anwesh43 <anweshthecool0@gmail.com>
2023-11-30 01:04:55 +0530

    rightsmalllineupview: adding Canvas extension methods

--- rightsmalllineupview/src/main/java/com/example/rightsmalllineupview/RightSmallLineUpView.kt
+++ rightsmalllineupview/src/main/java/com/example/rightsmalllineupview/RightSmallLineUpView.kt
@@ -20,9 +20,41 @@ val scGap : Float = 0.04f / parts
 val strokeFactor : Float = 90f
 val sizeFactor : Float = 4.9f
 val delay : Long = 20
-val rot : Float = 90f
+val rot : Float = 180f
 val backColor : Int = Color.parseColor("#BDBDBD")
 
 fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
+
+fun Canvas.drawXY(x : Float, y : Float, cb : () -> Unit) {
+    save()
+    translate(x, y)
+    cb()
+    restore()
+}
+
+fun Canvas.drawRightSmallLineUp(scale : Float, w : Float, h : Float, paint : Paint) {
+    val size : Float = Math.min(w, h) / sizeFactor
+    val dsc : (Int) -> Float = {
+        scale.divideScale(it, parts)
+    }
+    drawXY(w / 2, h / 2 - (h / 2) * dsc(3)) {
+        drawXY(0f, 0f) {
+            rotate(rot * dsc(2))
+            drawLine(0f, 0f, 0f, size * dsc(0), paint)
+        }
+        drawXY(0f, size * (1 - dsc(2))) {
+            drawLine(0f, 0f, size * 0.5f * dsc(1), 0f, paint)
+        }
+    }
+}
+
+fun Canvas.drawRSLUNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = Color.parseColor(colors[i])
+    paint.strokeCap = Paint.Cap.ROUND
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    drawRightSmallLineUp(scale, w, h, paint)
+}
