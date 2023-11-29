413136ef51442d85bce4b5b0a808c9c2b896586c
Anwesh43 <anweshthecool0@gmail.com>
2023-11-30 01:25:43 +0530

    rightsmalllineupview: adding RSLUNode class

--- rightsmalllineupview/src/main/java/com/example/rightsmalllineupview/RightSmallLineUpView.kt
+++ rightsmalllineupview/src/main/java/com/example/rightsmalllineupview/RightSmallLineUpView.kt
@@ -120,4 +120,45 @@ class RightSmallLineUpView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class RSLUNode(var i : Int = 0, val state : State = State()) {
+
+        private var next : RSLUNode? = null
+        private var prev : RSLUNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < colors.size - 1) {
+                next = RSLUNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawRSLUNode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : RSLUNode {
+            var curr : RSLUNode? = prev
+            if (dir == 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
