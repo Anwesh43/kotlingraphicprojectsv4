7d3c5cd74fec7c772b6ddc9e73bdad8b32824716
Anwesh43 <anweshthecool0@gmail.com>
2023-11-30 01:18:09 +0530

    rightsmalllineupview: adding RightSmallLineUpView class

--- rightsmalllineupview/src/main/java/com/example/rightsmalllineupview/RightSmallLineUpView.kt
+++ rightsmalllineupview/src/main/java/com/example/rightsmalllineupview/RightSmallLineUpView.kt
@@ -58,3 +58,19 @@ fun Canvas.drawRSLUNode(i : Int, scale : Float, paint : Paint) {
     paint.strokeWidth = Math.min(w, h) / strokeFactor
     drawRightSmallLineUp(scale, w, h, paint)
 }
+
+class RightSmallLineUpView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
