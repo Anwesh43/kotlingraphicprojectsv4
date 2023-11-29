481de653ee9d1e9c6e89e2e5a3a05f2cb4ae1c89
Anwesh43 <anweshthecool0@gmail.com>
2023-11-29 12:56:31 +0530

    taillinesemiarcview: adding TailLineSemiArc class

--- taillinesemiarcview/src/main/java/com/example/taillinesemiarcview/TailLineSemiArcView.kt
+++ taillinesemiarcview/src/main/java/com/example/taillinesemiarcview/TailLineSemiArcView.kt
@@ -161,4 +161,27 @@ class TailLineSemiArcView(ctx : Context) : View(ctx) {
             return this
         }
     }
+
+    data class TailLineSemiArc(var i : Int) {
+
+        private var curr : TLSANode = TLSANode(0)
+        private var dir : Int = 1
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            curr.draw(canvas, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            curr.update {
+                curr = curr.getNext(dir) {
+                    dir *= -1
+                }
+                cb(it)
+            }
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            curr.startUpdating(cb)
+        }
+    }
 }
