28c3d82053d1dc23ea8d34114385bd558a4884b9
Anwesh43 <anweshthecool0@gmail.com>
2023-11-29 10:43:07 +0530

    trihalfarcrotupview: adding view in activity

--- app/src/main/java/com/example/android_kotlin_graphic_projectsv4/MainActivity.kt
+++ app/src/main/java/com/example/android_kotlin_graphic_projectsv4/MainActivity.kt
@@ -2,7 +2,7 @@ package com.example.android_kotlin_graphic_projectsv4
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.example.biarcbentlinerightview.BiArcBentLineRotView
+import com.example.trihalfarcrotupview.TriHalfArcRotUpView
 
 
 class MainActivity : AppCompatActivity() {
@@ -10,6 +10,6 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         supportActionBar?.hide()
         //setContentView(R.layout.activity_main)
-        BiArcBentLineRotView.create(this)
+        TriHalfArcRotUpView.create(this)
     }
 }
