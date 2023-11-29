4f12d1f17a75ae7889a4babfda24ddd0a4b976be
Anwesh43 <anweshthecool0@gmail.com>
2023-11-29 13:04:10 +0530

    taillinesemiarcview: adding Renderer class

--- taillinesemiarcview/src/main/java/com/example/taillinesemiarcview/TailLineSemiArcView.kt
+++ taillinesemiarcview/src/main/java/com/example/taillinesemiarcview/TailLineSemiArcView.kt
@@ -184,4 +184,27 @@ class TailLineSemiArcView(ctx : Context) : View(ctx) {
             curr.startUpdating(cb)
         }
     }
+
+    data class Renderer(var view : TailLineSemiArcView) {
+
+        private val animator : Animator = Animator(view)
+        private val tlsa : TailLineSemiArc = TailLineSemiArc(0)
+        private val paint : Paint = Paint(Paint.ANTI_ALIAS_FLAG)
+
+        fun render(canvas : Canvas) {
+            canvas.drawColor(backColor)
+            tlsa.draw(canvas, paint)
+            animator.animate {
+                tlsa.update {
+                    animator.stop()
+                }
+            }
+        }
+
+        fun handleTap() {
+            tlsa.startUpdating {
+                animator.start()
+            }
+        }
+    }
 }
