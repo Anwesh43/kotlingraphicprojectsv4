5f6cfbeb9150b4c1446bac36da7fcf5fda82a911
Anwesh43 <anweshthecool0@gmail.com>
2023-12-01 18:47:30 +0530

    linearccompleterightview: adding Canvas extension methods

--- linearccompleterightview/src/main/java/com/example/linearccompleterightview/LineArcCompleteRightView.kt
+++ linearccompleterightview/src/main/java/com/example/linearccompleterightview/LineArcCompleteRightView.kt
@@ -28,3 +28,37 @@ val strokeFactor : Float = 90f
 fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
+
+fun Canvas.drawXY(x : Float, y : Float, cb : () -> Unit) {
+    save()
+    translate(x, y)
+    cb()
+    restore()
+}
+
+fun Canvas.drawLineArcCompleteRight(scale : Float, w : Float, h : Float, paint : Paint) {
+    val size : Float = Math.min(w, h) / sizeFactor
+    val dsc : (Int) -> Float = {
+        scale.divideScale(it, parts)
+    }
+    val r : Float = size / 4
+    drawXY(w / 2 - (w / 2 + size) * dsc(3), h / 2) {
+        drawXY(0f, -h * 0.5f * (1 - dsc(0))) {
+            rotate(rot * dsc(1))
+            drawLine(0f, 0f, 0f, -size, paint)
+        }
+        drawXY(w * 0.5f * (1 - dsc(0)), 0f) {
+            drawLine(0f, 0f, size, 0f, paint)
+        }
+        drawArc(RectF(-r, -r, r, r), 0f, sweep  * dsc(2), true, paint)
+    }
+}
+
+fun Canvas.drawLACRNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = Color.parseColor(colors[i])
+    paint.strokeCap = Paint.Cap.ROUND
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    drawLineArcCompleteRight(scale, w, h, paint)
+}
