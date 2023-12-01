7577dcc7fa7ee30028f73a2653830de072ade595
Anwesh43 <anweshthecool0@gmail.com>
2023-12-01 19:26:08 +0530

    linearccompleterightview: adding Renderer class

--- linearccompleterightview/src/main/java/com/example/linearccompleterightview/LineArcCompleteRightView.kt
+++ linearccompleterightview/src/main/java/com/example/linearccompleterightview/LineArcCompleteRightView.kt
@@ -189,4 +189,27 @@ class LineArcCompleteRightView(ctx : Context) : View(ctx) {
             curr.startUpdating(cb)
         }
     }
+
+    data class Renderer(var view : LineArcCompleteRightView) {
+
+        private val animator : Animator = Animator(view)
+        private val lacr : LineArcCompleteRight = LineArcCompleteRight(0)
+        private val paint : Paint = Paint(Paint.ANTI_ALIAS_FLAG)
+
+        fun render(canvas : Canvas) {
+            canvas.drawColor(backColor)
+            lacr.draw(canvas, paint)
+            animator.animate {
+                lacr.update {
+                    animator.stop()
+                }
+            }
+        }
+
+        fun handleTap() {
+            lacr.startUpdating {
+                animator.start()
+            }
+        }
+    }
 }
