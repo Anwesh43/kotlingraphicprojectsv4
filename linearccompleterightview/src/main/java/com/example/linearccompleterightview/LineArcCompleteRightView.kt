40305fa4134de75c3645cb4e49e8be0b9cc00fd0
Anwesh43 <anweshthecool0@gmail.com>
2023-12-01 19:24:37 +0530

    linearccompleterightview: adding LineArcCompleteRight class

--- linearccompleterightview/src/main/java/com/example/linearccompleterightview/LineArcCompleteRightView.kt
+++ linearccompleterightview/src/main/java/com/example/linearccompleterightview/LineArcCompleteRightView.kt
@@ -166,4 +166,27 @@ class LineArcCompleteRightView(ctx : Context) : View(ctx) {
             return this
         }
     }
+
+    data class LineArcCompleteRight(var i : Int) {
+
+        private var curr : LACRNode = LACRNode(0)
+        private var dir : Int = 1
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            curr.draw(canvas, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            curr.update {
+                curr = curr.getNext(dir) {
+                    dir *= -1
+                }
+                cb(it)
+            }
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            curr.startUpdating(cb)
+        }
+    }
 }
