d25061a4f50efc6f11804aa168d3f2b4388262da
Anwesh43 <anweshthecool0@gmail.com>
2023-12-01 19:32:00 +0530

    linearccompleterightview: adding static create method

--- linearccompleterightview/src/main/java/com/example/linearccompleterightview/LineArcCompleteRightView.kt
+++ linearccompleterightview/src/main/java/com/example/linearccompleterightview/LineArcCompleteRightView.kt
@@ -214,4 +214,13 @@ class LineArcCompleteRightView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    companion object {
+
+        fun create(activity : Activity) : LineArcCompleteRightView {
+            val view : LineArcCompleteRightView = LineArcCompleteRightView(activity)
+            activity.setContentView(view)
+            return view
+        }
+    }
 }
