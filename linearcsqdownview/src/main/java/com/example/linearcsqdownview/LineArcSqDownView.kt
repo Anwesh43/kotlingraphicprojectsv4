1c2c438bf97a3f0d42ce4c912b613ebaed3c2325
Anwesh43 <anweshthecool0@gmail.com>
2023-12-02 13:08:37 +0530

    linearcsqdownview: adding LineArcSqDownView class

--- linearcsqdownview/src/main/java/com/example/linearcsqdownview/LineArcSqDownView.kt
+++ linearcsqdownview/src/main/java/com/example/linearcsqdownview/LineArcSqDownView.kt
@@ -58,3 +58,19 @@ fun Canvas.drawLASDNode(i : Int, scale : Float, paint : Paint) {
     paint.strokeWidth = Math.min(w, h) / strokeFactor
     drawLineArcSqDown(scale, w, h, paint)
 }
+
+class LineArcSqDownView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
