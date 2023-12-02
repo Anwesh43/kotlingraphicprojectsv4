29dafcb0763e4892714556e195a692fbd0f0cb08
Anwesh43 <anweshthecool0@gmail.com>
2023-12-02 13:07:13 +0530

    linearcsqdownview: adding Canvas extension methods

--- linearcsqdownview/src/main/java/com/example/linearcsqdownview/LineArcSqDownView.kt
+++ linearcsqdownview/src/main/java/com/example/linearcsqdownview/LineArcSqDownView.kt
@@ -27,3 +27,34 @@ val rot : Float = 90f
 fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
+
+fun Canvas.drawXY(x : Float, y : Float, cb : () -> Unit) {
+    save()
+    translate(x, y)
+    cb()
+    restore()
+}
+
+fun Canvas.drawLineArcSqDown(scale : Float, w : Float, h : Float, paint : Paint) {
+    val size : Float = Math.min(w, h) / sizeFactor
+    val dsc : (Int) -> Float = {
+        scale.divideScale(it, parts)
+    }
+    drawXY(w / 2, h / 2 + (h / 2 + size / 2) * dsc(3)) {
+        rotate(rot * dsc(2))
+        drawLine(-size * 0.5f * dsc(0), 0f, size * 0.5f * dsc(0), 0f, paint)
+        drawXY(-size / 2, 0f) {
+            drawArc(RectF(-size / 4, -size / 4, size / 4, size / 4), 180f, 180f * dsc(1), true, paint)
+        }
+        drawRect(RectF(0f, 0f, size / 2, size * 0.5f * dsc(1)), paint)
+    }
+}
+
+fun Canvas.drawLASDNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = Color.parseColor(colors[i])
+    paint.strokeCap = Paint.Cap.ROUND
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    drawLineArcSqDown(scale, w, h, paint)
+}
