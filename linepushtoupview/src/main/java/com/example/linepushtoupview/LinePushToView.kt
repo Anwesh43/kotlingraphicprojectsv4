dba00ad057d97dfe72aaf7dc0f0c3201e2060290
Anwesh43 <anweshthecool0@gmail.com>
2023-12-03 14:13:56 +0530

    linepushtoupview: adding LinePushToUp class

--- linepushtoupview/src/main/java/com/example/linepushtoupview/LinePushToView.kt
+++ linepushtoupview/src/main/java/com/example/linepushtoupview/LinePushToView.kt
@@ -161,4 +161,27 @@ class LinePushToUpView(ctx : Context) : View(ctx) {
             return this
         }
     }
+
+    data class LinePushToUp(var i : Int) {
+
+        private var curr : LPTUNode = LPTUNode(0)
+        private var dir : Int = 1
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            curr.draw(canvas, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            curr.update {
+                curr = curr.getNext(dir) {
+                    dir *= -1
+                }
+                cb(it)
+            }
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            curr.startUpdating(cb)
+        }
+    }
 }
