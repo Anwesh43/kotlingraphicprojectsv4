c318103b600c6d1c08bd2302be86cfb2950f0d27
Anwesh43 <anweshthecool0@gmail.com>
2023-12-06 17:08:04 +0530

    halfwaybarrotdownview: adding HalfWayRotDownView class

--- halfwaybarrotdownview/src/main/java/com/example/halfwaybarrotdownview/HalfWayBarRotDownView.kt
+++ halfwaybarrotdownview/src/main/java/com/example/halfwaybarrotdownview/HalfWayBarRotDownView.kt
@@ -57,3 +57,19 @@ fun Canvas.drawHWRDNode(i : Int, scale : Float, paint : Paint) {
     paint.color = Color.parseColor(colors[i])
     drawHalfWayRotDown(scale, w, h, paint)
 }
+
+class HalfWayBarRotDownView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
