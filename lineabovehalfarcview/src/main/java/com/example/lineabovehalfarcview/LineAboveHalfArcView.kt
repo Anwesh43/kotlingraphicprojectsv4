247c777722dfd91c17de273ddd58f73a874a3f3c
Anwesh43 <anweshthecool0@gmail.com>
2023-12-07 16:50:18 +0530

    lineabovehalfarcview: adding Canvas extension methods

--- lineabovehalfarcview/src/main/java/com/example/lineabovehalfarcview/LineAboveHalfArcView.kt
+++ lineabovehalfarcview/src/main/java/com/example/lineabovehalfarcview/LineAboveHalfArcView.kt
@@ -27,3 +27,34 @@ val rot : Float = 90f
 fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
+
+fun Canvas.drawXY(x : Float, y : Float, cb : () -> Unit) {
+    save()
+    translate(x, y)
+    cb()
+    restore()
+}
+
+fun Canvas.drawLineAboveHalfArc(scale : Float, w : Float, h : Float, paint : Paint) {
+    val size : Float = Math.min(w, h) / sizeFactor
+    val dsc : (Int) -> Float = {
+        scale.divideScale(it, parts)
+    }
+    drawXY(w / 2 - (w / 2 + size / 2) * dsc(3) , h / 2) {
+        rotate(rot * dsc(2))
+        drawArc(RectF(-size / 2, -size / 2, size / 2, size / 2), 0f, 180f * dsc(0), true, paint)
+        drawXY(-size / 2, -size / 2) {
+            drawLine(0f, 0f, size * 0.5f * dsc(1).divideScale(0, 2), size * 0.5f * dsc(1).divideScale(0, 2), paint)
+        }
+        drawLine(0f, 0f, size * 0.5f * dsc(1).divideScale(1, 2), size * 0.5f * dsc(1).divideScale(1, 2), paint)
+    }
+}
+
+fun Canvas.drawLAHANode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = Color.parseColor(colors[i])
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    paint.strokeCap = Paint.Cap.ROUND
+    drawLineAboveHalfArc(scale, w, h, paint)
+}
