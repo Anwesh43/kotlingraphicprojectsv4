e2634ebbf80c35958e65e368342314b39d58bc44
Anwesh43 <anweshthecool0@gmail.com>
2023-12-07 17:11:16 +0530

    lineabovehalfarcview: adding LAHANode class

--- lineabovehalfarcview/src/main/java/com/example/lineabovehalfarcview/LineAboveHalfArcView.kt
+++ lineabovehalfarcview/src/main/java/com/example/lineabovehalfarcview/LineAboveHalfArcView.kt
@@ -121,4 +121,45 @@ class LineAboveHalfArcView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class LAHANode(var i : Int = 0, val state : State = State()) {
+
+        private var next : LAHANode? = null
+        private var prev : LAHANode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < colors.size - 1) {
+                next = LAHANode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawLAHANode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : LAHANode {
+            var curr : LAHANode? = prev
+            if (dir == 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
