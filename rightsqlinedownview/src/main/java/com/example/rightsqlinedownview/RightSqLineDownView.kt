95b076ea3d235f88ea45bc1290583d33bd337a97
Anwesh43 <anweshthecool0@gmail.com>
2023-12-08 18:20:40 +0530

    rightsqlinedownview: adding RightSqLineDownView class

--- rightsqlinedownview/src/main/java/com/example/rightsqlinedownview/RightSqLineDownView.kt
+++ rightsqlinedownview/src/main/java/com/example/rightsqlinedownview/RightSqLineDownView.kt
@@ -61,3 +61,19 @@ fun Canvas.drawRSLDNode(i : Int, scale : Float, paint : Paint) {
     paint.strokeWidth = Math.min(w, h) / strokeFactor
     drawRightSqLineDown(scale, w, h, paint)
 }
+
+class RightSqLineDownView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
