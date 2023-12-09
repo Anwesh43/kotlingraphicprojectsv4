e94e32a14999a62e8434bd5d007da03b7420b291
Anwesh43 <anweshthecool0@gmail.com>
2023-12-09 11:08:56 +0530

    linebentstrokecircleview: adding Renderer class

--- linebentstrokecircleview/src/main/java/com/example/linebentstrokecircleview/LineBentStrokeCircleView.kt
+++ linebentstrokecircleview/src/main/java/com/example/linebentstrokecircleview/LineBentStrokeCircleView.kt
@@ -187,4 +187,27 @@ class LineBentStrokeCircleView(ctx : Context) : View(ctx) {
             curr.startUpdating(cb)
         }
     }
+
+    data class Renderer(var view : LineBentStrokeCircleView) {
+
+        private val animator : Animator = Animator(view)
+        private val lbsc : LineBentStrokeCircle = LineBentStrokeCircle(0)
+        private val paint : Paint = Paint(Paint.ANTI_ALIAS_FLAG)
+
+        fun render(canvas : Canvas) {
+            canvas.drawColor(backColor)
+            lbsc.draw(canvas, paint)
+            animator.animate {
+                lbsc.update {
+                    animator.stop()
+                }
+            }
+        }
+
+        fun handleTap() {
+            lbsc.startUpdating {
+                animator.start()
+            }
+        }
+    }
 }
