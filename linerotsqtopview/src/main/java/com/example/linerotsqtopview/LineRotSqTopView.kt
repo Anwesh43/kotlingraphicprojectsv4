1a5145290ad02fd1a5f49fbb2b79a18c8534e203
Anwesh43 <anweshthecool0@gmail.com>
2023-12-10 02:04:40 +0530

    linerotsqtopview: adding Renderer class

--- linerotsqtopview/src/main/java/com/example/linerotsqtopview/LineRotSqTopView.kt
+++ linerotsqtopview/src/main/java/com/example/linerotsqtopview/LineRotSqTopView.kt
@@ -187,4 +187,27 @@ class LineRotSqTopView(ctx : Context) : View(ctx) {
             curr.startUpdating(cb)
         }
     }
+
+    data class Renderer(var view : LineRotSqTopView) {
+
+        private val animator : Animator = Animator(view)
+        private val lrst : LineRotSqTop = LineRotSqTop(0)
+        private val paint : Paint = Paint(Paint.ANTI_ALIAS_FLAG)
+
+        fun render(canvas : Canvas) {
+            canvas.drawColor(backColor)
+            lrst.draw(canvas, paint)
+            animator.animate {
+                lrst.update {
+                    animator.stop()
+                }
+            }
+        }
+
+        fun handleTap() {
+            lrst.startUpdating {
+                animator.start()
+            }
+        }
+    }
 }
