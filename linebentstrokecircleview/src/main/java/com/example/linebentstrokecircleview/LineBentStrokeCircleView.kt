41621ac83709a6184bf6f0c028fff50c286762e1
Anwesh43 <anweshthecool0@gmail.com>
2023-12-09 10:50:14 +0530

    linebentstrokecircleview: adding LineBentStrokeCircleView class

--- linebentstrokecircleview/src/main/java/com/example/linebentstrokecircleview/LineBentStrokeCircleView.kt
+++ linebentstrokecircleview/src/main/java/com/example/linebentstrokecircleview/LineBentStrokeCircleView.kt
@@ -60,3 +60,19 @@ fun Canvas.drawLBSCNode(i : Int, scale : Float, paint : Paint) {
     paint.style = Paint.Style.STROKE
     drawLineBentStrokeCircle(scale, w, h, paint)
 }
+
+class LineBentStrokeCircleView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
