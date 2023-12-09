d15447a5a47d1743a0b383eec6f9fb5fbd8009e1
Anwesh43 <anweshthecool0@gmail.com>
2023-12-10 02:01:51 +0530

    linerotsqtopview: adding LRSTNode class

--- linerotsqtopview/src/main/java/com/example/linerotsqtopview/LineRotSqTopView.kt
+++ linerotsqtopview/src/main/java/com/example/linerotsqtopview/LineRotSqTopView.kt
@@ -123,4 +123,45 @@ class LineRotSqTopView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class LRSTNode(var i : Int = 0, val state : State = State()) {
+
+        private var next : LRSTNode? = null
+        private var prev : LRSTNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < colors.size - 1) {
+                next = LRSTNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawLRSTNode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : LRSTNode {
+            var curr : LRSTNode? = prev
+            if (dir == 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
