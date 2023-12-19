2e31d95f92797c7b5e3373c360940f51a2707ac7
Anwesh43 <anweshthecool0@gmail.com>
2023-12-19 10:22:16 +0530

    linetripbentrightview: adding Canvas extension methods

--- linetribentrightview/src/main/java/com/example/linetribentrightview/LineTriBentRightView.kt
+++ linetribentrightview/src/main/java/com/example/linetribentrightview/LineTriBentRightView.kt
@@ -30,3 +30,35 @@ fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
 
+fun Canvas.drawXY(x : Float, y : Float, cb : () -> Unit) {
+    save()
+    translate(x, y)
+    cb()
+    restore()
+}
+
+fun Canvas.drawLineTriBentRight(scale : Float, w : Float, h : Float, paint : Paint) {
+    val size : Float = Math.min(w, h) / sizeFactor
+    val dsc : (Int) -> Float = {
+        scale.divideScale(it, parts)
+    }
+    drawXY(w / 2 + (w / 2 + size) * dsc(4), h / 2) {
+        for (j in 0..(lines - 1)) {
+            var deg : Float = startDeg - rot * j
+            if (deg == 0f) {
+                deg = -rot
+            }
+            rotate(deg * dsc(j + 1))
+            drawLine(0f, 0f, 0f, size * dsc(0), paint)
+        }
+    }
+}
+
+fun Canvas.drawLTBRNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = Color.parseColor(colors[i])
+    paint.strokeCap = Paint.Cap.ROUND
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    drawLineTriBentRight(scale, w, h, paint)
+}
