b4302cbd95187a36b682254eb188840e92ba0c10
Anwesh43 <anweshthecool0@gmail.com>
2023-12-19 10:34:58 +0530

    linetripbentrightview: adding static create method

--- linetribentrightview/src/main/java/com/example/linetribentrightview/LineTriBentRightView.kt
+++ linetribentrightview/src/main/java/com/example/linetribentrightview/LineTriBentRightView.kt
@@ -214,4 +214,13 @@ class LineTriBentRightView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    companion object {
+
+        fun create(activity : Activity) : LineTriBentRightView {
+            val view : LineTriBentRightView = LineTriBentRightView(activity)
+            activity.setContentView(view)
+            return view
+        }
+    }
 }
