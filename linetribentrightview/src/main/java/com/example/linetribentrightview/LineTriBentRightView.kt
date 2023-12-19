7d8dd103a23a6c7261957fe807094d480f45f049
Anwesh43 <anweshthecool0@gmail.com>
2023-12-19 10:31:06 +0530

    linetripbentrightview: adding LTBRNodeclass

--- linetribentrightview/src/main/java/com/example/linetribentrightview/LineTriBentRightView.kt
+++ linetribentrightview/src/main/java/com/example/linetribentrightview/LineTriBentRightView.kt
@@ -125,4 +125,45 @@ class LineTriBentRightView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class LTBRNode(var i : Int = 0, val state : State = State()) {
+
+        private var next : LTBRNode? = null
+        private var prev : LTBRNode? = null
+
+        init {
+
+        }
+
+        fun addNeighbor() {
+            if (i < colors.size - 1) {
+                next = LTBRNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawLTBRNode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : LTBRNode {
+            var curr : LTBRNode? = prev
+            if (dir == 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
