4bfbe10bbe967807d43322354bd5a400fd5730ea
Anwesh43 <anweshthecool0@gmail.com>
2023-12-19 10:32:29 +0530

    linetripbentrightview: adding LineTriBentRight class

--- linetribentrightview/src/main/java/com/example/linetribentrightview/LineTriBentRightView.kt
+++ linetribentrightview/src/main/java/com/example/linetribentrightview/LineTriBentRightView.kt
@@ -132,7 +132,7 @@ class LineTriBentRightView(ctx : Context) : View(ctx) {
         private var prev : LTBRNode? = null
 
         init {
-
+            addNeighbor()
         }
 
         fun addNeighbor() {
@@ -164,6 +164,29 @@ class LineTriBentRightView(ctx : Context) : View(ctx) {
             }
             cb()
             return this
+        }
+    }
+
+    data class LineTriBentRight(var i : Int = 0) {
+
+        private var curr : LTBRNode = LTBRNode(0)
+        private var dir : Int = 1
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            curr.draw(canvas, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            curr.update {
+                curr = curr.getNext(dir) {
+                    dir *= -1
+                }
+                cb(it)
+            }
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            curr.startUpdating(cb)
         }
     }
 }
