8ade96d66522557c7ba07da4951855a67f7cecd9
Anwesh43 <anweshthecool0@gmail.com>
2023-12-23 13:22:32 +0530

    linerotsemiarcupview: adding view in activity

--- app/src/main/java/com/example/android_kotlin_graphic_projectsv4/MainActivity.kt
+++ app/src/main/java/com/example/android_kotlin_graphic_projectsv4/MainActivity.kt
@@ -2,7 +2,7 @@ package com.example.android_kotlin_graphic_projectsv4
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.example.linetribentrightview.LineTriBentRightView
+import com.example.linerotsemiarcupview.LineRotSemiArcUpView
 
 
 class MainActivity : AppCompatActivity() {
@@ -10,6 +10,6 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         supportActionBar?.hide()
         //setContentView(R.layout.activity_main)
-        LineTriBentRightView.create(this)
+        LineRotSemiArcUpView.create(this)
     }
 }
