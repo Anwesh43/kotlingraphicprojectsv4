667f1f14df7fee76974db458864716f0a5d482cd
Anwesh43 <anweshthecool0@gmail.com>
2023-12-23 13:06:20 +0530

    linerotsemiarcupview: adding LRSAUNode class

--- linerotsemiarcupview/src/main/java/com/example/linerotsemiarcupview/LineRotSemiArcUpView.kt
+++ linerotsemiarcupview/src/main/java/com/example/linerotsemiarcupview/LineRotSemiArcUpView.kt
@@ -120,4 +120,45 @@ class LineRotSemiArcUpView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class LRSAUNode(var i : Int = 0, val state : State = State()) {
+
+        private var next : LRSAUNode? = null
+        private val prev : LRSAUNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < colors.size - 1) {
+                next = LRSAUNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawLRSAUNode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : LRSAUNode {
+            var curr : LRSAUNode? = prev
+            if (dir == 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
