9e4bfe93f9e0618adaac9063ae6560503c3b7b1c
Anwesh43 <anweshthecool0@gmail.com>
2023-12-24 16:01:10 +0530

    sqfromleftlineview: adding canvas extension methods

--- sqfromleftlineview/src/main/java/com/example/sqfromleftlineview/SqFromLeftLineView.kt
+++ sqfromleftlineview/src/main/java/com/example/sqfromleftlineview/SqFromLeftLineView.kt
@@ -6,6 +6,7 @@ import android.graphics.Paint
 import android.graphics.Canvas
 import android.graphics.Color
 import android.graphics.RectF
+import android.graphics.Path
 import android.app.Activity
 import android.content.Context
 
@@ -27,3 +28,43 @@ val rot : Float = 90f
 fun Int.inverse() : Float = 1f /this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
+
+fun Canvas.drawXY(x : Float, y : Float, cb : () -> Unit) {
+    save()
+    translate(x, y)
+    cb()
+    restore()
+}
+
+fun Canvas.drawTriLine(size : Float, paint : Paint) {
+    val path : Path = Path()
+    path.moveTo(0f, -size / 2)
+    path.lineTo(size / 2, 0f)
+    path.lineTo(0f, size / 2)
+    drawPath(path, paint)
+}
+
+fun Canvas.drawSqFromLeftLine(scale : Float, w : Float, h : Float, paint : Paint) {
+    val size : Float = Math.min(w, h) / sizeFactor
+    val dsc : (Int) -> Float = {
+        scale.divideScale(it, parts)
+    }
+    drawXY(w / 2, h / 2 + (h / 2 + size) * dsc(3)) {
+        rotate(rot * dsc(2))
+        drawXY(-w * 0.5f * (1 - dsc(0)), 0f) {
+            drawRect(RectF(-size / 2, -size / 2, 0f, size / 2), paint)
+        }
+        drawXY(w * 0.5f * (1 - dsc(1)), 0f) {
+            drawTriLine(size, paint)
+        }
+    }
+}
+
+fun Canvas.drawSFLLNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = Color.parseColor(colors[i])
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    paint.strokeCap = Paint.Cap.ROUND
+    drawSqFromLeftLine(scale, w, h, paint)
+}
