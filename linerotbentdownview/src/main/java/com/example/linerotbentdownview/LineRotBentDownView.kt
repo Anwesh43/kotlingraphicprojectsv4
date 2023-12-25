5035fb0940eceb78917d33d5da053ea5cf18235e
Anwesh43 <anweshthecool0@gmail.com>
2023-12-25 12:45:59 +0530

    linerotbentdownview: adding Canvas extension methods

--- linerotbentdownview/src/main/java/com/example/linerotbentdownview/LineRotBentDownView.kt
+++ linerotbentdownview/src/main/java/com/example/linerotbentdownview/LineRotBentDownView.kt
@@ -28,3 +28,36 @@ val rot : Float = 90f
 fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
+
+fun Canvas.drawXY(x : Float, y : Float, cb : () -> Unit) {
+    save()
+    translate(x, y)
+    cb()
+    restore()
+}
+
+fun Canvas.drawLineRotBentDown(scale : Float, w : Float, h : Float, paint : Paint) {
+    val size : Float = Math.min(w, h) / sizeFactor
+    val dsc : (Int) -> Float = {
+        scale.divideScale(it, parts)
+    }
+    drawXY(w / 2, h / 2) {
+        rotate(rot * dsc(2))
+        for (j in 0..1) {
+            drawXY(0f, 0f) {
+                rotate(-sweep * dsc(1) * j)
+                drawLine(0f, 0f, 0f, size * dsc(0), paint)
+            }
+        }
+        drawArc(RectF(-size / 4, -size / 4, size / 4, size / 4), -sweep * dsc(1), sweep * dsc(1), true, paint)
+    }
+}
+
+fun Canvas.drawLRBDNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = Color.parseColor(colors[i])
+    paint.strokeCap = Paint.Cap.ROUND
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    drawLineRotBentDown(scale, w, h, paint)
+}
