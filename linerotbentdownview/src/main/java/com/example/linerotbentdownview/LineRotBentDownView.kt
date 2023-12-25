d0b3d7dbf6d3bfaed002704369bb1e04b86bbcad
Anwesh43 <anweshthecool0@gmail.com>
2023-12-25 12:53:13 +0530

    linerotbentdownview: adding LineRotBentDownView class

--- linerotbentdownview/src/main/java/com/example/linerotbentdownview/LineRotBentDownView.kt
+++ linerotbentdownview/src/main/java/com/example/linerotbentdownview/LineRotBentDownView.kt
@@ -61,3 +61,19 @@ fun Canvas.drawLRBDNode(i : Int, scale : Float, paint : Paint) {
     paint.strokeWidth = Math.min(w, h) / strokeFactor
     drawLineRotBentDown(scale, w, h, paint)
 }
+
+class LineRotBentDownView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
