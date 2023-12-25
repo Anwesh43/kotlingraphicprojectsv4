d697845bbd28ea0d4c2ca0f3652679dd9e778513
Anwesh43 <anweshthecool0@gmail.com>
2023-12-25 12:41:36 +0530

    linerotbentdownview: adding Int and Float extension methods

--- linerotbentdownview/src/main/java/com/example/linerotbentdownview/LineRotBentDownView.kt
+++ linerotbentdownview/src/main/java/com/example/linerotbentdownview/LineRotBentDownView.kt
@@ -25,3 +25,6 @@ val backColor : Int = Color.parseColor("#BDBDBD")
 val sweep : Float = 45f
 val rot : Float = 90f
 
+fun Int.inverse() : Float = 1f / this
+fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
+fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
