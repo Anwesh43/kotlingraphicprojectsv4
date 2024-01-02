49bc0c415d83da1b32513f9992344395da3f094d
Anwesh43 <anweshthecool0@gmail.com>
2024-01-02 08:40:38 +0530

    bilinerotrightview: adding Canvas extension methods

--- bilinerotrightview/src/main/java/com/example/bilinerotrightview/BiLineRotRightView.kt
+++ bilinerotrightview/src/main/java/com/example/bilinerotrightview/BiLineRotRightView.kt
@@ -21,9 +21,44 @@ val scGap : Float = 0.04f / parts
 val strokeFactor : Float = 90f
 val sizeFactor : Float = 4.9f
 val delay : Long = 20
-val backColor : Int = Color.parseColor("#BDBDBD")
+val backColor : String = "#BDBDBD"
 val rot : Float = 90f
 
 fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
+
+fun Canvas.drawXY(x : Float, y : Float, cb : () ->  Unit) {
+    save()
+    translate(x, y)
+    cb()
+    restore()
+}
+
+fun Canvas.drawBiLineRotRight(scale : Float, w : Float, h : Float, paint : Paint) {
+    val size : Float = Math.min(w, h) / sizeFactor
+    val dsc : (Int) -> Float = {
+        scale.divideScale(it, parts)
+    }
+    drawXY(w / 2 + (w / 2 + size) * dsc(3), h / 2) {
+        drawArc(RectF(0f, -size / 2, size, size / 2), 180f, 180f * dsc(2), true, paint)
+        for (j in 0..1) {
+            drawXY(0f, 0f) {
+                scale(1f, 1f - 2 * j)
+                drawXY(0f, -(h / 2) * (1 - dsc(0))) {
+                    rotate(rot * dsc(1))
+                    drawLine(0f, 0f, 0f, size, paint)
+                }
+            }
+        }
+    }
+}
+
+fun Canvas.drawBLRRNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = Color.parseColor(backColor)
+    paint.strokeCap = Paint.Cap.ROUND
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    drawBiLineRotRight(scale, w, h, paint)
+}
