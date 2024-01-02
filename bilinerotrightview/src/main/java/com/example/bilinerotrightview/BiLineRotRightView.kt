b05bdc74a60986a1418bea4bbef82e34f9d54d81
Anwesh43 <anweshthecool0@gmail.com>
2024-01-02 08:54:33 +0530

    bilinerotrightview: adding BiLineRotRight class

--- bilinerotrightview/src/main/java/com/example/bilinerotrightview/BiLineRotRightView.kt
+++ bilinerotrightview/src/main/java/com/example/bilinerotrightview/BiLineRotRightView.kt
@@ -166,4 +166,27 @@ class BiLineRotRightView(ctx : Context) : View(ctx) {
             return this
         }
     }
+
+    data class BiLineRotRight(var i : Int) {
+
+        private var curr : BLRRNode = BLRRNode(0)
+        private var dir : Int = 1
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            curr.draw(canvas, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            curr.update {
+                curr = curr.getNext(dir) {
+                    dir *= -1
+                }
+                cb(it)
+            }
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            curr.startUpdating(cb)
+        }
+    }
 }
