f45c5bac4cae88bc64842e4649fe56cf12738415
Anwesh43 <anweshthecool0@gmail.com>
2024-01-02 08:58:50 +0530

    bilinerotrightview: adding static create method

--- bilinerotrightview/src/main/java/com/example/bilinerotrightview/BiLineRotRightView.kt
+++ bilinerotrightview/src/main/java/com/example/bilinerotrightview/BiLineRotRightView.kt
@@ -214,4 +214,13 @@ class BiLineRotRightView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    companion object {
+
+        fun create(activity: Activity) : BiLineRotRightView {
+            val view : BiLineRotRightView = BiLineRotRightView(activity)
+            activity.setContentView(view)
+            return view
+        }
+    }
 }
