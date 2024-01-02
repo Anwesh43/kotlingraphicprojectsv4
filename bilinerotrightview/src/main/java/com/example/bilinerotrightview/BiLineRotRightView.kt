a7c08c41a4465611253ddf2ec427c8201b88f8ad
Anwesh43 <anweshthecool0@gmail.com>
2024-01-02 08:41:58 +0530

    bilinerotrightview: adding BiLineRotRightView class

--- bilinerotrightview/src/main/java/com/example/bilinerotrightview/BiLineRotRightView.kt
+++ bilinerotrightview/src/main/java/com/example/bilinerotrightview/BiLineRotRightView.kt
@@ -62,3 +62,19 @@ fun Canvas.drawBLRRNode(i : Int, scale : Float, paint : Paint) {
     paint.strokeWidth = Math.min(w, h) / strokeFactor
     drawBiLineRotRight(scale, w, h, paint)
 }
+
+class BiLineRotRightView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
