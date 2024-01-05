be18979ebd48de96e39dad6a688251ffb11782e3
Anwesh43 <anweshthecool0@gmail.com>
2024-01-06 03:42:50 +0530

    linepeakrotrightview: adding LPRRNode class

--- linepeakrotrightview/src/main/java/com/example/linepeakrotrightview/LinePeakRotRightView.kt
+++ linepeakrotrightview/src/main/java/com/example/linepeakrotrightview/LinePeakRotRightView.kt
@@ -119,4 +119,45 @@ class LinePeakRotRightView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class LPRRNode(var i : Int = 0, val state : State = State()) {
+
+        private var next : LPRRNode? = null
+        private var prev : LPRRNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < colors.size - 1) {
+                next = LPRRNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawLPRRNode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : LPRRNode {
+            var curr : LPRRNode? = prev
+            if (dir == 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
