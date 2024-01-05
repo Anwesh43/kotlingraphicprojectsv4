321d6af61fac807c15b4a0b344ef4f90246215cd
Anwesh43 <anweshthecool0@gmail.com>
2024-01-06 03:36:45 +0530

    linepeakrotrightview: adding LinePeakRotRightView class

--- linepeakrotrightview/src/main/java/com/example/linepeakrotrightview/LinePeakRotRightView.kt
+++ linepeakrotrightview/src/main/java/com/example/linepeakrotrightview/LinePeakRotRightView.kt
@@ -56,3 +56,19 @@ fun Canvas.drawLPRRNode(i : Int, scale : Float, paint : Paint) {
     paint.strokeWidth = Math.min(w, h) / strokeFactor
     drawLinePeakRotRight(scale, w, h, paint)
 }
+
+class LinePeakRotRightView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
