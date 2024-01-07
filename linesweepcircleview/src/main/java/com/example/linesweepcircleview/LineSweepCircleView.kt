c4ad25256fc4d1168daacf6533078408fba7983a
Anwesh43 <anweshthecool0@gmail.com>
2024-01-07 11:52:20 +0530

    linesweepcircleview: Int and Float extension methods

--- linesweepcircleview/src/main/java/com/example/linesweepcircleview/LineSweepCircleView.kt
+++ linesweepcircleview/src/main/java/com/example/linesweepcircleview/LineSweepCircleView.kt
@@ -23,3 +23,7 @@ val scGap : Float = 0.04f / parts
 val delay : Long = 20
 val backColor : Int = Color.parseColor("#BDBDBD")
 val rot : Float = 90f
+
+fun Int.inverse() : Float = 1f / this
+fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
+fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
