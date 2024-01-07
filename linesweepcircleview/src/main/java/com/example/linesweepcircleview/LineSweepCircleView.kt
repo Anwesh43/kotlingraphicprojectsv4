f2d0bce35a4828c31d4bc5558dd06a1e094f5026
Anwesh43 <anweshthecool0@gmail.com>
2024-01-07 12:32:29 +0530

    linesweepcircleview: adding LSCNode class

--- linesweepcircleview/src/main/java/com/example/linesweepcircleview/LineSweepCircleView.kt
+++ linesweepcircleview/src/main/java/com/example/linesweepcircleview/LineSweepCircleView.kt
@@ -120,4 +120,45 @@ class LineSweepCircleView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class LSCNode(var i : Int = 0, val state : State = State()) {
+
+        private var next : LSCNode? = null
+        private var prev : LSCNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < colors.size - 1) {
+                next = LSCNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawLSCNode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : LSCNode {
+            var curr : LSCNode? = prev
+            if (dir == 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
