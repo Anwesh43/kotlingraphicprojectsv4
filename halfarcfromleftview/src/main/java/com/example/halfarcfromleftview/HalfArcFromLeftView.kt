93534ce00096d0e9791d4a7c268e28163df4c91c
Anwesh43 <anweshthecool0@gmail.com>
2024-01-08 03:04:00 +0530

    halfarcfromleftview: adding Canvas extension methods

--- halfarcfromleftview/src/main/java/com/example/halfarcfromleftview/HalfArcFromLeftView.kt
+++ halfarcfromleftview/src/main/java/com/example/halfarcfromleftview/HalfArcFromLeftView.kt
@@ -7,6 +7,8 @@ import android.app.Activity
 import android.graphics.Paint
 import android.graphics.Canvas
 import android.graphics.Color
+import android.graphics.RectF
+
 
 val colors : Array<String> = arrayOf(
     "#1A237E",
@@ -26,3 +28,30 @@ val backColor : Int = Color.parseColor("#BDBDBD")
 fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
+
+fun Canvas.drawXY(x : Float, y : Float, cb : () -> Unit) {
+    save()
+    translate(x, y)
+    cb()
+    restore()
+}
+
+fun Canvas.drawHalfArcFromLeft(scale : Float, w : Float, h : Float, paint : Paint) {
+    val size : Float = Math.min(w, h) / sizeFactor
+    val dsc : (Int) -> Float = {
+        scale.divideScale(it, parts)
+    }
+    drawXY(w / 2 + (w / 2) * dsc(3), h / 2) {
+        rotate(-rot * dsc(2))
+        drawXY(-(w * 0.5f - size / 2) * (1 - dsc(1)), 0f) {
+            drawArc(RectF(-size / 2, -size / 2, size / 2, size / 2), 0f, 180f * dsc(0), true, paint)
+        }
+    }
+}
+
+fun Canvas.drawHAFLNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = Color.parseColor(colors[i])
+    drawHalfArcFromLeft(scale, w, h, paint)
+}
