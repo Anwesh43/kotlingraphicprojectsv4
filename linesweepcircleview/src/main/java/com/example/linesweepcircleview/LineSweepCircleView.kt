11284f288451038b65113b8b706df2263d417186
Anwesh43 <anweshthecool0@gmail.com>
2024-01-07 12:02:32 +0530

    linesweepcircleview: adding LineSweepCircleView class

--- linesweepcircleview/src/main/java/com/example/linesweepcircleview/LineSweepCircleView.kt
+++ linesweepcircleview/src/main/java/com/example/linesweepcircleview/LineSweepCircleView.kt
@@ -57,3 +57,19 @@ fun Canvas.drawLSCNode(i : Int, scale : Float, paint : Paint) {
     paint.strokeWidth = Math.min(w, h) / strokeFactor
     drawLineSweepCircle(scale, w, h, paint)
 }
+
+class LineSweepCircleView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
