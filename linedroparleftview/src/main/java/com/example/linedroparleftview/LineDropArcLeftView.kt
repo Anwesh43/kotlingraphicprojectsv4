f769d526b8c57abecc1a7bdbc1748eebd0247855
Anwesh43 <anweshthecool0@gmail.com>
2024-01-10 01:56:22 +0530

    linedroparcleftview: adding Canvas extension methods

--- linedroparleftview/src/main/java/com/example/linedroparleftview/LineDropArcLeftView.kt
+++ linedroparleftview/src/main/java/com/example/linedroparleftview/LineDropArcLeftView.kt
@@ -22,8 +22,39 @@ val strokeFactor : Float = 90f
 val sizeFactor : Float = 4.9f
 val delay : Long = 20
 val backColor : Int = Color.parseColor("#BDBDBD")
-val rot : Float = 90f
+val rot : Float = 45f
 
 fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
+
+fun Canvas.drawXY(x : Float, y : Float, cb : () -> Unit) {
+    save()
+    translate(x, y)
+    cb()
+    restore()
+}
+
+fun Canvas.drawLineDropArcLeft(scale : Float, w : Float, h : Float, paint : Paint) {
+    val size : Float = Math.min(w, h) / sizeFactor
+    val dsc : (Int) -> Float = {
+        scale.divideScale(it, parts)
+    }
+    drawXY(w / 2 - (w / 2) * dsc(3), h / 2) {
+        drawXY(0f, 0f) {
+            rotate(-rot * dsc(2))
+            drawLine(0f, 0f, 0f, -size * dsc(1), paint)
+        }
+        drawArc(RectF(-size / 2, -size / 2, size / 2, size / 2), 90f, 90f * dsc(0), true, paint)
+    }
+}
+
+fun Canvas.drawLDALNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    val size : Float = Math.min(w, h) / sizeFactor
+    paint.color = Color.parseColor(colors[i])
+    paint.strokeCap = Paint.Cap.ROUND
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    drawLineDropArcLeft(scale, w, h, paint)
+}
