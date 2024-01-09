ed9a59469359bfc7072849dc679e9edc14889b55
Anwesh43 <anweshthecool0@gmail.com>
2024-01-10 02:46:01 +0530

    linedroparcleftview: adding LineDropArcLeft class

--- linedroparleftview/src/main/java/com/example/linedroparleftview/LineDropArcLeftView.kt
+++ linedroparleftview/src/main/java/com/example/linedroparleftview/LineDropArcLeftView.kt
@@ -161,6 +161,28 @@ class LineDropArcLeftView(ctx : Context) : View(ctx) {
             cb()
             return this
         }
+    }
 
+    data class LineDropArcLeft(var i : Int) {
+
+        private var curr : LDALNode = LDALNode(0)
+        private var dir : Int = 1
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            curr.draw(canvas, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            curr.update {
+                curr = curr.getNext(dir) {
+                    dir *= -1
+                }
+                cb(it)
+            }
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            curr.startUpdating(cb)
+        }
     }
 }
