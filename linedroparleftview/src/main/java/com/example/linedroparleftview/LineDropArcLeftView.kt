0672835241a02dc8afe85c826344fb06af953db4
Anwesh43 <anweshthecool0@gmail.com>
2024-01-10 01:57:53 +0530

    linedroparcleftview: adding LineDropArcLeftView class

--- linedroparleftview/src/main/java/com/example/linedroparleftview/LineDropArcLeftView.kt
+++ linedroparleftview/src/main/java/com/example/linedroparleftview/LineDropArcLeftView.kt
@@ -58,3 +58,19 @@ fun Canvas.drawLDALNode(i : Int, scale : Float, paint : Paint) {
     paint.strokeWidth = Math.min(w, h) / strokeFactor
     drawLineDropArcLeft(scale, w, h, paint)
 }
+
+class LineDropArcLeftView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
