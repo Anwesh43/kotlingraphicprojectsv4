3dda3ff8ea31ff1e3833a0081377783d5f8b5217
Anwesh43 <anweshthecool0@gmail.com>
2024-01-11 02:32:26 +0530

    linesqrotleftview: adding LineSqRotView class

--- linesqrotleftview/src/main/java/com/example/linesqrotleftview/LineSqRotLeftView.kt
+++ linesqrotleftview/src/main/java/com/example/linesqrotleftview/LineSqRotLeftView.kt
@@ -64,3 +64,19 @@ fun Canvas.drawLSRLNode(i : Int, scale : Float, paint : Paint) {
     paint.strokeWidth = Math.min(w, h) / strokeFactor
     drawLineSqRotLeft(scale, w, h, paint)
 }
+
+class LineSqRotLeftView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
