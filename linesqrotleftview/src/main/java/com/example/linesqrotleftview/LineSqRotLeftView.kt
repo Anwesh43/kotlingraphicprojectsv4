ae43d583218b0c85fc256325b3e8b1a85b030814
Anwesh43 <anweshthecool0@gmail.com>
2024-01-11 02:47:48 +0530

    linesqrotleftview: adding LineSqRotLeft class

--- linesqrotleftview/src/main/java/com/example/linesqrotleftview/LineSqRotLeftView.kt
+++ linesqrotleftview/src/main/java/com/example/linesqrotleftview/LineSqRotLeftView.kt
@@ -168,4 +168,27 @@ class LineSqRotLeftView(ctx : Context) : View(ctx) {
             return this
         }
     }
+
+    data class LineSqRotLeft(var i : Int) {
+
+        private var curr : LSRLNode = LSRLNode(0)
+        private var dir : Int = 1
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            curr.draw(canvas, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            curr.update {
+                curr = curr.getNext(dir) {
+                    dir *= -1
+                }
+                cb(it)
+            }
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            curr.startUpdating(cb)
+        }
+    }
 }
