13e71383d8ed33df6c94b25795f607337d451982
Anwesh43 <anweshthecool0@gmail.com>
2024-01-12 17:36:43 +0530

    linebisectrotrightview: adding LineBisectRotRight class

--- linebisectrotrigthview/src/main/java/com/example/linebisectrotrigthview/LineBisectRotRightView.kt
+++ linebisectrotrigthview/src/main/java/com/example/linebisectrotrigthview/LineBisectRotRightView.kt
@@ -162,4 +162,27 @@ class LineBisectRotRightView(ctx : Context) : View(ctx) {
             return this
         }
     }
+
+    data class LineBisectRotRight(var i : Int) {
+
+        private var curr : LBRRNode = LBRRNode(0)
+        private var dir : Int = 1
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            curr.draw(canvas, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            curr.update {
+                curr = curr.getNext(dir) {
+                    dir *= -1
+                }
+                cb(it)
+            }
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            curr.startUpdating(cb)
+        }
+    }
 }
