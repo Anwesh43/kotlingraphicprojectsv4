5f8df1bf8a72f20904386f642208ada8f1fb405d
Anwesh43 <anweshthecool0@gmail.com>
2024-01-12 17:26:37 +0530

    linebisectrotrightview: adding Canvas extension methods

--- linebisectrotrigthview/src/main/java/com/example/linebisectrotrigthview/LineBisectRotRightView.kt
+++ linebisectrotrigthview/src/main/java/com/example/linebisectrotrigthview/LineBisectRotRightView.kt
@@ -27,3 +27,34 @@ val rot : Float = 90f
 fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
+
+fun Canvas.drawXY(x : Float, y : Float, cb : () -> Unit) {
+    save()
+    translate(x, y)
+    cb()
+    restore()
+}
+
+fun Canvas.drawLineBisectRotRight(scale : Float, w : Float, h : Float, paint : Paint) {
+    val size : Float = Math.min(w, h) / sizeFactor
+    val dsc : (Int) -> Float = {
+        scale.divideScale(it, parts)
+    }
+    drawXY(w / 2 + (w / 2) * dsc(3), h / 2) {
+        rotate(rot * dsc(2))
+        for (j in 0..1) {
+            drawXY(-size / 4 + size * 0.5f * j, -h * 0.5f * (1 - dsc(j))) {
+                drawLine(0f, 0f, 0f, -size + (size / 2) * j)
+            }
+        }
+    }
+}
+
+fun Canvas.drawLBRRNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = Color.parseColor(colors[i])
+    paint.strokeCap = Paint.Cap.ROUND
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    drawLineBisectRotRight(scale, w, h, paint)
+}
