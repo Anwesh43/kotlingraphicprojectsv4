37b2c867def0d2c092fee1f62a57b91da3be2b3d
Anwesh43 <anweshthecool0@gmail.com>
2024-01-16 10:35:10 +0530

    linerotarcleftview: adding LARLNode class

--- linerotarcleftview/src/main/java/com/example/linerotarcleftview/LineArcRotLeftView.kt
+++ linerotarcleftview/src/main/java/com/example/linerotarcleftview/LineArcRotLeftView.kt
@@ -125,4 +125,45 @@ class LineArcRotLeftView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class LARLNode(var i : Int = 0, val state : State = State()) {
+
+        private var next : LARLNode? = null
+        private var prev : LARLNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < colors.size - 1) {
+                next = LARLNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawLARLNode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : LARLNode {
+            var curr : LARLNode? = prev
+            if (dir == 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
