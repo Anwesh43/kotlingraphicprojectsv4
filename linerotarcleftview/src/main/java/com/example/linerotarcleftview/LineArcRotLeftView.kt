fedad4267166d0ec38c38cb45b2f7065eda2f1e8
Anwesh43 <anweshthecool0@gmail.com>
2024-01-16 10:27:17 +0530

    linerotarcleftview: adding Canvas extension methods

--- linerotarcleftview/src/main/java/com/example/linerotarcleftview/LineArcRotLeftView.kt
+++ linerotarcleftview/src/main/java/com/example/linerotarcleftview/LineArcRotLeftView.kt
@@ -19,11 +19,46 @@ val colors : Array<String> = arrayOf(
 val parts : Int = 5
 val scGap : Float = 0.04f / parts
 val strokeFactor : Float = 90f
-val sizeFactory : Float = 4.9f
+val sizeFactor : Float = 4.9f
 val delay : Long = 20
 val backColor : Int = Color.parseColor("#BDBDBD")
 val rot : Float = 45f
+val deg : Float = 90f
 
 fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
+
+fun Canvas.drawXY(x : Float, y : Float, cb : () -> Unit) {
+    save()
+    translate(x, y)
+    cb()
+    restore()
+}
+
+fun Canvas.drawLineArcRotLeft(scale : Float, w : Float, h : Float, paint : Paint) {
+    val size : Float = Math.min(w, h) / sizeFactor
+    val dsc : (Int) -> Float = {
+        scale.divideScale(it, parts)
+    }
+    val a : Float = size * Math.cos(rot * Math.PI / 180f).toFloat()
+    drawXY(w / 2, h / 2) {
+        rotate(deg * dsc(3))
+        for (j in 0..1) {
+            drawXY(0f, -h * 0.5f * (1 - dsc(0))) {
+                rotate(rot * dsc(1) * (1f - 2 * j))
+                drawLine(0f, 0f, 0f, -size, paint)
+            }
+        }
+        drawArc(RectF(-a, -a, a, a), -rot, (360f - deg) * dsc(2), true, paint)
+    }
+}
+
+fun Canvas.drawLARLNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = Color.parseColor(colors[i])
+    paint.strokeCap = Paint.Cap.ROUND
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    drawLineArcRotLeft(scale, w, h, paint)
+}
