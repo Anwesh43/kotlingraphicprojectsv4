a1f325509499f3217cfed83989456f733d90fb49
Anwesh43 <anweshthecool0@gmail.com>
2024-01-16 10:39:01 +0530

    linerotarcleftview: adding Renderer in View

--- linerotarcleftview/src/main/java/com/example/linerotarcleftview/LineArcRotLeftView.kt
+++ linerotarcleftview/src/main/java/com/example/linerotarcleftview/LineArcRotLeftView.kt
@@ -65,14 +65,16 @@ fun Canvas.drawLARLNode(i : Int, scale : Float, paint : Paint) {
 
 class LineArcRotLeftView(ctx : Context) : View(ctx) {
 
-    override fun onDraw(canvas : Canvas) {
+    private val renderer : Renderer = Renderer(this)
 
+    override fun onDraw(canvas : Canvas) {
+        renderer.render(canvas)
     }
 
     override fun onTouchEvent(event : MotionEvent) : Boolean {
         when (event.action) {
             MotionEvent.ACTION_DOWN -> {
-
+                renderer.handleTap()
             }
         }
         return true
