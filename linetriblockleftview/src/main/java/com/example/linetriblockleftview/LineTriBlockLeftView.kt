d28affd88ac7b9aee5aac4180b456a4262e84893
Anwesh43 <anweshthecool0@gmail.com>
2024-01-18 03:56:23 +0530

    linetriblockleftview: adding LineTriBlockLeft class

--- linetriblockleftview/src/main/java/com/example/linetriblockleftview/LineTriBlockLeftView.kt
+++ linetriblockleftview/src/main/java/com/example/linetriblockleftview/LineTriBlockLeftView.kt
@@ -163,4 +163,27 @@ class LineTriBlockLeftView(ctx : Context) : View(ctx) {
             return this
         }
     }
+
+    data class LineTriBlockLeft(var i : Int) {
+
+        private var curr : LTBLNode = LTBLNode(0)
+        private var dir : Int = 1
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            curr.draw(canvas, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            curr.update {
+                curr = curr.getNext(dir) {
+                    dir *= -1
+                }
+                cb(it)
+            }
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            curr.startUpdating(cb)
+        }
+    }
 }
