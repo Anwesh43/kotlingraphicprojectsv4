7d4cf93f1c2a83e2abae6d4be992e2d623e0ad3e
Anwesh43 <anweshthecool0@gmail.com>
2024-01-18 03:54:14 +0530

    linetriblockleftview: adding LTBLNode class

--- linetriblockleftview/src/main/java/com/example/linetriblockleftview/LineTriBlockLeftView.kt
+++ linetriblockleftview/src/main/java/com/example/linetriblockleftview/LineTriBlockLeftView.kt
@@ -123,4 +123,44 @@ class LineTriBlockLeftView(ctx : Context) : View(ctx) {
             }
         }
     }
+    data class LTBLNode(var i : Int = 0, val state : State = State()) {
+
+        private var next : LTBLNode? = null
+        private var prev : LTBLNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < colors.size - 1) {
+                next = LTBLNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawLTBLNode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : LTBLNode {
+            var curr : LTBLNode? = prev
+            if (dir == 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
