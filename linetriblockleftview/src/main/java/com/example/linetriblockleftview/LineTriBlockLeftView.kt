80cb0cf3ae0c536560b1c7e2544d53e5cbf5c189
Anwesh43 <anweshthecool0@gmail.com>
2024-01-18 03:49:11 +0530

    linetriblockleftview: adding LineTriBlockLeftView class

--- linetriblockleftview/src/main/java/com/example/linetriblockleftview/LineTriBlockLeftView.kt
+++ linetriblockleftview/src/main/java/com/example/linetriblockleftview/LineTriBlockLeftView.kt
@@ -60,3 +60,19 @@ fun Canvas.drawLTBLNode(i : Int, scale : Float, paint : Paint) {
     paint.strokeWidth = Math.min(w, h) / strokeFactor
     drawLineTriBlockLeft(scale, w, h, paint)
 }
+
+class LineTriBlockLeftView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
