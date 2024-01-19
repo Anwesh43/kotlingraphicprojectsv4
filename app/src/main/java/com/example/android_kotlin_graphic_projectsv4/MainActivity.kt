8a43515b7481417df9215fa6c0626ba43a323558
Anwesh43 <anweshthecool0@gmail.com>
2024-01-19 16:24:34 +0530

    boxlinedroprightview: adding view in activity

--- app/src/main/java/com/example/android_kotlin_graphic_projectsv4/MainActivity.kt
+++ app/src/main/java/com/example/android_kotlin_graphic_projectsv4/MainActivity.kt
@@ -2,7 +2,7 @@ package com.example.android_kotlin_graphic_projectsv4
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.example.linetriblockleftview.LineTriBlockLeftView
+import com.example.boxlinedroprightview.BoxLineDropRightView
 
 
 class MainActivity : AppCompatActivity() {
@@ -10,6 +10,6 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         supportActionBar?.hide()
         //setContentView(R.layout.activity_main)
-        LineTriBlockLeftView.create(this)
+        BoxLineDropRightView.create(this)
     }
 }
