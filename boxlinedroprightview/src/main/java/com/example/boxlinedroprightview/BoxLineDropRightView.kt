374c69a19faa4826a5d6040474cb0998305fcf1e
Anwesh43 <anweshthecool0@gmail.com>
2024-01-19 16:20:29 +0530

    boxlinedroprightview: adding BoxLineDropRight class

--- boxlinedroprightview/src/main/java/com/example/boxlinedroprightview/BoxLineDropRightView.kt
+++ boxlinedroprightview/src/main/java/com/example/boxlinedroprightview/BoxLineDropRightView.kt
@@ -173,4 +173,27 @@ class BoxLineDropRightView(ctx : Context) : View(ctx) {
             return this
         }
     }
+
+    data class BoxLineDropRight(var i : Int) {
+
+        private var curr : BLDRNode = BLDRNode(0)
+        private var dir : Int = 1
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            curr.draw(canvas, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            curr.update {
+                curr = curr.getNext(dir) {
+                    dir *= -1
+                }
+                cb(it)
+            }
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            curr.startUpdating(cb)
+        }
+    }
 }
