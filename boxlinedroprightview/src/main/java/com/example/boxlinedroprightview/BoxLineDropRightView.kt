ebe6b3e1ce8da199e9e72be8893de2cf8f3eb3d6
Anwesh43 <anweshthecool0@gmail.com>
2024-01-19 16:02:38 +0530

    boxlinedroprightview: adding BoxLineDropRightView class

--- boxlinedroprightview/src/main/java/com/example/boxlinedroprightview/BoxLineDropRightView.kt
+++ boxlinedroprightview/src/main/java/com/example/boxlinedroprightview/BoxLineDropRightView.kt
@@ -69,3 +69,19 @@ fun Canvas.drawBLDRNode(i : Int, scale : Float, paint : Paint) {
     paint.style = Paint.Style.STROKE
     drawBoxLineDropRight(scale, w, h, paint)
 }
+
+class BoxLineDropRightView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
