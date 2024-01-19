d4930da3f86c1a5b896bfd348670e346f9c577af
Anwesh43 <anweshthecool0@gmail.com>
2024-01-19 16:07:38 +0530

    boxlinedroprightview: adding BLDRNode class

--- boxlinedroprightview/src/main/java/com/example/boxlinedroprightview/BoxLineDropRightView.kt
+++ boxlinedroprightview/src/main/java/com/example/boxlinedroprightview/BoxLineDropRightView.kt
@@ -104,4 +104,45 @@ class BoxLineDropRightView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class BLDRNode(var i : Int = 0, val state : State = State()) {
+
+        private var next : BLDRNode? = null
+        private var prev : BLDRNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < colors.size - 1) {
+                next = BLDRNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawBLDRNode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : BLDRNode {
+            var curr : BLDRNode? = prev
+            if (dir == 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
