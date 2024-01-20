3e9a6ee10e3f227b55d1bb60a18e3e235b48e1c9
Anwesh43 <anweshthecool0@gmail.com>
2024-01-21 01:33:58 +0530

    bilshapetosquareview: adding Renderer in View

--- bilshapetosquareview/src/main/java/com/example/bilshapetosquareview/BiLShapeToSquareView.kt
+++ bilshapetosquareview/src/main/java/com/example/bilshapetosquareview/BiLShapeToSquareView.kt
@@ -66,14 +66,16 @@ fun Canvas.drawBLSTSNode(i : Int, scale : Float, paint : Paint) {
 
 class BiLShapeToSquareView(ctx : Context) : View(ctx) {
 
-    override fun onDraw(canvas : Canvas) {
+    private val renderer : Renderer = Renderer(this)
 
+    override fun onDraw(canvas : Canvas) {
+        renderer.render(canvas)
     }
 
     override fun onTouchEvent(event : MotionEvent) : Boolean {
         when (event.action) {
             MotionEvent.ACTION_DOWN -> {
-
+                renderer.handleTap()
             }
         }
         return true
@@ -204,6 +206,12 @@ class BiLShapeToSquareView(ctx : Context) : View(ctx) {
                 blsts.update {
                     animator.stop()
                 }
+            }
+        }
+
+        fun handleTap() {
+            blsts.startUpdating {
+                animator.start()
             }
         }
     }
