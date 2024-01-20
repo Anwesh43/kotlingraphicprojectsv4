a73c640a58eced60b286f3941e5d0839cd947050
Anwesh43 <anweshthecool0@gmail.com>
2024-01-21 01:32:05 +0530

    bilshapetosquareview: adding BiLShapeToSquare class

--- bilshapetosquareview/src/main/java/com/example/bilshapetosquareview/BiLShapeToSquareView.kt
+++ bilshapetosquareview/src/main/java/com/example/bilshapetosquareview/BiLShapeToSquareView.kt
@@ -167,4 +167,27 @@ class BiLShapeToSquareView(ctx : Context) : View(ctx) {
             return this
         }
     }
+
+    data class BiLShapeToSquare(var i : Int) {
+
+        private var curr : BLSTSNode = BLSTSNode(0)
+        private var dir : Int = 1
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            curr.draw(canvas, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            curr.update {
+                curr = curr.getNext(dir) {
+                    dir *= -1
+                }
+                cb(it)
+            }
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            curr.startUpdating(cb)
+        }
+    }
 }
