891a912d176b79748a85a77bd4ed77520a45efea
Anwesh43 <anweshthecool0@gmail.com>
2024-01-21 01:01:01 +0530

    bilshapetosquareview: adding Canvas extension methods

--- bilshapetosquareview/src/main/java/com/example/bilshapetosquareview/BiLShapeToSquareView.kt
+++ bilshapetosquareview/src/main/java/com/example/bilshapetosquareview/BiLShapeToSquareView.kt
@@ -16,8 +16,8 @@ val colors : Array<String> = arrayOf(
     "#C51162",
     "#00C853"
 )
-val parts : Int = 4
-val scGap : Float = 0.04f / parts
+val parts : Int = 3
+val scGap : Float = 0.03f / parts
 val strokeFactor : Float = 90f
 val rot : Float = 180f
 val delay : Long = 20
@@ -28,3 +28,38 @@ val lSizeFactor : Float = 11.2f
 fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
+
+fun Canvas.drawXY(x : Float, y : Float, cb : () -> Unit) {
+    save()
+    translate(x, y)
+    cb()
+    restore()
+}
+
+fun Canvas.drawBiLShapeToSquare(scale : Float, w : Float, h : Float, paint : Paint) {
+    val size : Float = Math.min(w, h) / sizeFactor
+    val dsc : (Int) -> Float = {
+        scale.divideScale(it, parts)
+    }
+    drawXY(w / 2, h / 2 + (h / 2) * dsc(2)) {
+        for (j in 0..1) {
+            drawXY(0f, 0f) {
+                scale(1f - 2 * j, 1f)
+                drawXY(-w * 0.5f * (1 - dsc(0)), 0f) {
+                    rotate(rot * dsc(1))
+                    drawLine(0f, 0f, -size / 2, 0f, paint)
+                    drawLine(0f, 0f, 0f, -size, paint)
+                }
+            }
+        }
+    }
+}
+
+fun Canvas.drawBLSTSNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = Color.parseColor(colors[i])
+    paint.strokeCap = Paint.Cap.ROUND
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    drawBiLShapeToSquare(scale, w, h, paint)
+}
