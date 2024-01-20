4a69cbfbf0fbf51451122a909cbbb6bb601e710c
Anwesh43 <anweshthecool0@gmail.com>
2024-01-21 01:25:49 +0530

    bilshapetosquareview: adding BiLShapeToSquareView class

--- bilshapetosquareview/src/main/java/com/example/bilshapetosquareview/BiLShapeToSquareView.kt
+++ bilshapetosquareview/src/main/java/com/example/bilshapetosquareview/BiLShapeToSquareView.kt
@@ -63,3 +63,19 @@ fun Canvas.drawBLSTSNode(i : Int, scale : Float, paint : Paint) {
     paint.strokeWidth = Math.min(w, h) / strokeFactor
     drawBiLShapeToSquare(scale, w, h, paint)
 }
+
+class BiLShapeToSquareView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
