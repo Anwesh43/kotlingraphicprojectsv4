d45b8b89ae7e203a1bab9b1f8f53e04f1e799337
Anwesh43 <anweshthecool0@gmail.com>
2024-01-21 01:34:35 +0530

    bilshapetosquareview: adding static create method

--- bilshapetosquareview/src/main/java/com/example/bilshapetosquareview/BiLShapeToSquareView.kt
+++ bilshapetosquareview/src/main/java/com/example/bilshapetosquareview/BiLShapeToSquareView.kt
@@ -215,4 +215,13 @@ class BiLShapeToSquareView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    companion object {
+
+        fun create(activity: Activity) : BiLShapeToSquareView {
+            val view : BiLShapeToSquareView = BiLShapeToSquareView(activity)
+            activity.setContentView(view)
+            return view
+        }
+    }
 }
