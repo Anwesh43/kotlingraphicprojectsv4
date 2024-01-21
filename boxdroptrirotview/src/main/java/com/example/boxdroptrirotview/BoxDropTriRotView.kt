68739175433a885886db5ed055777c57ad7d5cca
Anwesh43 <anweshthecool0@gmail.com>
2024-01-22 01:48:27 +0530

    boxdroptrirotview: adding Canvas extension methods

--- boxdroptrirotview/src/main/java/com/example/boxdroptrirotview/BoxDropTriRotView.kt
+++ boxdroptrirotview/src/main/java/com/example/boxdroptrirotview/BoxDropTriRotView.kt
@@ -27,3 +27,36 @@ val rot : Float = 45f
 fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
+
+fun Canvas.drawXY(x : Float, y : Float, cb : () -> Unit) {
+    save()
+    translate(x, y)
+    cb()
+    restore()
+}
+
+fun Canvas.drawBoxDropTriRot(scale : Float, w : Float, h : Float, paint : Paint) {
+    val size : Float = Math.min(w, h) / sizeFactor
+    val dsc : (Int) -> Float = {
+        scale.divideScale(it, parts)
+    }
+    drawXY(w / 2 - (w / 2) * dsc(3), h / 2) {
+        drawXY(0f, h * 0.5f * (1 - dsc(0))) {
+            rotate(-rot * dsc(2))
+            drawLine(0f, 0f, size, size, paint)
+            drawLine(0f, 0f, -size, size, paint)
+        }
+        drawXY(0f, -h * 0.5f * (1 - dsc(1))) {
+            drawRect(RectF(-size / 2, -size, size / 2, 0f), paint)
+        }
+    }
+}
+
+fun Canvas.drawBDTRNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = Color.parseColor(colors[i])
+    paint.strokeCap = Paint.Cap.ROUND
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    drawBoxDropTriRot(scale, w, h, paint)
+}
