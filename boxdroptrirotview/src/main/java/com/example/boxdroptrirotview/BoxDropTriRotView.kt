a7f82298e9014e8e4dc9076d410f704bb9180882
Anwesh43 <anweshthecool0@gmail.com>
2024-01-22 02:04:53 +0530

    boxdroptrirotview: adding Renderer class

--- boxdroptrirotview/src/main/java/com/example/boxdroptrirotview/BoxDropTriRotView.kt
+++ boxdroptrirotview/src/main/java/com/example/boxdroptrirotview/BoxDropTriRotView.kt
@@ -187,4 +187,27 @@ class BoxDropTriRotView(ctx : Context) : View(ctx) {
             curr.startUpdating(cb)
         }
     }
+
+    data class Renderer(var view : BoxDropTriRotView) {
+
+        private val animator : Animator = Animator(view)
+        private val bdtr : BoxDropTriRot = BoxDropTriRot(0)
+        private val paint : Paint = Paint(Paint.ANTI_ALIAS_FLAG)
+
+        fun render(canvas : Canvas) {
+            canvas.drawColor(backColor)
+            bdtr.draw(canvas, paint)
+            animator.animate {
+                bdtr.update {
+                    animator.stop()
+                }
+            }
+        }
+
+        fun handleTap() {
+            bdtr.startUpdating {
+                animator.start()
+            }
+        }
+    }
 }
