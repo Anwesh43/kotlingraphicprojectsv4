70117415382847d6dc29bd7e414e70f5c7edebb1
Anwesh43 <anweshthecool0@gmail.com>
2024-01-22 01:52:38 +0530

    boxdroptrirotview: adding BoxDropTriRotView class

--- boxdroptrirotview/src/main/java/com/example/boxdroptrirotview/BoxDropTriRotView.kt
+++ boxdroptrirotview/src/main/java/com/example/boxdroptrirotview/BoxDropTriRotView.kt
@@ -60,3 +60,19 @@ fun Canvas.drawBDTRNode(i : Int, scale : Float, paint : Paint) {
     paint.strokeWidth = Math.min(w, h) / strokeFactor
     drawBoxDropTriRot(scale, w, h, paint)
 }
+
+class BoxDropTriRotView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
