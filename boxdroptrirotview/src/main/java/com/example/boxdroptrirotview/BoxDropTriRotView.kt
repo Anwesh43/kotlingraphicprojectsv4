3b76aabe0c200abf35fe17826383ac827fd452d0
Anwesh43 <anweshthecool0@gmail.com>
2024-01-22 01:57:49 +0530

    boxdroptrirotview: adding BDTRNode class

--- boxdroptrirotview/src/main/java/com/example/boxdroptrirotview/BoxDropTriRotView.kt
+++ boxdroptrirotview/src/main/java/com/example/boxdroptrirotview/BoxDropTriRotView.kt
@@ -123,4 +123,45 @@ class BoxDropTriRotView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class BDTRNode(var i : Int = 0, val state : State = State()) {
+
+        private var next : BDTRNode? = null
+        private var prev : BDTRNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < colors.size - 1) {
+                next = BDTRNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawBDTRNode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : BDTRNode {
+            var curr : BDTRNode? = prev
+            if (dir == 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
