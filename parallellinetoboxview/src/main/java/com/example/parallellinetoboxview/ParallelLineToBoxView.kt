96a49ba09dba0ca7aae5e0756bc989453b447309
Anwesh43 <anweshthecool0@gmail.com>
2024-01-23 00:48:56 +0530

    parallellinetoboxview: adding Canvas extension methods

--- parallellinetoboxview/src/main/java/com/example/parallellinetoboxview/ParallelLineToBoxView.kt
+++ parallellinetoboxview/src/main/java/com/example/parallellinetoboxview/ParallelLineToBoxView.kt
@@ -26,3 +26,40 @@ val backColor : Int = Color.parseColor("#BDBDBD")
 fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
+
+fun Canvas.drawXY(x : Float, y : Float, cb : () -> Unit) {
+    save()
+    translate(x, y)
+    cb()
+    restore()
+}
+
+fun Canvas.drawParallelLineToBox(scale : Float, w : Float, h : Float, paint : Paint) {
+    val size : Float = Math.min(w, h) / sizeFactor
+    val dsc : (Int) -> Float = {
+        scale.divideScale(it, parts)
+    }
+    drawXY(w / 2, h / 2 + (h / 2 + size / 2) * dsc(3)) {
+        rotate(rot * dsc(1))
+        for (j in 0..1) {
+            drawXY(0f, 0f) {
+                scale(1f - 2 * j, 1f - 2 * j)
+                drawXY(-size / 2, size / 8) {
+                    drawLine(0f, 0f, size * dsc(0), 0f, paint)
+                }
+                drawXY(size / 2, size / 8) {
+                    drawLine(0f, 0f, 0f, -size * 0.25f * dsc(2), paint)
+                }
+            }
+        }
+    }
+}
+
+fun Canvas.drawPLTBNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = Color.parseColor(colors[i])
+    paint.strokeCap = Paint.Cap.ROUND
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    drawParallelLineToBox(scale, w, h, paint)
+}
