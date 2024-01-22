c5a061e94511ad261829dd7981cf2e35aaa1d4c7
Anwesh43 <anweshthecool0@gmail.com>
2024-01-23 01:01:29 +0530

    parallellinetoboxview: adding PLTBNode class

--- parallellinetoboxview/src/main/java/com/example/parallellinetoboxview/ParallelLineToBoxView.kt
+++ parallellinetoboxview/src/main/java/com/example/parallellinetoboxview/ParallelLineToBoxView.kt
@@ -126,4 +126,45 @@ class ParallelLineToBoxView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class PLTBNode(var i : Int = 0, val state : State = State()) {
+
+        private var next : PLTBNode? = null
+        private var prev : PLTBNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < colors.size - 1) {
+                next = PLTBNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawPLTBNode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : PLTBNode {
+            var curr : PLTBNode? = prev
+            if (dir == 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
