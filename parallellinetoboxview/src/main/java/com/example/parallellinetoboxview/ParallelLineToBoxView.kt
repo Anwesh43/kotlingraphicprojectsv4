b425dd1efcb9fc61f12984a03ee82f50a2ab10b4
Anwesh43 <anweshthecool0@gmail.com>
2024-01-23 01:41:18 +0530

    parallellinetoboxview: adding Renderer class

--- parallellinetoboxview/src/main/java/com/example/parallellinetoboxview/ParallelLineToBoxView.kt
+++ parallellinetoboxview/src/main/java/com/example/parallellinetoboxview/ParallelLineToBoxView.kt
@@ -190,4 +190,27 @@ class ParallelLineToBoxView(ctx : Context) : View(ctx) {
             curr.startUpdating(cb)
         }
     }
+
+    data class Renderer(var view : ParallelLineToBoxView) {
+
+        private val animator : Animator = Animator(view)
+        private val pltb : ParallelLineToBox = ParallelLineToBox(0)
+        private val paint : Paint = Paint(Paint.ANTI_ALIAS_FLAG)
+
+        fun render(canvas : Canvas) {
+            canvas.drawColor(backColor)
+            pltb.draw(canvas, paint)
+            animator.animate {
+                pltb.update {
+                    animator.stop()
+                }
+            }
+        }
+
+        fun handleTap() {
+            pltb.startUpdating {
+                animator.start()
+            }
+        }
+    }
 }
