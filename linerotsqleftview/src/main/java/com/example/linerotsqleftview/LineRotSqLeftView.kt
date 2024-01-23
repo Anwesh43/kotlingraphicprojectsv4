2294866ca680f62ae4236036144a340c3566ca01
Anwesh43 <anweshthecool0@gmail.com>
2024-01-24 02:07:35 +0530

    linerotsqleftview: adding LRSLNode class

--- linerotsqleftview/src/main/java/com/example/linerotsqleftview/LineRotSqLeftView.kt
+++ linerotsqleftview/src/main/java/com/example/linerotsqleftview/LineRotSqLeftView.kt
@@ -122,4 +122,45 @@ class LineRotSqLeftView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class LRSLNode(var i : Int = 0, val state : State = State()) {
+
+        private var next : LRSLNode? = null
+        private var prev : LRSLNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < colors.size - 1) {
+                next = LRSLNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawLRSLNode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : LRSLNode {
+            var curr : LRSLNode? = prev
+            if (dir == 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
