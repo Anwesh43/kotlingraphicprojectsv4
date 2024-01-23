15bff6579e1d93af751693842f8fd1f4523a821d
Anwesh43 <anweshthecool0@gmail.com>
2024-01-24 02:04:39 +0530

    linerotsqleftview: adding Animator class

--- linerotsqleftview/src/main/java/com/example/linerotsqleftview/LineRotSqLeftView.kt
+++ linerotsqleftview/src/main/java/com/example/linerotsqleftview/LineRotSqLeftView.kt
@@ -94,4 +94,32 @@ class LineRotSqLeftView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class Animator(var view : View, var animated : Boolean = false) {
+
+        fun animate(cb : () -> Unit) {
+            if (animated) {
+                cb()
+                try {
+                    Thread.sleep(delay)
+                    view.invalidate()
+                } catch(ex : Exception) {
+
+                }
+            }
+        }
+
+        fun start() {
+            if (!animated) {
+                animated = true
+                view.postInvalidate()
+            }
+        }
+
+        fun stop() {
+            if (animated) {
+                animated = false
+            }
+        }
+    }
 }
