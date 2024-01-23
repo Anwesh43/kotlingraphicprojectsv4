468571828e534de8f7ac3084ba3ae41266056a9b
Anwesh43 <anweshthecool0@gmail.com>
2024-01-24 02:11:13 +0530

    linerotsqleftview: adding Renderer in View

--- linerotsqleftview/src/main/java/com/example/linerotsqleftview/LineRotSqLeftView.kt
+++ linerotsqleftview/src/main/java/com/example/linerotsqleftview/LineRotSqLeftView.kt
@@ -62,14 +62,16 @@ fun Canvas.drawLRSLNode(i : Int, scale : Float, paint : Paint) {
 
 class LineRotSqLeftView(ctx : Context) : View(ctx) {
 
-    override fun onDraw(canvas : Canvas) {
+    private val renderer : Renderer = Renderer(this)
 
+    override fun onDraw(canvas : Canvas) {
+        renderer.render(canvas)
     }
 
     override fun onTouchEvent(event : MotionEvent) : Boolean {
         when (event.action) {
             MotionEvent.ACTION_DOWN -> {
-
+                renderer.handleTap()
             }
         }
         return true
