a5389090f3b037b445fdf7f188679e1485bd419a
Anwesh43 <anweshthecool0@gmail.com>
2024-01-24 02:14:39 +0530

    linerotsqleftview: adding view in activity

--- app/src/main/java/com/example/android_kotlin_graphic_projectsv4/MainActivity.kt
+++ app/src/main/java/com/example/android_kotlin_graphic_projectsv4/MainActivity.kt
@@ -2,7 +2,7 @@ package com.example.android_kotlin_graphic_projectsv4
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.example.parallellinetoboxview.ParallelLineToBoxView
+import com.example.linerotsqleftview.LineRotSqLeftView
 
 
 class MainActivity : AppCompatActivity() {
@@ -10,6 +10,6 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         supportActionBar?.hide()
         //setContentView(R.layout.activity_main)
-        ParallelLineToBoxView.create(this)
+        LineRotSqLeftView.create(this)
     }
 }
