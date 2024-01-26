972b2fec06a8c6c6eb4e0a75031ac8d6807e5cd2
Anwesh43 <anweshthecool0@gmail.com>
2024-01-27 02:06:33 +0530

    linearccreaterightview: adding LineArcCrateRightView class

--- linearccreaterightview/src/main/java/com/example/linearccreaterightview/LineArcCreateRightView.kt
+++ linearccreaterightview/src/main/java/com/example/linearccreaterightview/LineArcCreateRightView.kt
@@ -60,3 +60,19 @@ fun Canvas.drawLACRNode(i : Int, scale : Float, paint : Paint) {
     paint.strokeCap = Paint.Cap.ROUND
     drawLineArcCreateRight(scale, w, h, paint)
 }
+
+class LineArcCreateRightView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
