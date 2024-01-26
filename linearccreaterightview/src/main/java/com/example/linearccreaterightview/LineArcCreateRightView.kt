5b994ca43e71a10ee0076a71f8f2ffd32fd95519
Anwesh43 <anweshthecool0@gmail.com>
2024-01-27 02:15:21 +0530

    linearccreaterightview: adding Renderer in view

--- linearccreaterightview/src/main/java/com/example/linearccreaterightview/LineArcCreateRightView.kt
+++ linearccreaterightview/src/main/java/com/example/linearccreaterightview/LineArcCreateRightView.kt
@@ -63,14 +63,16 @@ fun Canvas.drawLACRNode(i : Int, scale : Float, paint : Paint) {
 
 class LineArcCreateRightView(ctx : Context) : View(ctx) {
 
-    override fun onDraw(canvas : Canvas) {
+    private val renderer : Renderer = Renderer(this)
 
+    override fun onDraw(canvas : Canvas) {
+        renderer.render(canvas)
     }
 
     override fun onTouchEvent(event : MotionEvent) : Boolean {
         when (event.action) {
             MotionEvent.ACTION_DOWN -> {
-
+                renderer.handleTap()
             }
         }
         return true
