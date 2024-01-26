bdbd6ec317dfd923c90c4ee1f05545bd047e8b7b
Anwesh43 <anweshthecool0@gmail.com>
2024-01-27 02:05:24 +0530

    linearccreaterightview: adding Canvas extension methods

--- linearccreaterightview/src/main/java/com/example/linearccreaterightview/LineArcCreateRightView.kt
+++ linearccreaterightview/src/main/java/com/example/linearccreaterightview/LineArcCreateRightView.kt
@@ -28,3 +28,35 @@ val deg : Float = 45f
 fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
+
+fun Canvas.drawXY(x : Float, y : Float, cb : () -> Unit) {
+    save()
+    translate(x, y)
+    cb()
+    restore()
+}
+
+fun Canvas.drawLineArcCreateRight(scale : Float, w : Float, h : Float, paint : Paint) {
+    val size : Float = Math.min(w, h) / sizeFactor
+    val dsc : (Int) -> Float = {
+        scale.divideScale(it, parts)
+    }
+    drawXY(w / 2 + (w / 2) * dsc(3), h / 2) {
+        rotate(deg * dsc(2))
+        drawXY(0f, 0f) {
+            rotate(rot * dsc(1))
+            drawLine(0f, 0f, 0f, -size, paint)
+        }
+        drawArc(RectF(-size, -size, size, size), -rot * dsc(1), rot * dsc(1), false, paint)
+    }
+}
+
+fun Canvas.drawLACRNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = Color.parseColor(colors[i])
+    paint.style = Paint.Style.STROKE
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    paint.strokeCap = Paint.Cap.ROUND
+    drawLineArcCreateRight(scale, w, h, paint)
+}
