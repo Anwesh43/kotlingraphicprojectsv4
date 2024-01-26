1dcf72b412509b3da8540176819191c0dc10bfc1
Anwesh43 <anweshthecool0@gmail.com>
2024-01-26 12:22:08 +0530

    tlinesquarerightview: adding TLSRNode class

--- tlinesquarerightview/src/main/java/com/example/tlinesquarerightview/TLineSquareRightView.kt
+++ tlinesquarerightview/src/main/java/com/example/tlinesquarerightview/TLineSquareRightView.kt
@@ -125,4 +125,45 @@ class TLineSquareRightView(ctx : Context) {
             }
         }
     }
+
+    data class TLSRNode(var i : Int = 0, val state : State = State()) {
+
+        private var next : TLSRNode? = null
+        private var prev : TLSRNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < colors.size - 1) {
+                next = TLSRNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawTLSRNode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : TLSRNode {
+            var curr : TLSRNode? = prev
+            if (dir == 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
