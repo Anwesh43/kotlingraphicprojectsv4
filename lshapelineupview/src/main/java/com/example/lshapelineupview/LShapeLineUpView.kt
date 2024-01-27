9b63e47e6e4a148e482b8da85304b25236593c67
Anwesh43 <anweshthecool0@gmail.com>
2024-01-28 02:42:08 +0530

    lshapelineupview: adding LShapeLineUpView class

--- lshapelineupview/src/main/java/com/example/lshapelineupview/LShapeLineUpView.kt
+++ lshapelineupview/src/main/java/com/example/lshapelineupview/LShapeLineUpView.kt
@@ -58,3 +58,19 @@ fun Canvas.drawLSLUNode(i : Int, scale : Float, paint : Paint) {
     paint.strokeCap = Paint.Cap.ROUND
     drawLShapeLineUp(scale, w, h, paint)
 }
+
+class LShapeLineUpView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
