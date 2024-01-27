679e1a88f3d9e60dd69d2589106f21c8074f143e
Anwesh43 <anweshthecool0@gmail.com>
2024-01-28 02:56:03 +0530

    lshapelineupview: adding Renderer class

--- lshapelineupview/src/main/java/com/example/lshapelineupview/LShapeLineUpView.kt
+++ lshapelineupview/src/main/java/com/example/lshapelineupview/LShapeLineUpView.kt
@@ -185,4 +185,27 @@ class LShapeLineUpView(ctx : Context) : View(ctx) {
             curr.startUpdating(cb)
         }
     }
+
+    data class Renderer(var view : LShapeLineUpView) {
+
+        private val animator : Animator = Animator(view)
+        private val lslu : LineShapeLineUp = LineShapeLineUp(0)
+        private val paint : Paint = Paint(Paint.ANTI_ALIAS_FLAG)
+
+        fun render(canvas : Canvas) {
+            canvas.drawColor(backColor)
+            lslu.draw(canvas, paint)
+            animator.animate {
+                lslu.update {
+                    animator.stop()
+                }
+            }
+        }
+
+        fun handleTap() {
+            lslu.startUpdating {
+                animator.start()
+            }
+        }
+    }
 }
