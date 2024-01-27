3efb6875cbe1a2f742716d646289e5ec628a7713
Anwesh43 <anweshthecool0@gmail.com>
2024-01-28 02:50:52 +0530

    lshapelineupview: adding LSLUNode class

--- lshapelineupview/src/main/java/com/example/lshapelineupview/LShapeLineUpView.kt
+++ lshapelineupview/src/main/java/com/example/lshapelineupview/LShapeLineUpView.kt
@@ -121,4 +121,45 @@ class LShapeLineUpView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class LSLUNode(var i : Int = 0, val state : State = State()) {
+
+        private var next : LSLUNode? = null
+        private var prev : LSLUNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < colors.size - 1) {
+                next = LSLUNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawLSLUNode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : LSLUNode {
+            var curr : LSLUNode? = prev
+            if (dir == 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
