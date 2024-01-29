a2d776031b539fa5c87eeb86f2a4775be08a942f
Anwesh43 <anweshthecool0@gmail.com>
2024-01-30 00:56:03 +0530

    arcbreaklinerightview: adding ArcBreakLineRightView class

--- arcbreaklinerightview/src/main/java/com/example/arcbreaklinerightview/ArcBreakLineRightView.kt
+++ arcbreaklinerightview/src/main/java/com/example/arcbreaklinerightview/ArcBreakLineRightView.kt
@@ -65,3 +65,19 @@ fun Canvas.drawABLRNode(i : Int, scale : Float, paint : Paint) {
     paint.strokeWidth = Math.min(w, h) / strokeFactor
     drawArcBreakLineRight(scale, w, h, paint)
 }
+
+class ArcBreakLineRightView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
