a37fa41da6259e48b1b0685892c91536820d6579
Anwesh43 <anweshthecool0@gmail.com>
2024-01-30 00:53:40 +0530

    arcbreaklinerightview: adding Canvas extension methods

--- arcbreaklinerightview/src/main/java/com/example/arcbreaklinerightview/ArcBreakLineRightView.kt
+++ arcbreaklinerightview/src/main/java/com/example/arcbreaklinerightview/ArcBreakLineRightView.kt
@@ -30,3 +30,38 @@ val rot : Float = -90f
 fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
+
+fun Canvas.drawXY(x : Float, y : Float, cb : () -> Unit) {
+    save()
+    translate(x, y)
+    cb()
+    restore()
+}
+
+fun Canvas.drawArcBreakLineRight(scale : Float, w : Float, h : Float, paint : Paint) {
+    val size : Float = Math.min(w, h) / sizeFactor
+    val dsc : (Int) -> Float = {
+        scale.divideScale(it, parts)
+    }
+    drawXY(w / 2 + (w / 2 + size) * dsc(3), h / 2) {
+        for (j in 0..1) {
+            drawXY(w * 0.5f * (1 - dsc(1)), 0f) {
+                scale(1f, 1f - 2 * j)
+                rotate(rot * dsc(2))
+                drawArc(RectF(0f, -size / 2, size, size / 2), 180f, 180f, true, paint)
+            }
+        }
+        drawXY(-w * 0.5f * (1 - dsc(0)), 0f) {
+            drawLine(0f, 0f, -size, 0f, paint)
+        }
+    }
+}
+
+fun Canvas.drawABLRNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = Color.parseColor(colors[i])
+    paint.strokeCap = Paint.Cap.ROUND
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    drawArcBreakLineRight(scale, w, h, paint)
+}
