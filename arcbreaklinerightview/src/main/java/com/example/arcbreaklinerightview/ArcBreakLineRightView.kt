fe43c8715c0fbe202925abdfceba9115293f3a70
Anwesh43 <anweshthecool0@gmail.com>
2024-01-30 01:10:11 +0530

    arcbreaklinerightview: adding Renderer class

--- arcbreaklinerightview/src/main/java/com/example/arcbreaklinerightview/ArcBreakLineRightView.kt
+++ arcbreaklinerightview/src/main/java/com/example/arcbreaklinerightview/ArcBreakLineRightView.kt
@@ -192,4 +192,27 @@ class ArcBreakLineRightView(ctx : Context) : View(ctx) {
             curr.startUpdating(cb)
         }
     }
+
+    data class Renderer(var view : ArcBreakLineRightView) {
+
+        private val animator : Animator = Animator(view)
+        private val ablr : ArcBreakLineRight = ArcBreakLineRight(0)
+        private val paint : Paint = Paint(Paint.ANTI_ALIAS_FLAG)
+
+        fun render(canvas : Canvas) {
+            canvas.drawColor(backColor)
+            ablr.draw(canvas, paint)
+            animator.animate {
+                ablr.update {
+                    animator.stop()
+                }
+            }
+        }
+
+        fun handleTap() {
+            ablr.startUpdating {
+                animator.start()
+            }
+        }
+    }
 }
