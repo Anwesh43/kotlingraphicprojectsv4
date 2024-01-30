de659b3e969e68a2f4d0229e63383415c3f00675
Anwesh43 <anweshthecool0@gmail.com>
2024-01-31 01:00:17 +0530

    linebentthendownview: adding Canvas extension methods

--- linebentthendownview/src/main/java/com/example/linebentthendownview/LineBentThenDownView.kt
+++ linebentthendownview/src/main/java/com/example/linebentthendownview/LineBentThenDownView.kt
@@ -26,3 +26,35 @@ val backColor : Int = Color.parseColor("#BDBDBD")
 fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
+
+fun Canvas.drawXY(x : Float, y : Float, cb : () -> Unit) {
+    save()
+    translate(x, y)
+    cb()
+    restore()
+}
+
+fun Canvas.drawLineBentThenDown(scale : Float, w : Float, h : Float, paint : Paint) {
+    val size : Float = Math.min(w, h) / sizeFactor
+    val dsc : (Int) -> Float = {
+        scale.divideScale(it, parts)
+    }
+    drawXY(w / 2, h / 2 + (h / 2) * dsc(3)) {
+        drawXY(0f, 0f) {
+            rotate(rot * dsc(1))
+            drawLine(0f, 0f, 0f, size * dsc(0), paint)
+        }
+        drawXY(0f, h * 0.5f * (1 - dsc(2))) {
+            drawLine(0f, 0f, 0f, size, paint)
+        }
+    }
+}
+
+fun Canvas.drawLBTDNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = Color.parseColor(colors[i])
+    paint.strokeCap = Paint.Cap.ROUND
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    drawLineBentThenDown(scale, w, h, paint)
+}
