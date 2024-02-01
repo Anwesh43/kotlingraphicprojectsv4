24e62bca64b807c366b612f50fedbe5eb0d68184
Anwesh43 <anweshthecool0@gmail.com>
2024-02-01 12:40:58 +0530

    sqsemicircleview: adding SSCLNode class

--- sqsemicircleleftview/src/main/java/com/example/sqsemicircleleftview/SqSemiCircleLeftView.kt
+++ sqsemicircleleftview/src/main/java/com/example/sqsemicircleleftview/SqSemiCircleLeftView.kt
@@ -121,4 +121,45 @@ class SqSemiCircleLeftView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class SSCLNode(var i : Int = 0, val state : State = State()) {
+
+        private var next : SSCLNode? = null
+        private var prev : SSCLNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < colors.size - 1) {
+                next = SSCLNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawSSCLNode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : SSCLNode {
+            var curr : SSCLNode? = prev
+            if (dir == 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
