4d88a131630ab3d56ea5ee6a44007de717fc1de6
Anwesh43 <anweshthecool0@gmail.com>
2024-02-01 12:54:27 +0530

    sqsemicircleview: adding fixes

--- sqsemicircleleftview/src/main/java/com/example/sqsemicircleleftview/SqSemiCircleLeftView.kt
+++ sqsemicircleleftview/src/main/java/com/example/sqsemicircleleftview/SqSemiCircleLeftView.kt
@@ -40,12 +40,12 @@ fun Canvas.drawSqSemiCircleLeft(scale : Float, w : Float, h : Float, paint : Pai
     val dsc : (Int) -> Float = {
         scale.divideScale(it, parts)
     }
-    drawXY(w / 2, h / 2) {
+    drawXY(w / 2 - (w / 2) * dsc(3), h / 2) {
         drawXY((w/ 2) * (1 - dsc(0)), 0f) {
             rotate(rot * dsc(2))
             drawRect(RectF(0f, 0f, size, size), paint)
         }
-        drawXY(0f, h * 0.5f * (1 - dsc(1))) {
+        drawXY(0f, -h * 0.5f * (1 - dsc(1))) {
             rotate(-rot * dsc(2))
             drawArc(RectF(0f, -size / 2, size, size / 2), 180f, 180f, true, paint)
         }
