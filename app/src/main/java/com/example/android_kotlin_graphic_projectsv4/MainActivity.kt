409d2881519ad80a30789fd294aa90b174ebcf62
Anwesh43 <anweshthecool0@gmail.com>
2024-02-03 13:35:14 +0530

    linealternatesqdownview: adding view in activity

--- app/src/main/java/com/example/android_kotlin_graphic_projectsv4/MainActivity.kt
+++ app/src/main/java/com/example/android_kotlin_graphic_projectsv4/MainActivity.kt
@@ -2,14 +2,13 @@ package com.example.android_kotlin_graphic_projectsv4
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.example.linehalfarcalternateview.LineHalfArcAlternateView
-
+import com.example.linealternatesqdownview.LineAlternateSqDownView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         supportActionBar?.hide()
         //setContentView(R.layout.activity_main)
-        LineHalfArcAlternateView.create(this)
+        LineAlternateSqDownView.create(this)
     }
 }
