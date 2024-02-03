eff4b27016ffa87a44d1c39c2f062539308df982
Anwesh43 <anweshthecool0@gmail.com>
2024-02-03 13:02:32 +0530

    linealternatesqdownview: adding Canvas extension methods

--- linealternatesqdownview/src/main/java/com/example/linealternatesqdownview/LineAlternateSqDownView.kt
+++ linealternatesqdownview/src/main/java/com/example/linealternatesqdownview/LineAlternateSqDownView.kt
@@ -22,8 +22,45 @@ val strokeFactor : Float = 90f
 val sizeFactor : Float = 4.9f
 val delay : Long = 20
 val backColor : Int = Color.parseColor("#BDBDBD")
-val rot : Float = 90f
+val rot : Float = 180f
 
 fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
+
+fun Canvas.drawXY(x : Float, y : Float, cb : () -> Unit) {
+    save()
+    translate(x, y)
+    cb()
+    restore()
+}
+
+fun Canvas.drawLineAlternateSqDown(scale : Float, w : Float, h : Float, paint : Paint) {
+    val size : Float = Math.min(w, h) / sizeFactor
+    val dsc : (Int) -> Float = {
+        scale.divideScale(it, parts)
+    }
+    drawXY(w / 2, h / 2 + (h / 2) * dsc(3)) {
+        rotate(rot * dsc(2))
+        for (j in 0..1) {
+            drawXY(0f, 0f) {
+                scale(1f - 2 * j, 1f)
+                drawXY((w / 2) * (1 - dsc(0)), 0f) {
+                    drawLine(0f, 0f, size / 2, 0f, paint)
+                }
+            }
+        }
+        drawXY(0f, -h * 0.5f * (1 - dsc(1))) {
+            drawRect(RectF(-size / 2, -size, size / 2, 0f), paint)
+        }
+    }
+}
+
+fun Canvas.drawLASDNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = Color.parseColor(colors[i])
+    paint.strokeCap = Paint.Cap.ROUND
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    drawLineAlternateSqDown(scale, w, h, paint)
+}
