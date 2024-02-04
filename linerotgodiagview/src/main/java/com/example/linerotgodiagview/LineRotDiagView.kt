63150618a57da7990af3adfee37970b90547ab17
Anwesh43 <anweshthecool0@gmail.com>
2024-02-04 21:58:42 +0530

    linerotdiagview: adding Canvas extension methods

--- linerotgodiagview/src/main/java/com/example/linerotgodiagview/LineRotDiagView.kt
+++ linerotgodiagview/src/main/java/com/example/linerotgodiagview/LineRotDiagView.kt
@@ -27,3 +27,37 @@ val rot : Float = 45f
 fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
+
+fun Canvas.drawXY(x : Float, y : Float, cb : () -> Unit) {
+    save()
+    translate(x, y)
+    cb()
+    restore()
+}
+
+fun Canvas.drawLineRotDiag(scale : Float, w : Float, h : Float, paint : Paint) {
+    val size : Float = Math.min(w, h) / sizeFactor
+    val dsc : (Int) -> Float = {
+        scale.divideScale(it, parts)
+    }
+    drawXY(w / 2, h / 2) {
+        for (j in 0..1) {
+            drawXY(0f, 0f) {
+                scale(1f - 2 * j, 1f)
+                drawXY((w / 2) * (1 - dsc(0)) + (w / 2) * dsc(j + 2), -h * 0.5f * dsc(j + 2)) {
+                    rotate(-rot * dsc(1))
+                    drawLine(0f, 0f, size, 0f, paint)
+                }
+            }
+        }
+    }
+}
+
+fun Canvas.drawLRGDNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = Color.parseColor(colors[i])
+    paint.strokeCap = Paint.Cap.ROUND
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    drawLineRotDiag(scale, w, h, paint)
+}
