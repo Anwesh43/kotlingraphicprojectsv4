74fa2c062d4d5e8cb29a56b6cba35d4a893feeb1
Anwesh43 <anweshthecool0@gmail.com>
2024-02-05 00:12:03 +0530

    linerotdiagview: adding view in activity

--- app/src/main/java/com/example/android_kotlin_graphic_projectsv4/MainActivity.kt
+++ app/src/main/java/com/example/android_kotlin_graphic_projectsv4/MainActivity.kt
@@ -2,13 +2,13 @@ package com.example.android_kotlin_graphic_projectsv4
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.example.linealternatesqdownview.LineAlternateSqDownView
+import com.example.linerotgodiagview.LineRotDiagView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         supportActionBar?.hide()
         //setContentView(R.layout.activity_main)
-        LineAlternateSqDownView.create(this)
+        LineRotDiagView.create(this)
     }
 }
