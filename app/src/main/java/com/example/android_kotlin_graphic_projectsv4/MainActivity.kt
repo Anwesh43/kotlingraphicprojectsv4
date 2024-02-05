556dac2e8f5c1ad409aef7c490c73ea8fc4d6ade
Anwesh43 <anweshthecool0@gmail.com>
2024-02-06 00:29:02 +0530

    linetolshapeleftview: adding view in activity

--- app/src/main/java/com/example/android_kotlin_graphic_projectsv4/MainActivity.kt
+++ app/src/main/java/com/example/android_kotlin_graphic_projectsv4/MainActivity.kt
@@ -2,13 +2,13 @@ package com.example.android_kotlin_graphic_projectsv4
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.example.linerotgodiagview.LineRotDiagView
+import com.example.linetolshapeleftview.LineToLShapeLeftView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         supportActionBar?.hide()
         //setContentView(R.layout.activity_main)
-        LineRotDiagView.create(this)
+        LineToLShapeLeftView.create(this)
     }
 }
