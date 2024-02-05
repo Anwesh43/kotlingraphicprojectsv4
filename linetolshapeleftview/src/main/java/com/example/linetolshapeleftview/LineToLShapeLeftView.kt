eb1339e697cf3dee5764a771192a89805f4aace1
Anwesh43 <anweshthecool0@gmail.com>
2024-02-06 00:25:29 +0530

    linetolshapeleftview: adding LineToLShapeLeft class

--- linetolshapeleftview/src/main/java/com/example/linetolshapeleftview/LineToLShapeLeftView.kt
+++ linetolshapeleftview/src/main/java/com/example/linetolshapeleftview/LineToLShapeLeftView.kt
@@ -159,7 +159,29 @@ class LineToLShapeLeftView(ctx : Context) : View(ctx) {
             }
             cb()
             return this
+        }
+    }
 
+    data class LineToLShapeLeft(var i : Int) {
+
+        private var curr : LTLSLNode = LTLSLNode(0)
+        private var dir : Int = 1
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            curr.draw(canvas, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            curr.update {
+                curr = curr.getNext(dir) {
+                    dir *= -1
+                }
+                cb(it)
+            }
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            curr.startUpdating(cb)
         }
     }
 }
