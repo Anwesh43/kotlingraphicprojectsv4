ea24e96a5d9dfdf3b5b06702ebda7bae8d571a28
Anwesh43 <anweshthecool0@gmail.com>
2024-02-06 00:23:58 +0530

    linetolshapeleftview: adding LTLSLNode class

--- linetolshapeleftview/src/main/java/com/example/linetolshapeleftview/LineToLShapeLeftView.kt
+++ linetolshapeleftview/src/main/java/com/example/linetolshapeleftview/LineToLShapeLeftView.kt
@@ -120,4 +120,46 @@ class LineToLShapeLeftView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class LTLSLNode(var i : Int = 0, val state : State = State()) {
+
+        private var prev : LTLSLNode? = null
+        private var next : LTLSLNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < colors.size - 1) {
+                next = LTLSLNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawLTLSLNode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : LTLSLNode {
+            var curr : LTLSLNode? = prev
+            if (dir == 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+
+        }
+    }
 }
