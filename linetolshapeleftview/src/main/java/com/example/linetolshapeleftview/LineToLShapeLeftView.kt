33bb12fb519fd1728fe6e1ba3b0b1ac55d154e3f
Anwesh43 <anweshthecool0@gmail.com>
2024-02-06 00:16:27 +0530

    linetolshapeleftview: adding Canvas extension methods

--- linetolshapeleftview/src/main/java/com/example/linetolshapeleftview/LineToLShapeLeftView.kt
+++ linetolshapeleftview/src/main/java/com/example/linetolshapeleftview/LineToLShapeLeftView.kt
@@ -26,3 +26,34 @@ val backColor : Int = Color.parseColor("#BDBDBD")
 fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
+
+fun Canvas.drawXY(x : Float, y : Float, cb : () -> Unit) {
+    save()
+    translate(x, y)
+    cb()
+    restore()
+}
+
+fun Canvas.drawLineToLShapeLeft(scale : Float, w : Float, h : Float, paint : Paint) {
+    val size : Float = Math.min(w, h) / sizeFactor
+    val dsc : (Int) -> Float = {
+        scale.divideScale(it, parts)
+    }
+    drawXY(w / 2 - (w / 2) * dsc(3), h / 2) {
+        for (j in 0..1) {
+            drawXY(0f, 0f) {
+                rotate(rot * (j + 1) * dsc(2 - j))
+                drawLine(0f, 0f, size * dsc(0), 0f, paint)
+            }
+        }
+    }
+}
+
+fun Canvas.drawLTLSLNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = Color.parseColor(colors[i])
+    paint.strokeCap = Paint.Cap.ROUND
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    drawLineToLShapeLeft(scale, w, h, paint)
+}
