40d313300ba67f1cd8bd26d9f09a1f9b5351ad01
Anwesh43 <anweshthecool0@gmail.com>
2024-02-06 00:17:38 +0530

    linetolshapeleftview: adding LineToLShapeLeftView class

--- linetolshapeleftview/src/main/java/com/example/linetolshapeleftview/LineToLShapeLeftView.kt
+++ linetolshapeleftview/src/main/java/com/example/linetolshapeleftview/LineToLShapeLeftView.kt
@@ -57,3 +57,19 @@ fun Canvas.drawLTLSLNode(i : Int, scale : Float, paint : Paint) {
     paint.strokeWidth = Math.min(w, h) / strokeFactor
     drawLineToLShapeLeft(scale, w, h, paint)
 }
+
+class LineToLShapeLeftView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
