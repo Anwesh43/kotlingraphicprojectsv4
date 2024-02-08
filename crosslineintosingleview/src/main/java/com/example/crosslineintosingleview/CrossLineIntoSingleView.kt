5ad173cfc955200ce736ca449a1ccc9642e9fc4d
Anwesh43 <anweshthecool0@gmail.com>
2024-02-09 00:42:37 +0530

    crosslineintosingleview: adding fixes

--- crosslineintosingleview/src/main/java/com/example/crosslineintosingleview/CrossLineIntoSingleView.kt
+++ crosslineintosingleview/src/main/java/com/example/crosslineintosingleview/CrossLineIntoSingleView.kt
@@ -39,10 +39,10 @@ fun Canvas.drawCrossLineIntoSingle(scale : Float, w : Float, h : Float, paint : 
     val dsc : (Int) -> Float = {
         scale.divideScale(it, parts)
     }
-    drawXY(w / 2, h / 2) {
+    drawXY(w / 2, h / 2 + (h / 2 + size) * dsc(3)) {
         for (j in 0..1) {
             drawXY(0f, 0f) {
-                rotate(rot * (1f - 2 * j) * (1 - dsc(1)))
+                rotate(rot * (1f - 2 * j) * (1 - dsc(2)))
                 drawLine(0f, -size * 0.5f * dsc(j), 0f, size * 0.5f * dsc(j), paint)
             }
         }
