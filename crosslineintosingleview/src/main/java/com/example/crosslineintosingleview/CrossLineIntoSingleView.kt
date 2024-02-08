d648ba9b5b3a8d494fb55832e6e8ed26dfa81565
Anwesh43 <anweshthecool0@gmail.com>
2024-02-09 00:27:29 +0530

    crosslineintosingleview: adding CrossLineIntoSingleView class

--- crosslineintosingleview/src/main/java/com/example/crosslineintosingleview/CrossLineIntoSingleView.kt
+++ crosslineintosingleview/src/main/java/com/example/crosslineintosingleview/CrossLineIntoSingleView.kt
@@ -57,3 +57,19 @@ fun Canvas.drawLTSLNode(i : Int, scale : Float, paint : Paint) {
     paint.strokeWidth = Math.min(w, h) / strokeFactor
     drawCrossLineIntoSingle(scale, w, h, paint)
 }
+
+class CrossLineIntoSingleView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
