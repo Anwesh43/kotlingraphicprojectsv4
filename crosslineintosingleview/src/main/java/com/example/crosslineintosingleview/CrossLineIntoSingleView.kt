ea1664bef22337bc2fece8a6f573372499907ae7
Anwesh43 <anweshthecool0@gmail.com>
2024-02-09 00:26:23 +0530

    crosslineintosingleview: adding Canvas extension methods

--- crosslineintosingleview/src/main/java/com/example/crosslineintosingleview/CrossLineIntoSingleView.kt
+++ crosslineintosingleview/src/main/java/com/example/crosslineintosingleview/CrossLineIntoSingleView.kt
@@ -26,3 +26,34 @@ val rot : Float = 45f
 fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
+
+fun Canvas.drawXY(x : Float, y : Float, cb : () -> Unit) {
+    save()
+    translate(x, y)
+    cb()
+    restore()
+}
+
+fun Canvas.drawCrossLineIntoSingle(scale : Float, w : Float, h : Float, paint : Paint) {
+    val size : Float = Math.min(w, h) / sizeFactor
+    val dsc : (Int) -> Float = {
+        scale.divideScale(it, parts)
+    }
+    drawXY(w / 2, h / 2) {
+        for (j in 0..1) {
+            drawXY(0f, 0f) {
+                rotate(rot * (1f - 2 * j) * (1 - dsc(1)))
+                drawLine(0f, -size * 0.5f * dsc(j), 0f, size * 0.5f * dsc(j), paint)
+            }
+        }
+    }
+}
+
+fun Canvas.drawLTSLNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = Color.parseColor(colors[i])
+    paint.strokeCap = Paint.Cap.ROUND
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    drawCrossLineIntoSingle(scale, w, h, paint)
+}
