c2355e4c0149745da139de8901d706a17509bb8f
Anwesh43 <anweshthecool0@gmail.com>
2024-02-10 13:55:18 +0530

    linebarallternaterotview: adding Canvas extension methods

--- linebarallternaterotview/src/main/java/com/example/linebarallternaterotview/LineBarAlternateRotView.kt
+++ linebarallternaterotview/src/main/java/com/example/linebarallternaterotview/LineBarAlternateRotView.kt
@@ -27,3 +27,44 @@ val rot : Float = 45f
 fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
+
+fun Canvas.drawXY(x : Float, y : Float, cb : () -> Unit) {
+    save()
+    translate(x, y)
+    cb()
+    restore()
+}
+
+fun Canvas.drawLineBarRot(scale : Float, w : Float, h : Float, paint : Paint) {
+    val dsc : (Int) -> Float = {
+        scale.divideScale(it, parts)
+    }
+    val size : Float = Math.min(w, h) / sizeFactor
+    drawXY(0f, h * 0.5f * dsc(3)) {
+        rotate(-rot * dsc(1))
+        drawLine(0f, 0f, 0f, size * dsc(0), paint)
+    }
+    drawXY(0f, -h * 0.5f * dsc(3)) {
+        rotate(rot * dsc(2))
+        drawRect(RectF(0f, 0f, size * 0.5f, -size * dsc(0)), paint)
+    }
+}
+fun Canvas.drawLineBarAlternateRot(scale : Float, w : Float, h : Float, paint : Paint) {
+    drawXY(w / 2, h / 2) {
+        for (j in 0..1) {
+            drawXY(0f, 0f) {
+                scale(1f - 2 * j, 1f)
+                drawLineBarRot(scale, w, h, paint)
+            }
+        }
+    }
+}
+
+fun Canvas.drawLBARNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = Color.parseColor(colors[i])
+    paint.strokeCap = Paint.Cap.ROUND
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    drawLineBarAlternateRot(scale, w, h, paint)
+}
