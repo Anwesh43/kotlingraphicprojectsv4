74bdd18ba3498df7802d059d08f5f7f42b69465e
Anwesh43 <anweshthecool0@gmail.com>
2024-02-10 14:12:21 +0530

    linebarallternaterotview: adding LineBarAlternateRotView class

--- linebarallternaterotview/src/main/java/com/example/linebarallternaterotview/LineBarAlternateRotView.kt
+++ linebarallternaterotview/src/main/java/com/example/linebarallternaterotview/LineBarAlternateRotView.kt
@@ -68,3 +68,19 @@ fun Canvas.drawLBARNode(i : Int, scale : Float, paint : Paint) {
     paint.strokeWidth = Math.min(w, h) / strokeFactor
     drawLineBarAlternateRot(scale, w, h, paint)
 }
+
+class LineBarAlternateRotView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
