6543c681cacbda67bebbe2698e0d580ada42c772
Anwesh43 <anweshthecool0@gmail.com>
2024-02-11 00:18:13 +0530

    linebeakerupview: adding LineBeakerUpView class

--- linebeakerupview/src/main/java/com/example/linebeakerupview/LineBeakerUpView.kt
+++ linebeakerupview/src/main/java/com/example/linebeakerupview/LineBeakerUpView.kt
@@ -60,3 +60,19 @@ fun Canvas.drawLBUNode(i : Int, scale : Float, paint : Paint) {
     paint.strokeCap = Paint.Cap.ROUND
     drawLineBeakerUp(scale, w, h, paint)
 }
+
+class LineBeakerUpView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
