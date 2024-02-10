bf3ff2f8541a1beaedd76d89e8cd67bb4ab6f0b1
Anwesh43 <anweshthecool0@gmail.com>
2024-02-11 00:17:04 +0530

    linebeakerupview: adding Canvas extension methods

--- linebeakerupview/src/main/java/com/example/linebeakerupview/LineBeakerUpView.kt
+++ linebeakerupview/src/main/java/com/example/linebeakerupview/LineBeakerUpView.kt
@@ -26,3 +26,37 @@ val rot : Float = 180f
 fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
+
+fun Canvas.drawXY(x : Float, y : Float, cb : () -> Unit) {
+    save()
+    translate(x, y)
+    cb()
+    restore()
+}
+
+fun Canvas.drawLineBeakerUp(scale : Float, w : Float, h : Float, paint : Paint) {
+    val size : Float = Math.min(w, h) / strokeFactor
+    val dsc : (Int) -> Float = {
+        scale.divideScale(it, parts)
+    }
+    drawXY(w / 2, h / 2 - (h / 2) * dsc(3)) {
+        rotate(rot * dsc(2))
+        drawXY((w /2 + size / 2) * (1 - dsc(0)), 0f) {
+            drawLine(-size / 2, 0f, size / 2, 0f, paint)
+        }
+        for (j in 0..1) {
+            drawXY(-size / 2 + size * j, h * 0.5f * (1 - dsc(1))) {
+                drawLine(0f, 0f, 0f, size, paint)
+            }
+        }
+    }
+}
+
+fun Canvas.drawLBUNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = Color.parseColor(colors[i])
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    paint.strokeCap = Paint.Cap.ROUND
+    drawLineBeakerUp(scale, w, h, paint)
+}
