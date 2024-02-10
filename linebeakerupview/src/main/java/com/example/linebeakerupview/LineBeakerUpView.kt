80ab48de5978cda9bf38698cc12aa5473f832adc
Anwesh43 <anweshthecool0@gmail.com>
2024-02-11 00:39:11 +0530

    linebeakerupview: adding LBUNode class

--- linebeakerupview/src/main/java/com/example/linebeakerupview/LineBeakerUpView.kt
+++ linebeakerupview/src/main/java/com/example/linebeakerupview/LineBeakerUpView.kt
@@ -123,4 +123,45 @@ class LineBeakerUpView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class LBUNode(var i : Int = 0, val state : State = State()) {
+
+        private var next : LBUNode? = null
+        private var prev : LBUNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < colors.size - 1) {
+                next = LBUNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawLBUNode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : LBUNode {
+            var curr : LBUNode? = prev
+            if (dir == 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
