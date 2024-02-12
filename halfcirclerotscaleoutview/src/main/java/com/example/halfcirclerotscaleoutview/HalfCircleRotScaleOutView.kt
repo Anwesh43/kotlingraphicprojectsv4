41220c356eb721d9ad911462a994d59dd6cf14a8
Anwesh43 <anweshthecool0@gmail.com>
2024-02-12 13:53:17 +0530

    halfcirclerotscaleoutview: adding HCRSONode class

--- halfcirclerotscaleoutview/src/main/java/com/example/halfcirclerotscaleoutview/HalfCircleRotScaleOutView.kt
+++ halfcirclerotscaleoutview/src/main/java/com/example/halfcirclerotscaleoutview/HalfCircleRotScaleOutView.kt
@@ -121,4 +121,45 @@ class HalfCircleRotScaleOutView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class HCRSONode(var i : Int = 0, val state : State = State()) {
+
+        private var next : HCRSONode? = null
+        private var prev : HCRSONode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < colors.size - 1) {
+                next = HCRSONode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawHCRSONode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : HCRSONode {
+            var curr : HCRSONode? = prev
+            if (dir == 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
