6f9ba831c4faaa8902bd55c93b5ed0515249fafd
Anwesh43 <anweshthecool0@gmail.com>
2024-02-12 16:40:01 +0530

    halfcirclerotscaleoutview: adding view in activity

--- app/src/main/java/com/example/android_kotlin_graphic_projectsv4/MainActivity.kt
+++ app/src/main/java/com/example/android_kotlin_graphic_projectsv4/MainActivity.kt
@@ -2,13 +2,14 @@ package com.example.android_kotlin_graphic_projectsv4
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.example.linebeakerupview.LineBeakerUpView
+import com.example.halfcirclerotscaleoutview.HalfCircleRotScaleOutView
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         supportActionBar?.hide()
         //setContentView(R.layout.activity_main)
-        LineBeakerUpView.create(this)
+        HalfCircleRotScaleOutView.create(this)
     }
 }
