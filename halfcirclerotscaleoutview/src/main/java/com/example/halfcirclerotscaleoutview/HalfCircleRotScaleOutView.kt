baf6540e7365173d58a6327c63ef7cd3e155d4e8
Anwesh43 <anweshthecool0@gmail.com>
2024-02-12 16:35:50 +0530

    halfcirclerotscaleoutview: adding Renderer class

--- halfcirclerotscaleoutview/src/main/java/com/example/halfcirclerotscaleoutview/HalfCircleRotScaleOutView.kt
+++ halfcirclerotscaleoutview/src/main/java/com/example/halfcirclerotscaleoutview/HalfCircleRotScaleOutView.kt
@@ -185,4 +185,27 @@ class HalfCircleRotScaleOutView(ctx : Context) : View(ctx) {
             curr.startUpdating(cb)
         }
     }
+
+    data class Renderer(var view : HalfCircleRotScaleOutView) {
+
+        private val animator : Animator = Animator(view)
+        private val hcrso : HalfCircleRotScaleOut = HalfCircleRotScaleOut(0)
+        private val paint : Paint = Paint(Paint.ANTI_ALIAS_FLAG)
+
+        fun render(canvas : Canvas) {
+            canvas.drawColor(backColor)
+            hcrso.draw(canvas, paint)
+            animator.animate {
+                hcrso.update {
+                    animator.stop()
+                }
+            }
+        }
+
+        fun handleTap() {
+            hcrso.startUpdating {
+                animator.start()
+            }
+        }
+    }
 }
