b9f80aaa7c314287168960ca2abdd1bb364d5b1d
Anwesh43 <anweshthecool0@gmail.com>
2024-02-16 01:49:25 +0530

    linerotsqrigthview: adding LRSRNode class

--- linerotsqrightview/src/main/java/com/example/linerotsqrightview/LineRotSqRightView.kt
+++ linerotsqrightview/src/main/java/com/example/linerotsqrightview/LineRotSqRightView.kt
@@ -122,4 +122,45 @@ class LineRotSqRightView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class LRSRNode(var i : Int = 0, val state : State = State()) {
+
+        private var next : LRSRNode? = null
+        private var prev : LRSRNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < colors.size - 1) {
+                next = LRSRNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawLRSSNode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : LRSRNode {
+            var curr : LRSRNode? = prev
+            if (dir == 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
