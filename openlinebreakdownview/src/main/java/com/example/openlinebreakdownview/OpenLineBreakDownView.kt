01794559a7490b525c40d1c763acd40f77effc0b
Anwesh43 <anweshthecool0@gmail.com>
2024-02-15 13:01:56 +0530

    openlinebreakdownview: adding OLBDNode class

--- openlinebreakdownview/src/main/java/com/example/openlinebreakdownview/OpenLineBreakDownView.kt
+++ openlinebreakdownview/src/main/java/com/example/openlinebreakdownview/OpenLineBreakDownView.kt
@@ -123,4 +123,45 @@ class OpenLineBreakDownView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class OLBDNode(var i : Int = 0, val state : State = State()) {
+
+        private var next : OLBDNode? = null
+        private var prev : OLBDNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < colors.size - 1) {
+                next = OLBDNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawOLBDNode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : OLBDNode {
+            var curr : OLBDNode? = prev
+            if (dir == 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
