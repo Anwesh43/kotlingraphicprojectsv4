49e9a003cd91b746bb57f849c4a4f9d5ec5f8ddd
Anwesh43 <anweshthecool0@gmail.com>
2024-02-16 00:55:53 +0530

    linerotsqrigthview: adding Canvas extension methods

--- linerotsqrightview/src/main/java/com/example/linerotsqrightview/LineRotSqRightView.kt
+++ linerotsqrightview/src/main/java/com/example/linerotsqrightview/LineRotSqRightView.kt
@@ -27,3 +27,35 @@ val backColor : Int = Color.parseColor("#BDBDBD")
 fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
+
+fun Canvas.drawXY(x : Float, y : Float, cb : () -> Unit) {
+    save()
+    translate(x, y)
+    cb()
+    restore()
+}
+
+fun Canvas.drawLineRotSqRight(scale : Float, w : Float, h : Float, paint : Paint) {
+    val size : Float = Math.min(w, h) / sizeFactor
+    val dsc : (Int) -> Float = {
+        scale.divideScale(it, parts)
+    }
+    drawXY(w / 2 + (w / 2) * dsc(3), h / 2) {
+        drawXY((w / 2) * (1 - dsc(0)), 0f) {
+            rotate(rot * dsc(1))
+            drawLine(0f, 0f, size, 0f, paint)
+        }
+        drawXY((w / 2) * (1 - dsc(2)), 0f) {
+            drawRect(RectF(0f, 0f, size, size), paint)
+        }
+    }
+}
+
+fun Canvas.drawLRSSNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = Color.parseColor(colors[i])
+    paint.strokeCap = Paint.Cap.ROUND
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    drawLineRotSqRight(scale, w, h, paint)
+}
