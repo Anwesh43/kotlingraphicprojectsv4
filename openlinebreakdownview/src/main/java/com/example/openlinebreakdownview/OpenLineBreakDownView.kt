52a3f0130d3f79728a596a1b2b846db2d9fba1d3
Anwesh43 <anweshthecool0@gmail.com>
2024-02-15 12:52:37 +0530

    openlinebreakdownview: adding Canas extension methods

--- openlinebreakdownview/src/main/java/com/example/openlinebreakdownview/OpenLineBreakDownView.kt
+++ openlinebreakdownview/src/main/java/com/example/openlinebreakdownview/OpenLineBreakDownView.kt
@@ -26,3 +26,38 @@ val delay : Long = 20
 fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
+
+fun Canvas.drawXY(x : Float, y : Float, cb : () -> Unit) {
+    save()
+    translate(x, y)
+    cb()
+    restore()
+}
+
+fun Canvas.drawOpenLineBreakDown(scale : Float, w : Float, h : Float, paint : Paint) {
+    val size : Float = Math.min(w, h) / sizeFactor
+    val dsc : (Int) -> Float = {
+        scale.divideScale(it, parts)
+    }
+    drawXY(w / 2, h / 2 + (h / 2 + size) * dsc(3)) {
+        drawXY(0f, -h * 0.5f * (1 - dsc(1))) {
+            drawLine(0f, 0f, 0f, -size, paint)
+        }
+        for (j in 0..1) {
+            drawXY(0f, h * 0.5f * (1 - dsc(0))) {
+                rotate(rot * (1f - 2 * j) * dsc(1))
+                drawLine(0f, 0f, 0f, size, paint)
+            }
+        }
+    }
+}
+
+fun Canvas.drawOLBDNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = Color.parseColor(colors[i])
+    paint.strokeCap = Paint.Cap.ROUND
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    drawOpenLineBreakDown(scale, w, h, paint)
+}
+
