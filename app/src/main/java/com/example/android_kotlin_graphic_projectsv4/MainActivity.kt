f3510591cd4e88fbf040e5e45d3073b675699eee
Anwesh43 <anweshthecool0@gmail.com>
2024-02-17 17:48:31 +0530

    linedividebisectview: adding view in activity

--- app/src/main/java/com/example/android_kotlin_graphic_projectsv4/MainActivity.kt
+++ app/src/main/java/com/example/android_kotlin_graphic_projectsv4/MainActivity.kt
@@ -2,7 +2,7 @@ package com.example.android_kotlin_graphic_projectsv4
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.example.linerotsqrightview.LineRotSqRightView
+import com.example.linedividebisectview.LineDivideBisectView
 
 
 class MainActivity : AppCompatActivity() {
@@ -10,6 +10,6 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         supportActionBar?.hide()
         //setContentView(R.layout.activity_main)
-        LineRotSqRightView.create(this)
+        LineDivideBisectView.create(this)
     }
 }
