f63b68e49f304815848995ac86f8a003fe5d62e1
Anwesh43 <anweshthecool0@gmail.com>
2024-02-17 15:26:55 +0530

    linedividebisectview: adding Canvas extension methods

--- linedividebisectview/src/main/java/com/example/linedividebisectview/LineDivideBisectView.kt
+++ linedividebisectview/src/main/java/com/example/linedividebisectview/LineDivideBisectView.kt
@@ -23,7 +23,40 @@ val backColor : Int = Color.parseColor("#BDBDBD")
 val rot : Float = 90f
 val sizeFactor : Float = 4.9f
 val strokeFactor : Float = 90f
+val deg : Float = 30f
 
 fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
+
+fun Canvas.drawXY(x : Float, y : Float, cb : () -> Unit) {
+    save()
+    translate(x, y)
+    cb()
+    restore()
+}
+
+fun Canvas.drawLineDivideBisect(scale : Float, w : Float, h : Float, paint : Paint) {
+    val size : Float = Math.min(w, h) / sizeFactor
+    val dsc : (Int) -> Float = {
+        scale.divideScale(it, parts)
+    }
+    drawXY(w / 2, h / 2) {
+        rotate(rot * dsc(2))
+        for (j in 0..2) {
+            drawXY((w / 2) * (1 - dsc(0)), 0f) {
+                rotate((j - 1) * deg * dsc(1))
+                drawLine(0f, 0f, size, 0f, paint)
+            }
+        }
+    }
+}
+
+fun Canvas.drawLDBNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = Color.parseColor(colors[i])
+    paint.strokeCap = Paint.Cap.ROUND
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    drawLineDivideBisect(scale, w, h, paint)
+}
