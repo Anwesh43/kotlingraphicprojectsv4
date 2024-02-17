2afc5122cf61ad5e469b9d0dd956a26edc0f7a18
Anwesh43 <anweshthecool0@gmail.com>
2024-02-17 17:18:56 +0530

    linedividebisectview: adding LDBNode class

--- linedividebisectview/src/main/java/com/example/linedividebisectview/LineDivideBisectView.kt
+++ linedividebisectview/src/main/java/com/example/linedividebisectview/LineDivideBisectView.kt
@@ -123,4 +123,45 @@ class LineDivideBisectView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class LDBNode(var i : Int = 0, val state : State = State()) {
+
+        private var prev : LDBNode? = null
+        private var next : LDBNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < colors.size - 1) {
+                next = LDBNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawLDBNode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : LDBNode {
+            var curr : LDBNode? = prev
+            if (dir == 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
