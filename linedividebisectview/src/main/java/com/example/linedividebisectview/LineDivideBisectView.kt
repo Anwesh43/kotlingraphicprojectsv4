7cfe22b3571b764f77f452ac90387d191bbb0141
Anwesh43 <anweshthecool0@gmail.com>
2024-02-17 17:39:47 +0530

    linedividebisectview: adding LineDivideBisect class

--- linedividebisectview/src/main/java/com/example/linedividebisectview/LineDivideBisectView.kt
+++ linedividebisectview/src/main/java/com/example/linedividebisectview/LineDivideBisectView.kt
@@ -164,4 +164,27 @@ class LineDivideBisectView(ctx : Context) : View(ctx) {
             return this
         }
     }
+
+    data class LineDivideBisect(var i : Int) {
+
+        private var curr : LDBNode = LDBNode(0)
+        private var dir : Int = 1
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            curr.draw(canvas, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            curr.update {
+                curr = curr.getNext(dir) {
+                    dir *= -1
+                }
+                cb(it)
+            }
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            curr.startUpdating(cb)
+        }
+    }
 }
