c6f91574791cc5a44d207f5ac1281ca0ae43c028
Anwesh43 <anweshthecool0@gmail.com>
2024-02-18 23:10:41 +0530

    linebarrotupview: adding Canvas extension methods

--- linebarrotupview/src/main/java/com/example/linebarrotupview/LineBarRotUpView.kt
+++ linebarrotupview/src/main/java/com/example/linebarrotupview/LineBarRotUpView.kt
@@ -27,3 +27,33 @@ val rot : Float = 45f
 fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
+
+fun Canvas.drawXY(x : Float, y : Float, cb : () -> Unit) {
+    save()
+    translate(x, y)
+    cb()
+    restore()
+}
+
+fun Canvas.drawLineBarRotUp(scale : Float, w : Float, h : Float, paint : Paint) {
+    val size : Float = Math.min(w, h) / sizeFactor
+    val dsc : (Int) -> Float = {
+        scale.divideScale(it, parts)
+    }
+    drawXY(w / 2, h / 2) {
+        drawXY(-w * 0.5f * (1 - dsc(1)), 0f) {
+            rotate(rot * dsc(2))
+            drawLine(0f, 0f, -size, 0f, paint)
+        }
+        drawRect(RectF(0f, 0f, size * dsc(0), size / 4), paint)
+    }
+}
+
+fun Canvas.drawLBRUNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = Color.parseColor(colors[i])
+    paint.strokeCap = Paint.Cap.ROUND
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    drawLineBarRotUp(scale, w, h, paint)
+}
