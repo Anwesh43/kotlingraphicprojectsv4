61aa1e65fd67ac6c92a25e675dab20610374a48c
Anwesh43 <anweshthecool0@gmail.com>
2024-02-19 00:44:45 +0530

    linebarrotupview: adding view in activity

--- app/src/main/java/com/example/android_kotlin_graphic_projectsv4/MainActivity.kt
+++ app/src/main/java/com/example/android_kotlin_graphic_projectsv4/MainActivity.kt
@@ -2,7 +2,7 @@ package com.example.android_kotlin_graphic_projectsv4
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.example.linedividebisectview.LineDivideBisectView
+import com.example.linebarrotupview.LineBarRotUpView
 
 
 class MainActivity : AppCompatActivity() {
@@ -10,6 +10,6 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         supportActionBar?.hide()
         //setContentView(R.layout.activity_main)
-        LineDivideBisectView.create(this)
+        LineBarRotUpView.create(this)
     }
 }
