338df35c9320c37106b1415f256c8accfbc52de8
Anwesh43 <anweshthecool0@gmail.com>
2024-02-22 14:10:02 +0530

    barfromupshrinkview: adding BarFromUpShrink class

--- barfromupshrinkview/src/main/java/com/example/barfromupshrinkview/BarFromUpShrinkView.kt
+++ barfromupshrinkview/src/main/java/com/example/barfromupshrinkview/BarFromUpShrinkView.kt
@@ -158,4 +158,27 @@ class BarFromUpShrinkView(ctx : Context) : View(ctx) {
             return this
         }
     }
+
+    data class BarFromUpShrink(var i : Int = 0) {
+
+        private var curr : BFUSNode = BFUSNode(0)
+        private var dir : Int = 1
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            curr.draw(canvas, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            curr.update {
+                curr = curr.getNext(dir) {
+                    dir *= -1
+                }
+                cb(it)
+            }
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            curr.startUpdating(cb)
+        }
+    }
 }
