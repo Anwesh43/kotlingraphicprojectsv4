a18ab9100431348e69fff25f2d2b2c14b5d2c205
Anwesh43 <anweshthecool0@gmail.com>
2024-02-22 13:46:52 +0530

    barfromupshrinkview: adding Canvas extension methods

--- barfromupshrinkview/src/main/java/com/example/barfromupshrinkview/BarFromUpShrinkView.kt
+++ barfromupshrinkview/src/main/java/com/example/barfromupshrinkview/BarFromUpShrinkView.kt
@@ -26,3 +26,30 @@ val rot : Float = 180f
 fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
+
+fun Canvas.drawXY(x : Float, y : Float, cb : () -> Unit) {
+    save()
+    translate(x, y)
+    cb()
+    restore()
+}
+
+fun Canvas.drawBarFromUpShrink(scale : Float, w : Float, h : Float, paint : Paint) {
+    val dsc : (Int) -> Float = {
+        scale.divideScale(it, parts)
+    }
+    val size : Float = Math.min(w, h) / sizeFactor
+    drawXY(w / 2 - (w / 2) * dsc(3), h / 2) {
+        rotate(rot * dsc(1))
+        drawXY(0f, -h * 0.5f * (1 - dsc(0))) {
+            drawRect(RectF(0f, -size + size * 0.5f * dsc(2), size, 0f), paint)
+        }
+    }
+}
+
+fun Canvas.drawBFUSNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = Color.parseColor(colors[i])
+    drawBarFromUpShrink(scale, w, h, paint)
+}
