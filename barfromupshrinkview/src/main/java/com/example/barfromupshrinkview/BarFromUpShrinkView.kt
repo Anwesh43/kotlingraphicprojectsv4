a2b725abf72b7f2f53f254a6d7a94e06d87ce816
Anwesh43 <anweshthecool0@gmail.com>
2024-02-22 13:52:19 +0530

    barfromupshrinkview: adding BarFromUpShrinkView class

--- barfromupshrinkview/src/main/java/com/example/barfromupshrinkview/BarFromUpShrinkView.kt
+++ barfromupshrinkview/src/main/java/com/example/barfromupshrinkview/BarFromUpShrinkView.kt
@@ -53,3 +53,20 @@ fun Canvas.drawBFUSNode(i : Int, scale : Float, paint : Paint) {
     paint.color = Color.parseColor(colors[i])
     drawBarFromUpShrink(scale, w, h, paint)
 }
+
+class BarFromUpShrinkView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+
+        }
+        return true
+    }
+}
