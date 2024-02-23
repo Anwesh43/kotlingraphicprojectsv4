10acbfa8f3eef152a98fddcbca2fceaf8041dac2
Anwesh43 <anweshthecool0@gmail.com>
2024-02-23 14:27:24 +0530

    arclinerotdownview: adding Canvas extension methods

--- arclinerotdownview/src/main/java/com/example/arclinerotdownview/ArcLineRotDownView.kt
+++ arclinerotdownview/src/main/java/com/example/arclinerotdownview/ArcLineRotDownView.kt
@@ -26,3 +26,33 @@ val backColor : Int = Color.parseColor("#BDBDBD")
 fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
+
+fun Canvas.drawXY(x : Float, y : Float, cb : () -> Unit) {
+    save()
+    translate(x, y)
+    cb()
+    restore()
+}
+
+fun Canvas.drawArcLineRotDown(scale : Float, w : Float, h : Float, paint : Paint) {
+    val size : Float = Math.min(w, h) / sizeFactor
+    val dsc : (Int) -> Float = {
+        scale.divideScale(it, parts)
+    }
+    drawXY(w / 2, h / 2) {
+        rotate(rot * dsc(2))
+        drawArc(RectF(0f, -size / 2, size, size / 2), 180f, 180f * dsc(0), true, paint)
+        drawXY(size, 0f) {
+            drawLine(0f, 0f, size * dsc(1), 0f, paint)
+        }
+    }
+}
+
+fun Canvas.drawALRDNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = Color.parseColor(colors[i])
+    paint.strokeCap = Paint.Cap.ROUND
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    drawArcLineRotDown(scale, w, h, paint)
+}
