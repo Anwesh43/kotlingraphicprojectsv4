359833239e36610eaf8f72b9187cee4661b505c1
Anwesh43 <anweshthecool0@gmail.com>
2024-02-23 14:34:58 +0530

    arclinerotdownview: adding ALRDNode class

--- arclinerotdownview/src/main/java/com/example/arclinerotdownview/ArcLineRotDownView.kt
+++ arclinerotdownview/src/main/java/com/example/arclinerotdownview/ArcLineRotDownView.kt
@@ -114,4 +114,45 @@ class ArcLineRotDownView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class ALRDNode(var i : Int = 0, val state : State = State()) {
+
+        private var next : ALRDNode? = null
+        private var prev : ALRDNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < colors.size - 1) {
+                next = ALRDNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawALRDNode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : ALRDNode {
+            var curr : ALRDNode? = prev
+            if (dir == 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
