bbf603441af2014dbd09e890b6e88f3e7e53fad6
Anwesh43 <anweshthecool0@gmail.com>
2024-02-24 16:52:32 +0530

    slantlinerotdownview: adding Renderer in View

--- slantlinerotdownview/src/main/java/com/example/slantlinerotdownview/SlantLineRotDownView.kt
+++ slantlinerotdownview/src/main/java/com/example/slantlinerotdownview/SlantLineRotDownView.kt
@@ -62,14 +62,16 @@ fun Canvas.drawSLRDNode(i : Int, scale : Float, paint : Paint) {
 
 class SlantLineRotDownView(ctx : Context) : View(ctx) {
 
-    override fun onDraw(canvas : Canvas) {
+    private val renderer : Renderer = Renderer(this)
 
+    override fun onDraw(canvas : Canvas) {
+        renderer.render(canvas)
     }
 
     override fun onTouchEvent(event : MotionEvent) : Boolean {
         when (event.action) {
             MotionEvent.ACTION_DOWN -> {
-
+                renderer.handleTap()
             }
         }
         return true
