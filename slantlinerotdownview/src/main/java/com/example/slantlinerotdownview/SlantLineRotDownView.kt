41cea2295e6e41010653de766290aa368f948b9e
Anwesh43 <anweshthecool0@gmail.com>
2024-02-24 15:51:59 +0530

    slantlinerotdownview: adding Canvas extension methods

--- slantlinerotdownview/src/main/java/com/example/slantlinerotdownview/SlantLineRotDownView.kt
+++ slantlinerotdownview/src/main/java/com/example/slantlinerotdownview/SlantLineRotDownView.kt
@@ -19,10 +19,43 @@ val parts : Int = 4
 val scGap : Float = 0.04f / parts
 val strokeFactor : Float = 90f
 val sizeFactor : Float = 4.9f
-val rot : Float = 90f
+val rot : Float = 180f
 val backColor : Int = Color.parseColor("#BDBDBD")
 val delay : Long = 20
+val deg : Float = -45f
 
 fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
+
+fun Canvas.drawXY(x : Float, y : Float, cb : () -> Unit) {
+    save()
+    translate(x, y)
+    cb()
+    restore()
+}
+
+fun Canvas.drawSlantLineRotDown(scale : Float, w : Float, h : Float, paint : Paint) {
+    val size : Float = Math.min(w, h) / sizeFactor
+    val dsc : (Int) -> Float = {
+        scale.divideScale(it, parts)
+    }
+    drawXY(w / 2, h / 2 + (h / 2) * dsc(3)) {
+        rotate(rot * dsc(2))
+        for (j in 0..1) {
+            drawXY(0f, -h * 0.5f * (1 - dsc(0))) {
+                rotate(-deg * j * dsc(1))
+                drawLine(0f, 0f, 0f, -size, paint)
+            }
+        }
+    }
+}
+
+fun Canvas.drawSLRDNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = Color.parseColor(colors[i])
+    paint.strokeCap = Paint.Cap.ROUND
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    drawSlantLineRotDown(scale, w, h, paint)
+}
