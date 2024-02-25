8286a63062e1a9dc2f7141ab76e7c6336f40ad71
Anwesh43 <anweshthecool0@gmail.com>
2024-02-25 15:36:14 +0530

    barsemicircledownview: adding Canvas extension methods

--- barsemicircledownview/src/main/java/com/example/barsemicircledownview/BarSemiCircleDownView.kt
+++ barsemicircledownview/src/main/java/com/example/barsemicircledownview/BarSemiCircleDownView.kt
@@ -19,7 +19,7 @@ val colors : Array<String> = arrayOf(
 val parts : Int = 4
 val scGap : Float = 0.04f / parts
 val strokeFactor : Float = 90f
-val rot : Float = 90f
+val rot : Float = -90f
 val delay : Long = 20
 val backColor : Int = Color.parseColor("#BDBDBD")
 val sizeFactor : Float = 4.9f
@@ -27,3 +27,33 @@ val sizeFactor : Float = 4.9f
 fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
+
+fun Canvas.drawXY(x : Float, y : Float, cb : () -> Unit) {
+    save()
+    translate(x, y)
+    cb()
+    restore()
+}
+
+fun Canvas.drawBarSemiCircleDown(scale : Float, w : Float, h : Float, paint : Paint) {
+    val size : Float = Math.min(w, h) / sizeFactor
+    val dsc : (Int) -> Float = {
+        scale.divideScale(it, parts)
+    }
+    drawXY(w / 2, h / 2) {
+        rotate(rot * dsc(2))
+        drawXY(-w * 0.5f * (1 - dsc(0)), 0f) {
+            drawRect(RectF(-size, 0f, 0f, size / 4), paint)
+        }
+        drawXY(-size / 2, 0f) {
+            drawArc(RectF(-size / 2, -size / 2, size / 2, size / 2), 180f, 180f * dsc(1), true, paint)
+        }
+    }
+}
+
+fun Canvas.drawBSCDNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = Color.parseColor(colors[i])
+    drawBarSemiCircleDown(scale, w, h, paint)
+}
