ca199e0eae2da6d15f8ac9d75712fac0db541d1b
Anwesh43 <anweshthecool0@gmail.com>
2024-02-26 13:01:34 +0530

    halfcapsulerotdownview: adding HCRDNode class

--- halfcapsulerotdownview/src/main/java/com/example/halfcapsulerotdownview/HalfCapsuleRotDownView.kt
+++ halfcapsulerotdownview/src/main/java/com/example/halfcapsulerotdownview/HalfCapsuleRotDownView.kt
@@ -117,4 +117,45 @@ class HalfCapsuleRotDownView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class HCRDNode(var i : Int = 0, val state : State = State()) {
+
+        private var next : HCRDNode? = null
+        private var prev : HCRDNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < colors.size - 1) {
+                next = HCRDNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawHCRDNode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : HCRDNode {
+            var curr : HCRDNode? = prev
+            if (dir == 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
