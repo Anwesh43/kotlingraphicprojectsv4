3bec386720c60daa220691e11228182c3280f7a0
Anwesh43 <anweshthecool0@gmail.com>
2024-02-26 12:53:31 +0530

    halfcapsulerotdownview: adding HalfCapsuleDownView class

--- halfcapsulerotdownview/src/main/java/com/example/halfcapsulerotdownview/HalfCapsuleRotDownView.kt
+++ halfcapsulerotdownview/src/main/java/com/example/halfcapsulerotdownview/HalfCapsuleRotDownView.kt
@@ -54,3 +54,19 @@ fun Canvas.drawHCRDNode(i : Int, scale : Float, paint : Paint) {
     paint.color = Color.parseColor(colors[i])
     drawHalfCapsuleRotDown(scale, w, h, paint)
 }
+
+class HalfCapsuleRotDownView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
