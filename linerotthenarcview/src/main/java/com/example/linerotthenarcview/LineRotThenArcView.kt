6bc319ae6fce14749faeabcf6f278d20e39125a6
Anwesh43 <anweshthecool0@gmail.com>
2024-02-28 16:59:24 +0530

    linerotthenarcview: adding LineRotThenArcView class

--- linerotthenarcview/src/main/java/com/example/linerotthenarcview/LineRotThenArcView.kt
+++ linerotthenarcview/src/main/java/com/example/linerotthenarcview/LineRotThenArcView.kt
@@ -57,3 +57,19 @@ fun Canvas.drawLRTANode(i : Int, scale : Float, paint : Paint) {
     paint.strokeWidth = Math.min(w, h) / strokeFactor
     drawLineRotThenArc(scale, w, h, paint)
 }
+
+class LineRotThenArcView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent): Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
