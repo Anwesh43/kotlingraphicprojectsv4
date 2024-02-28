57bd5a4e36ee6fdf654c3940fd4bfd8052d9d854
Anwesh43 <anweshthecool0@gmail.com>
2024-02-28 14:41:18 +0530

    linerotthenarcview: adding Canvas extension metods

--- linerotthenarcview/src/main/java/com/example/linerotthenarcview/LineRotThenArcView.kt
+++ linerotthenarcview/src/main/java/com/example/linerotthenarcview/LineRotThenArcView.kt
@@ -27,3 +27,33 @@ val rot : Float = 90f
 fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
+
+fun Canvas.drawXY(x : Float, y : Float, cb : () -> Unit) {
+    save()
+    translate(x, y)
+    cb()
+    restore()
+}
+
+fun Canvas.drawLineRotThenArc(scale : Float, w : Float, h : Float, paint : Paint) {
+    val size : Float = Math.min(w, h) / sizeFactor
+    val dsc : (Int) -> Float = {
+        scale.divideScale(it, parts)
+    }
+    drawXY(w / 2, h / 2 + (h / 2) * dsc(3)) {
+        drawXY(w * 0.5f * (1 - dsc(0)), 0f) {
+            rotate(rot * dsc(1))
+            drawLine(0f, 0f, size, 0f, paint)
+        }
+        drawArc(RectF(-size / 2, 0f, size / 2, size), -rot, rot * 2 * dsc(2), true, paint)
+    }
+}
+
+fun Canvas.drawLRTANode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = Color.parseColor(colors[i])
+    paint.strokeCap = Paint.Cap.ROUND
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    drawLineRotThenArc(scale, w, h, paint)
+}
