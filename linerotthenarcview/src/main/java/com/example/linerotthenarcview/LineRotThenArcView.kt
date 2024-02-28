8c33ab5f1b827af2fe351f618409a1925f54bd88
Anwesh43 <anweshthecool0@gmail.com>
2024-02-28 20:30:15 +0530

    linerotthenarcview: adding Renderer class

--- linerotthenarcview/src/main/java/com/example/linerotthenarcview/LineRotThenArcView.kt
+++ linerotthenarcview/src/main/java/com/example/linerotthenarcview/LineRotThenArcView.kt
@@ -184,4 +184,27 @@ class LineRotThenArcView(ctx : Context) : View(ctx) {
             curr.startUpdating(cb)
         }
     }
+
+    data class Renderer(var view : LineRotThenArcView) {
+
+        private val animator : Animator = Animator(view)
+        private val lrta : LineRotThenArc = LineRotThenArc(0)
+        private val paint : Paint = Paint(Paint.ANTI_ALIAS_FLAG)
+
+        fun render(canvas : Canvas) {
+            canvas.drawColor(backColor)
+            lrta.draw(canvas, paint)
+            animator.animate {
+                lrta.update {
+                    animator.stop()
+                }
+            }
+        }
+
+        fun handleTap() {
+            lrta.startUpdating {
+                animator.start()
+            }
+        }
+    }
 }
