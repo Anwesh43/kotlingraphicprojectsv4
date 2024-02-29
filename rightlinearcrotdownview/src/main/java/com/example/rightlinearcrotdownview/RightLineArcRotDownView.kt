26878f4fd51ba87ecffbb09c66ce72d5265e56b9
Anwesh43 <anweshthecool0@gmail.com>
2024-02-29 22:10:22 +0530

    rightlinearcrotdownview: adding Canvas extension methods

--- rightlinearcrotdownview/src/main/java/com/example/rightlinearcrotdownview/RightLineArcRotDownView.kt
+++ rightlinearcrotdownview/src/main/java/com/example/rightlinearcrotdownview/RightLineArcRotDownView.kt
@@ -27,3 +27,40 @@ val rot : Float = 90f
 fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
+
+fun Canvas.drawXY(x : Float, y : Float, cb : () -> Unit) {
+    save()
+    translate(x, y)
+    cb()
+    restore()
+}
+
+fun Canvas.drawRightLineArcRotDown(scale : Float, w : Float, h : Float, paint : Paint) {
+    val size : Float = Math.min(w, h) / sizeFactor
+    val dsc : (Int) -> Float = {
+        scale.divideScale(it, parts)
+    }
+    drawXY(w / 2 - (w / 2) * dsc(3), h / 2) {
+        drawXY(-size / 2, 0f) {
+            drawArc(RectF(-size / 2, -size / 2, size / 2, size / 2), 180f, 180f * dsc(0), false, paint)
+        }
+        drawXY(0f, 0f) {
+            rotate(rot * dsc(2))
+            for (j in 0..1) {
+                drawXY(0f, 0f) {
+                    rotate(rot * j)
+                    drawLine(0f, 0f, size * dsc(1), 0f, paint)
+                }
+            }
+        }
+    }
+}
+
+fun Canvas.drawRLARDNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = Color.parseColor(colors[i])
+    paint.style = Paint.Style.STROKE
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    drawRightLineArcRotDown(scale, w, h, paint)
+}
