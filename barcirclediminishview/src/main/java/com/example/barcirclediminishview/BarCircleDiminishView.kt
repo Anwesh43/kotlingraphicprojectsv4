d943e5dccc6a77d14ba02b7a47c891c11c5a88a6
Anwesh43 <anweshthecool0@gmail.com>
2024-03-01 14:13:05 +0530

    barcirclediminishview: adding BarCircleDiminishView class

--- barcirclediminishview/src/main/java/com/example/barcirclediminishview/BarCircleDiminishView.kt
+++ barcirclediminishview/src/main/java/com/example/barcirclediminishview/BarCircleDiminishView.kt
@@ -64,3 +64,19 @@ fun Canvas.drawBCDNode(i : Int, scale : Float, paint : Paint) {
     drawBarCircleDiminish(scale, w, h, paint)
 }
 
+class BarCircleDiminishView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
+
