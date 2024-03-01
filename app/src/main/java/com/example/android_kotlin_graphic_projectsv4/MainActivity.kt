b12abb086ab2479c4fb95fde0741d236b157fec0
Anwesh43 <anweshthecool0@gmail.com>
2024-03-01 16:43:55 +0530

    barcirclediminishview: adding view in activity

--- app/src/main/java/com/example/android_kotlin_graphic_projectsv4/MainActivity.kt
+++ app/src/main/java/com/example/android_kotlin_graphic_projectsv4/MainActivity.kt
@@ -2,7 +2,7 @@ package com.example.android_kotlin_graphic_projectsv4
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.example.rightlinearcrotdownview.RightLineArcRotDownView
+import com.example.barcirclediminishview.BarCircleDiminishView
 
 
 class MainActivity : AppCompatActivity() {
@@ -10,6 +10,6 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         supportActionBar?.hide()
         //setContentView(R.layout.activity_main)
-        RightLineArcRotDownView.create(this)
+        BarCircleDiminishView.create(this)
     }
 }
