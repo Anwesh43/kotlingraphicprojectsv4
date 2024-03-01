db3a6ff38385192f373af6c71c7821d429f32527
Anwesh43 <anweshthecool0@gmail.com>
2024-03-01 13:56:08 +0530

    barcirclediminishview: adding constants ad libraries

---
+++ barcirclediminishview/src/main/java/com/example/barcirclediminishview/BarCircleDiminishView.kt
@@ -0,0 +1,25 @@
+package com.example.barcirclediminishview
+
+import android.view.View
+import android.view.MotionEvent
+import android.graphics.Paint
+import android.graphics.Canvas
+import android.graphics.Color
+import android.graphics.RectF
+import android.content.Context
+import android.app.Activity
+
+val colors : Array<String> = arrayOf(
+    "#1A237E",
+    "#EF5350",
+    "#AA00FF",
+    "#C51162",
+    "#00C853"
+)
+val parts : Int = 4
+val scGap : Float = 0.04f / parts
+val sizeFactor : Float = 4.9f
+val rot : Float = 360f
+val barHFactor : Float = 11.2f
+val delay : Long = 20
+val backColor : Int = Color.parseColor("#BDBDBD")
