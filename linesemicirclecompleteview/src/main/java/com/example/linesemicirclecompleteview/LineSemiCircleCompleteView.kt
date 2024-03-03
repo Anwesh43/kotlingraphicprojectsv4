9717bde2d2b6dae0ff893747e3311e6685d5a4ef
Anwesh43 <anweshthecool0@gmail.com>
2024-03-03 21:48:40 +0530

    linesemicirclecompleteview: adding LineSemiCircleComplete class

--- linesemicirclecompleteview/src/main/java/com/example/linesemicirclecompleteview/LineSemiCircleCompleteView.kt
+++ linesemicirclecompleteview/src/main/java/com/example/linesemicirclecompleteview/LineSemiCircleCompleteView.kt
@@ -163,4 +163,27 @@ class LineSemiCircleCompleteView(ctx : Context) : View(ctx) {
             return this
         }
     }
+
+    data class LineSemiCircleComplete(var i : Int) {
+
+        private var curr : LSCCNode = LSCCNode(0)
+        private var dir : Int = 1
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            curr.draw(canvas, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            curr.update {
+                curr = curr.getNext(dir) {
+                    dir *= -1
+                }
+                cb(it)
+            }
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            curr.startUpdating(cb)
+        }
+    }
 }
