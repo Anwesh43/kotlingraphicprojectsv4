f6d095b701c6e120a3444cf8623a943f415592d0
Anwesh43 <anweshthecool0@gmail.com>
2024-03-03 19:55:09 +0530

    linesemicirclecompleteview: adding State class

--- linesemicirclecompleteview/src/main/java/com/example/linesemicirclecompleteview/LineSemiCircleCompleteView.kt
+++ linesemicirclecompleteview/src/main/java/com/example/linesemicirclecompleteview/LineSemiCircleCompleteView.kt
@@ -67,9 +67,30 @@ class LineSemiCircleCompleteView(ctx : Context) : View(ctx) {
     }
 
     override fun onTouchEvent(event: MotionEvent?): Boolean {
-        when (event.action) {
+        when (event?.action) {
             MotionEvent.ACTION_DOWN -> {
 
+            }
+        }
+        return true
+    }
+
+    data class State(var scale : Float = 0f, var dir : Float = 0f, var prevScale : Float = 0f) {
+
+        fun update(cb : (Float) -> Unit) {
+            scale += scGap * dir
+            if (Math.abs(scale - prevScale) > 1) {
+                scale = prevScale + dir
+                dir = 0f
+                prevScale = scale
+                cb(prevScale)
+            }
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            if (dir == 0f) {
+                dir = 1f - 2 * prevScale
+                cb()
             }
         }
     }
