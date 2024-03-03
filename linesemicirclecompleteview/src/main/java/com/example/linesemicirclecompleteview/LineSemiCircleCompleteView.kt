3b9f23fd8b32835877120888e88157ee1ef1a64a
Anwesh43 <anweshthecool0@gmail.com>
2024-03-03 22:42:31 +0530

    linesemicirclecompleteview: adding fixes

--- linesemicirclecompleteview/src/main/java/com/example/linesemicirclecompleteview/LineSemiCircleCompleteView.kt
+++ linesemicirclecompleteview/src/main/java/com/example/linesemicirclecompleteview/LineSemiCircleCompleteView.kt
@@ -57,6 +57,7 @@ fun Canvas.drawLSCCNode(i : Int, scale : Float, paint : Paint) {
     paint.color = Color.parseColor(colors[i])
     paint.strokeCap = Paint.Cap.ROUND
     paint.strokeWidth = Math.min(w, h) / strokeFactor
+    paint.style = Paint.Style.STROKE
     drawLineSemiCircleComplete(scale, w, h, paint)
 }
 
