452577385c74474a6da81f2e94456a4b77ae9157
Anwesh43 <anweshthecool0@gmail.com>
2024-03-03 19:50:22 +0530

    linesemicirclecompleteview: adding LineSemiCircleCompleteView class

--- linesemicirclecompleteview/src/main/java/com/example/linesemicirclecompleteview/LineSemiCircleCompleteView.kt
+++ linesemicirclecompleteview/src/main/java/com/example/linesemicirclecompleteview/LineSemiCircleCompleteView.kt
@@ -59,3 +59,18 @@ fun Canvas.drawLSCCNode(i : Int, scale : Float, paint : Paint) {
     paint.strokeWidth = Math.min(w, h) / strokeFactor
     drawLineSemiCircleComplete(scale, w, h, paint)
 }
+
+class LineSemiCircleCompleteView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event: MotionEvent?): Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+    }
+}
