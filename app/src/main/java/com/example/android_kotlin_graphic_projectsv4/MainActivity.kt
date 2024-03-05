a9500d29433599963517a179a7b2487181b1bff9
Anwesh43 <anweshthecool0@gmail.com>
2024-03-06 01:20:25 +0530

    linecuplineextendview: adding view in activity

--- app/src/main/java/com/example/android_kotlin_graphic_projectsv4/MainActivity.kt
+++ app/src/main/java/com/example/android_kotlin_graphic_projectsv4/MainActivity.kt
@@ -2,7 +2,7 @@ package com.example.android_kotlin_graphic_projectsv4
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.example.linesemicirclecompleteview.LineSemiCircleCompleteView
+import com.example.linecuplineextendview.LineCupLineExtendView
 
 
 class MainActivity : AppCompatActivity() {
@@ -10,6 +10,6 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         supportActionBar?.hide()
         //setContentView(R.layout.activity_main)
-        LineSemiCircleCompleteView.create(this)
+        LineCupLineExtendView.create(this)
     }
 }
