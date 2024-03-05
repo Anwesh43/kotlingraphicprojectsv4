8e67076a81925f266d9881a2991ab36d46c63551
Anwesh43 <anweshthecool0@gmail.com>
2024-03-06 00:50:06 +0530

    linecuplineextendview: adding Canvas extension methods

--- linecuplineextendview/src/main/java/com/example/linecuplineextendview/LineCupLineExtendView.kt
+++ linecuplineextendview/src/main/java/com/example/linecuplineextendview/LineCupLineExtendView.kt
@@ -28,3 +28,33 @@ fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
 
+fun Canvas.drawXY(x : Float, y : Float, cb : () -> Unit) {
+    save()
+    translate(x, y)
+    cb()
+    restore()
+}
+fun Canvas.drawLineCupLineExtend(scale : Float, w : Float, h : Float, paint : Paint) {
+    val size : Float = Math.min(w, h) / sizeFactor
+    val dsc : (Int) -> Float = {
+        scale.divideScale(it, parts)
+    }
+    drawXY(w / 2 - (w / 2 + size) * dsc(3), h / 2) {
+        drawXY(size, size / 2) {
+            drawLine(0f, 0f, -size * dsc(0), 0f, paint)
+        }
+        drawArc(RectF(-size / 2, -size / 2, size / 2, size / 2), 90f, rot * dsc(1), false, paint)
+        drawXY(0f, -size / 2) {
+            drawLine(0f, 0f, size * dsc(2), 0f, paint)
+        }
+    }
+}
+fun Canvas.drawLCLENode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = Color.parseColor(colors[i])
+    paint.strokeCap = Paint.Cap.ROUND
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    drawLineCupLineExtend(scale, w, h, paint)
+}
+
