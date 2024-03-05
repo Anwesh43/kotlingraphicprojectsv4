c6a55be181bc9419552938d266de9a37ad1cb707
Anwesh43 <anweshthecool0@gmail.com>
2024-03-06 01:17:28 +0530

    linecuplineextendview: adding Renderer class

--- linecuplineextendview/src/main/java/com/example/linecuplineextendview/LineCupLineExtendView.kt
+++ linecuplineextendview/src/main/java/com/example/linecuplineextendview/LineCupLineExtendView.kt
@@ -184,4 +184,27 @@ class LineCupLineExtendView(ctx : Context) : View(ctx) {
             curr.startUpdating(cb)
         }
     }
+
+    data class Renderer(var view : LineCupLineExtendView) {
+
+        private val animator : Animator = Animator(view)
+        private val lcle : LineCupLineExtend = LineCupLineExtend(0)
+        private val paint : Paint = Paint(Paint.ANTI_ALIAS_FLAG)
+
+        fun render(canvas : Canvas) {
+            canvas.drawColor(backColor)
+            lcle.draw(canvas, paint)
+            animator.animate {
+                lcle.update {
+                    animator.stop()
+                }
+            }
+        }
+
+        fun handleTap() {
+            lcle.startUpdating {
+                animator.start()
+            }
+        }
+    }
 }
