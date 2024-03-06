edf9689ba0b80940eca4f88aa7f654ec28f5774a
Anwesh43 <anweshthecool0@gmail.com>
2024-03-07 00:21:29 +0530

    rotsqalternatemoveview: adding Canvas extension methods

--- rotsqalternatemoveview/src/main/java/com/example/rotsqalternatemoveview/RotSqAlternateMoveView.kt
+++ rotsqalternatemoveview/src/main/java/com/example/rotsqalternatemoveview/RotSqAlternateMoveView.kt
@@ -27,3 +27,33 @@ val backColor : Int = Color.parseColor("#BDBDBD")
 fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
+
+fun Canvas.drawXY(x : Float, y : Float, cb : () -> Unit) {
+    save()
+    translate(x, y)
+    cb()
+    restore()
+}
+
+fun Canvas.drawRotSqAlternateMove(scale : Float, w : Float, h : Float, paint : Paint) {
+    val size : Float = Math.min(w, h) / sizeFactor
+    val dsc : (Int) -> Float = {
+        scale.divideScale(it, parts)
+    }
+    drawXY(w / 2, h / 2) {
+        rotate(rot * dsc(2))
+        drawXY(0f, -h * 0.5f * (1 - dsc(1)) + (w / 2) * dsc(3)) {
+            drawCircle(0f, -size / 2, size / 2, paint)
+        }
+        drawXY(0f, h * 0.5f * (1 - dsc(0)) - (w / 2) * dsc(3)) {
+            drawRect(RectF(-size / 2, 0f, size / 2, size), paint)
+        }
+    }
+}
+
+fun Canvas.drawRSAMNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = Color.parseColor(colors[i])
+    drawRotSqAlternateMove(scale, w, h, paint)
+}
