9051c9485a4b9c7a2179f4896bd7af886a47ca08
Anwesh43 <anweshthecool0@gmail.com>
2024-03-08 01:45:27 +0530

    halfcirclelinerightview: adding HALRNode class

--- halfarclinerightview/src/main/java/com/example/halfarclinerightview/HalfArcLineRightView.kt
+++ halfarclinerightview/src/main/java/com/example/halfarclinerightview/HalfArcLineRightView.kt
@@ -120,4 +120,45 @@ class HalfArcLineRightView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class HALRNode(var i : Int = 0, val state : State = State()) {
+
+        private var prev : HALRNode? = null
+        private var next : HALRNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < colors.size - 1) {
+                next = HALRNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawHALRNode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : HALRNode {
+            var curr : HALRNode? = prev
+            if (dir == 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
