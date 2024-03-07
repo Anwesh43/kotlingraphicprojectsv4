7cc56f98d83f93a400ec081b607ca17267615bc4
Anwesh43 <anweshthecool0@gmail.com>
2024-03-08 01:23:23 +0530

    halfcirclelinerightview: adding HalfArcLineRightView class

--- halfarclinerightview/src/main/java/com/example/halfarclinerightview/HalfArcLineRightView.kt
+++ halfarclinerightview/src/main/java/com/example/halfarclinerightview/HalfArcLineRightView.kt
@@ -57,3 +57,19 @@ fun Canvas.drawHALRNode(i : Int, scale : Float, paint : Paint) {
     paint.strokeWidth = Math.min(w, h) / strokeFactor
     drawHalfArcLineRight(scale, w, h, paint)
 }
+
+class HalfArcLineRightView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
