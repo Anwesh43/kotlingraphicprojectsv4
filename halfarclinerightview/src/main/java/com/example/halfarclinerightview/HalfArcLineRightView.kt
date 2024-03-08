7ce8a3ff2f63ee51c60a7d7029afe62249498fe0
Anwesh43 <anweshthecool0@gmail.com>
2024-03-08 11:11:44 +0530

    halfarclinerightview: adding HalfArcLineRight class

--- halfarclinerightview/src/main/java/com/example/halfarclinerightview/HalfArcLineRightView.kt
+++ halfarclinerightview/src/main/java/com/example/halfarclinerightview/HalfArcLineRightView.kt
@@ -161,4 +161,26 @@ class HalfArcLineRightView(ctx : Context) : View(ctx) {
             return this
         }
     }
+
+    data class HalfArcLineRight(var i : Int) {
+
+        private var curr : HALRNode = HALRNode(0)
+        private var dir : Int = 1
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            curr.draw(canvas, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            curr.update {
+                curr = curr.getNext(dir) {
+                    dir *= -1
+                }
+            }
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            curr.startUpdating(cb)
+        }
+    }
 }
