6e5fd49c688afd2f07368ef2cefe5975305deed0
Anwesh43 <anweshthecool0@gmail.com>
2024-03-08 12:38:15 +0530

    halfarclinerightview: adding view in activity

--- app/src/main/java/com/example/android_kotlin_graphic_projectsv4/MainActivity.kt
+++ app/src/main/java/com/example/android_kotlin_graphic_projectsv4/MainActivity.kt
@@ -2,7 +2,7 @@ package com.example.android_kotlin_graphic_projectsv4
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.example.rotsqalternatemoveview.RotSqAlternateMoveView
+import com.example.halfarclinerightview.HalfArcLineRightView
 
 
 class MainActivity : AppCompatActivity() {
@@ -10,6 +10,6 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         supportActionBar?.hide()
         //setContentView(R.layout.activity_main)
-        RotSqAlternateMoveView.create(this)
+        HalfArcLineRightView.create(this)
     }
 }
