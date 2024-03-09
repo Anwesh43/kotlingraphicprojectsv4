7d67492362d60ddc6d4b937fb4fe2c36e919a0ac
Anwesh43 <anweshthecool0@gmail.com>
2024-03-09 14:48:52 +0530

    rightlinesquareupview: adding RLSUNode class

--- rightlinesquareupview/src/main/java/com/example/rightlinesquareupview/RightLineSquareUpView.kt
+++ rightlinesquareupview/src/main/java/com/example/rightlinesquareupview/RightLineSquareUpView.kt
@@ -125,4 +125,45 @@ class RightLineSquareUpView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class RLSUNode(var i : Int = 0, val state : State = State()) {
+
+        private var next : RLSUNode? = null
+        private var prev : RLSUNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < colors.size - 1) {
+                next = RLSUNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawRLSUNode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : RLSUNode {
+            var curr : RLSUNode? = prev
+            if (dir == 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
