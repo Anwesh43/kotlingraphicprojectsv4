adb5050b3f42c6408ace7c2a8ec63dc80a3208b4
Anwesh43 <anweshthecool0@gmail.com>
2024-03-11 00:42:47 +0530

    lineballrotdownview: adding view in activity

--- app/src/main/java/com/example/android_kotlin_graphic_projectsv4/MainActivity.kt
+++ app/src/main/java/com/example/android_kotlin_graphic_projectsv4/MainActivity.kt
@@ -2,7 +2,7 @@ package com.example.android_kotlin_graphic_projectsv4
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.example.rightlinesquareupview.RightLineSquareUpView
+import com.example.lineballrotdownview.LineBallRotDownView
 
 
 class MainActivity : AppCompatActivity() {
@@ -10,6 +10,6 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         supportActionBar?.hide()
         //setContentView(R.layout.activity_main)
-        RightLineSquareUpView.create(this)
+        LineBallRotDownView.create(this)
     }
 }
