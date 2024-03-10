b6e40d6305fc157227a894dfd8a8a54cfc1d0095
Anwesh43 <anweshthecool0@gmail.com>
2024-03-11 00:30:36 +0530

    lineballrotdownview: adding LBRDNode class

--- lineballrotdownview/src/main/java/com/example/lineballrotdownview/LineBallRotDownView.kt
+++ lineballrotdownview/src/main/java/com/example/lineballrotdownview/LineBallRotDownView.kt
@@ -122,4 +122,45 @@ class LineBallRotDownView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class LBRDNode(var i : Int = 0, val state : State = State()) {
+
+        private var next : LBRDNode? = null
+        private var prev : LBRDNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < colors.size - 1) {
+                next = LBRDNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawLBRDNode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : LBRDNode {
+            var curr : LBRDNode? = prev
+            if (dir == 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
