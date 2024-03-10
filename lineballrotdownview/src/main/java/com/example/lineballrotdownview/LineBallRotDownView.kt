e68c2448b4a2622e151d847a12c09246edad0e10
Anwesh43 <anweshthecool0@gmail.com>
2024-03-11 00:16:24 +0530

    lineballrotdownview: adding Canvas extension methods

--- lineballrotdownview/src/main/java/com/example/lineballrotdownview/LineBallRotDownView.kt
+++ lineballrotdownview/src/main/java/com/example/lineballrotdownview/LineBallRotDownView.kt
@@ -19,7 +19,7 @@ val colors : Array<String> = arrayOf(
 val parts : Int = 4
 val scGap : Float = 0.04f / parts
 val strokeFactor : Float = 90f
-val sizeFactory : Float = 4.9f
+val sizeFactor : Float = 4.9f
 val delay : Long = 20
 val backColor : Int = Color.parseColor("#BDBDBD")
 val rot : Float = 90f
@@ -27,3 +27,35 @@ val rot : Float = 90f
 fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
+
+fun Canvas.drawXY(x : Float, y : Float, cb : () -> Unit) {
+    save()
+    translate(x, y)
+    cb()
+    restore()
+}
+
+fun Canvas.drawLineBallRotDown(scale : Float, w : Float, h : Float, paint : Paint) {
+    val size : Float = Math.min(w, h) / sizeFactor
+    val dsc : (Int) -> Float = {
+        scale.divideScale(it, parts)
+    }
+    drawXY(w / 2, h / 2 + (h / 2 + size) * dsc(3)) {
+        drawXY((w / 2) * (1 - dsc(1)), 0f) {
+            rotate(rot * dsc(2))
+            drawCircle(size / 3, 0f, size / 3, paint)
+        }
+        drawXY(0f, (h / 2) * (1 - dsc(0))) {
+            drawLine(0f, 0f, 0f, -size, paint)
+        }
+    }
+}
+
+fun Canvas.drawLBRDNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = Color.parseColor(colors[i])
+    paint.strokeCap = Paint.Cap.ROUND
+    paint.strokeWidth = Math.min(w, h) / sizeFactor
+
+}
