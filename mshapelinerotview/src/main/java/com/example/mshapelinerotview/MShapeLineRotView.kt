c9a1379cc32f452a55b3157c7bc9e0b515383388
Anwesh43 <anweshthecool0@gmail.com>
2024-03-12 20:48:17 +0530

    mshapelinerotview : adding Int and Float extension methods

--- mshapelinerotview/src/main/java/com/example/mshapelinerotview/MShapeLineRotView.kt
+++ mshapelinerotview/src/main/java/com/example/mshapelinerotview/MShapeLineRotView.kt
@@ -23,3 +23,6 @@ val delay : Long = 20
 val rot : Float = 180f
 val backColor : Int = Color.parseColor("#BDBDBD")
 
+fun Int.inverse() : Float = 1f / this
+fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
+fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
