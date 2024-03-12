cb210ee52edf28cf8865b0af6b07c01ee1cf21dc
Anwesh43 <anweshthecool0@gmail.com>
2024-03-12 21:31:50 +0530

    mshapelinerotview : adding Canvas extension methods

--- mshapelinerotview/src/main/java/com/example/mshapelinerotview/MShapeLineRotView.kt
+++ mshapelinerotview/src/main/java/com/example/mshapelinerotview/MShapeLineRotView.kt
@@ -26,3 +26,39 @@ val backColor : Int = Color.parseColor("#BDBDBD")
 fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
+
+fun Canvas.drawXY(x : Float, y : Float, cb : () -> Unit) {
+    save()
+    translate(x, y)
+    cb()
+    restore()
+}
+
+fun Canvas.drawMShapeLineRot(scale : Float, w : Float, h : Float, paint : Paint) {
+    val size : Float = Math.min(w, h) / sizeFactor
+    val dsc : (Int) -> Float = {
+        scale.divideScale(it, parts)
+    }
+    drawXY(w / 2, h / 2) {
+        for (j in 0..1) {
+            drawXY(-size + size * j, 0f) {
+                rotate(rot * j * dsc(2))
+                val a : Float = size * 0.5f * dsc(j).divideScale(0, 2)
+                val b : Float = size * 0.5f * dsc(j).divideScale(1, 2)
+                drawLine(0f, 0f, a, -a, paint)
+                drawXY(size / 2, -size / 2) {
+                    drawLine(0f, 0f, a, b, paint)
+                }
+            }
+        }
+    }
+}
+
+fun Canvas.drawMSLRNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = Color.parseColor(colors[i])
+    paint.strokeCap = Paint.Cap.ROUND
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    drawMShapeLineRot(scale, w, h, paint)
+}
