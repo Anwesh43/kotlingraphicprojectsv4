a02d209c5182d42f19599a4867c9d10bd0bc9c1b
Anwesh43 <anweshthecool0@gmail.com>
2024-03-13 23:58:19 +0530

    linearcjoindownview: adding LAJDNode class

--- linearcjoindownview/src/main/java/com/example/linearcjoindownview/LineArcJoinDownView.kt
+++ linearcjoindownview/src/main/java/com/example/linearcjoindownview/LineArcJoinDownView.kt
@@ -120,4 +120,45 @@ class LineArcJoinDownView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class LAJDNode(var i : Int = 0, val state : State = State()) {
+
+        private var next : LAJDNode? = null
+        private var prev : LAJDNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < colors.size - 1) {
+                next = LAJDNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawLAJDNode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : LAJDNode {
+            var curr : LAJDNode? = prev
+            if (dir == 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
