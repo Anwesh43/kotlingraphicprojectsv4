99f2b3e3733d0d31ae11cbb4ea5ad49a683ff63b
Anwesh43 <anweshthecool0@gmail.com>
2024-03-13 22:49:08 +0530

    linearcjoindownview: adding LineArcJoinDownView class

--- linearcjoindownview/src/main/java/com/example/linearcjoindownview/LineArcJoinDownView.kt
+++ linearcjoindownview/src/main/java/com/example/linearcjoindownview/LineArcJoinDownView.kt
@@ -57,3 +57,19 @@ fun Canvas.drawLAJDNode(i : Int, scale : Float, paint : Paint) {
     paint.strokeWidth = Math.min(w, h) / strokeFactor
     drawLineArcJoinDown(scale, w, h, paint)
 }
+
+class LineArcJoinDownView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
