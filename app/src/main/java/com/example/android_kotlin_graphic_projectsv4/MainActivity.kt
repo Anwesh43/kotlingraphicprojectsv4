8efcda8804d5f3beedcbea3862094270545b8e0f
Anwesh43 <anweshthecool0@gmail.com>
2024-03-14 00:21:32 +0530

    linearcjoindownview: adding view in activity

--- app/src/main/java/com/example/android_kotlin_graphic_projectsv4/MainActivity.kt
+++ app/src/main/java/com/example/android_kotlin_graphic_projectsv4/MainActivity.kt
@@ -2,7 +2,7 @@ package com.example.android_kotlin_graphic_projectsv4
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.example.mshapelinerotview.MShapeLineRotView
+import com.example.linearcjoindownview.LineArcJoinDownView
 
 
 class MainActivity : AppCompatActivity() {
@@ -10,6 +10,6 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         supportActionBar?.hide()
         //setContentView(R.layout.activity_main)
-        MShapeLineRotView.create(this)
+        LineArcJoinDownView.create(this)
     }
 }
