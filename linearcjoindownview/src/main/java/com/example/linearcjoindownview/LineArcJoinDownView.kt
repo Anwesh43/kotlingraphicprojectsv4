e762b803666eb30bd378f4f87dd5af61e0888b03
Anwesh43 <anweshthecool0@gmail.com>
2024-03-14 00:18:38 +0530

    linearcjoindownview: adding Renderer in View

--- linearcjoindownview/src/main/java/com/example/linearcjoindownview/LineArcJoinDownView.kt
+++ linearcjoindownview/src/main/java/com/example/linearcjoindownview/LineArcJoinDownView.kt
@@ -60,14 +60,16 @@ fun Canvas.drawLAJDNode(i : Int, scale : Float, paint : Paint) {
 
 class LineArcJoinDownView(ctx : Context) : View(ctx) {
 
-    override fun onDraw(canvas : Canvas) {
+    private val renderer : Renderer = Renderer(this)
 
+    override fun onDraw(canvas : Canvas) {
+        renderer.render(canvas)
     }
 
     override fun onTouchEvent(event : MotionEvent) : Boolean {
         when (event.action) {
             MotionEvent.ACTION_DOWN -> {
-
+                renderer.handleTap()
             }
         }
         return true
