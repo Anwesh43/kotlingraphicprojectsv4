c4832fc5fa570a0e3cae68cb9c80aaf700f22766
Anwesh43 <anweshthecool0@gmail.com>
2024-03-15 02:32:02 +0530

    rightalternaterotview: adding Canvas extension methods

--- rightalternaterotview/src/main/java/com/example/rightalternaterotview/RightAlternateRotView.kt
+++ rightalternaterotview/src/main/java/com/example/rightalternaterotview/RightAlternateRotView.kt
@@ -22,8 +22,40 @@ val strokeFactor : Float = 90f
 val sizeFactor : Float = 4.9f
 val delay : Long = 20
 val backColor : Int = Color.parseColor("#BDBDBD")
-val rot : Float = 90f
+val rot : Float = 180f
 
 fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
+
+fun Canvas.drawXY(x : Float, y : Float, cb : () -> Unit) {
+    save()
+    translate(x, y)
+    cb()
+    restore()
+}
+
+fun Canvas.drawRightAlternateRot(scale : Float, w : Float, h : Float, paint : Paint) {
+    val size : Float = Math.min(w, h) / sizeFactor
+    val dsc : (Int) -> Float = {
+        scale.divideScale(it, parts)
+    }
+    drawXY(w / 2, h / 2 - (h / 2) * dsc(3)) {
+        drawXY(0f, -h * 0.5f * (1 - dsc(0))) {
+            drawLine(0f, 0f, 0f, -size, paint)
+        }
+        drawXY(0f, h * 0.5f * (1 - dsc(1))) {
+            rotate(rot * dsc(2))
+            drawLine(0f, 0f, size, 0f, paint)
+        }
+    }
+}
+
+fun Canvas.drawRARNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = Color.parseColor(colors[i])
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    paint.strokeCap = Paint.Cap.ROUND
+    drawRightAlternateRot(scale, w, h, paint)
+}
