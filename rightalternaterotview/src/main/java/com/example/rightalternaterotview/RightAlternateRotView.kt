9eb3ffad001da3a4a5cdc1e14698026a54f2677b
Anwesh43 <anweshthecool0@gmail.com>
2024-03-15 02:38:37 +0530

    rightalternaterotview: adding RARNode class

--- rightalternaterotview/src/main/java/com/example/rightalternaterotview/RightAlternateRotView.kt
+++ rightalternaterotview/src/main/java/com/example/rightalternaterotview/RightAlternateRotView.kt
@@ -122,4 +122,45 @@ class RightAlternateRotView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class RARNode(var i : Int = 0, val state : State = State()) {
+
+        private var next : RARNode? = null
+        private var prev : RARNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < colors.size - 1) {
+                next = RARNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawRARNode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : RARNode {
+            var curr : RARNode? = prev
+            if (dir == 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
