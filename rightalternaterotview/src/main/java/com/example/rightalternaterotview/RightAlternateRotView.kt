d8451ed8ef53633461086dd379f37373eba73825
Anwesh43 <anweshthecool0@gmail.com>
2024-03-15 02:33:03 +0530

    rightalternaterotview: adding RightAlternateRotView class

--- rightalternaterotview/src/main/java/com/example/rightalternaterotview/RightAlternateRotView.kt
+++ rightalternaterotview/src/main/java/com/example/rightalternaterotview/RightAlternateRotView.kt
@@ -59,3 +59,19 @@ fun Canvas.drawRARNode(i : Int, scale : Float, paint : Paint) {
     paint.strokeCap = Paint.Cap.ROUND
     drawRightAlternateRot(scale, w, h, paint)
 }
+
+class RightAlternateRotView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
