eb299499cfe3baa1cfb80b339c7682bfc1d40471
Anwesh43 <anweshthecool0@gmail.com>
2024-03-18 01:13:48 +0530

    linerotjoinrightview: adding Renderer in View

--- linerotjoinrightview/src/main/java/com/example/linerotjoinrightview/RightLineAlternateRotView.kt
+++ linerotjoinrightview/src/main/java/com/example/linerotjoinrightview/RightLineAlternateRotView.kt
@@ -59,14 +59,16 @@ fun Canvas.drawLRJRNode(i : Int, scale : Float, paint : Paint) {
 
 class LineRotJoinRightView(ctx : Context) : View(ctx) {
 
-    override fun onDraw(canvas : Canvas) {
+    private val renderer : Renderer = Renderer(this)
 
+    override fun onDraw(canvas : Canvas) {
+        renderer.render(canvas)
     }
 
     override fun onTouchEvent(event : MotionEvent) : Boolean {
         when (event.action) {
             MotionEvent.ACTION_DOWN -> {
-
+                renderer.handleTap()
             }
         }
         return true
