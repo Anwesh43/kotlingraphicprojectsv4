ffa8e43e73f68c5db7ec31862dc8bf825091c2f2
Anwesh43 <anweshthecool0@gmail.com>
2024-03-25 13:42:49 +0530

    bentlineleftview: adding Canvas extension methods

--- bentlineleftview/src/main/java/com/example/bentlineleftview/BentLineLeftView.kt
+++ bentlineleftview/src/main/java/com/example/bentlineleftview/BentLineLeftView.kt
@@ -18,7 +18,7 @@ val colors : Array<String> = arrayOf(
 val parts : Int = 4
 val scGap : Float = 0.04f / parts
 val strokeFactor : Float = 90f
-val sizeFactory : Float = 4.9f
+val sizeFactor : Float = 4.9f
 val delay : Long = 20
 val rot : Float = 135f
 val backColor : Int = Color.parseColor("#BDBDBD")
@@ -26,3 +26,34 @@ val backColor : Int = Color.parseColor("#BDBDBD")
 fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
+
+fun Canvas.drawXY(x : Float, y : Float, cb : () -> Unit) {
+    save()
+    translate(x, y)
+    cb()
+    restore()
+}
+
+fun Canvas.drawBentLineLeft(scale : Float, w : Float, h : Float, paint : Paint) {
+    val size : Float = Math.min(w, h) / sizeFactor
+    val dsc : (Int) -> Float = {
+        scale.divideScale(it, parts)
+    }
+    drawXY(w / 2 + (w / 2 + size) * dsc(3), h / 2) {
+        for (j in 0..1) {
+            drawXY(0f, (h / 2) * (1 - 2 * j) * dsc(j)) {
+                rotate((1 - j) * rot * dsc(2))
+                drawLine(0f, 0f, 0f, size * (1f - 2 * j), paint)
+            }
+        }
+    }
+}
+
+fun Canvas.drawRLARNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = Color.parseColor(colors[i])
+    paint.strokeCap = Paint.Cap.ROUND
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    drawBentLineLeft(scale, w, h, paint)
+}
