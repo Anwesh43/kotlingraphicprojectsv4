70bae96a5eb9dc6b93e089ef97a764cd161dcc28
Anwesh43 <anweshthecool0@gmail.com>
2024-03-25 14:59:29 +0530

    bentlineleftview: adding view in activity

--- app/src/main/java/com/example/android_kotlin_graphic_projectsv4/MainActivity.kt
+++ app/src/main/java/com/example/android_kotlin_graphic_projectsv4/MainActivity.kt
@@ -2,7 +2,7 @@ package com.example.android_kotlin_graphic_projectsv4
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.example.linerotjoinrightview.LineRotJoinRightView
+import com.example.bentlineleftview.BentLineLeftView
 
 
 class MainActivity : AppCompatActivity() {
@@ -10,6 +10,6 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         supportActionBar?.hide()
         //setContentView(R.layout.activity_main)
-        LineRotJoinRightView.create(this)
+        BentLineLeftView.create(this)
     }
 }
