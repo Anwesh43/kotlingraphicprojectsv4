c6a14415d7b6c9851945247662da7b7e60a822ab
Anwesh43 <anweshthecool0@gmail.com>
2024-03-29 00:34:22 +0530

    squarebentlineleftview: adding Renderer in View

--- squarebentlineleftview/src/main/java/com/example/squarebentlineleftview/SquareBentLineLeftView.kt
+++ squarebentlineleftview/src/main/java/com/example/squarebentlineleftview/SquareBentLineLeftView.kt
@@ -62,13 +62,17 @@ fun Canvas.drawSBLLNode(i : Int, scale : Float, paint : Paint) {
 
 class SquareBentLineLeftView(ctx : Context) : View(ctx) {
 
-    override fun onDraw(canvas : Canvas) {
+    private val renderer : Renderer = Renderer(this)
 
+    override fun onDraw(canvas : Canvas) {
+        renderer.render(canvas)
     }
 
     override fun onTouchEvent(event : MotionEvent) : Boolean {
         when (event.action) {
-
+           MotionEvent.ACTION_DOWN -> {
+               renderer.handleTap()
+           }
         }
         return true
     }
