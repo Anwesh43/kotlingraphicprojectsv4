9adb60b5dfe53c4731e3e826255fbf66810bc4eb
Anwesh43 <anweshthecool0@gmail.com>
2024-03-29 00:11:12 +0530

    squarebentlineleftview: adding Canvas extension methods

--- squarebentlineleftview/src/main/java/com/example/squarebentlineleftview/SquareBentLineLeftView.kt
+++ squarebentlineleftview/src/main/java/com/example/squarebentlineleftview/SquareBentLineLeftView.kt
@@ -27,3 +27,26 @@ val rot : Float = -45f
 fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
+
+fun Canvas.drawXY(x : Float, y : Float, cb : () -> Unit) {
+    save()
+    translate(x, y)
+    cb()
+    restore()
+}
+
+fun Canvas.drawSquareBentLineLeft(scale : Float, w : Float, h : Float, paint : Paint) {
+    val size : Float = Math.min(w, h) / sizeFactor
+    val dsc : (Int) -> Float = {
+        scale.divideScale(it, parts)
+    }
+    drawXY(w / 2 - (w / 2 + size) * dsc(3), h / 2) {
+        drawXY(0f, 0f) {
+            rotate(rot * dsc(2))
+            drawRect(RectF(-size * dsc(1), 0f, 0f, size), paint)
+        }
+        drawXY(size, 0f) {
+            drawLine(0f, 0f, -size * dsc(0), 0f, paint)
+        }
+    }
+}
