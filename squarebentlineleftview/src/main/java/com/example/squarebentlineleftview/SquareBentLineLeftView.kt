32d945013ca37454acd6caf8977bac065933245e
Anwesh43 <anweshthecool0@gmail.com>
2024-03-29 00:34:55 +0530

    squarebentlineleftview: adding static create method

--- squarebentlineleftview/src/main/java/com/example/squarebentlineleftview/SquareBentLineLeftView.kt
+++ squarebentlineleftview/src/main/java/com/example/squarebentlineleftview/SquareBentLineLeftView.kt
@@ -211,4 +211,13 @@ class SquareBentLineLeftView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    companion object {
+
+        fun create(activity: Activity) : SquareBentLineLeftView {
+            val view : SquareBentLineLeftView = SquareBentLineLeftView(activity)
+            activity.setContentView(view)
+            return view
+        }
+    }
 }
