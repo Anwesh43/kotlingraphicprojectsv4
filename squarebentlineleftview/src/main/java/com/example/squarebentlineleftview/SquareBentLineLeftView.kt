5d7922d16256dff9e12507377d348c8b5e215705
Anwesh43 <anweshthecool0@gmail.com>
2024-03-29 00:30:56 +0530

    squarebentlineleftview: adding SBLLNode class

--- squarebentlineleftview/src/main/java/com/example/squarebentlineleftview/SquareBentLineLeftView.kt
+++ squarebentlineleftview/src/main/java/com/example/squarebentlineleftview/SquareBentLineLeftView.kt
@@ -120,4 +120,45 @@ class SquareBentLineLeftView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class SBLLNode(var i : Int = 0, val state : State = State()) {
+
+        private var next : SBLLNode? = null
+        private var prev : SBLLNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < colors.size - 1) {
+                next = SBLLNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawSBLLNode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : SBLLNode {
+            var curr : SBLLNode? = prev
+            if (dir == 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
