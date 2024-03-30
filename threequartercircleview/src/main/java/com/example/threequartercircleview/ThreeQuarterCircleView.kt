48ac48d28fe895aeafc27da4e85e5e768164bbbf
Anwesh43 <anweshthecool0@gmail.com>
2024-03-30 23:14:56 +0530

    threequartercircleview: adding handletap

--- threequartercircleview/src/main/java/com/example/threequartercircleview/ThreeQuarterCircleView.kt
+++ threequartercircleview/src/main/java/com/example/threequartercircleview/ThreeQuarterCircleView.kt
@@ -66,7 +66,7 @@ class ThreeQuarterCircleView(ctx : Context) : View(ctx) {
     override fun onTouchEvent(event: MotionEvent?): Boolean {
         when (event?.action) {
             MotionEvent.ACTION_DOWN -> {
-
+                renderer.handleTap()
             }
         }
         return true
