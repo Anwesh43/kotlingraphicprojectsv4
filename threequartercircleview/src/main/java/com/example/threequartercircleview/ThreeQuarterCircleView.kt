232938920ff4a54542e51de6ad9142f7e47021fe
Anwesh43 <anweshthecool0@gmail.com>
2024-03-30 11:51:35 +0530

    threequartercircleview: adding Canvas extension methods

--- threequartercircleview/src/main/java/com/example/threequartercircleview/ThreeQuarterCircleView.kt
+++ threequartercircleview/src/main/java/com/example/threequartercircleview/ThreeQuarterCircleView.kt
@@ -27,3 +27,30 @@ val rot : Float = 270f
 fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
+
+fun Canvas.drawXY(x : Float, y : Float, cb : () -> Unit) {
+    save()
+    translate(x, y)
+    cb()
+    restore()
+}
+
+fun Canvas.drawThreeQuarterCircle(scale : Float, w : Float, h : Float, paint : Paint) {
+    val r : Float = Math.min(w, h) / sizeFactor
+    val dsc : (Int) -> Float = {
+        scale.divideScale(it, parts)
+    }
+    drawXY(w / 2, h / 2) {
+        drawXY((w / 2 - r) * (1 - dsc(2)), 0f) {
+            drawArc(RectF(-r, -r, r, r), -90f, 90f * dsc(1), true, paint)
+        }
+        drawArc(RectF(-r, -r, r, r), 0f, rot * dsc(0), true, paint)
+    }
+}
+
+fun Canvas.drawTQCNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = Color.parseColor(colors[i])
+    drawThreeQuarterCircle(scale, w, h, paint)
+}
