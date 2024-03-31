0e5a90965731e6c508cdf58aa7d37e3daa701e70
Anwesh43 <anweshthecool0@gmail.com>
2024-03-31 14:36:03 +0530

    rightanglearccontainerview: adding RightAngleArcContainerView class

--- rightanglearccontainerview/src/main/java/com/example/rightanglearccontainerview/RightAngleArcContainerView.kt
+++ rightanglearccontainerview/src/main/java/com/example/rightanglearccontainerview/RightAngleArcContainerView.kt
@@ -62,3 +62,20 @@ fun Canvas.drawRAACNode(i : Int, scale : Float, paint : Paint) {
     paint.strokeWidth = Math.min(w, h) / strokeFactor
     drawRightAngleArcContainer(scale, w, h, paint)
 }
+
+class RightAngleArcContainerView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
+
