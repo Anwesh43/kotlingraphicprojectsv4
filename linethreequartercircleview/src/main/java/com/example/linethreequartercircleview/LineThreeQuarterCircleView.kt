55da3a02abf28feb21d9f4e135d2127c1e4036cd
Anwesh43 <anweshthecool0@gmail.com>
2024-04-01 09:53:05 +0530

    linethreequartercircleview: adding Canvas extension methods

--- linethreequartercircleview/src/main/java/com/example/linethreequartercircleview/LineThreeQuarterCircleView.kt
+++ linethreequartercircleview/src/main/java/com/example/linethreequartercircleview/LineThreeQuarterCircleView.kt
@@ -27,3 +27,33 @@ val rot : Float = 90f
 fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
+
+fun Canvas.drawXY(x : Float, y : Float, cb : () -> Unit) {
+    save()
+    translate(x, y)
+    cb()
+    restore()
+}
+
+fun Canvas.drawLineThreeQuarterCircle(scale : Float, w : Float, h : Float, paint : Paint) {
+    val size : Float = Math.min(w, h) / sizeFactor
+    val dsc : (Int) -> Float = {
+        scale.divideScale(it, parts)
+    }
+    drawXY(w / 2, h / 2 + (h / 2 + size) * dsc(3)) {
+        rotate(rot * dsc(2))
+        drawArc(RectF(-size / 2, -size / 2, size / 2, size / 2), -90f, 270f * dsc(0), false, paint)
+        drawXY(-size / 2, 0f) {
+            drawLine(0f, 0f, -size * 0.5f * dsc(1), 0f, paint)
+        }
+    }
+}
+
+fun Canvas.drawLTQCNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = Color.parseColor(colors[i])
+    paint.strokeCap = Paint.Cap.ROUND
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    drawLineThreeQuarterCircle(scale, w, h, paint)
+}
