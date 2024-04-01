705e0d2035ecd139d04d0da354ec6a7271a0e45f
Anwesh43 <anweshthecool0@gmail.com>
2024-04-01 23:59:23 +0530

    linethreequartercircleview: adding view in activity

--- app/src/main/java/com/example/android_kotlin_graphic_projectsv4/MainActivity.kt
+++ app/src/main/java/com/example/android_kotlin_graphic_projectsv4/MainActivity.kt
@@ -2,7 +2,7 @@ package com.example.android_kotlin_graphic_projectsv4
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.example.rightanglearccontainerview.RightAngleArcContainerView
+import com.example.linethreequartercircleview.LineThreeQuarterCircleView
 
 
 class MainActivity : AppCompatActivity() {
@@ -10,6 +10,6 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         supportActionBar?.hide()
         //setContentView(R.layout.activity_main)
-        RightAngleArcContainerView.create(this)
+        LineThreeQuarterCircleView.create(this)
     }
 }
