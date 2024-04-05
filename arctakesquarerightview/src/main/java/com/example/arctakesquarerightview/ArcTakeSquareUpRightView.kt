d7f12c24c5c0c1493c6cf537a88e13ca3afbbf11
Anwesh43 <anweshthecool0@gmail.com>
2024-04-05 07:04:03 +0530

    arctakesquarerightview: adding Canvas extension methods

--- arctakesquarerightview/src/main/java/com/example/arctakesquarerightview/ArcTakeSquareUpRightView.kt
+++ arctakesquarerightview/src/main/java/com/example/arctakesquarerightview/ArcTakeSquareUpRightView.kt
@@ -27,3 +27,33 @@ val rot : Float = 180f
 fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
+
+fun Canvas.drawXY(x : Float, y : Float, cb : () -> Unit) {
+    save()
+    translate(x, y)
+    cb()
+    restore()
+}
+
+fun Canvas.drawArcTakeSquareUpRight(scale : Float, w : Float, h : Float, paint : Paint) {
+    val dsc : (Int) -> Float = {
+        scale.divideScale(it, parts)
+    }
+    val size : Float = Math.min(w, h) / sizeFactor
+    drawXY(w / 2, h / 2) {
+        drawXY(0f, -h * 0.5f * (1 - dsc(0))) {
+            rotate(rot * dsc(1))
+            drawRect(RectF(-size, -size, 0f, 0f), paint)
+        }
+        drawXY(-w * 0.5f * (1 - dsc(2)), 0f) {
+            drawArc(RectF(-size, 0f, 0f, size), 0f, 360f, true, paint)
+        }
+    }
+}
+
+fun Canvas.drawATSURNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = Color.parseColor(colors[i])
+    drawArcTakeSquareUpRight(scale, w, h, paint)
+}
