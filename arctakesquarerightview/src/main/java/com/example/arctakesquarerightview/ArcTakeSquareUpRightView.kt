258eb714cf5d41d4b4f111f222b8c7b24c095da2
Anwesh43 <anweshthecool0@gmail.com>
2024-04-05 07:18:58 +0530

    arctakesquarerightview: adding ArcTakeSquareUpRight class

--- arctakesquarerightview/src/main/java/com/example/arctakesquarerightview/ArcTakeSquareUpRightView.kt
+++ arctakesquarerightview/src/main/java/com/example/arctakesquarerightview/ArcTakeSquareUpRightView.kt
@@ -161,4 +161,27 @@ class ArcTakeSquareUpRightView(ctx : Context) : View(ctx) {
             return this
         }
     }
+
+    data class ArcTakeSquareUpRight(var i : Int) {
+
+        private var curr : ATSURNode = ATSURNode(0)
+        private var dir : Int = 1
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            curr.draw(canvas, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            curr.update {
+                curr = curr.getNext(dir) {
+                    dir *= -1
+                }
+                cb(it)
+            }
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            curr.startUpdating(cb)
+        }
+    }
 }
