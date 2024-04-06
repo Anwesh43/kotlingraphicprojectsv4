3cba0561e7cbfbf724552322d934a45edf6053a7
Anwesh43 <anweshthecool0@gmail.com>
2024-04-06 11:28:26 +0530

    linebentgrowleftview: adding Canvas extesion methods

--- linebentgrowleftview/src/main/java/com/example/linebentgrowleftview/LineBentGrowLeftView.kt
+++ linebentgrowleftview/src/main/java/com/example/linebentgrowleftview/LineBentGrowLeftView.kt
@@ -26,3 +26,33 @@ val rot : Float = 45f
 fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
+
+fun Canvas.drawXY(x : Float, y : Float, cb : () -> Unit) {
+    save()
+    translate(x, y)
+    cb()
+    restore()
+}
+
+fun Canvas.drawLineBentGrowLeft(scale : Float, w : Float, h : Float, paint : Paint) {
+    val size : Float = Math.min(w, h) / sizeFactor
+    val dsc : (Int) -> Float = {
+        scale.divideScale(it, parts)
+    }
+    drawXY(w / 2 - (w / 2 + size) * dsc(3), h / 2) {
+        drawXY(0f, 0f) {
+            rotate(rot * dsc(1))
+            drawLine(0f, 0f, 0f, -size * dsc(0), paint)
+        }
+        drawLine(0f, 0f, -size * dsc(2), 0f, paint)
+    }
+}
+
+fun Canvas.drawLBGLNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = Color.parseColor(colors[i])
+    paint.strokeCap = Paint.Cap.ROUND
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    drawLineBentGrowLeft(scale, w, h, paint)
+}
