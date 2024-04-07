4445d42d477b8cb7468434996866fdefd99512d5
Anwesh43 <anweshthecool0@gmail.com>
2024-04-07 17:35:19 +0530

    sqrightangleleftview: adding SqRightAngleLeftView class

--- sqrightangleleftview/src/main/java/com/example/sqrightangleleftview/SqRightAngleLeftView.kt
+++ sqrightangleleftview/src/main/java/com/example/sqrightangleleftview/SqRightAngleLeftView.kt
@@ -57,3 +57,19 @@ fun Canvas.drawSRALNode(i : Int, scale : Float, paint : Paint) {
     paint.color = Color.parseColor(colors[i])
     drawSqRightAngleLeft(scale, w, h, paint)
 }
+
+class SqRightAngleLeftView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
