79317780ef0849f99bb1751ea19d660700c7021e
Anwesh43 <anweshthecool0@gmail.com>
2024-04-07 17:31:27 +0530

    sqrightangleleftview: adding Canvas extension methods

--- sqrightangleleftview/src/main/java/com/example/sqrightangleleftview/SqRightAngleLeftView.kt
+++ sqrightangleleftview/src/main/java/com/example/sqrightangleleftview/SqRightAngleLeftView.kt
@@ -26,3 +26,34 @@ val rot : Float = 90f
 fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
+
+fun Canvas.drawXY(x : Float, y : Float, cb : () -> Unit) {
+    save()
+    translate(x, y)
+    cb()
+    restore()
+}
+
+fun Canvas.drawSqRightAngleLeft(scale : Float, w : Float, h : Float, paint : Paint) {
+    val size : Float = Math.min(w, h) / sizeFactor
+    val dsc : (Int) -> Float = {
+        scale.divideScale(it, parts)
+    }
+    drawXY(w / 2 - (w / 2) * dsc(3), h / 2) {
+        rotate(rot * dsc(2))
+        for (j in 0..1) {
+            drawXY(-(w / 2) * (1 - dsc(0)), 0f) {
+                rotate(-rot * dsc(1) * j)
+                scale(1f, 1f - 2 * j)
+                drawRect(RectF(-size, -size / 2, 0f, 0f), paint)
+            }
+        }
+    }
+}
+
+fun Canvas.drawSRALNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = Color.parseColor(colors[i])
+    drawSqRightAngleLeft(scale, w, h, paint)
+}
