003414889bc7f84692a376ebf7a0cbdfebc4ab62
Anwesh43 <anweshthecool0@gmail.com>
2024-04-07 17:41:46 +0530

    sqrightangleleftview: adding SqRightAngleLeft class

--- sqrightangleleftview/src/main/java/com/example/sqrightangleleftview/SqRightAngleLeftView.kt
+++ sqrightangleleftview/src/main/java/com/example/sqrightangleleftview/SqRightAngleLeftView.kt
@@ -161,4 +161,27 @@ class SqRightAngleLeftView(ctx : Context) : View(ctx) {
             return this
         }
     }
+
+    data class SqRightAngleLeft(var i : Int) {
+
+        private var curr : SRALNode = SRALNode(0)
+        private var dir : Int = 1
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            curr.draw(canvas, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            curr.update {
+                curr = curr.getNext(dir) {
+                    dir *= -1
+                }
+                cb(it)
+            }
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            curr.startUpdating(cb)
+        }
+    }
 }
