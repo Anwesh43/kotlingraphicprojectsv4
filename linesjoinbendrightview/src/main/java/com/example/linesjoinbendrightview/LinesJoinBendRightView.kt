cf47b90a410962d27bbaeead0e78291bbe85e2a8
Anwesh43 <anweshthecool0@gmail.com>
2024-04-09 13:14:35 +0530

    linesjoinbendrightview: adding Canvas extension methods

--- linesjoinbendrightview/src/main/java/com/example/linesjoinbendrightview/LinesJoinBendRightView.kt
+++ linesjoinbendrightview/src/main/java/com/example/linesjoinbendrightview/LinesJoinBendRightView.kt
@@ -27,3 +27,34 @@ fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
 
+fun Canvas.drawXY(x : Float, y : Float, cb : () -> Unit) {
+    save()
+    translate(x, y)
+    cb()
+    restore()
+}
+
+fun Canvas.drawLinesJoinBendRight(scale : Float, w : Float, h : Float, paint : Paint) {
+    val size : Float = Math.min(w, h) / sizeFactor
+    val dsc : (Int) -> Float = {
+        scale.divideScale(it, parts)
+    }
+    drawXY(w / 2 + (w / 2 + size) * dsc(3), h / 2) {
+        drawXY(-(w / 2) * (1 - dsc(0)), 0f) {
+            drawLine(0f, 0f, -size, 0f, paint)
+        }
+        drawXY(0f, (h / 2) * (1 - dsc(1))) {
+            rotate(-rot * dsc(2))
+            drawLine(0f, 0f, 0f, -size, paint)
+        }
+    }
+}
+
+fun Canvas.drawLJBRNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = Color.parseColor(colors[i])
+    paint.strokeCap = Paint.Cap.ROUND
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    drawLinesJoinBendRight(scale, w, h, paint)
+}
