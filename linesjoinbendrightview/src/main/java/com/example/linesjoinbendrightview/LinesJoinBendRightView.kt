ec4da7ebc36f8bff946c872e2dd2c74963fbf4c4
Anwesh43 <anweshthecool0@gmail.com>
2024-04-09 14:21:40 +0530

    linesjoinbendrightview: adding LinesJoinBendRight class

--- linesjoinbendrightview/src/main/java/com/example/linesjoinbendrightview/LinesJoinBendRightView.kt
+++ linesjoinbendrightview/src/main/java/com/example/linesjoinbendrightview/LinesJoinBendRightView.kt
@@ -162,4 +162,27 @@ class LinesJoinBendRightView(ctx : Context) : View(ctx) {
             return this
         }
     }
+
+    data class LineJoinBendRight(var i : Int) {
+
+        private var curr : LJBRNode = LJBRNode(0)
+        private var dir : Int = 1
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            curr.draw(canvas, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            curr.update {
+                curr = curr.getNext(dir) {
+                    dir *= -1
+                }
+                cb(it)
+            }
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            curr.startUpdating(cb)
+        }
+    }
 }
