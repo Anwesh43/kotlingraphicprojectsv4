c250bdcdb5578aeb918b8fa2019e00c30b6f1dfa
Anwesh43 <anweshthecool0@gmail.com>
2024-04-11 14:00:54 +0530

    linerightseparateview: adding renderer in view

--- linerightseparateview/src/main/java/com/example/linerightseparateview/LineRightSeparateView.kt
+++ linerightseparateview/src/main/java/com/example/linerightseparateview/LineRightSeparateView.kt
@@ -60,14 +60,16 @@ fun Canvas.drawLRSNode(i : Int, scale : Float, paint : Paint) {
 
 class LineRightSeparateView(ctx : Context) : View(ctx) {
 
-    override fun onDraw(canvas : Canvas) {
+    private val renderer : Renderer = Renderer(this)
 
+    override fun onDraw(canvas : Canvas) {
+        renderer.render(canvas)
     }
 
     override fun onTouchEvent(event : MotionEvent) : Boolean {
         when (event.action) {
             MotionEvent.ACTION_DOWN -> {
-
+                renderer.handleTap()
             }
         }
         return true
