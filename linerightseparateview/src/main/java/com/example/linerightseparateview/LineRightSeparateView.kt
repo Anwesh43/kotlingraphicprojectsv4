f3a13c009d421c3aa19d62224799410783a8bc48
Anwesh43 <anweshthecool0@gmail.com>
2024-04-11 13:27:59 +0530

    linerightseparateview: adding LRSNode class

--- linerightseparateview/src/main/java/com/example/linerightseparateview/LineRightSeparateView.kt
+++ linerightseparateview/src/main/java/com/example/linerightseparateview/LineRightSeparateView.kt
@@ -119,4 +119,45 @@ class LineRightSeparateView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class LRSNode(var i : Int = 0, val state : State = State()) {
+
+        private var next : LRSNode? = null
+        private var prev : LRSNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < colors.size - 1) {
+                next = LRSNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawLRSNode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : LRSNode {
+            var curr : LRSNode? = prev
+            if (dir == 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
