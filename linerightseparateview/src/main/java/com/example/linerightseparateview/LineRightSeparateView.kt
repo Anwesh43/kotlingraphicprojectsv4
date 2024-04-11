9e7cec3bc3acdf6889445436b1ee6be45b80d270
Anwesh43 <anweshthecool0@gmail.com>
2024-04-11 13:29:14 +0530

    linerightseparateview: adding LineRightSeparate class

--- linerightseparateview/src/main/java/com/example/linerightseparateview/LineRightSeparateView.kt
+++ linerightseparateview/src/main/java/com/example/linerightseparateview/LineRightSeparateView.kt
@@ -160,4 +160,27 @@ class LineRightSeparateView(ctx : Context) : View(ctx) {
             return this
         }
     }
+
+    data class LineRightSeparate(var i : Int) {
+
+        private var curr : LRSNode = LRSNode(0)
+        private var dir : Int = 1
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            curr.draw(canvas, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            curr.update {
+                curr = curr.getNext(dir) {
+                    dir *= -1
+                }
+                cb(it)
+            }
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            curr.startUpdating(cb)
+        }
+    }
 }
