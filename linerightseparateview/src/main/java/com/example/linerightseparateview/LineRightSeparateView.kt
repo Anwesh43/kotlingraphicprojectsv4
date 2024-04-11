f17eda48f493c8879b75b4c9147327339a1300b1
Anwesh43 <anweshthecool0@gmail.com>
2024-04-11 13:23:26 +0530

    linerightseparateview: adding Animator class

--- linerightseparateview/src/main/java/com/example/linerightseparateview/LineRightSeparateView.kt
+++ linerightseparateview/src/main/java/com/example/linerightseparateview/LineRightSeparateView.kt
@@ -92,4 +92,31 @@ class LineRightSeparateView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class Animator(var view : View, var animated : Boolean = false) {
+
+        fun animate(cb : () -> Unit) {
+            if (animated) {
+                cb()
+                try {
+
+                } catch(ex : Exception) {
+
+                }
+            }
+        }
+
+        fun start() {
+            if (!animated) {
+                animated = true
+                view.postInvalidate()
+            }
+        }
+
+        fun stop() {
+            if (animated) {
+                animated = false
+            }
+        }
+    }
 }
