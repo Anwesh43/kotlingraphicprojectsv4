d6d36b128f8f9346baf7d7774bb453f5311722fd
Anwesh43 <anweshthecool0@gmail.com>
2024-04-13 01:19:43 +0530

    linebigshortarcview: adding Canvas extenxion methods

--- linebigshortarcview/src/main/java/com/example/linebigshortarcview/LineBigShortArcView.kt
+++ linebigshortarcview/src/main/java/com/example/linebigshortarcview/LineBigShortArcView.kt
@@ -26,3 +26,32 @@ val backColor : Int = Color.parseColor("#BDBDBD")
 fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
+
+fun Canvas.drawXY(x : Float, y : Float, cb : () -> Unit) {
+    save()
+    translate(x, y)
+    cb()
+    restore()
+}
+
+fun Canvas.drawLineBigShortArc(scale : Float, w : Float, h : Float, paint : Paint) {
+    val size : Float = Math.min(w, h) / sizeFactor
+    val dsc : (Int) -> Float = {
+        scale.divideScale(it, parts)
+    }
+    drawXY(w / 2, h / 2 + (h / 2 + size) * dsc(3)) {
+        drawLine(-size / 2, -size * (1 - dsc(0)), -size / 2, -size, paint)
+        drawArc(RectF(-size / 2, -size / 2, size / 2, size / 2), 180f * (1 - dsc(1)), 180f * dsc(1), false, paint)
+        drawLine(size / 2, 0f, size / 2, -size * 0.5f * dsc(2), paint)
+    }
+}
+
+fun Canvas.drawLBSANode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = Color.parseColor(colors[i])
+    paint.strokeCap = Paint.Cap.ROUND
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    paint.style = Paint.Style.STROKE
+    drawLineBigShortArc(scale, w, h, paint)
+}
