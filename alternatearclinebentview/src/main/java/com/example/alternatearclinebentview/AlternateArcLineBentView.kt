e4835d889215c7771e3e3f0b7740f9afbcb6fd6a
Anwesh43 <anweshthecool0@gmail.com>
2024-04-12 17:53:17 +0530

    alternatearclinebentview: adding AlternateArcLineBentView class

--- alternatearclinebentview/src/main/java/com/example/alternatearclinebentview/AlternateArcLineBentView.kt
+++ alternatearclinebentview/src/main/java/com/example/alternatearclinebentview/AlternateArcLineBentView.kt
@@ -65,3 +65,19 @@ fun Canvas.drawAALBNode(i : Int, scale : Float, paint : Paint) {
     paint.strokeWidth = Math.min(w, h) / strokeFactor
     drawAlternateArcLine(scale, w, h, paint)
 }
+
+class AlternateArcLineBentView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
