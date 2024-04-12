74335adf3885ed193e3f43461c0cddf3931feda7
Anwesh43 <anweshthecool0@gmail.com>
2024-04-12 18:16:35 +0530

    alternatearclinebentview: adding Animator class

--- alternatearclinebentview/src/main/java/com/example/alternatearclinebentview/AlternateArcLineBentView.kt
+++ alternatearclinebentview/src/main/java/com/example/alternatearclinebentview/AlternateArcLineBentView.kt
@@ -100,4 +100,32 @@ class AlternateArcLineBentView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class Animator(var view : View, var animated : Boolean = false) {
+
+        fun animate(cb : () -> Unit) {
+            if (animated) {
+                cb()
+                try {
+                    Thread.sleep(delay)
+                    view.invalidate()
+                } catch(ex : Exception) {
+
+                }
+            }
+        }
+
+        fun start() {
+            if (!animated) {
+                animated = true
+                view.postInvalidate()
+            }
+        }
+
+        fun stop() {
+            if (animated) {
+                animated = false
+            }
+        }
+    }
 }
