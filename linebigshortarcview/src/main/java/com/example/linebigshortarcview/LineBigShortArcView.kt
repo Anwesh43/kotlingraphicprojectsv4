fbe6c148f007bd7a6e4bfd56feb1a983d98535c3
Anwesh43 <anweshthecool0@gmail.com>
2024-04-13 01:20:55 +0530

    linebigshortarcview: adding LineBigShortArcView class

--- linebigshortarcview/src/main/java/com/example/linebigshortarcview/LineBigShortArcView.kt
+++ linebigshortarcview/src/main/java/com/example/linebigshortarcview/LineBigShortArcView.kt
@@ -55,3 +55,19 @@ fun Canvas.drawLBSANode(i : Int, scale : Float, paint : Paint) {
     paint.style = Paint.Style.STROKE
     drawLineBigShortArc(scale, w, h, paint)
 }
+
+class LineBigShortArcView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas: Canvas) {
+
+    }
+
+    override fun onTouchEvent(event: MotionEvent): Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
