931abb654b51c259df8eabc5b4e70a51a4e0d88f
Anwesh43 <anweshthecool0@gmail.com>
2024-04-14 21:57:58 +0530

    rightlinearcturnview: adding RightLineArcTurnView class

--- rightlinearcturnview/src/main/java/com/example/rightlinearcturnview/RightLineArcTurnView.kt
+++ rightlinearcturnview/src/main/java/com/example/rightlinearcturnview/RightLineArcTurnView.kt
@@ -61,3 +61,19 @@ fun Canvas.drawRightLineArcTurn(scale : Float, w : Float, h : Float, paint : Pai
         drawRightLineArcTurn(scale, w, h, paint)
     }
 }
+
+class RightLineArcTurnView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
