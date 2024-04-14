cc14684fa9e583ce0de68c0be87554d2f11f2863
Anwesh43 <anweshthecool0@gmail.com>
2024-04-14 18:04:42 +0530

    linebigshortarcview: adding view in activity

--- app/src/main/java/com/example/android_kotlin_graphic_projectsv4/MainActivity.kt
+++ app/src/main/java/com/example/android_kotlin_graphic_projectsv4/MainActivity.kt
@@ -2,13 +2,13 @@ package com.example.android_kotlin_graphic_projectsv4
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.example.alternatearclinebentview.AlternateArcLineBentView
+import com.example.linebigshortarcview.LineBigShortArcView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         supportActionBar?.hide()
         //setContentView(R.layout.activity_main)
-        AlternateArcLineBentView.create(this)
+        LineBigShortArcView.create(this)
     }
 }
