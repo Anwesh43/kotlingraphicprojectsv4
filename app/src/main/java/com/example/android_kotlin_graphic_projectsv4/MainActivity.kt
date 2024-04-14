b1bbda4f2d06fbb5271ecd49b94d46216a50de70
Anwesh43 <anweshthecool0@gmail.com>
2024-04-14 23:59:30 +0530

    rightlinearcturnview: adding view in activity

--- app/src/main/java/com/example/android_kotlin_graphic_projectsv4/MainActivity.kt
+++ app/src/main/java/com/example/android_kotlin_graphic_projectsv4/MainActivity.kt
@@ -2,13 +2,13 @@ package com.example.android_kotlin_graphic_projectsv4
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.example.linebigshortarcview.LineBigShortArcView
+import com.example.rightlinearcturnview.RightLineArcTurnView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         supportActionBar?.hide()
         //setContentView(R.layout.activity_main)
-        LineBigShortArcView.create(this)
+        RightLineArcTurnView.create(this)
     }
 }
