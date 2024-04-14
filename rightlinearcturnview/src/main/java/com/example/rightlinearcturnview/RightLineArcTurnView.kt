43080d4d1cece5a8299f6ff2c6b9444a80d9d8c6
Anwesh43 <anweshthecool0@gmail.com>
2024-04-14 23:58:48 +0530

    rightlinearcturnview: adding static create method

--- rightlinearcturnview/src/main/java/com/example/rightlinearcturnview/RightLineArcTurnView.kt
+++ rightlinearcturnview/src/main/java/com/example/rightlinearcturnview/RightLineArcTurnView.kt
@@ -213,4 +213,13 @@ class RightLineArcTurnView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    companion object {
+
+        fun create(activity : Activity) : RightLineArcTurnView {
+            val view : RightLineArcTurnView = RightLineArcTurnView(activity)
+            activity.setContentView(view)
+            return view
+        }
+    }
 }
