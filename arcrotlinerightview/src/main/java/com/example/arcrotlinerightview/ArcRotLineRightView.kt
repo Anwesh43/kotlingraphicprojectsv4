441b7c8e39b3d91291425879c265cea5557be5b0
Anwesh43 <anweshthecool0@gmail.com>
2024-04-17 14:07:39 +0530

    arcrotlinerightview: adding ARLRNode class

--- arcrotlinerightview/src/main/java/com/example/arcrotlinerightview/ArcRotLineRightView.kt
+++ arcrotlinerightview/src/main/java/com/example/arcrotlinerightview/ArcRotLineRightView.kt
@@ -121,5 +121,46 @@ class ArcRotLineRightView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class ARLRNode(var i : Int = 0, val state : State = State()) {
+
+        private var next : ARLRNode? = null
+        private var prev : ARLRNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < colors.size - 1) {
+                next = ARLRNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawARLRNode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : ARLRNode {
+            var curr : ARLRNode? = prev
+            if (dir == 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
 
