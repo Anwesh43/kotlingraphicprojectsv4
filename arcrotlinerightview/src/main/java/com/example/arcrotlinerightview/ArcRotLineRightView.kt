95aed05a02589c98822e3fc10f4a8bb5e5f9acf9
Anwesh43 <anweshthecool0@gmail.com>
2024-04-17 14:09:03 +0530

    arcrotlinerightview: adding ArcRotLineRight class

--- arcrotlinerightview/src/main/java/com/example/arcrotlinerightview/ArcRotLineRightView.kt
+++ arcrotlinerightview/src/main/java/com/example/arcrotlinerightview/ArcRotLineRightView.kt
@@ -162,5 +162,28 @@ class ArcRotLineRightView(ctx : Context) : View(ctx) {
             return this
         }
     }
+
+    data class ArcRotLineRight(var i : Int) {
+
+        private var curr : ARLRNode = ARLRNode(0)
+        private var dir : Int = 1
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            curr.draw(canvas, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            curr.update {
+                curr = curr.getNext(dir) {
+                    dir *= -1
+                }
+                cb(it)
+            }
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            curr.startUpdating(cb)
+        }
+    }
 }
 
