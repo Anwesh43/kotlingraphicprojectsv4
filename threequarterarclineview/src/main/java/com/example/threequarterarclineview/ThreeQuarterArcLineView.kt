9bb866ec4c52ef6b5d115468a286dc3f1d3e3f35
Anwesh43 <anweshthecool0@gmail.com>
2024-04-18 18:36:58 +0530

    threequarterarclineview: adding Canvas extension methods

--- threequarterarclineview/src/main/java/com/example/threequarterarclineview/ThreeQuarterArcLineView.kt
+++ threequarterarclineview/src/main/java/com/example/threequarterarclineview/ThreeQuarterArcLineView.kt
@@ -29,3 +29,32 @@ val sweep : Float = 3 * rot
 fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
+
+fun Canvas.drawXY(x : Float, y : Float, cb : () -> Unit) {
+    save()
+    translate(x, y)
+    cb()
+    restore()
+}
+
+fun Canvas.drawThreeQuarterArcLine(scale : Float, w : Float, h : Float, paint : Paint) {
+    val size : Float = Math.min(w, h) / sizeFactor
+    val dsc : (Int) -> Float = {
+        scale.divideScale(it, parts)
+    }
+    drawXY(w / 2, h / 2 + (h / 2 + size) * dsc(3)) {
+        rotate(rot * dsc(2))
+        drawArc(RectF(0f, -size / 2, size, size / 2), start, sweep * dsc(0), false, paint)
+        drawLine(0f, 0f, -size * dsc(1), 0f, paint)
+    }
+}
+
+fun Canvas.drawTQALNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = Color.parseColor(colors[i])
+    paint.strokeCap = Paint.Cap.ROUND
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    paint.style = Paint.Style.STROKE
+    drawThreeQuarterArcLine(scale, w, h, paint)
+}
