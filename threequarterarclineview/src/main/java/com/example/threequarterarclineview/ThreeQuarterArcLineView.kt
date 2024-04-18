6e53b9d476fe4d0ed91c15f7f600668385207381
Anwesh43 <anweshthecool0@gmail.com>
2024-04-18 19:11:10 +0530

    threequarterarclineview: adding ThreeQuarterArcLine class

--- threequarterarclineview/src/main/java/com/example/threequarterarclineview/ThreeQuarterArcLineView.kt
+++ threequarterarclineview/src/main/java/com/example/threequarterarclineview/ThreeQuarterArcLineView.kt
@@ -162,4 +162,27 @@ class ThreeQuarterArcLineView(ctx : Context) : View(ctx) {
             return this
         }
     }
+
+    data class ThreeQuarterArcLine(var i : Int) {
+
+        private var curr : TQALNode = TQALNode(0)
+        private var dir : Int = 1
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            curr.draw(canvas, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            curr.update {
+                curr = curr.getNext(dir) {
+                    dir *= -1
+                }
+                cb(it)
+            }
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            curr.startUpdating(cb)
+        }
+    }
 }
