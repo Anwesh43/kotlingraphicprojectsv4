333b7d2f14c11e2f45069f698fcef3adfc5cd78e
Anwesh43 <anweshthecool0@gmail.com>
2024-04-18 18:56:44 +0530

    threequarterarclineview: adding TQALNode class

--- threequarterarclineview/src/main/java/com/example/threequarterarclineview/ThreeQuarterArcLineView.kt
+++ threequarterarclineview/src/main/java/com/example/threequarterarclineview/ThreeQuarterArcLineView.kt
@@ -121,4 +121,45 @@ class ThreeQuarterArcLineView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class TQALNode(var i : Int = 0, val state : State = State()) {
+
+        private var next : TQALNode? = null
+        private var prev : TQALNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < colors.size - 1) {
+                next = TQALNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawTQALNode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : TQALNode {
+            var curr : TQALNode? = prev
+            if (dir == 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
