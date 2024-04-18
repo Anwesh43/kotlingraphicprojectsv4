a5e7b7cd1823497a05f9ee1ba7cd0bb8a2fccd3a
Anwesh43 <anweshthecool0@gmail.com>
2024-04-18 19:41:03 +0530

    threequarterarclineview: adding view in activity

--- app/src/main/java/com/example/android_kotlin_graphic_projectsv4/MainActivity.kt
+++ app/src/main/java/com/example/android_kotlin_graphic_projectsv4/MainActivity.kt
@@ -2,13 +2,13 @@ package com.example.android_kotlin_graphic_projectsv4
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.example.arcrotlinerightview.ArcRotLineRightView
+import com.example.threequarterarclineview.ThreeQuarterArcLineView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         supportActionBar?.hide()
         //setContentView(R.layout.activity_main)
-        ArcRotLineRightView.create(this)
+        ThreeQuarterArcLineView.create(this)
     }
 }
