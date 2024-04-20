6db839724bd1dcb186d31e90f5e21e5fad72e9c6
Anwesh43 <anweshthecool0@gmail.com>
2024-04-20 13:27:20 +0530

    linetorightarcview: adding LineToRightArcView class

--- linetorightarcview/src/main/java/com/example/linetorightarcview/LineToRightArcView.kt
+++ linetorightarcview/src/main/java/com/example/linetorightarcview/LineToRightArcView.kt
@@ -61,3 +61,19 @@ fun Canvas.drawLTRANode(i : Int, scale : Float, paint : Paint) {
     paint.style = Paint.Style.STROKE
     drawLineToRightArc(scale, w, h, paint)
 }
+
+class LineToRightArcView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
