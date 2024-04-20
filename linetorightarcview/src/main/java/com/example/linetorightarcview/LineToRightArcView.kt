7e745779114a1109416b4537252eb6937c510fe6
Anwesh43 <anweshthecool0@gmail.com>
2024-04-20 14:29:26 +0530

    linetorightarcview: adding Renderer class

--- linetorightarcview/src/main/java/com/example/linetorightarcview/LineToRightArcView.kt
+++ linetorightarcview/src/main/java/com/example/linetorightarcview/LineToRightArcView.kt
@@ -188,4 +188,27 @@ class LineToRightArcView(ctx : Context) : View(ctx) {
             curr.startUpdating(cb)
         }
     }
+
+    data class Renderer(var view : LineToRightArcView) {
+
+        private val animator : Animator = Animator(view)
+        private val lrta : LineToRightArc = LineToRightArc(0)
+        private val paint : Paint = Paint(Paint.ANTI_ALIAS_FLAG)
+
+        fun render(canvas : Canvas) {
+            canvas.drawColor(backColor)
+            lrta.draw(canvas, paint)
+            animator.animate {
+                lrta.update {
+                    animator.stop()
+                }
+            }
+        }
+
+        fun handleTap() {
+            lrta.startUpdating {
+                animator.start()
+            }
+        }
+    }
 }
