65d9f9889e3f88a314e136e9ee2e9f9cceeb2011
Anwesh43 <anweshthecool0@gmail.com>
2024-04-20 14:19:15 +0530

    linetorightarcview: adding LTRANode class

--- linetorightarcview/src/main/java/com/example/linetorightarcview/LineToRightArcView.kt
+++ linetorightarcview/src/main/java/com/example/linetorightarcview/LineToRightArcView.kt
@@ -124,4 +124,45 @@ class LineToRightArcView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class LTRANode(var i : Int = 0, val state : State = State()) {
+
+        private var next : LTRANode? = null
+        private var prev : LTRANode? = null
+
+        init {
+
+        }
+
+        fun addNeighbor() {
+            if (i < colors.size - 1) {
+                next = LTRANode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawLTRANode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : LTRANode {
+            var curr : LTRANode? = prev
+            if (dir == 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
