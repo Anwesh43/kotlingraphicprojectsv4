0c8886b6b9be03be04484bc973ca35caef9f2aa6
Anwesh43 <anweshthecool0@gmail.com>
2024-04-21 14:04:26 +0530

    doublearclineview: adding DALNode class

--- doublearclineview/src/main/java/com/example/doublearclineview/DoubleArcLineView.kt
+++ doublearclineview/src/main/java/com/example/doublearclineview/DoubleArcLineView.kt
@@ -123,4 +123,45 @@ class DoubleArcLineView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class DALNode(var i : Int = 0, val state : State = State()) {
+
+        private var next : DALNode? = null
+        private var prev : DALNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < colors.size - 1) {
+                next = DALNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawDALNode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : DALNode {
+            var curr : DALNode? = prev
+            if (dir == 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
