31c362e89f0703a10b5c3fc65206812af230b852
Anwesh43 <anweshthecool0@gmail.com>
2024-04-21 14:09:05 +0530

    doublearclineview: adding view in activity

--- app/src/main/java/com/example/android_kotlin_graphic_projectsv4/MainActivity.kt
+++ app/src/main/java/com/example/android_kotlin_graphic_projectsv4/MainActivity.kt
@@ -2,13 +2,13 @@ package com.example.android_kotlin_graphic_projectsv4
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.example.linetorightarcview.LineToRightArcView
+import com.example.doublearclineview.DoubleArcLineView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         supportActionBar?.hide()
         //setContentView(R.layout.activity_main)
-        LineToRightArcView.create(this)
+        DoubleArcLineView.create(this)
     }
 }
