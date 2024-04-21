7464825c1ec949b097e6a6a6275266005039c882
Anwesh43 <anweshthecool0@gmail.com>
2024-04-21 13:29:35 +0530

    doublearclineview: adding DoubleArcLineView class

--- doublearclineview/src/main/java/com/example/doublearclineview/DoubleArcLineView.kt
+++ doublearclineview/src/main/java/com/example/doublearclineview/DoubleArcLineView.kt
@@ -60,3 +60,19 @@ fun Canvas.drawDALNode(i : Int, scale : Float, paint : Paint) {
     paint.style = Paint.Style.STROKE
     drawDoubleArcLine(scale, w, h, paint)
 }
+
+class DoubleArcLineView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
