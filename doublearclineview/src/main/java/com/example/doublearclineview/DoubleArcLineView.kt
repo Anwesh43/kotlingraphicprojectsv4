edfae5084e12ab44085dbc2c59580183f2e8b1a9
Anwesh43 <anweshthecool0@gmail.com>
2024-04-21 14:05:49 +0530

    doublearclineview: adding DoubleArcLine class

--- doublearclineview/src/main/java/com/example/doublearclineview/DoubleArcLineView.kt
+++ doublearclineview/src/main/java/com/example/doublearclineview/DoubleArcLineView.kt
@@ -164,4 +164,27 @@ class DoubleArcLineView(ctx : Context) : View(ctx) {
             return this
         }
     }
+
+    data class DoubleArcLine(var i : Int) {
+
+        private var curr : DALNode = DALNode(0)
+        private var dir : Int = 1
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            curr.draw(canvas, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            curr.update {
+                curr = curr.getNext(dir) {
+                    dir *= -1
+                }
+                cb(it)
+            }
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            curr.startUpdating(cb)
+        }
+    }
 }
