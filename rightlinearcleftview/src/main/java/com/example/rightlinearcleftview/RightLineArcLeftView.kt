5f8d03231e27025e791049640a2f094411d9846a
Anwesh43 <anweshthecool0@gmail.com>
2024-04-22 20:51:51 +0530

    rightlinearcleftview: adding Renderer class

--- rightlinearcleftview/src/main/java/com/example/rightlinearcleftview/RightLineArcLeftView.kt
+++ rightlinearcleftview/src/main/java/com/example/rightlinearcleftview/RightLineArcLeftView.kt
@@ -185,4 +185,27 @@ class RightLineArcLeftView(ctx : Context) : View(ctx) {
             curr.startUpdating(cb)
         }
     }
+
+    data class Renderer(var view : RightLineArcLeftView) {
+
+        private val animator : Animator = Animator(view)
+        private val rlal : RightLineArcLeft = RightLineArcLeft(0)
+        private val paint : Paint = Paint(Paint.ANTI_ALIAS_FLAG)
+
+        fun render(canvas : Canvas) {
+            canvas.drawColor(backColor)
+            rlal.draw(canvas, paint)
+            animator.animate {
+                rlal.update {
+                    animator.stop()
+                }
+            }
+        }
+
+        fun handleTap() {
+            rlal.startUpdating {
+                animator.start()
+            }
+        }
+    }
 }
