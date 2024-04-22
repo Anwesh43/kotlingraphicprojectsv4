333155ea3953cb236f72b5e095d90700b0481bbb
Anwesh43 <anweshthecool0@gmail.com>
2024-04-22 20:49:36 +0530

    rightlinearcleftview: adding RightLineArcLeft class

--- rightlinearcleftview/src/main/java/com/example/rightlinearcleftview/RightLineArcLeftView.kt
+++ rightlinearcleftview/src/main/java/com/example/rightlinearcleftview/RightLineArcLeftView.kt
@@ -162,4 +162,27 @@ class RightLineArcLeftView(ctx : Context) : View(ctx) {
             return this
         }
     }
+
+    data class RightLineArcLeft(var i : Int) {
+
+        private var curr : RLALNode = RLALNode(0)
+        private var dir : Int = 1
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            curr.draw(canvas, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            curr.update {
+                curr = curr.getNext(dir) {
+                    dir *= -1
+                }
+                cb(it)
+            }
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            curr.startUpdating(cb)
+        }
+    }
 }
