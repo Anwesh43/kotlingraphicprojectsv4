b81beb634e007271a66b7885d67a115eef7993a3
Anwesh43 <anweshthecool0@gmail.com>
2024-04-22 20:42:48 +0530

    rightlinearcleftview: adding RLALNode class

--- rightlinearcleftview/src/main/java/com/example/rightlinearcleftview/RightLineArcLeftView.kt
+++ rightlinearcleftview/src/main/java/com/example/rightlinearcleftview/RightLineArcLeftView.kt
@@ -121,4 +121,45 @@ class RightLineArcLeftView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class RLALNode(var i : Int = 0, val state : State = State()) {
+
+        private var next : RLALNode? = null
+        private var prev : RLALNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < colors.size - 1) {
+                next = RLALNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawRLALNode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : RLALNode {
+            var curr : RLALNode? = prev
+            if (dir == 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
