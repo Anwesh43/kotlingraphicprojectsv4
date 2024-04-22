2156937c9d191967962d8323fcec0832fa7366f8
Anwesh43 <anweshthecool0@gmail.com>
2024-04-22 20:55:43 +0530

    rightlinearcleftview: adding view in activity

--- app/src/main/java/com/example/android_kotlin_graphic_projectsv4/MainActivity.kt
+++ app/src/main/java/com/example/android_kotlin_graphic_projectsv4/MainActivity.kt
@@ -2,13 +2,13 @@ package com.example.android_kotlin_graphic_projectsv4
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.example.doublearclineview.DoubleArcLineView
+import com.example.rightlinearcleftview.RightLineArcLeftView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         supportActionBar?.hide()
         //setContentView(R.layout.activity_main)
-        DoubleArcLineView.create(this)
+        RightLineArcLeftView.create(this)
     }
 }
