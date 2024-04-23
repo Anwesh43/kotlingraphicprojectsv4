bd0e6a42b0fb38e26ec37d03dbc7faad367b4480
Anwesh43 <anweshthecool0@gmail.com>
2024-04-23 10:27:49 +0530

    threequarterlinerotview: adding TQLRNode class

--- threequarterlinerotview/src/main/java/com/example/threequarterlinerotview/ThreeQuarterLineRotView.kt
+++ threequarterlinerotview/src/main/java/com/example/threequarterlinerotview/ThreeQuarterLineRotView.kt
@@ -121,4 +121,45 @@ class ThreeQuarterLineRotView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class TQLRNode(var i : Int = 0, val state : State = State()) {
+
+        private var next : TQLRNode? = null
+        private var prev : TQLRNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < colors.size - 1) {
+                next = TQLRNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawTQLRNode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : TQLRNode {
+            var curr : TQLRNode? = prev
+            if (dir == 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
