b9f4f09e8f1e1902530104301049ad559e56d2cf
Anwesh43 <anweshthecool0@gmail.com>
2024-04-23 10:11:09 +0530

    threequarterlinerotview: adding Int and Float extension methods

--- threequarterlinerotview/src/main/java/com/example/threequarterlinerotview/ThreeQuarterLineRotView.kt
+++ threequarterlinerotview/src/main/java/com/example/threequarterlinerotview/ThreeQuarterLineRotView.kt
@@ -23,3 +23,7 @@ val strokeFactor : Float = 90f
 val delay : Long = 20
 val backColor : Int = Color.parseColor("#BDBDBD")
 val scGap : Float = 0.04f / parts
+
+fun Int.inverse() : Float = 1f / this
+fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
+fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
