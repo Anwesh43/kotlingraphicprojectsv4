d1397441eaf7280bfcb394b472579dc8aead288e
Anwesh43 <anweshthecool0@gmail.com>
2024-04-23 10:17:35 +0530

    threequarterlinerotview: adding Canvas extension methods

--- threequarterlinerotview/src/main/java/com/example/threequarterlinerotview/ThreeQuarterLineRotView.kt
+++ threequarterlinerotview/src/main/java/com/example/threequarterlinerotview/ThreeQuarterLineRotView.kt
@@ -27,3 +27,35 @@ val scGap : Float = 0.04f / parts
 fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
+
+fun Canvas.drawXY(x : Float, y : Float, cb : () -> Unit) {
+    save()
+    translate(x, y)
+    cb()
+    restore()
+}
+
+fun Canvas.drawThreeQuarterLineRot(scale : Float, w : Float, h : Float, paint : Paint) {
+    val size : Float = Math.min(w, h) / sizeFactor
+    val dsc : (Int) -> Float = {
+        scale.divideScale(it, parts)
+    }
+    drawXY(w / 2, h / 2 - (h / 2 + size) * dsc(3)) {
+        rotate(rot * dsc(2))
+        drawXY(-size / 2, 0f) {
+            drawLine(0f, 0f, -size * 0.5f * dsc(0), 0f, paint)
+        }
+        drawArc(RectF(0f, -size / 2, size, size / 2), 180f, 270f * dsc(1), false, paint)
+    }
+}
+
+fun Canvas.drawTQLRNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = Color.parseColor(colors[i])
+    paint.strokeCap  = Paint.Cap.ROUND
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    paint.style = Paint.Style.STROKE
+    drawThreeQuarterLineRot(scale, w, h, paint)
+}
+
