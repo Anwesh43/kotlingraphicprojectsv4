cff60a71d6b7c690d1769423f310b2e185058fee
Anwesh43 <anweshthecool0@gmail.com>
2024-04-27 18:12:57 +0530

    arcexapndshrinklineview: adding Renderer class

--- arcexpandshrinklineview/src/main/java/com/example/arcexpandshrinklineview/ArcExpandShrinkView.kt
+++ arcexpandshrinklineview/src/main/java/com/example/arcexpandshrinklineview/ArcExpandShrinkView.kt
@@ -183,4 +183,27 @@ class ArcExpandShrinkView(ctx : Context) : View(ctx) {
             curr.startUpdating(cb)
         }
     }
+
+    data class Renderer(var view : ArcExpandShrinkView) {
+
+        private val animator : Animator = Animator(view)
+        private val aes : ArcExpandShrinkLine = ArcExpandShrinkLine(0)
+        private val paint : Paint = Paint(Paint.ANTI_ALIAS_FLAG)
+
+        fun render(canvas : Canvas) {
+            canvas.drawColor(backColor)
+            aes.draw(canvas, paint)
+            animator.animate {
+                aes.update {
+                    animator.stop()
+                }
+            }
+        }
+
+        fun handleTap() {
+            aes.startUpdating {
+                animator.start()
+            }
+        }
+    }
 }
