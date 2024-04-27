b40930eab4fd3553ef403693f2c6e220895bb5dd
Anwesh43 <anweshthecool0@gmail.com>
2024-04-27 18:10:39 +0530

    arcexapndshrinklineview: adding AESNode class

--- arcexpandshrinklineview/src/main/java/com/example/arcexpandshrinklineview/ArcExpandShrinkView.kt
+++ arcexpandshrinklineview/src/main/java/com/example/arcexpandshrinklineview/ArcExpandShrinkView.kt
@@ -119,4 +119,45 @@ class ArcExpandShrinkView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class AESNode(var i : Int = 0, val state : State = State()) {
+
+        private var next : AESNode? = null
+        private var prev : AESNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < colors.size - 1) {
+                next = AESNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawAESNode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : AESNode {
+            var curr : AESNode? = prev
+            if (dir == 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
