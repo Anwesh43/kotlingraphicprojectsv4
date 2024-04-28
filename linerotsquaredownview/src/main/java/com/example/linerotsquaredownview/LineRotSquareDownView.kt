73ebcd0b950c439bd1197ae6629d8fbc335b400e
Anwesh43 <anweshthecool0@gmail.com>
2024-04-28 15:59:47 +0530

    linerotsquaredownview: adding static create method

--- linerotsquaredownview/src/main/java/com/example/linerotsquaredownview/LineRotSquareDownView.kt
+++ linerotsquaredownview/src/main/java/com/example/linerotsquaredownview/LineRotSquareDownView.kt
@@ -211,4 +211,13 @@ class LineRotSquareDownView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    companion object {
+
+        fun create(activity: Activity) : LineRotSquareDownView {
+            val view : LineRotSquareDownView = LineRotSquareDownView(activity)
+            activity.setContentView(view)
+            return view
+        }
+    }
 }
