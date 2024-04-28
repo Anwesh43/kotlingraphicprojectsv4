e953a9641f9333f8a9acf97ede57cf4bd6fe8e8f
Anwesh43 <anweshthecool0@gmail.com>
2024-04-28 16:21:25 +0530

    linerotsquaredownview: adding view in activity

--- app/src/main/java/com/example/android_kotlin_graphic_projectsv4/MainActivity.kt
+++ app/src/main/java/com/example/android_kotlin_graphic_projectsv4/MainActivity.kt
@@ -2,13 +2,13 @@ package com.example.android_kotlin_graphic_projectsv4
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.example.arcexpandshrinklineview.ArcExpandShrinkView
+import com.example.linerotsquaredownview.LineRotSquareDownView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         supportActionBar?.hide()
         //setContentView(R.layout.activity_main)
-        ArcExpandShrinkView.create(this)
+        LineRotSquareDownView.create(this)
     }
 }
