7bbc4753235e820130de70fe2b119755222a590c
Anwesh43 <anweshthecool0@gmail.com>
2024-04-29 19:12:36 +0530

    halfarcsqdownview: adding HASDNode class

--- halfarcsqdownview/src/main/java/com/example/halfarcsqdownview/HalfArcSqDownView.kt
+++ halfarcsqdownview/src/main/java/com/example/halfarcsqdownview/HalfArcSqDownView.kt
@@ -120,4 +120,45 @@ class HalfArcSqDownView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class HASDNode(var i : Int = 0, val state : State = State()) {
+
+        private var next : HASDNode? = null
+        private var prev : HASDNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < colors.size - 1) {
+                next = HASDNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawHASDNode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : HASDNode {
+            var curr : HASDNode? = prev
+            if (dir == 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
