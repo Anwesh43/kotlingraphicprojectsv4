b37a72432984b0b9420992f51fa206f2f6ad7677
Anwesh43 <anweshthecool0@gmail.com>
2024-05-01 15:55:52 +0530

    partialarctohalfview: adding Canvas extension methods

--- partialarctohalfview/src/main/java/com/example/partialarctohalfview/PatialArcToHalfView.kt
+++ partialarctohalfview/src/main/java/com/example/partialarctohalfview/PatialArcToHalfView.kt
@@ -16,7 +16,7 @@ val colors : Array<String> = arrayOf(
     "#C51162",
     "#00C853"
 )
-val rot : Float = 90f
+val rot : Float = 180f
 val sizeFactor : Float = 4.9f
 val delay : Long = 20
 val backColor : Int = Color.parseColor("#BDBDBD")
@@ -27,3 +27,31 @@ val scGap : Float = 0.04f / parts
 fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
+
+fun Canvas.drawXY(x : Float, y : Float, cb : () -> Unit) {
+    save()
+    translate(x, y)
+    cb()
+    restore()
+}
+
+fun Canvas.drawPartialArcToHalf(scale : Float, w : Float, h : Float, paint : Paint) {
+    val size : Float = Math.min(w, h) / sizeFactor
+    val dsc : (Int) -> Float = {
+        scale.divideScale(it, parts)
+    }
+    drawXY(w / 2 + (w / 2) * dsc(3), h / 2) {
+        rotate(rot * dsc(1))
+        drawArc(RectF(-size / 2, -size / 2, size / 2, size / 2), 30f + 60f * dsc(2), 300f * dsc(0) - 120f * dsc(2), false, paint)
+    }
+}
+
+fun Canvas.drawPATHNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = Color.parseColor(colors[i])
+    paint.strokeCap = Paint.Cap.ROUND
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    paint.style = Paint.Style.STROKE
+    drawPartialArcToHalf(scale, w, h, paint)
+}
