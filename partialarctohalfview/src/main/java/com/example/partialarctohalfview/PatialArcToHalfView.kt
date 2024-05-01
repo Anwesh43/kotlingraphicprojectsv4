73f62751bb3437ea3b6ca9d640030110ebd33409
Anwesh43 <anweshthecool0@gmail.com>
2024-05-01 15:57:59 +0530

    partialarctohalfview: adding PartialArcToHalfView class

--- partialarctohalfview/src/main/java/com/example/partialarctohalfview/PatialArcToHalfView.kt
+++ partialarctohalfview/src/main/java/com/example/partialarctohalfview/PatialArcToHalfView.kt
@@ -55,3 +55,19 @@ fun Canvas.drawPATHNode(i : Int, scale : Float, paint : Paint) {
     paint.style = Paint.Style.STROKE
     drawPartialArcToHalf(scale, w, h, paint)
 }
+
+class PartialArcToHalfView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
