4915898e8c0afb1000dac14a0af47f360c39ba65
Anwesh43 <anweshthecool0@gmail.com>
2024-05-03 16:19:47 +0530

    rotarcbilineview: adding RotArcBiLineView class

--- rotarcbilineview/src/main/java/com/example/rotarcbilineview/RotArcBiLineView.kt
+++ rotarcbilineview/src/main/java/com/example/rotarcbilineview/RotArcBiLineView.kt
@@ -59,3 +59,19 @@ fun Canvas.drawRABLNode(i : Int, scale : Float, paint : Paint) {
     paint.strokeWidth = Math.min(w, h) / strokeFactor
     drawRotArcBiLine(scale, w, h, paint)
 }
+
+class RotArcBiLineView(ctx : Context)  : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
