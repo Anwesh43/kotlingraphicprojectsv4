c16e50111bfaa0bfb88078fca9c00cc757958995
Anwesh43 <anweshthecool0@gmail.com>
2024-05-03 16:40:17 +0530

    rotarcbilineview: adding RABLNode class

--- rotarcbilineview/src/main/java/com/example/rotarcbilineview/RotArcBiLineView.kt
+++ rotarcbilineview/src/main/java/com/example/rotarcbilineview/RotArcBiLineView.kt
@@ -122,4 +122,45 @@ class RotArcBiLineView(ctx : Context)  : View(ctx) {
             }
         }
     }
+
+    data class RABLNode(var i : Int = 0, val state : State = State()) {
+
+        private var next : RABLNode? = null
+        private var prev : RABLNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < colors.size - 1) {
+                next = RABLNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawRABLNode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : RABLNode {
+            var curr : RABLNode? = prev
+            if (dir == 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
