db05fbcd30a70fa478bfeedff33aa07f548ace14
Anwesh43 <anweshthecool0@gmail.com>
2024-05-04 12:21:38 +0530

    biinealtdropview: adding Canvas extension methods

--- bilinealtdropview/src/main/java/com/example/bilinealtdropview/BiLineAltDropView.kt
+++ bilinealtdropview/src/main/java/com/example/bilinealtdropview/BiLineAltDropView.kt
@@ -27,3 +27,39 @@ fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
 
+fun Canvas.drawXY(x : Float, y : Float, cb : () -> Unit) {
+    save()
+    translate(x, y)
+    cb()
+    restore()
+}
+
+fun Canvas.drawBiLineAltDrop(scale : Float, w : Float, h : Float, paint : Paint) {
+    val size : Float = Math.min(w, h) / sizeFactor
+    val dsc : (Int) -> Float = {
+        scale.divideScale(it, parts)
+    }
+    drawXY(w / 2 - (w / 2 + size) * dsc(3), h / 2) {
+        for (j in 0..1) {
+            drawXY(0f, 0f) {
+                scale(1f - 2 * j, 1f)
+                drawXY(w * 0.5f * (1 - dsc(0)), 0f) {
+                    drawLine(0f, 0f, size / 2, 0f, paint)
+                }
+                drawXY(0f, h * 0.5f * (1 - dsc(1))) {
+                    rotate(rot * dsc(1))
+                    drawLine(0f, 0f, 0f, -size / 2, paint)
+                }
+            }
+        }
+    }
+}
+
+fun Canvas.drawBLADNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = Color.parseColor(colors[i])
+    paint.strokeCap = Paint.Cap.ROUND
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    drawBiLineAltDrop(scale, w, h, paint)
+}
