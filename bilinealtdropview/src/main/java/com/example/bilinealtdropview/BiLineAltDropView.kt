5736475fd2c63585df49141bf4f03254592cdcfe
Anwesh43 <anweshthecool0@gmail.com>
2024-05-04 22:01:20 +0530

    biinealtdropview: adding Renderer class

--- bilinealtdropview/src/main/java/com/example/bilinealtdropview/BiLineAltDropView.kt
+++ bilinealtdropview/src/main/java/com/example/bilinealtdropview/BiLineAltDropView.kt
@@ -190,4 +190,27 @@ class BiLineAltDropView(ctx : Context) : View(ctx) {
             curr.startUpdating(cb)
         }
     }
+
+    data class Renderer(var view : BiLineAltDropView) {
+
+        private val blad : BiLineAltDrop = BiLineAltDrop(0)
+        private val animator : Animator = Animator(view)
+        private val paint : Paint = Paint(Paint.ANTI_ALIAS_FLAG)
+
+        fun render(canvas : Canvas) {
+            canvas.drawColor(backColor)
+            blad.draw(canvas, paint)
+            animator.animate {
+                blad.update {
+                    animator.stop()
+                }
+            }
+        }
+
+        fun handleTap() {
+            blad.startUpdating {
+                animator.start()
+            }
+        }
+    }
 }
