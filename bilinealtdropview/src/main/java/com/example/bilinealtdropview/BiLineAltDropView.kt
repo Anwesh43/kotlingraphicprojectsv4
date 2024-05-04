c5109b317f91846309b3a90eed45a2fbbf981982
Anwesh43 <anweshthecool0@gmail.com>
2024-05-04 21:59:43 +0530

    biinealtdropview: adding BiLineAltDrop class

--- bilinealtdropview/src/main/java/com/example/bilinealtdropview/BiLineAltDropView.kt
+++ bilinealtdropview/src/main/java/com/example/bilinealtdropview/BiLineAltDropView.kt
@@ -167,4 +167,27 @@ class BiLineAltDropView(ctx : Context) : View(ctx) {
             return this
         }
     }
+
+    data class BiLineAltDrop(var i : Int) {
+
+        private var curr : BLADNode = BLADNode(i + 1)
+        private var dir : Int = 1
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            curr.draw(canvas, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            curr.update {
+                curr = curr.getNext(dir) {
+                    dir *= -1
+                }
+                cb(it)
+            }
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            curr.startUpdating(cb)
+        }
+    }
 }
