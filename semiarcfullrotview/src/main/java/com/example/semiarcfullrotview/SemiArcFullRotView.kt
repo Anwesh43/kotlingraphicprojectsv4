d84e5be774ae6a4e6d1774abcd35542f7eb00204
Anwesh43 <anweshthecool0@gmail.com>
2024-05-06 02:14:15 +0530

    semiarcfullrotview: adding Renderer in View

--- semiarcfullrotview/src/main/java/com/example/semiarcfullrotview/SemiArcFullRotView.kt
+++ semiarcfullrotview/src/main/java/com/example/semiarcfullrotview/SemiArcFullRotView.kt
@@ -58,14 +58,16 @@ fun Canvas.drawSAFRNode(i : Int, scale : Float, paint : Paint) {
 
 class SemiArcFullRotView(ctx : Context) : View(ctx) {
 
-    override fun onDraw(canvas : Canvas) {
+    private val renderer : Renderer = Renderer(this)
 
+    override fun onDraw(canvas : Canvas) {
+        renderer.render(canvas)
     }
 
     override fun onTouchEvent(event : MotionEvent) : Boolean {
         when (event.action) {
             MotionEvent.ACTION_DOWN -> {
-
+                renderer.handleTap()
             }
         }
         return true
