6acb9e2fb024ba27a0ddc6510efd9430cc27769d
Anwesh43 <anweshthecool0@gmail.com>
2024-05-05 20:28:22 +0530

    irregularrotlineview: adding IrregularRotLineView class

--- irregularrotlineview/src/main/java/com/example/irregularrotlineview/IrregularRotLineView.kt
+++ irregularrotlineview/src/main/java/com/example/irregularrotlineview/IrregularRotLineView.kt
@@ -59,3 +59,19 @@ fun Canvas.drawIRLNode(i : Int, scale : Float, paint : Paint) {
     paint.strokeWidth = Math.min(w, h) / strokeFactor
     drawIrrgularRotLine(scale, w, h, paint)
 }
+
+class IrregularRotLineView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
