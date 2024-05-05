525118823b5c2bef8688547bec303e442516653f
Anwesh43 <anweshthecool0@gmail.com>
2024-05-05 20:36:08 +0530

    irregularrotlineview: adding IRLNode class

--- irregularrotlineview/src/main/java/com/example/irregularrotlineview/IrregularRotLineView.kt
+++ irregularrotlineview/src/main/java/com/example/irregularrotlineview/IrregularRotLineView.kt
@@ -122,4 +122,45 @@ class IrregularRotLineView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class IRLNode(var i : Int = 0, val state : State = State()) {
+
+        private var next : IRLNode? = null
+        private var prev : IRLNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < colors.size - 1) {
+                next = IRLNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawIRLNode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : IRLNode {
+            var curr : IRLNode? = prev
+            if (dir == 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
