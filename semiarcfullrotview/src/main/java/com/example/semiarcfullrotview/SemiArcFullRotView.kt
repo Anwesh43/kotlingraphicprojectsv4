f6750d9ad08afbec25e1e1b99fa326b1d5b274bb
Anwesh43 <anweshthecool0@gmail.com>
2024-05-06 02:01:09 +0530

    semiarcfullrotview: adding SemiArcFullRotView class

--- semiarcfullrotview/src/main/java/com/example/semiarcfullrotview/SemiArcFullRotView.kt
+++ semiarcfullrotview/src/main/java/com/example/semiarcfullrotview/SemiArcFullRotView.kt
@@ -55,3 +55,19 @@ fun Canvas.drawSAFRNode(i : Int, scale : Float, paint : Paint) {
     paint.style = Paint.Style.STROKE
     drawSemiArcFullRot(scale, w, h, paint)
 }
+
+class SemiArcFullRotView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
