05fd872532427f6303bb3e1980f85a7caa34072e
Anwesh43 <anweshthecool0@gmail.com>
2024-05-06 02:10:34 +0530

    semiarcfullrotview: adding Renderer class

--- semiarcfullrotview/src/main/java/com/example/semiarcfullrotview/SemiArcFullRotView.kt
+++ semiarcfullrotview/src/main/java/com/example/semiarcfullrotview/SemiArcFullRotView.kt
@@ -182,4 +182,27 @@ class SemiArcFullRotView(ctx : Context) : View(ctx) {
             curr.startUpdating(cb)
         }
     }
+
+    data class Renderer(var view : SemiArcFullRotView) {
+
+        private val animator : Animator = Animator(view)
+        private val safr : SemiArcFullRot = SemiArcFullRot(0)
+        private val paint : Paint = Paint(Paint.ANTI_ALIAS_FLAG)
+
+        fun render(canvas : Canvas) {
+            canvas.drawColor(backColor)
+            safr.draw(canvas, paint)
+            animator.animate {
+                safr.update {
+                    animator.stop()
+                }
+            }
+        }
+
+        fun handleTap() {
+            safr.startUpdating {
+                animator.start()
+            }
+        }
+    }
 }
