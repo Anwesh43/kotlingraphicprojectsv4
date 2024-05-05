424a8258a8604937037e13431fcb183892737c3d
Anwesh43 <anweshthecool0@gmail.com>
2024-05-06 02:00:06 +0530

    semiarcfullrotview: adding Canvas extension methods

--- semiarcfullrotview/src/main/java/com/example/semiarcfullrotview/SemiArcFullRotView.kt
+++ semiarcfullrotview/src/main/java/com/example/semiarcfullrotview/SemiArcFullRotView.kt
@@ -27,3 +27,31 @@ val rot : Float = 90f
 fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
+
+fun Canvas.drawXY(x : Float, y : Float, cb : () -> Unit) {
+    save()
+    translate(x, y)
+    cb()
+    restore()
+}
+
+fun Canvas.drawSemiArcFullRot(scale : Float, w : Float, h : Float, paint : Paint) {
+    val size : Float = Math.min(w, h) / sizeFactor
+    val dsc : (Int) -> Float = {
+        scale.divideScale(it, parts)
+    }
+    drawXY(w / 2, h / 2 + (h / 2) * dsc(3)) {
+        rotate(rot * dsc(1))
+        drawArc(RectF(0f, -size / 2, size, size / 2), 180f, 180f * (dsc(0) + dsc(2)), false, paint)
+    }
+}
+
+fun Canvas.drawSAFRNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = Color.parseColor(colors[i])
+    paint.strokeCap = Paint.Cap.ROUND
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    paint.style = Paint.Style.STROKE
+    drawSemiArcFullRot(scale, w, h, paint)
+}
