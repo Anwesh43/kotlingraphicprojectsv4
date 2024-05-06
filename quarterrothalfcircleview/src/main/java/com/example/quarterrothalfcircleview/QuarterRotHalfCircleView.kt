e1a976fe9ed96064e4d303e94d1ceb3cd2c9eef0
Anwesh43 <anweshthecool0@gmail.com>
2024-05-07 01:46:32 +0530

    quarterrothalfcircleview: adding Animator class

--- quarterrothalfcircleview/src/main/java/com/example/quarterrothalfcircleview/QuarterRotHalfCircleView.kt
+++ quarterrothalfcircleview/src/main/java/com/example/quarterrothalfcircleview/QuarterRotHalfCircleView.kt
@@ -90,4 +90,32 @@ class QuarterRotHalfCircleView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class Animator(var view : View, var animated : Boolean = false) {
+
+        fun animate(cb : () -> Unit) {
+            if (animated) {
+                cb()
+                try {
+                    Thread.sleep(delay)
+                    view.invalidate()
+                } catch(ex : Exception) {
+
+                }
+            }
+        }
+
+        fun start() {
+            if (!animated) {
+                animated = true
+                view.postInvalidate()
+            }
+        }
+
+        fun stop() {
+            if (animated) {
+                animated = false
+            }
+        }
+    }
 }
