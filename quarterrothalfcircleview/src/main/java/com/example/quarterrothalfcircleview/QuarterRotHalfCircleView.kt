15f0dc12228040ec0761ef3aca8e6ecbfa150864
Anwesh43 <anweshthecool0@gmail.com>
2024-05-07 01:43:23 +0530

    quarterrothalfcircleview: adding QuarterRotHalfCircleView class

--- quarterrothalfcircleview/src/main/java/com/example/quarterrothalfcircleview/QuarterRotHalfCircleView.kt
+++ quarterrothalfcircleview/src/main/java/com/example/quarterrothalfcircleview/QuarterRotHalfCircleView.kt
@@ -55,3 +55,19 @@ fun Canvas.drawQRHCNode(i : Int, scale : Float, paint : Paint) {
     paint.strokeCap = Paint.Cap.ROUND
     drawQuarterRotHalfCircle(scale, w, h, paint)
 }
+
+class QuarterRotHalfCircleView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
