e0a658dd89d6c572995be726a0e4301478b89acc
Anwesh43 <anweshthecool0@gmail.com>
2024-05-07 02:02:30 +0530

    quarterrothalfcircleview: adding Renderer in View

--- quarterrothalfcircleview/src/main/java/com/example/quarterrothalfcircleview/QuarterRotHalfCircleView.kt
+++ quarterrothalfcircleview/src/main/java/com/example/quarterrothalfcircleview/QuarterRotHalfCircleView.kt
@@ -58,14 +58,16 @@ fun Canvas.drawQRHCNode(i : Int, scale : Float, paint : Paint) {
 
 class QuarterRotHalfCircleView(ctx : Context) : View(ctx) {
 
-    override fun onDraw(canvas : Canvas) {
+    private val renderer : Renderer = Renderer(this)
 
+    override fun onDraw(canvas : Canvas) {
+        renderer.render(canvas)
     }
 
     override fun onTouchEvent(event : MotionEvent) : Boolean {
         when (event.action) {
             MotionEvent.ACTION_DOWN -> {
-
+                renderer.handleTap()
             }
         }
         return true
@@ -125,7 +127,7 @@ class QuarterRotHalfCircleView(ctx : Context) : View(ctx) {
         private var prev : QRHCNode? = null
 
         init {
-
+            addNeighbor()
         }
 
         fun addNeighbor() {
