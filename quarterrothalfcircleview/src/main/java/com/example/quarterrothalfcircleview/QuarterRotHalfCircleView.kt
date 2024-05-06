4e9f7579601a079a5fa1170369390c7b3fc5faac
Anwesh43 <anweshthecool0@gmail.com>
2024-05-07 01:41:30 +0530

    quarterrothalfcircleview: adding Canvas extension methods

--- quarterrothalfcircleview/src/main/java/com/example/quarterrothalfcircleview/QuarterRotHalfCircleView.kt
+++ quarterrothalfcircleview/src/main/java/com/example/quarterrothalfcircleview/QuarterRotHalfCircleView.kt
@@ -27,3 +27,31 @@ val rot : Float = 90f
 fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
+
+fun Canvas.drawXY(x : Float, y : Float, cb : () -> Unit) {
+    save()
+    translate(x, y)
+    cb()
+    restore()
+}
+
+fun Canvas.drawQuarterRotHalfCircle(scale : Float, w : Float, h : Float, paint : Paint) {
+    val size : Float = Math.min(w, h) / sizeFactor
+    val dsc : (Int) -> Float = {
+        scale.divideScale(it, parts)
+    }
+    drawXY(w / 2, h / 2 + (h / 2) * dsc(3)) {
+        rotate(rot * dsc(1))
+        drawArc(RectF(-size / 2, -size / 2, size / 2, size / 2), -90f, 90f * (dsc(0) + dsc(1)), false, paint)
+    }
+}
+
+fun Canvas.drawQRHCNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = Color.parseColor(colors[i])
+    paint.style = Paint.Style.STROKE
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    paint.strokeCap = Paint.Cap.ROUND
+    drawQuarterRotHalfCircle(scale, w, h, paint)
+}
