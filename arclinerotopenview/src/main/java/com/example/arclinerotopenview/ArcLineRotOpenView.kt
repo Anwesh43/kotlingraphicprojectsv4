4808d8e26b68a794e16029c0ae9ff1f1443ee8a9
Anwesh43 <anweshthecool0@gmail.com>
2024-05-09 00:10:56 +0530

    arclinrotopenview: adding Renderer class

--- arclinerotopenview/src/main/java/com/example/arclinerotopenview/ArcLineRotOpenView.kt
+++ arclinerotopenview/src/main/java/com/example/arclinerotopenview/ArcLineRotOpenView.kt
@@ -188,4 +188,27 @@ class ArcLineRotOpenView(ctx : Context) : View(ctx) {
             curr.startUpdating(cb)
         }
     }
+
+    data class Renderer(var view : ArcLineRotOpenView) {
+
+        private val animator : Animator = Animator(view)
+        private val arlo : ArcLineRotOpen = ArcLineRotOpen(0)
+        private val paint : Paint = Paint(Paint.ANTI_ALIAS_FLAG)
+
+        fun render(canvas : Canvas) {
+            canvas.drawColor(backColor)
+            arlo.draw(canvas, paint)
+            animator.animate {
+                arlo.update {
+                    animator.stop()
+                }
+            }
+        }
+
+        fun handleTap() {
+            arlo.startUpdating {
+                animator.start()
+            }
+        }
+    }
 }
