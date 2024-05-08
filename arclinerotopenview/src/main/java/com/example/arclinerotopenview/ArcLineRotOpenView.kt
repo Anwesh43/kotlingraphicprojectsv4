d111e2f58a4c719d1b7ee03554f7a81eb27ee726
Anwesh43 <anweshthecool0@gmail.com>
2024-05-08 23:52:12 +0530

    arclinrotopenview: adding ALRONode class

--- arclinerotopenview/src/main/java/com/example/arclinerotopenview/ArcLineRotOpenView.kt
+++ arclinerotopenview/src/main/java/com/example/arclinerotopenview/ArcLineRotOpenView.kt
@@ -124,4 +124,45 @@ class ArcLineRotOpenView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class ALRONode(var i : Int = 0, val state : State = State()) {
+
+        private var next : ALRONode? = null
+        private var prev : ALRONode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < colors.size - 1) {
+                next = ALRONode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawALRONode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : ALRONode {
+            var curr : ALRONode? = prev
+            if (dir == 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
