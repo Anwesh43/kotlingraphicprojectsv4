b9777632efe91825bc1fbc161d78d26e32949005
Anwesh43 <anweshthecool0@gmail.com>
2024-05-08 23:40:54 +0530

    arclinrotopenview: adding ArcLineRotOpenView class

--- arclinerotopenview/src/main/java/com/example/arclinerotopenview/ArcLineRotOpenView.kt
+++ arclinerotopenview/src/main/java/com/example/arclinerotopenview/ArcLineRotOpenView.kt
@@ -61,3 +61,19 @@ fun Canvas.drawALRONode(i : Int, scale : Float, paint : Paint) {
     paint.style = Paint.Style.STROKE
     drawArcLineRotOpen(scale, w, h, paint)
 }
+
+class ArcLineRotOpenView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
