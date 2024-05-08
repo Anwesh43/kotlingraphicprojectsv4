886765f682778fc940256459c324f2e8068ab792
Anwesh43 <anweshthecool0@gmail.com>
2024-05-09 00:23:29 +0530

    arclinrotopenview: adding view in activit

--- app/src/main/java/com/example/android_kotlin_graphic_projectsv4/MainActivity.kt
+++ app/src/main/java/com/example/android_kotlin_graphic_projectsv4/MainActivity.kt
@@ -2,13 +2,13 @@ package com.example.android_kotlin_graphic_projectsv4
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.example.quarterrothalfcircleview.QuarterRotHalfCircleView
+import com.example.arclinerotopenview.ArcLineRotOpenView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         supportActionBar?.hide()
         //setContentView(R.layout.activity_main)
-        QuarterRotHalfCircleView.create(this)
+        ArcLineRotOpenView.create(this)
     }
 }
