58028bcaff47419bb52dc73a2fd8b426779ef40c
Anwesh43 <anweshthecool0@gmail.com>
2024-05-08 12:42:55 +0530

    arclinrotopenview: adding Canvas extension methods

--- arclinerotopenview/src/main/java/com/example/arclinerotopenview/ArcLineRotOpenView.kt
+++ arclinerotopenview/src/main/java/com/example/arclinerotopenview/ArcLineRotOpenView.kt
@@ -27,3 +27,37 @@ val backColor : Int = Color.parseColor("#BDBDBD")
 fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
+
+fun Canvas.drawXY(x : Float, y : Float, cb : () -> Unit) {
+    save()
+    translate(x, y)
+    cb()
+    restore()
+}
+
+fun Canvas.drawArcLineRotOpen(scale : Float, w : Float, h : Float, paint : Paint) {
+    val size : Float = Math.min(w, h) / sizeFactor
+    val dsc : (Int) -> Float = {
+        scale.divideScale(it, parts)
+    }
+    drawXY(w / 2, h / 2  - (h / 2) * dsc(3)) {
+        drawArc(RectF(-size / 2, -size / 2, size / 2, size / 2), 360f - 180f * dsc(0), 180f * dsc(0), false, paint)
+        for (j in 0..1) {
+            drawXY(0f, 0f) {
+                scale(1f - 2 * j, 1f)
+                rotate(rot * dsc(2))
+                drawLine(0f, 0f, 0f, -size * dsc(1), paint)
+            }
+        }
+    }
+}
+
+fun Canvas.drawALRONode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = Color.parseColor(colors[i])
+    paint.strokeCap = Paint.Cap.ROUND
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    paint.style = Paint.Style.STROKE
+    drawArcLineRotOpen(scale, w, h, paint)
+}
