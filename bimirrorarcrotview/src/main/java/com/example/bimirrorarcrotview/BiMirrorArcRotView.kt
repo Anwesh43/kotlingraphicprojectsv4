b42a2298816c2956e8038e95f9ba434552817336
Anwesh43 <anweshthecool0@gmail.com>
2024-05-11 12:20:46 +0530

    bimirrorarcrotview: adding fixes

--- bimirrorarcrotview/src/main/java/com/example/bimirrorarcrotview/BiMirrorArcRotView.kt
+++ bimirrorarcrotview/src/main/java/com/example/bimirrorarcrotview/BiMirrorArcRotView.kt
@@ -58,6 +58,7 @@ fun Canvas.drawBMARNode(i : Int, scale : Float, paint : Paint) {
     paint.color = Color.parseColor(colors[i])
     paint.strokeCap = Paint.Cap.ROUND
     paint.strokeWidth = Math.min(w, h) / strokeFactor
+    paint.style = Paint.Style.STROKE
     drawBiMirrorArcRot(scale, w, h, paint)
 }
 
