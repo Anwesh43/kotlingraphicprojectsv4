df312da01d88b97d0f7104886786d4694c90da89
Anwesh43 <anweshthecool0@gmail.com>
2024-05-11 11:40:16 +0530

    bimirrorarcrotview: adding BMARNode class

--- bimirrorarcrotview/src/main/java/com/example/bimirrorarcrotview/BiMirrorArcRotView.kt
+++ bimirrorarcrotview/src/main/java/com/example/bimirrorarcrotview/BiMirrorArcRotView.kt
@@ -123,4 +123,45 @@ class BiMirrorArcRotView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class BMARNode(var i : Int = 0, val state : State = State()) {
+
+        private var next : BMARNode? = null
+        private var prev: BMARNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < colors.size - 1) {
+                next = BMARNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawBMARNode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : BMARNode {
+            var curr : BMARNode? = prev
+            if (dir == 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
