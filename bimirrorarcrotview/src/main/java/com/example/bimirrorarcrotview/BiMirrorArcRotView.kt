6fccca3db42f3aed24866f6a59e469b708e07b85
Anwesh43 <anweshthecool0@gmail.com>
2024-05-11 12:10:39 +0530

    bimirrorarcrotview: adding BiMirrorArcRot class

--- bimirrorarcrotview/src/main/java/com/example/bimirrorarcrotview/BiMirrorArcRotView.kt
+++ bimirrorarcrotview/src/main/java/com/example/bimirrorarcrotview/BiMirrorArcRotView.kt
@@ -164,4 +164,27 @@ class BiMirrorArcRotView(ctx : Context) : View(ctx) {
             return this
         }
     }
+
+    data class BiMirrorArcRot(var i : Int) {
+
+        private var curr : BMARNode = BMARNode(0)
+        private var dir : Int = 1
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            curr.draw(canvas, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            curr.update {
+                curr = curr.getNext(dir) {
+                    dir *= -1
+                }
+                cb(it)
+            }
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            curr.startUpdating(cb)
+        }
+    }
 }
