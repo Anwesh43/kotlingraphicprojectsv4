aeeda9ed4ef5bb76300d79fbf5a0c02a32d81f10
Anwesh43 <anweshthecool0@gmail.com>
2024-05-11 11:11:29 +0530

    bimirrorarcrotview: adding Canvas extension methods

--- bimirrorarcrotview/src/main/java/com/example/bimirrorarcrotview/BiMirrorArcRotView.kt
+++ bimirrorarcrotview/src/main/java/com/example/bimirrorarcrotview/BiMirrorArcRotView.kt
@@ -28,3 +28,35 @@ fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
 
+fun Canvas.drawXY(x : Float, y : Float, cb : () -> Unit) {
+    save()
+    translate(x, y)
+    cb()
+    restore()
+}
+
+fun Canvas.drawBiMirrorArcRot(scale : Float, w : Float, h : Float, paint : Paint) {
+    val size : Float = Math.min(w, h) / sizeFactor
+    val dsc : (Int) -> Float = {
+        scale.divideScale(it, parts)
+    }
+    drawXY(w / 2, h / 2) {
+        rotate(rot * dsc(2))
+        for (j in 0..1) {
+            drawXY(0f, 0f) {
+                scale(1f - 2 * j, 1f)
+                rotate(rot * dsc(1))
+                drawArc(RectF(-size / 2, -size / 2, size / 2, size / 2), 90f, rot * dsc(0), false, paint)
+            }
+        }
+    }
+}
+
+fun Canvas.drawBMARNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = Color.parseColor(colors[i])
+    paint.strokeCap = Paint.Cap.ROUND
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    drawBiMirrorArcRot(scale, w, h, paint)
+}
