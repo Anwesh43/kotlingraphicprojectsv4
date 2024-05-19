372425d3a87d881c411a848376ef531d344a7330
Anwesh43 <anweshthecool0@gmail.com>
2024-05-19 19:03:38 +0530

    birotlinearcview: adding BLRANode class

--- bilinearcrotview/src/main/java/com/example/bilinearcrotview/BiLineRotArcView.kt
+++ bilinearcrotview/src/main/java/com/example/bilinearcrotview/BiLineRotArcView.kt
@@ -119,4 +119,45 @@ class BiLineRotArcView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class BLRANode(var i : Int = 0, val state : State = State()) {
+
+        private var next : BLRANode? = null
+        private var prev : BLRANode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < colors.size - 1) {
+                next = BLRANode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawBLRANode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : BLRANode {
+            var curr : BLRANode? = prev
+            if (dir == 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
