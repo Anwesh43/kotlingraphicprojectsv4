74b439606b00767cd992d78f8b0e830388b54d43
Anwesh43 <anweshthecool0@gmail.com>
2024-05-19 18:54:12 +0530

    birotlinearcview: adding BiLineRotArcView class

--- bilinearcrotview/src/main/java/com/example/bilinearcrotview/BiLineRotArcView.kt
+++ bilinearcrotview/src/main/java/com/example/bilinearcrotview/BiLineRotArcView.kt
@@ -56,3 +56,19 @@ fun Canvas.drawBLRANode(i : Int, scale : Float, paint : Paint) {
     paint.strokeWidth = Math.min(w, h) / strokeFactor
     drawBiLineRotArc(scale, w, h, paint)
 }
+
+class BiLineRotArcView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
