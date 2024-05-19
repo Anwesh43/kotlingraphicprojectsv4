4991483ba93834cd8cf82e48649208b4aea33ec5
Anwesh43 <anweshthecool0@gmail.com>
2024-05-19 19:13:19 +0530

    birotlinearcview: adding Renderer class

--- bilinearcrotview/src/main/java/com/example/bilinearcrotview/BiLineRotArcView.kt
+++ bilinearcrotview/src/main/java/com/example/bilinearcrotview/BiLineRotArcView.kt
@@ -183,4 +183,27 @@ class BiLineRotArcView(ctx : Context) : View(ctx) {
             curr.startUpdating(cb)
         }
     }
+
+    data class Renderer(var view : BiLineRotArcView) {
+
+        private val animator : Animator = Animator(view)
+        private val paint : Paint = Paint(Paint.ANTI_ALIAS_FLAG)
+        private val blra : BiLineRotArc = BiLineRotArc(0)
+
+        fun render(canvas : Canvas) {
+            canvas.drawColor(backColor)
+            blra.draw(canvas, paint)
+            animator.animate {
+                blra.update {
+                    animator.stop()
+                }
+            }
+        }
+
+        fun handleTap() {
+            blra.startUpdating {
+                animator.start()
+            }
+        }
+    }
 }
