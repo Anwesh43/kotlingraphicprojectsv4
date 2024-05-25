78a90bb872bd014b931aa6fc6c7ed95f6665f7aa
Anwesh43 <anweshthecool0@gmail.com>
2024-05-25 07:07:37 +0530

    lineextendrotview: adding constants and libraries

---
+++ lineextendrotview/src/main/java/com/example/lineextendrotview/LineExtendRotView.kt
@@ -0,0 +1,25 @@
+package com.example.lineextendrotview
+
+import android.view.View
+import android.view.MotionEvent
+import android.graphics.Paint
+import android.graphics.Canvas
+import android.graphics.RectF
+import android.graphics.Color
+import android.app.Activity
+import android.content.Context
+
+val colors : Array<String> = arrayOf(
+    "#1A237E",
+    "#EF5350",
+    "#AA00FF",
+    "#C51162",
+    "#00C853"
+)
+val parts : Int = 4
+val scGap : Float = 0.04f / parts
+val strokeFactor : Float = 90f
+val sizeFactor : Float = 4.9f
+val rot : Float = 180f
+val backColor : Int = Color.parseColor("#BDBDBD")
+val delay : Long = 20
