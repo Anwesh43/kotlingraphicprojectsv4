d49b0b15f0774db2f8e6f7dfa53a1756ab65eb10
Anwesh43 <anweshthecool0@gmail.com>
2024-05-25 07:16:57 +0530

    lineextendrotview: adding Canvas extension methods

--- lineextendrotview/src/main/java/com/example/lineextendrotview/LineExtendRotView.kt
+++ lineextendrotview/src/main/java/com/example/lineextendrotview/LineExtendRotView.kt
@@ -27,3 +27,33 @@ val delay : Long = 20
 fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
+
+fun Canvas.drawXY(x : Float, y : Float, cb : () -> Unit) {
+    save()
+    translate(x, y)
+    cb()
+    restore()
+}
+
+fun Canvas.drawLineExtendRot(scale : Float, w : Float, h : Float, paint : Paint) {
+    val size : Float = Math.min(w, h) / sizeFactor
+    val dsc : (Int) -> Float = {
+        scale.divideScale(it, parts)
+    }
+    drawXY(w / 2 + (w / 2 + size / 2) * dsc(3), h / 2) {
+        rotate(rot * dsc(2))
+        drawLine(0f, 0f, -size * 0.5f * dsc(0), 0f, paint)
+        for (j in 0..1) {
+            drawLine(0f, 0f, size * 0.5f * j * dsc(1), -size * dsc(1), paint)
+        }
+    }
+}
+
+fun Canvas.drawLERNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = Color.parseColor(colors[i])
+    paint.strokeCap = Paint.Cap.ROUND
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    drawLineExtendRot(scale, w, h, paint)
+}
