775611b5d4b3b9544678a6d15d106f81b57e6c65
Anwesh43 <anweshthecool0@gmail.com>
2024-05-25 07:41:47 +0530

    lineextendrotview: adding LineExtendRot class

--- lineextendrotview/src/main/java/com/example/lineextendrotview/LineExtendRotView.kt
+++ lineextendrotview/src/main/java/com/example/lineextendrotview/LineExtendRotView.kt
@@ -160,4 +160,27 @@ class LineExtendRotView(ctx : Context) : View(ctx) {
             return this
         }
     }
+
+    data class LineExtendRot(var i : Int) {
+
+        private var curr : LERNode = LERNode(0)
+        private var dir : Int = 1
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            curr.draw(canvas, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            curr.update {
+                curr = curr.getNext(dir) {
+                    dir *= -1
+                }
+                cb(it)
+            }
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            curr.startUpdating(cb)
+        }
+    }
 }
