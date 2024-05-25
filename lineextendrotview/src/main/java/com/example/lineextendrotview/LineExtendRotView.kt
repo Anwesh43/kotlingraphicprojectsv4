9cd11b8b350638de2de083bd2782b0f2ecee93ec
Anwesh43 <anweshthecool0@gmail.com>
2024-05-25 07:34:01 +0530

    lineextendrotview: adding LERNode class

--- lineextendrotview/src/main/java/com/example/lineextendrotview/LineExtendRotView.kt
+++ lineextendrotview/src/main/java/com/example/lineextendrotview/LineExtendRotView.kt
@@ -120,4 +120,44 @@ class LineExtendRotView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class LERNode(var i : Int = 0, val state : State = State()) {
+
+        private var next : LERNode? = null
+        private var prev : LERNode? = null
+
+        init {
+            addNeighbor()
+        }
+        fun addNeighbor() {
+            if (i < colors.size - 1) {
+                next = LERNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawLERNode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : LERNode {
+            var curr : LERNode? = prev
+            if (dir == 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
