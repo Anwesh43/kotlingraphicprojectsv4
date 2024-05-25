150c4deab62e726ca1a3bc067e9b63ddfbe2af30
Anwesh43 <anweshthecool0@gmail.com>
2024-05-25 07:09:51 +0530

    lineextendrotview: adding Int and Float extension methods

--- lineextendrotview/src/main/java/com/example/lineextendrotview/LineExtendRotView.kt
+++ lineextendrotview/src/main/java/com/example/lineextendrotview/LineExtendRotView.kt
@@ -23,3 +23,7 @@ val sizeFactor : Float = 4.9f
 val rot : Float = 180f
 val backColor : Int = Color.parseColor("#BDBDBD")
 val delay : Long = 20
+
+fun Int.inverse() : Float = 1f / this
+fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
+fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
