4cac2b5f3a20a713ab1acc85612f134920980cf5
Anwesh43 <anweshthecool0@gmail.com>
2024-05-26 16:19:43 +0530

    linesteplinerightview: adding Renderer class

--- linesteplinerightview/src/main/java/com/example/linesteplinerightview/LineStepLineRightView.kt
+++ linesteplinerightview/src/main/java/com/example/linesteplinerightview/LineStepLineRightView.kt
@@ -185,4 +185,27 @@ class LineStepLineRightView(ctx : Context) : View(ctx) {
             curr.startUpdating(cb)
         }
     }
+
+    data class Renderer(var view : LineStepLineRightView) {
+
+        private val animator : Animator = Animator(view)
+        private val lslr : LineStepLineRight = LineStepLineRight(0)
+        private val paint : Paint = Paint(Paint.ANTI_ALIAS_FLAG)
+
+        fun render(canvas : Canvas) {
+            canvas.drawColor(backColor)
+            lslr.draw(canvas, paint)
+            animator.animate {
+                lslr.update {
+                    animator.stop()
+                }
+            }
+        }
+
+        fun handleTap() {
+            lslr.startUpdating {
+                animator.start()
+            }
+        }
+    }
 }
