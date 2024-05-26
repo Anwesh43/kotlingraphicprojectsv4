21494418501c49724082b32870fa4ea226030db9
Anwesh43 <anweshthecool0@gmail.com>
2024-05-26 16:20:13 +0530

    linesteplinerightview: adding Renderer in View

--- linesteplinerightview/src/main/java/com/example/linesteplinerightview/LineStepLineRightView.kt
+++ linesteplinerightview/src/main/java/com/example/linesteplinerightview/LineStepLineRightView.kt
@@ -63,14 +63,16 @@ fun Canvas.drawLSLRNode(i : Int, scale : Float, paint : Paint) {
 
 class LineStepLineRightView(ctx : Context) : View(ctx) {
 
-    override fun onDraw(canvas : Canvas) {
+    private val renderer : Renderer = Renderer(this)
 
+    override fun onDraw(canvas : Canvas) {
+        renderer.render(canvas)
     }
 
     override fun onTouchEvent(event : MotionEvent) : Boolean {
         when (event.action) {
             MotionEvent.ACTION_DOWN -> {
-
+                renderer.handleTap()
             }
         }
         return true
