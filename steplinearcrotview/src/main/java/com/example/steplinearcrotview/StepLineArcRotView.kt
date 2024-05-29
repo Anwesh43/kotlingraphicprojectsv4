ed6c489551dbf76be962a5babbefd35fc6185f1a
Anwesh43 <anweshthecool0@gmail.com>
2024-05-29 13:42:52 +0530

    steplinearcrotview: adding Canvas extension methods

--- steplinearcrotview/src/main/java/com/example/steplinearcrotview/StepLineArcRotView.kt
+++ steplinearcrotview/src/main/java/com/example/steplinearcrotview/StepLineArcRotView.kt
@@ -27,3 +27,40 @@ val rot : Float = 180f
 fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
+
+fun Canvas.drawXY(x : Float, y : Float, cb : () -> Unit) {
+    save()
+    translate(x, y)
+    cb()
+    restore()
+}
+
+fun Canvas.drawStepLineArcRot(scale : Float, w : Float, h : Float, paint : Paint) {
+    val size : Float = Math.min(w, h) / sizeFactor
+    val dsc : (Int) -> Float = {
+        scale.divideScale(it, parts)
+    }
+    drawXY(w / 2, h / 2 - (h / 2) * dsc(3)) {
+        rotate(rot * dsc(2))
+        drawLine(0f, 0f, 0f, size * 0.25f * dsc(0), paint)
+        for (j in 0..1) {
+            drawXY(0f, 0f) {
+                scale(1f - 2 * j, 1f)
+                drawXY(-size / 2, -size / 2) {
+                    drawLine(0f, 0f, 0f, size * 0.25f * dsc(0), paint)
+                }
+                drawArc(RectF(-size / 2, -size / 2, size / 2, size / 2), 90f, 90f * dsc(1), false, paint)
+            }
+        }
+    }
+}
+
+fun Canvas.drawSLARNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = Color.parseColor(colors[i])
+    paint.strokeCap = Paint.Cap.ROUND
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    paint.style = Paint.Style.STROKE
+    drawStepLineArcRot(scale, w, h, paint)
+}
