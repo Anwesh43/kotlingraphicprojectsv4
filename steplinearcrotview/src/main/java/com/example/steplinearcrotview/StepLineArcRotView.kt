3d3a496cb78c74a0c7cb4c1dc75f887d9481cec2
Anwesh43 <anweshthecool0@gmail.com>
2024-05-29 14:17:16 +0530

    steplinearcrotview: adding StepLineArcRot class

--- steplinearcrotview/src/main/java/com/example/steplinearcrotview/StepLineArcRotView.kt
+++ steplinearcrotview/src/main/java/com/example/steplinearcrotview/StepLineArcRotView.kt
@@ -168,4 +168,27 @@ class StepLineArcRotView(ctx : Context) : View(ctx) {
             return this
         }
     }
+
+    data class StepLineArcRot(var i : Int) {
+
+        private var curr : SLARNode = SLARNode(0)
+        private var dir : Int = 1
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            curr.draw(canvas, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            curr.update {
+                curr = curr.getNext(dir) {
+                    dir *= -1
+                }
+                cb(it)
+            }
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            curr.startUpdating(cb)
+        }
+    }
 }
