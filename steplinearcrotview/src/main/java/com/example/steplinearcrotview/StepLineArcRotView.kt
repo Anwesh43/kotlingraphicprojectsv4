f5bd02f2e92e22d0965b52b8d2beff6990a4d53e
Anwesh43 <anweshthecool0@gmail.com>
2024-05-29 13:44:16 +0530

    steplinearcrotview: adding StepLineArcRotView class

--- steplinearcrotview/src/main/java/com/example/steplinearcrotview/StepLineArcRotView.kt
+++ steplinearcrotview/src/main/java/com/example/steplinearcrotview/StepLineArcRotView.kt
@@ -64,3 +64,19 @@ fun Canvas.drawSLARNode(i : Int, scale : Float, paint : Paint) {
     paint.style = Paint.Style.STROKE
     drawStepLineArcRot(scale, w, h, paint)
 }
+
+class StepLineArcRotView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
