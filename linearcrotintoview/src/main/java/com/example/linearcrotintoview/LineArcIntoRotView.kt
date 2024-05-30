65e58daccd2d6055825ea65038af8ca830dee318
Anwesh43 <anweshthecool0@gmail.com>
2024-05-30 10:26:14 +0530

    linearcrotintoview: adding LineArcIntoRot class

--- linearcrotintoview/src/main/java/com/example/linearcrotintoview/LineArcIntoRotView.kt
+++ linearcrotintoview/src/main/java/com/example/linearcrotintoview/LineArcIntoRotView.kt
@@ -164,4 +164,27 @@ class LineArcIntoRotView(ctx : Context) : View(ctx) {
             return this
         }
     }
+
+    data class LineArcIntoRot(var i : Int) {
+
+        private var curr : LAIRNode = LAIRNode(0)
+        private var dir : Int = 1
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            curr.draw(canvas, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            curr.update {
+                curr = curr.getNext(dir) {
+                    dir *= -1
+                }
+                cb(it)
+            }
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            curr.startUpdating(cb)
+        }
+    }
 }
