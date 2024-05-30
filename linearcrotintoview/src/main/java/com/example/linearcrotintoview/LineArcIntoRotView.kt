7a05183671b6d7bf4e60c5ff92bdc5e253bae2e7
Anwesh43 <anweshthecool0@gmail.com>
2024-05-30 10:24:10 +0530

    linearcrotintoview: adding LAIRNode class

--- linearcrotintoview/src/main/java/com/example/linearcrotintoview/LineArcIntoRotView.kt
+++ linearcrotintoview/src/main/java/com/example/linearcrotintoview/LineArcIntoRotView.kt
@@ -123,4 +123,45 @@ class LineArcIntoRotView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class LAIRNode(var i : Int = 0, val state : State = State()) {
+
+        private var next : LAIRNode? = null
+        private var prev : LAIRNode? = null
+
+        init {
+
+        }
+
+        fun addNeighbor() {
+            if (i < colors.size - 1) {
+                next = LAIRNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawLAIRNode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : LAIRNode {
+            var curr : LAIRNode? = prev
+            if (dir == 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
