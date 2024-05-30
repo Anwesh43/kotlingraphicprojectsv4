4f405d726e70ae7fbbec50e5944a9ce8f1297dfc
Anwesh43 <anweshthecool0@gmail.com>
2024-05-30 10:28:18 +0530

    linearcrotintoview: adding Renderer in View

--- linearcrotintoview/src/main/java/com/example/linearcrotintoview/LineArcIntoRotView.kt
+++ linearcrotintoview/src/main/java/com/example/linearcrotintoview/LineArcIntoRotView.kt
@@ -63,14 +63,16 @@ fun Canvas.drawLAIRNode(i : Int, scale : Float, paint : Paint) {
 
 class LineArcIntoRotView(ctx : Context) : View(ctx) {
 
-    override fun onDraw(canvas : Canvas) {
+    private val renderer : Renderer = Renderer(this)
 
+    override fun onDraw(canvas : Canvas) {
+        renderer.render(canvas)
     }
 
     override fun onTouchEvent(event : MotionEvent) : Boolean {
         when (event.action) {
             MotionEvent.ACTION_DOWN -> {
-
+                renderer.handleTap()
             }
         }
         return true
