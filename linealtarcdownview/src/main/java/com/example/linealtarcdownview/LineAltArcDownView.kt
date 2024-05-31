d95c80b1b12ec94b392f0a25196078cd51d74502
Anwesh43 <anweshthecool0@gmail.com>
2024-05-31 19:36:36 +0530

    linealtarcdownview: adding Canvas extension methods

--- linealtarcdownview/src/main/java/com/example/linealtarcdownview/LineAltArcDownView.kt
+++ linealtarcdownview/src/main/java/com/example/linealtarcdownview/LineAltArcDownView.kt
@@ -27,3 +27,39 @@ val backColor : Int = Color.parseColor("#BDBDBD")
 fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
+
+fun Canvas.drawXY(x : Float, y : Float, cb : () -> Unit) {
+    save()
+    translate(x, y)
+    cb()
+    restore()
+}
+
+fun Canvas.drawLineAltArcDown(scale : Float, w : Float, h : Float, paint : Paint) {
+    val size : Float = Math.min(w, h) / sizeFactor
+    val dsc : (Int) -> Float = {
+        scale.divideScale(it, parts)
+    }
+    drawXY(w / 2, h / 2 - (h / 2 + size) * dsc(3)) {
+        drawXY((w / 2) * (1 - dsc(0)), 0f) {
+            rotate(rot * dsc(2))
+            drawLine(0f, 0f, size, 0f, paint)
+        }
+        for (j in 0..1) {
+            drawXY(0f, 0f) {
+                scale(1f - 2 * j, 1f)
+                drawArc(RectF(0f, -size / 2, size, size / 2), 180f, 90f * dsc(1), false, paint)
+            }
+        }
+    }
+}
+
+fun Canvas.drawLAADNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = Color.parseColor(colors[i])
+    paint.strokeCap = Paint.Cap.ROUND
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    paint.style = Paint.Style.STROKE
+    drawLineAltArcDown(scale, w, h, paint)
+}
