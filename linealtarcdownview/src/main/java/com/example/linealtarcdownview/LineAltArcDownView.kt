7c4864e13afe73b64fed05e8dd7ecb6dfb9c7bc3
Anwesh43 <anweshthecool0@gmail.com>
2024-05-31 19:37:40 +0530

    linealtarcdownview: adding LineAltArcDownView class

--- linealtarcdownview/src/main/java/com/example/linealtarcdownview/LineAltArcDownView.kt
+++ linealtarcdownview/src/main/java/com/example/linealtarcdownview/LineAltArcDownView.kt
@@ -63,3 +63,19 @@ fun Canvas.drawLAADNode(i : Int, scale : Float, paint : Paint) {
     paint.style = Paint.Style.STROKE
     drawLineAltArcDown(scale, w, h, paint)
 }
+
+class LineAltArcDownView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
