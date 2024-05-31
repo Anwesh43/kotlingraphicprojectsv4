ba80161c9504d62c339afa7d2d8c349fb55cec01
Anwesh43 <anweshthecool0@gmail.com>
2024-05-31 20:00:20 +0530

    linealtarcdownview: adding LineAltArcDown class

--- linealtarcdownview/src/main/java/com/example/linealtarcdownview/LineAltArcDownView.kt
+++ linealtarcdownview/src/main/java/com/example/linealtarcdownview/LineAltArcDownView.kt
@@ -167,4 +167,27 @@ class LineAltArcDownView(ctx : Context) : View(ctx) {
             return this
         }
     }
+
+    data class LineAltArcDown(var i : Int) {
+
+        private var curr : LAADNode = LAADNode(0)
+        private var dir : Int = 1
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            curr.draw(canvas, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            curr.update {
+                curr = curr.getNext(dir) {
+                    dir *= -1
+                }
+                cb(it)
+            }
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            curr.startUpdating(cb)
+        }
+    }
 }
