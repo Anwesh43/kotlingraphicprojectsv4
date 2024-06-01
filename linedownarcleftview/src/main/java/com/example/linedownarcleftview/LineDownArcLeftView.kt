df6ff228a7bf5cee6c56bb04c59e6299638fb829
Anwesh43 <anweshthecool0@gmail.com>
2024-06-02 00:26:46 +0530

    linedownarcleftview: adding LDALNode class

--- linedownarcleftview/src/main/java/com/example/linedownarcleftview/LineDownArcLeftView.kt
+++ linedownarcleftview/src/main/java/com/example/linedownarcleftview/LineDownArcLeftView.kt
@@ -120,4 +120,45 @@ class LineDownArcLeftView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class LDALNode(var i : Int = 0, val state : State = State()) {
+
+        private var next : LDALNode? = null
+        private var prev : LDALNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < colors.size - 1) {
+                next = LDALNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawLDALNode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : LDALNode {
+            var curr : LDALNode? = prev
+            if (dir == 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
