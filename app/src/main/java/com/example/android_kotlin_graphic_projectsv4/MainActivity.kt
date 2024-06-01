e1fc06cc855e9634e7706378015ab0a8381b6dc4
Anwesh43 <anweshthecool0@gmail.com>
2024-06-01 12:24:19 +0530

    lineballholderleftview: adding view in activity

--- app/src/main/java/com/example/android_kotlin_graphic_projectsv4/MainActivity.kt
+++ app/src/main/java/com/example/android_kotlin_graphic_projectsv4/MainActivity.kt
@@ -2,13 +2,13 @@ package com.example.android_kotlin_graphic_projectsv4
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.example.linealtarcdownview.LineAltArcDownView
+import com.example.lineballholderleftview.LineBallHolderLeftView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         supportActionBar?.hide()
         //setContentView(R.layout.activity_main)
-        LineAltArcDownView.create(this)
+        LineBallHolderLeftView.create(this)
     }
 }
