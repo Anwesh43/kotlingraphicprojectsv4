ef1ec3d95c88f162bf7e9be289b470459fed62b0
Anwesh43 <anweshthecool0@gmail.com>
2024-06-01 11:57:36 +0530

    lineballholderleftview: adding LineBallHolderLeftView class

--- lineballholderleftview/src/main/java/com/example/lineballholderleftview/LineBallHolderLeftView.kt
+++ lineballholderleftview/src/main/java/com/example/lineballholderleftview/LineBallHolderLeftView.kt
@@ -63,3 +63,19 @@ fun Canvas.drawLBHLNode(i : Int, scale : Float, paint : Paint) {
     paint.strokeWidth = Math.min(w, h) / strokeFactor
     drawLineBallHolderLeft(scale, w, h, paint)
 }
+
+class LineBallHolderLeftView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
