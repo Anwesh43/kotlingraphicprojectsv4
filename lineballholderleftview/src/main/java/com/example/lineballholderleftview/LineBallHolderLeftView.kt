40a6a0bb1a9c221676d4fb0ce99ec023c7f9f578
Anwesh43 <anweshthecool0@gmail.com>
2024-06-01 11:51:31 +0530

    lineballholderleftview: adding Canvas extension methods

--- lineballholderleftview/src/main/java/com/example/lineballholderleftview/LineBallHolderLeftView.kt
+++ lineballholderleftview/src/main/java/com/example/lineballholderleftview/LineBallHolderLeftView.kt
@@ -16,7 +16,7 @@ val colors : Array<String> = arrayOf(
     "#C51162",
     "#00C853"
 )
-val parts : Int = 4
+val parts : Int = 5
 val scGap : Float = 0.04f / parts
 val strokeFactor : Float = 90f
 val sizeFactor : Float = 4.9f
@@ -27,3 +27,39 @@ val backColor : Int = Color.parseColor("#BDBDBD")
 fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
+
+fun Canvas.drawXY(x : Float, y : Float, cb : () -> Unit) {
+    save()
+    translate(x, y)
+    cb()
+    restore()
+}
+
+fun Canvas.drawLineBallHolderLeft(scale : Float, w : Float, h : Float, paint : Paint) {
+    val size : Float = Math.min(w, h) / sizeFactor
+    val dsc : (Int) -> Float = {
+        scale.divideScale(it, parts)
+    }
+    drawXY(w / 2, h / 2) {
+        rotate(rot * dsc(3))
+        drawLine(-size * 0.5f * dsc(0), 0f, size * 0.5f * dsc(0), 0f, paint)
+        for (j in 0..1) {
+            drawXY(size / 2 - (size) * j, -h * 0.5f * (1 - dsc(j))) {
+                if (j == 0) {
+                    drawLine(0f, 0f, 0f, -size / 2, paint)
+                } else {
+                    drawCircle(0f, -size / 4, size / 4, paint)
+                }
+            }
+        }
+    }
+}
+
+fun Canvas.drawLBHLNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = Color.parseColor(colors[i])
+    paint.strokeCap = Paint.Cap.ROUND
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    drawLineBallHolderLeft(scale, w, h, paint)
+}
