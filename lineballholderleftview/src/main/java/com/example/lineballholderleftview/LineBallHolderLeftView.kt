bdbdb62a8d13afd949ba0f938033d6e9dfdb0201
Anwesh43 <anweshthecool0@gmail.com>
2024-06-01 12:44:13 +0530

    lineballholderleftview: removing unnecessary library

--- lineballholderleftview/src/main/java/com/example/lineballholderleftview/LineBallHolderLeftView.kt
+++ lineballholderleftview/src/main/java/com/example/lineballholderleftview/LineBallHolderLeftView.kt
@@ -5,7 +5,6 @@ import android.content.Context
 import android.view.View
 import android.view.MotionEvent
 import android.graphics.Paint
-import android.graphics.RectF
 import android.graphics.Canvas
 import android.graphics.Color
 
