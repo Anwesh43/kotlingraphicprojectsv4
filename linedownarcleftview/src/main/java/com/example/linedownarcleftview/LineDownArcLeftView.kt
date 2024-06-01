118af1266d2a7f1a41d4054b77fafffae701d882
Anwesh43 <anweshthecool0@gmail.com>
2024-06-02 00:29:49 +0530

    linedownarcleftview: adding LineDownArcLeft class

--- linedownarcleftview/src/main/java/com/example/linedownarcleftview/LineDownArcLeftView.kt
+++ linedownarcleftview/src/main/java/com/example/linedownarcleftview/LineDownArcLeftView.kt
@@ -161,4 +161,27 @@ class LineDownArcLeftView(ctx : Context) : View(ctx) {
             return this
         }
     }
+
+    data class LineDownArcLeft(var i : Int) {
+
+        private var curr : LDALNode = LDALNode(0)
+        private var dir : Int = 1
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            curr.draw(canvas, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            curr.update {
+                curr = curr.getNext(dir) {
+                    dir *= -1
+                }
+                cb(it)
+            }
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            curr.startUpdating(cb)
+        }
+    }
 }
