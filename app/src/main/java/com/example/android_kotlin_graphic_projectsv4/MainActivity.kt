2308e42f14d18d79fbe385a513cc7a627456223e
Anwesh43 <anweshthecool0@gmail.com>
2024-06-07 03:51:34 +0530

    sixlineloaderview: adding view in activity

--- app/src/main/java/com/example/android_kotlin_graphic_projectsv4/MainActivity.kt
+++ app/src/main/java/com/example/android_kotlin_graphic_projectsv4/MainActivity.kt
@@ -2,13 +2,13 @@ package com.example.android_kotlin_graphic_projectsv4
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.example.linedownarcleftview.LineDownArcLeftView
+import com.example.sixlineloaderview.SixLineLoaderView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         supportActionBar?.hide()
         //setContentView(R.layout.activity_main)
-        LineDownArcLeftView.create(this)
+        SixLineLoaderView.create(this)
     }
 }
