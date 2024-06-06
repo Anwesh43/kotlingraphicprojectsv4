36c3f64cc66d0003df8b6738c2c64ab2ce2aa8cf
Anwesh43 <anweshthecool0@gmail.com>
2024-06-07 03:34:32 +0530

    sixlineloaderview: adding Canvas extension methods

--- sixlineloaderview/src/main/java/com/example/sixlineloaderview/SixLineLoaderView.kt
+++ sixlineloaderview/src/main/java/com/example/sixlineloaderview/SixLineLoaderView.kt
@@ -27,3 +27,37 @@ val rot : Float = 360f / parts
 fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
+
+fun Canvas.drawXY(x : Float, y : Float, cb : () -> Unit) {
+    save()
+    translate(x, y)
+    cb()
+    restore()
+}
+
+fun Canvas.drawSixLineLoader(scale : Float, w : Float, h : Float, paint : Paint) {
+    val size : Float = Math.min(w, h) / sizeFactor
+    val dsc : (Int) -> Float = {
+        scale.divideScale(it, parts)
+    }
+    drawXY(w / 2, h / 2) {
+        for (j in 0..(parts - 1)) {
+            val scj : Float = dsc(j)
+            val scj1 : Float = scj.divideScale(0, 2)
+            val scj2 : Float = scj.divideScale(1, 2)
+            drawXY(0f, 0f) {
+                rotate(rot + rot * j)
+                drawLine(0f, 0f, 0f, -size * (scj1 - scj2), paint)
+            }
+        }
+    }
+}
+
+fun Canvas.drawSLLNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = Color.parseColor(colors[i])
+    paint.strokeCap = Paint.Cap.ROUND
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    drawSixLineLoader(scale, w, h, paint)
+}
