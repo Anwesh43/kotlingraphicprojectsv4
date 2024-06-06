d1b508c46a6c75387891b090e82847a3a1973dc6
Anwesh43 <anweshthecool0@gmail.com>
2024-06-07 03:43:31 +0530

    sixlineloaderview: adding SLLNode class

--- sixlineloaderview/src/main/java/com/example/sixlineloaderview/SixLineLoaderView.kt
+++ sixlineloaderview/src/main/java/com/example/sixlineloaderview/SixLineLoaderView.kt
@@ -124,4 +124,45 @@ class SixLineLoaderView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class SLLNode(var i : Int = 0, val state : State = State()) {
+
+        private var next : SLLNode? = null
+        private var prev : SLLNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < colors.size - 1) {
+                next = SLLNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawSLLNode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : SLLNode {
+            var curr : SLLNode? = prev
+            if (dir === 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
