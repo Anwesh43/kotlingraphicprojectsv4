318558f49269204de844d32d7a853a6b687c8888
Anwesh43 <anweshthecool0@gmail.com>
2024-06-08 13:49:11 +0530

    altbiarcjoinview: adding Renderer in View

--- altbiarcjoinview/src/main/java/com/example/altbiarcjoinview/AltBiArcJoinView.kt
+++ altbiarcjoinview/src/main/java/com/example/altbiarcjoinview/AltBiArcJoinView.kt
@@ -75,14 +75,16 @@ fun Canvas.drawABAJNode( i : Int, scale : Float, paint : Paint) {
 
 class AltBiArcJoinView(ctx : Context) : View(ctx) {
 
-    override fun onDraw(canvas : Canvas) {
+    private val renderer : Renderer = Renderer(this)
 
+    override fun onDraw(canvas : Canvas) {
+        renderer.render(canvas)
     }
 
     override fun onTouchEvent(event : MotionEvent) : Boolean {
         when (event.action) {
             MotionEvent.ACTION_DOWN -> {
-
+                renderer.handleTap()
             }
         }
         return true
