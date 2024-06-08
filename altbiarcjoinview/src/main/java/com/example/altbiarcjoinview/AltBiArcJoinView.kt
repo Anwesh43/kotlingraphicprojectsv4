772096bf6b548d5b2b55a9a4cd01dcdb1be79f97
Anwesh43 <anweshthecool0@gmail.com>
2024-06-08 13:29:42 +0530

    altbiarcjoinview: adding Canvas extension methods

--- altbiarcjoinview/src/main/java/com/example/altbiarcjoinview/AltBiArcJoinView.kt
+++ altbiarcjoinview/src/main/java/com/example/altbiarcjoinview/AltBiArcJoinView.kt
@@ -16,7 +16,7 @@ val colors : Array<String> = arrayOf(
     "#C51162",
     "#00C853"
 )
-val parts : Int = 4
+val parts : Int = 5
 val scGap : Float = 0.04f / parts
 val strokeFactor : Float = 90f
 val sizeFactor : Float = 4.9f
@@ -27,3 +27,48 @@ val rot : Float = 90f
 fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
+
+fun Canvas.drawXY(x : Float, y : Float, cb : () -> Unit) {
+    save()
+    translate(x, y)
+    cb()
+    restore()
+}
+
+fun Canvas.drawAltBiArcJoin(scale : Float, w : Float, h : Float, paint : Paint) {
+    val size : Float = Math.min(w, h) / sizeFactor
+    val dsc : (Int) -> Float = {
+        scale.divideScale(it, parts)
+    }
+    drawXY(w / 2, h / 2 - (h / 2 + size) * dsc(4)) {
+        for (j in 0..1) {
+            drawXY(0f, 0f) {
+                rotate(rot * dsc(2))
+                scale(1f - 2 * j, 1f)
+                drawXY(-size / 2 + size * j, 0f) {
+                    drawArc(
+                        RectF(
+                            -size / 2, -size / 2, size / 2, size / 2),
+                        0f,
+                        180f * dsc(1 - j),
+                        false,
+                        paint
+                    )
+                }
+            }
+        }
+        drawXY(0f, -h * 0.5f * (1 - dsc(3))) {
+            drawLine(0f, 0f, 0f, size, paint)
+        }
+    }
+}
+
+fun Canvas.drawABAJNode(scale : Float, i : Int, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = Color.parseColor(colors[i])
+    paint.strokeCap = Paint.Cap.ROUND
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    paint.style = Paint.Style.STROKE
+    drawAltBiArcJoin(scale, w, h, paint)
+}
