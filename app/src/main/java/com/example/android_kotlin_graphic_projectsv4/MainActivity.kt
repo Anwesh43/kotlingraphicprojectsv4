26e6007877fa0a73941215fa0ca801950fc253a9
Anwesh43 <anweshthecool0@gmail.com>
2024-06-13 02:31:06 +0530

    benttolinearcview: adding view in activity

--- app/src/main/java/com/example/android_kotlin_graphic_projectsv4/MainActivity.kt
+++ app/src/main/java/com/example/android_kotlin_graphic_projectsv4/MainActivity.kt
@@ -2,13 +2,13 @@ package com.example.android_kotlin_graphic_projectsv4
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.example.rightlinearcdownview.RightLineArcDownView
+import com.example.benttolinearcview.BentToLineArcView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         supportActionBar?.hide()
         //setContentView(R.layout.activity_main)
-        RightLineArcDownView.create(this)
+        BentToLineArcView.create(this)
     }
 }
