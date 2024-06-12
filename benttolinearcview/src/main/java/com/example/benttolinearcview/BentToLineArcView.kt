1ceb88862320be0f633a52adf5669b230948721c
Anwesh43 <anweshthecool0@gmail.com>
2024-06-13 02:01:40 +0530

    benttolinearcview: adding BentToLineArcView class

--- benttolinearcview/src/main/java/com/example/benttolinearcview/BentToLineArcView.kt
+++ benttolinearcview/src/main/java/com/example/benttolinearcview/BentToLineArcView.kt
@@ -59,3 +59,19 @@ fun Canvas.drawBTALNode(i : Int, scale : Float, paint : Paint) {
     paint.strokeWidth = Math.min(w, h) / strokeFactor
     drawBentToLineArc(scale, w, h, paint)
 }
+
+class BentToLineArcView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
