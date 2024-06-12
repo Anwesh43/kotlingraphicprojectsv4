c331cadffb36d7bac3a1a61ed349cb719a9beef8
Anwesh43 <anweshthecool0@gmail.com>
2024-06-12 17:28:26 +0530

    rightlnearcdownview: adding RightLineArcDownView class

--- rightlinearcdownview/src/main/java/com/example/rightlinearcdownview/RightLineArcDownView.kt
+++ rightlinearcdownview/src/main/java/com/example/rightlinearcdownview/RightLineArcDownView.kt
@@ -62,3 +62,19 @@ fun Canvas.drawRLADNode(i : Int, scale : Float, paint : Paint) {
     paint.strokeWidth = Math.min(w, h) / strokeFactor
     drawRightLineArcDown(scale, w, h, paint)
 }
+
+class RightLineArcDownView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
