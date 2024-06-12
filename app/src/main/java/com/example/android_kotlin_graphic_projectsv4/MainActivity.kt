a2c06b815a5c28eb5521eb70791eca9f86335a9d
Anwesh43 <anweshthecool0@gmail.com>
2024-06-12 18:07:28 +0530

    rightlnearcdownview: adding view in activity

--- app/src/main/java/com/example/android_kotlin_graphic_projectsv4/MainActivity.kt
+++ app/src/main/java/com/example/android_kotlin_graphic_projectsv4/MainActivity.kt
@@ -2,13 +2,13 @@ package com.example.android_kotlin_graphic_projectsv4
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.example.altbiarcjoinview.AltBiArcJoinView
+import com.example.rightlinearcdownview.RightLineArcDownView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         supportActionBar?.hide()
         //setContentView(R.layout.activity_main)
-        AltBiArcJoinView.create(this)
+        RightLineArcDownView.create(this)
     }
 }
