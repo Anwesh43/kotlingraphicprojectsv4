c9ee85c55dd8cff4f9979d41b9b6b8bc027b5740
Anwesh43 <anweshthecool0@gmail.com>
2024-06-13 02:28:53 +0530

    benttolinearcview: adding static create method

--- benttolinearcview/src/main/java/com/example/benttolinearcview/BentToLineArcView.kt
+++ benttolinearcview/src/main/java/com/example/benttolinearcview/BentToLineArcView.kt
@@ -211,4 +211,13 @@ class BentToLineArcView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    companion object {
+
+        fun create(activity: Activity) : BentToLineArcView {
+            val view : BentToLineArcView = BentToLineArcView(activity)
+            activity.setContentView(view)
+            return view
+        }
+    }
 }
