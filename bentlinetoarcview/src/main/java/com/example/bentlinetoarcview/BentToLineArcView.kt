49caa353b7df7016f7581f869c9f9d450fd0fea1
Anwesh43 <anweshthecool0@gmail.com>
2024-06-14 01:38:36 +0530

    bentlinetoarcview: adding Canvas extension methods

--- bentlinetoarcview/src/main/java/com/example/bentlinetoarcview/BentToLineArcView.kt
+++ bentlinetoarcview/src/main/java/com/example/bentlinetoarcview/BentToLineArcView.kt
@@ -28,3 +28,33 @@ val deg : Float = -90f
 fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
+
+fun Canvas.drawXY(x : Float, y : Float, cb : () -> Unit) {
+    save()
+    translate(x, y)
+    cb()
+    restore()
+}
+
+fun Canvas.drawBentToLineArc(scale : Float, w : Float, h : Float, paint : Paint) {
+    val size : Float = Math.min(w, h) / sizeFactor
+    val dsc : (Int) -> Float = {
+        scale.divideScale(it, parts)
+    }
+    drawXY(w / 2 + (w / 2) * dsc(3), h / 2) {
+        drawXY(0f, 0f) {
+            rotate(rot * (1 - dsc(1)) - deg * dsc(2))
+            drawLine(0f, size * (1 - dsc(0)), 0f, size, paint)
+        }
+        drawArc(RectF(-size / 2, -size / 2, size / 2, size / 2), 90f * (1 - dsc(2)), 90f * dsc(2), true, paint)
+    }
+}
+
+fun Canvas.drawBTLANode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = Color.parseColor(colors[i])
+    paint.strokeCap = Paint.Cap.ROUND
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    drawBentToLineArc(scale, w, h, paint)
+}
