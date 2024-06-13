fc6b30417aba813c272011d29b92880342832769
Anwesh43 <anweshthecool0@gmail.com>
2024-06-14 01:44:05 +0530

    bentlinetoarcview: adding BTLANode class

--- bentlinetoarcview/src/main/java/com/example/bentlinetoarcview/BentToLineArcView.kt
+++ bentlinetoarcview/src/main/java/com/example/bentlinetoarcview/BentToLineArcView.kt
@@ -121,4 +121,45 @@ class BentToLineArcView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class BTLANode(var i : Int = 0, val state : State = State()) {
+
+        private var next : BTLANode? = null
+        private var prev : BTLANode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < colors.size - 1) {
+                next = BTLANode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawBTLANode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : BTLANode {
+            var curr : BTLANode? = prev
+            if (dir === 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
