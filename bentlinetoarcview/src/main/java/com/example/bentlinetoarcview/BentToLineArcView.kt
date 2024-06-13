05bbdbc1ac7588a1d401291e738e7b4ae69f3def
Anwesh43 <anweshthecool0@gmail.com>
2024-06-14 01:45:13 +0530

    bentlinetoarcview: adding BentToLineArc class

--- bentlinetoarcview/src/main/java/com/example/bentlinetoarcview/BentToLineArcView.kt
+++ bentlinetoarcview/src/main/java/com/example/bentlinetoarcview/BentToLineArcView.kt
@@ -162,4 +162,27 @@ class BentToLineArcView(ctx : Context) : View(ctx) {
             return this
         }
     }
+
+    data class BentToLineArc(var i : Int) {
+
+        private var curr : BTLANode = BTLANode(0)
+        private var dir : Int = 1
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            curr.draw(canvas, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            curr.update {
+                curr = curr.getNext(dir) {
+                    dir *= -1
+                }
+                cb(it)
+            }
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            curr.startUpdating(cb)
+        }
+    }
 }
