4f4fe3d7fd9f19983aaa6a395ac41c316cd77c55
Anwesh43 <anweshthecool0@gmail.com>
2024-06-15 12:26:14 +0530

    partlineblockholderview: adding PartLineBlockHolderView class

--- partlineblockholderview/src/main/java/com/example/partlineblockholderview/PartLineBlockHolderView.kt
+++ partlineblockholderview/src/main/java/com/example/partlineblockholderview/PartLineBlockHolderView.kt
@@ -62,3 +62,19 @@ fun Canvas.drawPLBHNode(i : Int, scale : Float, paint : Paint) {
     paint.strokeWidth = Math.min(w, h) / strokeFactor
     drawPartLineBlockHolder(scale, w, h, paint)
 }
+
+class PartLineBlockHolderView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
