61593a4b99a99840ffffe9deedbdf574a26b948f
Anwesh43 <anweshthecool0@gmail.com>
2024-06-15 12:54:03 +0530

    partlineblockholderview: adding static create method

--- partlineblockholderview/src/main/java/com/example/partlineblockholderview/PartLineBlockHolderView.kt
+++ partlineblockholderview/src/main/java/com/example/partlineblockholderview/PartLineBlockHolderView.kt
@@ -214,4 +214,13 @@ class PartLineBlockHolderView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    companion object {
+
+        fun create(activity: Activity) : PartLineBlockHolderView {
+            val view : PartLineBlockHolderView = PartLineBlockHolderView(activity)
+            activity.setContentView(view)
+            return view
+        }
+    }
 }
