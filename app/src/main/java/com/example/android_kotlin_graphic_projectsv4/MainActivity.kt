a494d58a0da50fa3db992a7614af9ba63b65d405
Anwesh43 <anweshthecool0@gmail.com>
2024-06-15 12:56:28 +0530

    partlineblockholderview: adding view in activity

--- app/src/main/java/com/example/android_kotlin_graphic_projectsv4/MainActivity.kt
+++ app/src/main/java/com/example/android_kotlin_graphic_projectsv4/MainActivity.kt
@@ -2,13 +2,13 @@ package com.example.android_kotlin_graphic_projectsv4
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.example.bentlinetoarcview.BentToLineArcView
+import com.example.partlineblockholderview.PartLineBlockHolderView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         supportActionBar?.hide()
         //setContentView(R.layout.activity_main)
-        BentToLineArcView.create(this)
+        PartLineBlockHolderView.create(this)
     }
 }
