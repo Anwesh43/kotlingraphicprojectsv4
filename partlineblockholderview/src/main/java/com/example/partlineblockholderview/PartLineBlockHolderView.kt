1bea2acd3acc7affb3f19a3c5d77ecae2786ff39
Anwesh43 <anweshthecool0@gmail.com>
2024-06-15 12:07:53 +0530

    partlineblockholderview: adding constants and libraries

---
+++ partlineblockholderview/src/main/java/com/example/partlineblockholderview/PartLineBlockHolderView.kt
@@ -0,0 +1,25 @@
+package com.example.partlineblockholderview
+
+import android.view.View
+import android.view.MotionEvent
+import android.app.Activity
+import android.content.Context
+import android.graphics.Paint
+import android.graphics.RectF
+import android.graphics.Color
+import android.graphics.Canvas
+
+val colors : Array<String> = arrayOf(
+    "#1A237E",
+    "#EF5350",
+    "#AA00FF",
+    "#C51162",
+    "#00C853"
+)
+val parts : Int = 4
+val scGap : Float = 0.04f / parts
+val strokeFactor : Float = 90f
+val sizeFactor : Float = 4.9f
+val delay : Long = 20
+val backColor : Int = Color.parseColor("#BDBDBD")
+val rot : Float = 180f
