1fae558869b9ac53ae3cf37460268d3439a54878
Anwesh43 <anweshthecool0@gmail.com>
2024-06-15 12:24:49 +0530

    partlineblockholderview: adding Canvas extension methods

--- partlineblockholderview/src/main/java/com/example/partlineblockholderview/PartLineBlockHolderView.kt
+++ partlineblockholderview/src/main/java/com/example/partlineblockholderview/PartLineBlockHolderView.kt
@@ -16,7 +16,7 @@ val colors : Array<String> = arrayOf(
     "#C51162",
     "#00C853"
 )
-val parts : Int = 4
+val parts : Int = 5
 val scGap : Float = 0.04f / parts
 val strokeFactor : Float = 90f
 val sizeFactor : Float = 4.9f
@@ -27,3 +27,38 @@ val rot : Float = 180f
 fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
+
+fun Canvas.drawXY(x : Float, y : Float, cb : () -> Unit) {
+    save()
+    translate(x, y)
+    cb()
+    restore()
+}
+
+fun Canvas.drawPartLineBlockHolder(scale : Float, w : Float, h : Float, paint : Paint) {
+    val size : Float = Math.min(w, h) / sizeFactor
+    val dsc : (Int) -> Float = {
+        scale.divideScale(it, parts)
+    }
+    val blockSize : Float = size / 10
+    drawXY(w / 2 - (w / 2) * dsc(4), h / 2) {
+        drawXY(0f, 0f) {
+            rotate(rot * dsc(1))
+            drawLine(0f, 0f, size * dsc(1), 0f, paint)
+        }
+        for (j in 0..1) {
+            drawXY((-size + blockSize) * (1 - j), -h * 0.5f * (1 - dsc(2 + j))) {
+                drawRect(RectF(-blockSize, -blockSize, 0f, 0f), paint)
+            }
+        }
+    }
+}
+
+fun Canvas.drawPLBHNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = Color.parseColor(colors[i])
+    paint.strokeCap = Paint.Cap.ROUND
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    drawPartLineBlockHolder(scale, w, h, paint)
+}
